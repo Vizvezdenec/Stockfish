fa317b47427606cca0f82a311981ec5b75393338
Vizvezdenec <Vizvezdenec@gmail.com>
2018-10-08 17:04:43 +0300

    tweak. bench 4282290

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -92,7 +92,7 @@ namespace {
   constexpr int KingAttackWeights[PIECE_TYPE_NB] = { 0, 0, 77, 55, 44, 10 };
 
   // Penalties for enemy's safe checks
-  constexpr int QueenSafeCheck  = 580;
+  constexpr int QueenSafeCheck  = 530;
   constexpr int RookSafeCheck   = 880;
   constexpr int BishopSafeCheck = 435;
   constexpr int KnightSafeCheck = 790;
