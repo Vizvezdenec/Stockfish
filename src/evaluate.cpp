cb3883cfb6bd295942367aacb5d6b5471e95ea41
Vizvezdenec <Vizvezdenec@gmail.com>
2018-10-11 01:04:07 +0300

    tweak. bench 4804390

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -446,7 +446,7 @@ namespace {
         // Enemy queen safe checks
         if ((b1 | b2) & attackedBy[Them][QUEEN] & safe & ~attackedBy[Us][QUEEN])
             kingDanger += QueenSafeCheck;
-        else unsafeChecks |= ((b1 | b2) & attackedBy[Them][QUEEN]);
+        else unsafeChecks |= ((b1 | b2) && attackedBy[Them][QUEEN] && ~attackedBy[Us][QUEEN]);
 
         b1 &= attackedBy[Them][ROOK];
         b2 &= attackedBy[Them][BISHOP];
