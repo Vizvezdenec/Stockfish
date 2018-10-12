94aa82e7d81636a1f13a04f358d8d145a2f539c0
Vizvezdenec <Vizvezdenec@gmail.com>
2018-10-12 21:32:50 +0300

    tweak. bench 4252217

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -165,7 +165,7 @@ namespace {
   constexpr Score MinorBehindPawn    = S( 16,  0);
   constexpr Score Overload           = S( 13,  6);
   constexpr Score PawnlessFlank      = S( 19, 84);
-  constexpr Score QueenOnPawn        = S( 20,  0);
+  constexpr Score QueenOnPawn        = S( 40, 20);
   constexpr Score RookOnPawn         = S( 10, 30);
   constexpr Score SliderOnQueen      = S( 42, 21);
   constexpr Score ThreatByKing       = S( 23, 76);
