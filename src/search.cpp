0b3e5549d688319fae5742ab23594d43acb4a42e
Vizvezdenec <Vizvezdenec@gmail.com>
2018-10-17 00:30:05 +0300

    tweak. bench 4380366

--- src/search.cpp
+++ src/search.cpp
@@ -1039,7 +1039,7 @@ moves_loop: // When in check, search starts from here
               if (ss->statScore >= 0 && (ss-1)->statScore < 0)
                   r -= ONE_PLY;
 
-              else if ((ss-1)->statScore >= 0 && ss->statScore < 0 && rootNode)
+              else if ((ss-1)->statScore >= 0 && ss->statScore < 0)
                   r += ONE_PLY;
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
