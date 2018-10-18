9c8af3ef75ee91ccbb45351795ac78b5a99ee742
Vizvezdenec <Vizvezdenec@gmail.com>
2018-10-18 11:15:29 +0300

    tweak. bench 4506882

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -602,8 +602,8 @@ namespace {
         score += SliderOnQueen * popcount(b & safe & attackedBy2[Us]);
     }
     b = ~attackedBy[Them][PAWN] & ~attackedBy[Them][KNIGHT] & ~attackedBy[Them][BISHOP];
-    int rank3control = popcount(attackedBy[Us][ROOK] & ~LowRanks & b);
-    score += make_score(1, 0) * rank3control;
+    int rank3control = popcount(attackedBy[Us][ROOK] & ~LowRanks & b) - 5;
+    score += make_score(2, 2) * rank3control;
     if (T)
         Trace::add(THREAT, Us, score);
 
