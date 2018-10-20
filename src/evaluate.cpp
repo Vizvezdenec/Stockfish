a350addeb840ccca4f8e5d5808d97375b49edce3
Vizvezdenec <Vizvezdenec@gmail.com>
2018-10-20 09:56:15 +0300

    tweak. bench 4211684

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -489,8 +489,8 @@ namespace {
             kingDanger = std::max(0, kingDanger + mobilityDanger);
             score -= make_score(kingDanger * kingDanger / 4096, kingDanger / 16);
         }
-        else if ((mobilityDanger > 0) && (pos.count<BISHOP>(Us) + pos.count<KNIGHT>(Us) < pos.count<BISHOP>(Them) + pos.count<KNIGHT>(Them)))
-            score -= make_score(mobilityDanger * mobilityDanger / 8192, 0);
+        else if (mobilityDanger > 0 && ((pos.count<QUEEN>(Us) != pos.count<QUEEN>(Them)) || (pos.count<ROOK>(Us) != pos.count<ROOK>(Them))))
+            score -= make_score(mobilityDanger * mobilityDanger / 2048, mobilityDanger / 16);
     }
 
     // Penalty when our king is on a pawnless flank
