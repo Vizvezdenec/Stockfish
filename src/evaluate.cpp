a10e2f57977540aee4c222c978a8f38c13183ed2
Vizvezdenec <Vizvezdenec@gmail.com>
2018-10-22 13:55:02 +0300

    tweak. bench 4498502

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -166,6 +166,7 @@ namespace {
   constexpr Score Overload           = S( 13,  6);
   constexpr Score PawnlessFlank      = S( 19, 84);
   constexpr Score RookOnPawn         = S( 10, 30);
+  constexpr Score SliderImbalance    = S( 20,  0);
   constexpr Score SliderOnQueen      = S( 42, 21);
   constexpr Score ThreatByKing       = S( 23, 76);
   constexpr Score ThreatByPawnPush   = S( 45, 40);
@@ -598,7 +599,7 @@ namespace {
         b =  (attackedBy[Us][BISHOP] & pos.attacks_from<BISHOP>(s))
            | (attackedBy[Us][ROOK  ] & pos.attacks_from<ROOK  >(s));
 
-        score += SliderOnQueen * popcount(b & safe & attackedBy2[Us]);
+        score += (SliderOnQueen + SliderImbalance * (pos.count<QUEEN>(Us)==0)) * popcount(b & safe & attackedBy2[Us]);
     }
 
     if (T)
