c6b17eef1849766437f5857a2f18ba0727e46427
Vizvezdenec <Vizvezdenec@gmail.com>
2018-10-25 06:34:16 +0300

    tweak. bench 4510739

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -750,14 +750,14 @@ namespace {
                             && (pos.pieces(PAWN) & KingSide);
     Bitboard blocked = (pos.pieces(WHITE, PAWN) & (shift<SOUTH>(pos.pieces(BLACK))))
                        | (pos.pieces(BLACK, PAWN) & (shift<NORTH>(pos.pieces(WHITE))));
-    int pawnStructureVolatility = std::min(pos.count<PAWN>() - popcount(blocked), 4);
+    int pawnStructureVolatility = std::min(pos.count<PAWN>() - popcount(blocked), 2);
     // Compute the initiative bonus for the attacking side
     int complexity =   8 * pe->pawn_asymmetry()
                     + 12 * pos.count<PAWN>()
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
-                    + 16 * pawnStructureVolatility
+                    + 30 * pawnStructureVolatility
                     -182 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
