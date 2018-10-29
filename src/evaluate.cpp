f8eca6d8466ed06110475b7066b3270311a8dc93
Vizvezdenec <Vizvezdenec@gmail.com>
2018-10-29 14:31:56 +0300

    twig. bench 3476450

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -156,7 +156,7 @@ namespace {
 
   // Assorted bonuses and penalties
   constexpr Score BishopPawns        = S(  3,  7);
-  constexpr Score BlockedRook        = S(100, 50);
+  constexpr Score BlockedRook        = S(200,200);
   constexpr Score CloseEnemies       = S(  6,  0);
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score Hanging            = S( 57, 32);
@@ -172,7 +172,7 @@ namespace {
   constexpr Score ThreatByPawnPush   = S( 45, 40);
   constexpr Score ThreatByRank       = S( 16,  3);
   constexpr Score ThreatBySafePawn   = S(173,102);
-  constexpr Score TrappedRook        = S( 92,  0);
+//  constexpr Score TrappedRook        = S( 92,  0);
   constexpr Score WeakQueen          = S( 50, 10);
   constexpr Score WeakUnopposedPawn  = S(  5, 29);
 
@@ -381,12 +381,12 @@ namespace {
                 score += RookOnFile[bool(pe->semiopen_file(Them, file_of(s)))];
 
             // Penalty when trapped by the king, even more if the king cannot castle
-            else if (mob <= 3)
-            {
-                File kf = file_of(pos.square<KING>(Us));
-                if ((kf < FILE_E) == (file_of(s) < kf))
-                    score -= (TrappedRook - make_score(mob * 22, 0)) * (1 + !pos.can_castle(Us));
-            }
+           // else if (mob <= 3)
+            //{
+             //   File kf = file_of(pos.square<KING>(Us));
+            //    if ((kf < FILE_E) == (file_of(s) < kf))
+           //         score -= (TrappedRook - make_score(mob * 22, 0)) * (1 + !pos.can_castle(Us));
+            //}
         }
 
         if (Pt == QUEEN)
