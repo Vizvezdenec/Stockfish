a14341c7616d93318f087d4f93dfc0b511158ae0
Vizvezdenec <Vizvezdenec@gmail.com>
2018-10-31 14:51:20 +0300

    twig. bench 3234778

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -757,8 +757,8 @@ namespace {
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
-                    + 24 * broadPawnStructure
-                    -124 ;
+                    + 32 * broadPawnStructure
+                    -118 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
