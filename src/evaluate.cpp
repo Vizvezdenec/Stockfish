7e19e6fb77303281e3679126906f64b36aadfbcf
Vizvezdenec <Vizvezdenec@gmail.com>
2018-11-03 13:32:57 +0300

    twig. bench 3539607

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -606,7 +606,8 @@ namespace {
         score += SliderOnQueen * popcount(b & safe & attackedBy2[Us]);
     }
     if (!pos.can_castle(Us) && (pos.pieces(Us, ROOK) & LeftEdge) && (pos.pieces(Us, ROOK) & RightEdge) 
-        && (more_than_one(pos.pieces(Us, ROOK) & TRank12BB)) && (pos.pieces(Us, KING) & TRank1BB & ~RightEdge & ~LeftEdge))
+        && (more_than_one(pos.pieces(Us, ROOK) & TRank12BB)) && (pos.pieces(Us, KING) & TRank1BB)
+        && !(pos.pieces(Us, KING) & (FileABB | FileHBB)))
         score -= UncastledRook;
 
     if (T)
