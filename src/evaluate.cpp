96de2bdee4f165664b649b375c751ecac916eb3a
Vizvezdenec <Vizvezdenec@gmail.com>
2018-11-04 21:07:54 +0300

    twig. bench 3137287

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -385,7 +385,7 @@ namespace {
                 if ((kf < FILE_E) == (file_of(s) < kf))
                 {
                 mob -= !pos.can_castle(Us) * popcount(b & mobilityArea[Us] & shift<Down>(pos.pieces(Us, PAWN)));
-                score -= (TrappedRook - make_score(mob * 22, 0)); 
+                score -= (TrappedRook - make_score(mob * 22, 0)) * (1 + !pos.can_castle(Us)); 
                 }
             }
         }
