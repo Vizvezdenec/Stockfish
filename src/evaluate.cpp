4652bca31618490bcec46b48b75fc4bf0b468f27
Vizvezdenec <Vizvezdenec@gmail.com>
2018-11-05 23:37:31 +0300

    twig. bench 3336594

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -479,7 +479,7 @@ namespace {
                      +  69 * kingAttacksCount[Them]
                      + 185 * popcount(kingRing[Us] & weak)
                      + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
-                     +  25 * (2 - kingMob)
+                     +  25 * std::max(2 - kingMob, 0)
                      - 873 * !pos.count<QUEEN>(Them)
                      -   6 * mg_value(score) / 8
                      +       mg_value(mobility[Them] - mobility[Us])
