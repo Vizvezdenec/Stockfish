8eec669a906e8101d06e8e6ff2ce99945ffa8469
Vizvezdenec <Vizvezdenec@gmail.com>
2018-11-08 21:24:21 +0300

    twig. bench 3452506

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -156,7 +156,7 @@ namespace {
 
   // Assorted bonuses and penalties
   constexpr Score BishopPawns        = S(  3,  7);
-  constexpr Score BishopPawns1       = S(  2,  4);
+  constexpr Score BishopPawns1       = S(  3,  7);
   constexpr Score CloseEnemies       = S(  6,  0);
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score Hanging            = S( 57, 32);
