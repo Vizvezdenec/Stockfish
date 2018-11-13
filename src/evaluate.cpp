1ff05581b4059fd450f8bc782ac2cc7124210fd2
Vizvezdenec <Vizvezdenec@gmail.com>
2018-11-13 15:09:47 +0300

    twig. bench 4204379

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -393,7 +393,7 @@ namespace {
                 score -= WeakQueen;
             if (pos.count<QUEEN>(Them) == 0
                 && (pos.count<BISHOP>(Them)+pos.count<KNIGHT>(Them)-pos.count<BISHOP>(Us)-pos.count<KNIGHT>(Us) > 1))
-                mobility[Us] += MobilityBonus[Pt - 2][mob];
+                mobility[Us] += MobilityBonus[Pt - 2][mob] / 2;
         }
     }
     if (T)
