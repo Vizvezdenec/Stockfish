4a2624a9b7e90091195cf5686c6746b0ab38623c
Vizvezdenec <Vizvezdenec@gmail.com>
2018-11-14 17:09:49 +0300

    twick. bench 3650511

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -371,7 +371,7 @@ namespace {
             // Bonus for aligning rook with enemy pawns on the same rank/file
             if (relative_rank(Us, s) >= RANK_5)
                 score += RookOnPawn * popcount(pos.pieces(Them, PAWN) & PseudoAttacks[ROOK][s]);
-
+            File kf = file_of(pos.square<KING>(Us));
             // Bonus for rook on an open or semi-open file
             if (pe->semiopen_file(Us, file_of(s)))
                 score += RookOnFile[bool(pe->semiopen_file(Them, file_of(s)))];
@@ -379,10 +379,11 @@ namespace {
             // Penalty when trapped by the king, even more if the king cannot castle
             else if (mob <= 3)
             {
-                File kf = file_of(pos.square<KING>(Us));
                 if ((kf < FILE_E) == (file_of(s) < kf))
                     score -= (TrappedRook - make_score(mob * 22, 0)) * (1 + !pos.can_castle(Us));
             }
+                if (rank_of(pos.square<KING>(Us)) == rank_of(s) && ((kf < FILE_E) == (file_of(s) < kf)))
+                    score -= make_score(50,0) * !pos.can_castle(Us);
         }
 
         if (Pt == QUEEN)
