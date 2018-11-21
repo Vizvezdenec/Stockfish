8610639fde0480261adf35a781dfe735cc617a96
Vizvezdenec <Vizvezdenec@gmail.com>
2018-11-21 17:20:12 +0300

    twick. bench 3919401

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -481,8 +481,7 @@ namespace {
                      - 873 * !pos.count<QUEEN>(Them)
                      -   6 * mg_value(score) / 8
                      -  30 * lockedAttack
-                     +       mg_value(mobility[Them] - mobility[Us])
-                     -   30;
+                     +       mg_value(mobility[Them] - mobility[Us]);
 
         // Transform the kingDanger units into a Score, and subtract it from the evaluation
         if (kingDanger > 0)
