a908c8b120a02b4090a4995be09ece161120e38b
Vizvezdenec <Vizvezdenec@gmail.com>
2018-11-21 20:21:06 +0300

    twick. bench 3637527

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -510,7 +510,7 @@ namespace {
     constexpr Direction Up       = (Us == WHITE ? NORTH   : SOUTH);
     constexpr Bitboard  TRank3BB = (Us == WHITE ? Rank3BB : Rank6BB);
 
-    Bitboard b, bb, weak, defended, nonPawnEnemies, stronglyProtected, safe, restricted;
+    Bitboard b, bb, bbb, weak, defended, nonPawnEnemies, stronglyProtected, safe, restricted;
     Score score = SCORE_ZERO;
 
     // Non-pawn enemies
@@ -606,7 +606,8 @@ namespace {
         Square s1 = pos.square<KING>(Them);
         b = attackedBy[Us][KNIGHT] & pos.attacks_from<KNIGHT>(s) & pos.attacks_from<KNIGHT>(s1);
         bb = b & ~attackedBy[Them][PAWN];
-        score += KnightFork * (bool(b) + bool(bb));
+        bbb = b & ~attackedBy2[Them];
+        score += KnightFork * (bool(b) + bool(bb) + bool(bbb));
     }
 
     if (T)
