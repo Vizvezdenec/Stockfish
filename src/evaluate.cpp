9848a20afe9f6f96dc5c916fdc934773e969f9e6
Vizvezdenec <Vizvezdenec@gmail.com>
2018-11-22 21:22:43 +0300

    twick. bench 3735216

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -165,7 +165,7 @@ namespace {
   constexpr Score RestrictedPiece    = S(  7,  6);
   constexpr Score RookOnPawn         = S( 10, 28);
   constexpr Score SliderOnQueen      = S( 49, 21);
-  constexpr Score SliderOnQueen2     = S( 20, 10);
+  constexpr Score SliderOnQueen2     = S(  5,  2);
   constexpr Score ThreatByKing       = S( 21, 84);
   constexpr Score ThreatByPawnPush   = S( 48, 42);
   constexpr Score ThreatByRank       = S( 14,  3);
