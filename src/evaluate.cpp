78fefd27010c132a1efe3913671ec2478c4a391d
Vizvezdenec <Vizvezdenec@gmail.com>
2018-11-23 23:36:24 +0300

    twick. bench 3512513

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -428,7 +428,8 @@ namespace {
     {
         int kingDanger = 0;
         unsafeChecks = 0;
-
+        Bitboard b3 = (attackedBy[Us][ALL_PIECES] & (attackedBy2[Us] | attackedBy[Us][KING])) & kingFlank & Camp;
+        int tropismDifference = tropism - popcount(b3);
         // Attacked squares defended at most once by our queen or king
         weak =  attackedBy[Them][ALL_PIECES]
               & ~attackedBy2[Us]
@@ -475,7 +476,8 @@ namespace {
                      +  69 * kingAttacksCount[Them]
                      + 185 * popcount(kingRing[Us] & weak)
                      + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
-                     +   4 * tropism
+                     +       tropism * tropism / 4
+                     +       tropismDifference * abs(tropismDifference)
                      - 873 * !pos.count<QUEEN>(Them)
                      -   6 * mg_value(score) / 8
                      +       mg_value(mobility[Them] - mobility[Us])
