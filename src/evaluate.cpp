a9cece6d58eb1316029001377fb0548bb98334b7
Vizvezdenec <Vizvezdenec@gmail.com>
2018-11-24 14:06:03 +0300

    tweaq. bench 3562136

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -79,9 +79,9 @@ namespace {
   constexpr Bitboard Center      = (FileDBB | FileEBB) & (Rank4BB | Rank5BB);
 
   constexpr Bitboard KingFlank[FILE_NB] = {
-    QueenSide ^ FileDBB, QueenSide, QueenSide | FileEBB,
-    CenterFiles | FileBBB, CenterFiles | FileGBB,
-    KingSide | FileDBB, KingSide, KingSide ^ FileEBB
+    QueenSide ^ FileDBB, QueenSide, QueenSide,
+    CenterFiles, CenterFiles,
+    KingSide, KingSide, KingSide ^ FileEBB
   };
 
   // Threshold for lazy and space evaluation
@@ -408,9 +408,13 @@ namespace {
     constexpr Color    Them = (Us == WHITE ? BLACK : WHITE);
     constexpr Bitboard Camp = (Us == WHITE ? AllSquares ^ Rank6BB ^ Rank7BB ^ Rank8BB
                                            : AllSquares ^ Rank1BB ^ Rank2BB ^ Rank3BB);
+    constexpr Bitboard TropismFlank[FILE_NB] = {
+    QueenSide ^ FileDBB, QueenSide, QueenSide | FileEBB,
+    CenterFiles | FileBBB, CenterFiles | FileGBB,
+    KingSide | FileDBB, KingSide, KingSide ^ FileEBB };
 
     const Square ksq = pos.square<KING>(Us);
-    Bitboard kingFlank, weak, b, b1, b2, safe, unsafeChecks;
+    Bitboard kingFlank, tropismFlank, weak, b, b1, b2, safe, unsafeChecks;
 
     // King shelter and enemy pawns storm
     Score score = pe->king_safety<Us>(pos);
@@ -418,7 +422,9 @@ namespace {
     // Find the squares that opponent attacks in our king flank, and the squares
     // which are attacked twice in that flank.
     kingFlank = KingFlank[file_of(ksq)];
-    b1 = attackedBy[Them][ALL_PIECES] & kingFlank & Camp;
+    tropismFlank = TropismFlank[file_of(ksq)];
+
+    b1 = attackedBy[Them][ALL_PIECES] & tropismFlank & Camp;
     b2 = b1 & attackedBy2[Them];
 
     int tropism = popcount(b1) + popcount(b2);
