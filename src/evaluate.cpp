1e9146ef7245d68ba8648299f5ef3ebd8fdfcdaf
Vizvezdenec <Vizvezdenec@gmail.com>
2018-11-24 04:44:41 +0300

    tweaq. bench 3677811

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -478,7 +478,7 @@ namespace {
                      + 185 * popcount(kingRing[Us] & weak)
                      + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                      +       tropism * tropism / 4
-                     +       tropismDifference * abs (tropismDifference) * 2 / 3
+                     +       tropismDifference * abs (tropismDifference) * 3 / 4
                      - 873 * !pos.count<QUEEN>(Them)
                      -   6 * mg_value(score) / 8
                      +       mg_value(mobility[Them] - mobility[Us])
