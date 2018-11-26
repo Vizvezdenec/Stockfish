6aff5f5ac18431d19e29650da80d09ac310eb3d9
Vizvezdenec <Vizvezdenec@gmail.com>
2018-11-26 13:22:19 +0300

    tweaq. bench 3722271

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -478,7 +478,7 @@ namespace {
                      +       tropism * tropism / 4
                      - 873 * !pos.count<QUEEN>(Them)
                      -   6 * mg_value(score) / 8
-                     +       mg_value(mobility[Them] - mobility[Us]) * abs(mg_value(mobility[Them] - mobility[Us])) / 100
+                     +       mg_value(mobility[Them] - mobility[Us]) * abs(mg_value(mobility[Them] - mobility[Us])) / 50
                      -   30;
 
         // Transform the kingDanger units into a Score, and subtract it from the evaluation
