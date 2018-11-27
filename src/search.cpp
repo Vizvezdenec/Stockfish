a189380e348be754ba684fa8c3de0974219d1345
Vizvezdenec <Vizvezdenec@gmail.com>
2018-11-28 02:14:22 +0300

    tweaq. bench 3767303

--- src/search.cpp
+++ src/search.cpp
@@ -781,7 +781,7 @@ namespace {
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor)
-        && (abs(eval - pureStaticEval) < 1000 + pos.non_pawn_material(us) / 3))
+        && !(abs(pos.non_pawn_material(us) - pos.non_pawn_material(~us)) > 1000 && abs(eval) < 200))
     {
         assert(eval - beta >= 0);
 
