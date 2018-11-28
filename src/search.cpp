e726ac4b417608eaecd9424200a9b7332202c5be
Vizvezdenec <Vizvezdenec@gmail.com>
2018-11-28 06:35:26 +0300

    tweaq. bench 3420891

--- src/search.cpp
+++ src/search.cpp
@@ -786,7 +786,7 @@ namespace {
 
         // Null move dynamic reduction based on depth and value
         Depth R = ((823 + 67 * depth / ONE_PLY) / 256 + std::min(int(eval - beta) / 200, 3) 
-                  - (pos.non_pawn_material(us) - pos.non_pawn_material(~us)) / 1000) * ONE_PLY;
+                  - abs(pos.non_pawn_material(us) - pos.non_pawn_material(~us)) / 1000) * ONE_PLY;
 
         ss->currentMove = MOVE_NULL;
         ss->continuationHistory = &thisThread->continuationHistory[NO_PIECE][0];
