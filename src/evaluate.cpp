a903359cc9484c4de6ea119bf05695c4ccb3e1c7
Vizvezdenec <Vizvezdenec@gmail.com>
2018-11-29 21:22:06 +0300

    tweaq. bench 3242721

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -162,7 +162,7 @@ namespace {
   constexpr Score MinorBehindPawn    = S( 16,  0);
   constexpr Score Overload           = S( 12,  6);
   constexpr Score PawnlessFlank      = S( 18, 94);
-  constexpr Score QueenOverload      = S( 80, 40);
+  constexpr Score QueenOverload      = S( 40, 80);
   constexpr Score RestrictedPiece    = S(  7,  6);
   constexpr Score RookOnPawn         = S( 10, 28);
   constexpr Score SliderOnQueen      = S( 49, 21);
