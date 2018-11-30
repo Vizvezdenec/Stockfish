454f76800edfd1a3eebced8139783f50fecd978d
Vizvezdenec <Vizvezdenec@gmail.com>
2018-12-01 01:54:29 +0300

    tweaq. bench 3227025

--- src/search.cpp
+++ src/search.cpp
@@ -1195,7 +1195,7 @@ moves_loop: // When in check, search starts from here
                 && !pos.captured_piece())
             update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -stat_bonus(depth));
         else if ((ss-1)->moveCount == 2 && !pos.captured_piece())
-            update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -stat_bonus(depth - ONE_PLY));
+            update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -stat_bonus(depth));
     }
     // Bonus for prior countermove that caused the fail low
     else if (   (depth >= 3 * ONE_PLY || PvNode)
