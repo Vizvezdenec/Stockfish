469070468cdc1f711f17dbf999a358ea525e1f69
Vizvezdenec <Vizvezdenec@gmail.com>
2018-12-02 22:54:07 +0300

    tweek. bench 3669146

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -309,7 +309,7 @@ namespace {
         attackedBy[Us][Pt] |= b;
         attackedBy[Us][ALL_PIECES] |= b;
 
-        if (b & kingRing[Them] & ~(double_pawn_attacks_bb<Them>(pos.pieces(Them, PAWN)) & pos.pieces(Them,PAWN)))
+        if (b & kingRing[Them] & ~(double_pawn_attacks_bb<Them>(pos.pieces(Them, PAWN)) & pos.pieces(Them)))
         {
             kingAttackersCount[Us]++;
             kingAttackersWeight[Us] += KingAttackWeights[Pt];
