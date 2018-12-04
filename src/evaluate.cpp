2ad5746042a895eaaee9bd6691a3bbc30b401890
Vizvezdenec <Vizvezdenec@gmail.com>
2018-12-04 16:17:16 +0300

    tweek. bench 3241388

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -477,7 +477,7 @@ namespace {
                      +  69 * kingAttacksCount[Them]
                      + 185 * popcount(kingRing[Us] & weak)
                      + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
-                     +       (tropism + theirPieces) * tropism / 4
+                     +       (tropism + theirPieces) * (tropism + theirPieces) / 4
                      - 873 * !pos.count<QUEEN>(Them)
                      -   6 * mg_value(score) / 8
                      +       mg_value(mobility[Them] - mobility[Us])
