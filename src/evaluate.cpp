94c5a60989d976ab3f459f8d73845742f951361d
Vizvezdenec <Vizvezdenec@gmail.com>
2018-12-04 15:45:17 +0300

    tweek. bench 3546279

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -153,7 +153,7 @@ namespace {
 
   // Assorted bonuses and penalties
   constexpr Score BishopPawns        = S(  3,  8);
-  constexpr Score CloseEnemies       = S(  5,  0);
+  constexpr Score CloseEnemies       = S(  6,  0);
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score Hanging            = S( 62, 34);
   constexpr Score KingProtector      = S(  6,  7);
