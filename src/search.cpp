b0a9c615224d0ed21c0319e2c7b708fad6719f44
Vizvezdenec <Vizvezdenec@gmail.com>
2018-12-06 06:21:20 +0300

    tweek. bench 3390467

--- src/search.cpp
+++ src/search.cpp
@@ -833,7 +833,7 @@ namespace {
         int probCutCount = 0;
 
         while (  (move = mp.next_move()) != MOVE_NONE
-               && probCutCount < 3 + (ss-1)->moveCount / 10)
+               && probCutCount < 3 + (ss-1)->moveCount / 15)
             if (move != excludedMove && pos.legal(move))
             {
                 probCutCount++;
