d70a38201743d70177ff270828f87a48a1bbe8e9
Vizvezdenec <Vizvezdenec@gmail.com>
2018-12-07 19:15:56 +0300

    tweek. bench 3198135

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -272,7 +272,9 @@ namespace {
 
         else if (file_of(pos.square<KING>(Us)) == FILE_A)
             kingRing[Us] |= shift<EAST>(kingRing[Us]);
-
+        
+        kingRing[Us] |= (shift<NORTH>(kingRing[Us]) | shift<SOUTH>(kingRing[Us]) | shift<WEST>(kingRing[Us]) | 
+                        shift<EAST>(kingRing[Us])) &  double_pawn_attacks_bb<Them>(pos.pieces(Them, PAWN));
         kingAttackersCount[Them] = popcount(kingRing[Us] & pe->pawn_attacks(Them));
         kingAttacksCount[Them] = kingAttackersWeight[Them] = 0;
     }
