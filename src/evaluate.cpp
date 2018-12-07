7c35be172bfd6137c80aaffb3ab9211a8de9f6e7
Vizvezdenec <Vizvezdenec@gmail.com>
2018-12-07 03:06:39 +0300

    tweek. bench 3330044

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -601,6 +601,10 @@ namespace {
         score += SliderOnQueen * popcount(b & safe & attackedBy2[Us]);
     }
 
+    b = pos.pieces(Us, KNIGHT, BISHOP) & kingRing[Them] 
+        & ~attackedBy[Them][ALL_PIECES];
+        score += make_score(0, 7) * popcount(b);
+
     if (T)
         Trace::add(THREAT, Us, score);
 
