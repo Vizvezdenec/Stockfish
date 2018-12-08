15345f742f13622a0f60bd26a5ccb16b25cfb55b
Vizvezdenec <Vizvezdenec@gmail.com>
2018-12-08 20:33:29 +0300

    tweek. bench 3472511

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -480,7 +480,7 @@ namespace {
                      - 873 * !pos.count<QUEEN>(Them)
                      -   6 * mg_value(score) / 8
                      +       mg_value(mobility[Them] - mobility[Us])
-                     +  50 * oppCastling
+                     + 100 * oppCastling
                      -   30;
 
         // Transform the kingDanger units into a Score, and subtract it from the evaluation
