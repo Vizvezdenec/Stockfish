b214099643009ef1f2ccb2f55dbe66590cb1c987
Vizvezdenec <Vizvezdenec@gmail.com>
2018-12-12 01:21:54 +0300

    tweek. bench 3463797

--- src/search.cpp
+++ src/search.cpp
@@ -626,7 +626,7 @@ namespace {
     // statScore of the previous grandchild. This influences the reduction rules in
     // LMR which are based on the statScore of parent position.
     (ss+2)->statScore = 0;
-    if (!rootNode)
+    if (!PvNode)
          ss->statScore = 0;
 
     // Step 4. Transposition table lookup. We don't want the score of a partial
