bac8a48e22ab6972cffd3c2490676b5f8b126d49
Vizvezdenec <Vizvezdenec@gmail.com>
2018-12-11 18:51:14 +0300

    tweek. bench 3531019

--- src/search.cpp
+++ src/search.cpp
@@ -778,7 +778,7 @@ namespace {
         &&  pureStaticEval >= beta - 36 * depth / ONE_PLY + 225
         && !excludedMove
         &&  pos.non_pawn_material(us)
-        && (improving || pos.rule50_count() < 25)
+        && (improving || pos.rule50_count() < 10)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))
     {
         assert(eval - beta >= 0);
