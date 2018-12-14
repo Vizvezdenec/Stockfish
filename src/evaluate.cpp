502cd950b4389bb171bb46d4fe76e88b6e546ea1
Vizvezdenec <Vizvezdenec@gmail.com>
2018-12-14 13:38:36 +0300

    tweek. bench 3333634

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -475,7 +475,7 @@ namespace {
         kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                      +  69 * kingAttacksCount[Them]
                      + 185 * popcount(kingRing[Us] & weak)
-                     +       weakTropism * weakTropism
+                     +   5 * weakTropism
                      + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                      +       tropism * tropism / 4
                      - 873 * !pos.count<QUEEN>(Them)
