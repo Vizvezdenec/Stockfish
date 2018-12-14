c9f8681bb8e3eb48720c1bd5404617291031283c
Vizvezdenec <Vizvezdenec@gmail.com>
2018-12-14 11:43:53 +0300

    tweek. bench 3203390

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -476,14 +476,17 @@ namespace {
         // the square is in the attacker's mobility area.
         unsafeChecks &= mobilityArea[Them];
 
+        int tropismAttackers = popcount(pos.pieces(Them) & ~pos.pieces(Them, PAWN) & ~pos.pieces(Them, KING) & kingFlank);
+
         kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                      +  69 * kingAttacksCount[Them]
                      + 185 * popcount(kingRing[Us] & weak)
                      + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
-                     +       (tropism + notTropismDefenders[Us]) * (tropism + notTropismDefenders[Us]) / 4
+                     +       tropism * tropism / 4
                      - 873 * !pos.count<QUEEN>(Them)
                      -   6 * mg_value(score) / 8
                      +       mg_value(mobility[Them] - mobility[Us])
+                     +   5 * notTropismDefenders[Us] * tropismAttackers
                      -   30;
 
         // Transform the kingDanger units into a Score, and subtract it from the evaluation
