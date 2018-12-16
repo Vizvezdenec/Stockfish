3390fb231dc2bcf6a5cf9d01ff8957507cf65279
Vizvezdenec <Vizvezdenec@gmail.com>
2018-12-16 12:16:00 +0300

    tweek. bench 3290471

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -649,7 +649,8 @@ namespace {
                 bonus -= make_score(0, king_proximity(Us, blockSq + Up) * w);
 
             // If the pawn is free to advance, then increase the bonus
-            if (pos.empty(blockSq) || ((pos.pieces(Them, QUEEN) & blockSq) && (pos.count<KNIGHT>(Us) + pos.count<BISHOP>(Us)>2)))
+            if (pos.empty(blockSq) || ((pos.pieces(Them, QUEEN) & blockSq) 
+                && pos.non_pawn_material(Us) <= QueenValueMg + 2 * RookValueMg))
             {
                 // If there is a rook or queen attacking/defending the pawn from behind,
                 // consider all the squaresToQueen. Otherwise consider only the squares
