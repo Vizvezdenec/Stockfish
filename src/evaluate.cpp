a86fb98f2c4f1545866660768347ee10abf76b40
Vizvezdenec <Vizvezdenec@gmail.com>
2018-12-19 11:03:48 +0300

    tweek. bench 3152588

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -564,7 +564,7 @@ namespace {
                 &  attackedBy[Us][ALL_PIECES];
     score += RestrictedPiece * popcount(restricted);
 
-    score += RestrictedPiece * popcount(attackedBy2[Them] & attackedBy[Us][PAWN] & ~pos.pieces(Them, PAWN));
+    score += RestrictedPiece * popcount(attackedBy2[Them] & attackedBy[Us][PAWN] & ~pos.pieces(Them, PAWN) & ~attackedBy[Them][PAWN]);
 
     // Bonus for enemy unopposed weak pawns
     if (pos.pieces(Us, ROOK, QUEEN))
