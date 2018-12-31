caba61b9a671ef53f94e37fdbb2a56846e6193e7
Vizvezdenec <Vizvezdenec@gmail.com>
2018-12-31 20:48:48 +0300

    tweek. bench 3354759

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -140,8 +140,8 @@ namespace {
         if (doubled && !support)
             score -= Doubled;
 
-        if (more_than_one(forward_file_bb(Us, s) & ourPawns) && !neighbours)
-            score -= make_score(50, 150);
+        if (more_than_one(forward_file_bb(Us, s) & ourPawns))
+            score -= make_score(20, 170);
     }
 
     return score;
