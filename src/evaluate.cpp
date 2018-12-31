386483c45fc44741709657f8c3a5012a9ba25618
Vizvezdenec <Vizvezdenec@gmail.com>
2019-01-01 02:48:33 +0300

    tweek. bench 3065171

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -388,9 +388,8 @@ namespace {
             if (pos.slider_blockers(pos.pieces(Them, ROOK, BISHOP), s, queenPinners))
                 score -= WeakQueen;
 
-            if (pos.count<BISHOP>(Them) == 1 
-                && (((pos.pieces(Them, BISHOP) & DarkSquares) && (DarkSquares & s))
-                || ((pos.pieces(Them, BISHOP) & ~DarkSquares) && (~DarkSquares & s))))
+            if (((pos.pieces(Them, BISHOP) & DarkSquares) && (DarkSquares & s))
+                || ((pos.pieces(Them, BISHOP) & ~DarkSquares) && (~DarkSquares & s)))
                 score -= make_score(5,5);
         }
     }
