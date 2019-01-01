df18be51bd23b7ef92ea0c5b2a70be7b7dfb2fd3
Vizvezdenec <Vizvezdenec@gmail.com>
2019-01-01 23:39:06 +0300

    tweek. bench 3110222

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -209,7 +209,6 @@ namespace {
     // possibly via x-ray or by one pawn and one piece. Diagonal x-ray through
     // pawn or squares attacked by 2 pawns are not explicitly added.
     Bitboard attackedBy2[COLOR_NB];
-    Bitboard pseudoattacked2[COLOR_NB];
 
     // kingRing[color] are the squares adjacent to the king, plus (only for a
     // king on its first rank) the squares two ranks in front. For instance,
@@ -258,7 +257,6 @@ namespace {
     attackedBy[Us][PAWN] = pe->pawn_attacks(Us);
     attackedBy[Us][ALL_PIECES] = attackedBy[Us][KING] | attackedBy[Us][PAWN];
     attackedBy2[Us]            = attackedBy[Us][KING] & attackedBy[Us][PAWN];
-    pseudoattacked2[Us] = 0;
 
     // Init our king safety tables
     kingRing[Us] = attackedBy[Us][KING];
@@ -299,13 +297,11 @@ namespace {
         b = Pt == BISHOP ? attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(QUEEN))
           : Pt ==   ROOK ? attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(QUEEN) ^ pos.pieces(Us, ROOK))
                          : pos.attacks_from<Pt>(s);
-        Bitboard pseudoAtt = (Pt == BISHOP || Pt == ROOK || Pt == QUEEN) ? attacks_bb<Pt>(s, pos.pieces(PAWN) | pos.pieces(Them))
-                         : 0;
+
         if (pos.blockers_for_king(Us) & s)
             b &= LineBB[pos.square<KING>(Us)][s];
 
         attackedBy2[Us] |= attackedBy[Us][ALL_PIECES] & b;
-        pseudoattacked2[Us] |= attackedBy[Us][ALL_PIECES] & pseudoAtt;
         attackedBy[Us][Pt] |= b;
         attackedBy[Us][ALL_PIECES] |= b;
 
@@ -391,6 +387,9 @@ namespace {
             Bitboard queenPinners;
             if (pos.slider_blockers(pos.pieces(Them, ROOK, BISHOP), s, queenPinners))
                 score -= WeakQueen;
+            if ((attacks_bb<QUEEN>(s, pos.pieces(PAWN)) & attackedBy[Us][PAWN] & shift<Down>(pos.pieces(Them, KING))) 
+                 && relative_rank(Us, pos.square<KING>(Them)) == RANK_8)
+                score += make_score(20, 50);
         }
     }
     if (T)
@@ -418,7 +417,7 @@ namespace {
     // which are attacked twice in that flank.
     kingFlank = KingFlank[file_of(ksq)];
     b1 = attackedBy[Them][ALL_PIECES] & kingFlank & Camp;
-    b2 = b1 & (attackedBy2[Them] | pseudoattacked2[Them]);
+    b2 = b1 & attackedBy2[Them];
 
     int tropism = popcount(b1) + popcount(b2);
 
@@ -827,10 +826,10 @@ namespace {
     initialize<BLACK>();
 
     // Pieces should be evaluated first (populate attack tables)
-    score +=  pieces<WHITE, KNIGHT>() - pieces<BLACK, KNIGHT>();
-    score += pieces<WHITE, BISHOP>() - pieces<BLACK, BISHOP>();
-    score += pieces<WHITE, ROOK  >() - pieces<BLACK, ROOK  >();
-    score += pieces<WHITE, QUEEN >() - pieces<BLACK, QUEEN >();
+    score +=  pieces<WHITE, KNIGHT>() - pieces<BLACK, KNIGHT>()
+            + pieces<WHITE, BISHOP>() - pieces<BLACK, BISHOP>()
+            + pieces<WHITE, ROOK  >() - pieces<BLACK, ROOK  >()
+            + pieces<WHITE, QUEEN >() - pieces<BLACK, QUEEN >();
 
     score += mobility[WHITE] - mobility[BLACK];
 
