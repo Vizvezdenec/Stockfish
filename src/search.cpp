ace166c091b2b2c96c4249a7f922d490c86fcc6f
Vizvezdenec <Vizvezdenec@gmail.com>
2019-01-06 03:31:41 +0300

    tweek. bench 3557333

--- src/search.cpp
+++ src/search.cpp
@@ -969,7 +969,9 @@ moves_loop: // When in check, search starts from here
       {
           if (   !captureOrPromotion
               && !givesCheck
-              && !pos.advanced_pawn_push(move))
+              && !pos.advanced_pawn_push(move)
+              && (ss-1)->statScore < 30000
+             )
           {
               // Move count based pruning (~30 Elo)
               if (moveCountPruning)
