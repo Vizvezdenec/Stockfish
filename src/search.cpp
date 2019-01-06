39e4598b0535ac0b2aacf37747b615ee7f5ba32f
Vizvezdenec <Vizvezdenec@gmail.com>
2019-01-06 11:25:41 +0300

    tweek. bench 3395985

--- src/search.cpp
+++ src/search.cpp
@@ -994,7 +994,7 @@ moves_loop: // When in check, search starts from here
                   continue;
 
               // Prune moves with negative SEE (~10 Elo)
-              if (!pos.see_ge(move, Value(-(29 + std::max(0, int(QueenValueMg - pos.non_pawn_material(us)))/200) * lmrDepth * lmrDepth)))
+              if (!pos.see_ge(move, Value(-(29 + std::max(0, int(QueenValueMg - pos.non_pawn_material(us)))/400) * lmrDepth * lmrDepth)))
                   continue;
           }
           else if (   !extension // (~20 Elo)
