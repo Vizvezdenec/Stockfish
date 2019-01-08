99558f3c46580208d2bcc794622c1f94ea7bc5f0
Vizvezdenec <Vizvezdenec@gmail.com>
2019-01-08 19:48:08 +0300

    tweek. bench 3113940

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -347,7 +347,7 @@ namespace {
                     score += LongDiagonalBishop;
             }
             else if (!(pos.pieces() & (DistanceRingBB[s][1] | DistanceRingBB[s][2])))
-                score -= make_score(20, 100);
+                score -= make_score(10, 35);
 
             // An important Chess960 pattern: A cornered bishop blocked by a friendly
             // pawn diagonally in front of it is a very serious problem, especially
