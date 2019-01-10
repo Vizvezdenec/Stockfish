17559f4a26da13c23c0779fefe582ce22cd50756
Vizvezdenec <Vizvezdenec@gmail.com>
2019-01-10 19:32:14 +0300

    tweek. bench 4085092

--- src/search.cpp
+++ src/search.cpp
@@ -962,7 +962,7 @@ moves_loop: // When in check, search starts from here
           // search without the ttMove. So we assume this expected Cut-node is not singular,
           // that is multiple moves fail high, and we can prune the whole subtree by returning
           // the hard beta bound.
-          else if (cutNode && singularBeta > beta && pvHit)
+          else if (cutNode && singularBeta > beta && (ss-1)->statScore < 27000)
               return beta;
       }
       else if (    givesCheck // Check extension (~2 Elo)
