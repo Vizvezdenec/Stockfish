0cd2d48398a96d66e8ae73bb08ae4d5aff6cc356
Vizvezdenec <Vizvezdenec@gmail.com>
2019-01-12 23:51:46 +0300

    tweek. bench 3700108

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -577,7 +577,10 @@ namespace {
     b = pawn_attacks_bb<Us>(b) & nonPawnEnemies;
     score += ThreatBySafePawn * popcount(b);
 
-    score += make_score(5,5) * popcount(attackedBy[Us][ALL_PIECES] & pos.pieces(Them, PAWN) & ~attackedBy[Them][PAWN]);
+    score += make_score(5,5) 
+              * popcount(
+              ((attackedBy[Us][ALL_PIECES] & ~attackedBy[Us][PAWN]) | attackedBy2[Us])
+              & pos.pieces(Them, PAWN) & ~attackedBy[Them][PAWN]);
     // Bonus for threats on the next moves against enemy queen
     if (pos.count<QUEEN>(Them) == 1)
     {
