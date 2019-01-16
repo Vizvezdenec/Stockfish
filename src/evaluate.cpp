6ef3b070e75ba7fc10f270b7a97ab5b1411c00b9
Vizvezdenec <Vizvezdenec@gmail.com>
2019-01-16 20:28:12 +0300

    tweek. bench 3701499

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -332,13 +332,11 @@ namespace {
 
             // Penalty if the piece is far from the king
             score -= KingProtector * distance(s, pos.square<KING>(Us));
-
+            Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
             if (Pt == BISHOP)
             {
                 // Penalty according to number of pawns on the same color square as the
                 // bishop, bigger when the center files are blocked with pawns.
-                Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
-
                 score -= BishopPawns * pe->pawns_on_same_color_squares(Us, s)
                                      * (1 + popcount(blocked & CenterFiles));
 
@@ -346,6 +344,9 @@ namespace {
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
                     score += LongDiagonalBishop;
             }
+            else if (!(pos.pieces() & (DistanceRingBB[s][1] | DistanceRingBB[s][2]) 
+                      & ~(pos.pieces(Them, PAWN) & attackedBy[Them][PAWN]) & ~blocked))
+                score -= make_score(15, 75);
 
             // An important Chess960 pattern: A cornered bishop blocked by a friendly
             // pawn diagonally in front of it is a very serious problem, especially
@@ -777,9 +778,6 @@ namespace {
             && pos.non_pawn_material(WHITE) == BishopValueMg
             && pos.non_pawn_material(BLACK) == BishopValueMg)
             sf = 8 + 4 * pe->pawn_asymmetry();
-        else if (pos.non_pawn_material(strongSide) == QueenValueMg
-            && pos.count<QUEEN>(~strongSide) == 0)
-            sf = std::min(40 + 7 * pos.count<PAWN>(strongSide) - 30 * (pe->win_ability(strongSide) < 3), sf);
         else
             sf = std::min(40 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide), sf);
 
