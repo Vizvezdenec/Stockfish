ce74dce436d288ae5a3f6847c994b4ad4caccff3
Vizvezdenec <Vizvezdenec@gmail.com>
2019-01-18 05:28:20 +0300

    tweek. bench 4017731

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -464,12 +464,13 @@ namespace {
     // the square is in the attacker's mobility area.
     unsafeChecks &= mobilityArea[Them];
 
-    weak |= attackedBy[Them][PAWN] & (attackedBy[Them][KNIGHT] | attackedBy[Them][BISHOP])
+    Bitboard weakToMinors = attackedBy[Them][PAWN] & (attackedBy[Them][KNIGHT] | attackedBy[Them][BISHOP])
             & ~attackedBy[Us][PAWN] & ~attackedBy[Us][BISHOP] & ~attackedBy[Us][KNIGHT];
 
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  +  69 * kingAttacksCount[Them]
                  + 185 * popcount(kingRing[Us] & weak)
+                 +  75 * popcount(kingRing[Us] & weakToMinors)
                  + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                  +       tropism * tropism / 4
                  - 873 * !pos.count<QUEEN>(Them)
