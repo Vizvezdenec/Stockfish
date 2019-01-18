5e5308b23543f773df7dd3dcb93950e8556fca22
Vizvezdenec <Vizvezdenec@gmail.com>
2019-01-18 23:23:37 +0300

    tweek. bench 3665973

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -565,7 +565,7 @@ namespace {
     b |= shift<Up>(b & TRank3BB) & ~pos.pieces();
 
     // Keep only the squares which are relatively safe
-    b &= ~attackedBy[Them][PAWN] & safe;
+    b &= ~pawn_attacks_bb<Them>(pos.pieces(Them,PAWN) & ~pos.blockers_for_king(Them)) & safe;
 
     // Bonus for safe pawn threats on the next move
     b = pawn_attacks_bb<Us>(b) & pos.pieces(Them);
