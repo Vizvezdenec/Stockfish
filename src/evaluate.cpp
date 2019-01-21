d45b19b6688df51cac6fce8da1ff5933abf6a446
Vizvezdenec <Vizvezdenec@gmail.com>
2019-01-21 15:06:42 +0300

    tweek. bench 4096808

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -168,7 +168,7 @@ namespace {
   constexpr Score ThreatByPawnPush   = S( 48, 39);
   constexpr Score ThreatByRank       = S( 13,  0);
   constexpr Score ThreatBySafePawn   = S(173, 94);
-  constexpr Score TrappedRook        = S( 47,  4);
+  constexpr Score TrappedRook        = S( 30,  4);
   constexpr Score WeakQueen          = S( 49, 15);
   constexpr Score WeakUnopposedPawn  = S( 12, 23);
 
@@ -374,11 +374,7 @@ namespace {
 
             // Penalty when trapped by the king, even more if the king cannot castle
             else if (mob <= 3)
-            {
-                File kf = file_of(pos.square<KING>(Us));
-                if ((kf < FILE_E) == (file_of(s) < kf))
                     score -= TrappedRook * (1 + !pos.castling_rights(Us));
-            }
         }
 
         if (Pt == QUEEN)
