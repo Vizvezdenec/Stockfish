cf71f03201057ebc3fc2b9eeb3be938ea6bd6071
Vizvezdenec <Vizvezdenec@gmail.com>
2019-01-23 05:36:27 +0300

    tweek. bench 4021371

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -345,6 +345,11 @@ namespace {
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
                     score += LongDiagonalBishop;
+
+                if (DarkSquares & s)
+                    score -= make_score(0, 50) * !(pos.pieces(Them) & ~pos.pieces(Them, KING) & DarkSquares);
+                else 
+                    score -= make_score(0, 50) * !(pos.pieces(Them) & ~pos.pieces(Them, KING) & ~DarkSquares);
             }
 
             // An important Chess960 pattern: A cornered bishop blocked by a friendly
