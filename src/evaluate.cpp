3f62f04d65e2655fce1f84e7e2078a51c2eef90b
Vizvezdenec <Vizvezdenec@gmail.com>
2019-02-03 06:08:03 +0300

    twikk. bench 4027134

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -479,7 +479,7 @@ namespace {
     // the square is in the attacker's mobility area.
     unsafeChecks &= mobilityArea[Them];
 
-    Bitboard bishopProtection = attackedBy[Us][BISHOP] & attackedBy[Us][KING];
+    Bitboard bishopProtection = attackedBy[Us][BISHOP] & kingRing[Us];
 
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  +  69 * kingAttacksCount[Them]
@@ -492,7 +492,7 @@ namespace {
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
-                 -   25;
+                 -   10;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 0)
