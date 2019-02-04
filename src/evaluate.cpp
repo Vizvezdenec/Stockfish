4c05221dd510b6cfcc39551cd4fe063a73ea0722
Vizvezdenec <Vizvezdenec@gmail.com>
2019-02-04 06:29:13 +0300

    twikk. bench 3947990

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -483,7 +483,7 @@ namespace {
                 KnightCheckers |= pos.attacks_from<KNIGHT>(s) & pos.pieces(Them, KNIGHT);
                 }
             if (more_than_one(KnightCheckers))
-                kingDanger += KnightSafeCheck / 4;
+                kingDanger += KnightSafeCheck / 6;
             }
         }
     else
