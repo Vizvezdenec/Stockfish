a7736934cd8570fedfe956e1d3c19b9ff3c678d2
Vizvezdenec <Vizvezdenec@gmail.com>
2019-02-05 18:18:14 +0300

    twikk. bench 3841089

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -296,8 +296,8 @@ namespace {
         // Find attacked squares, including x-ray attacks for bishops and rooks
         b = Pt == BISHOP ? attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(QUEEN))
           : Pt ==   ROOK ? attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(QUEEN) ^ pos.pieces(Us, ROOK))
-          : Pt == QUEEN ? attacks_bb<QUEEN>(s, pos.pieces() ^ pos.pieces(Us, QUEEN) 
-                  ^ (pos.pieces(Us, ROOK) & (RankBB[s] | FileBB[s])) ^ (pos.pieces(Us, BISHOP) & ~(RankBB[s] | FileBB[s])))
+          : Pt == QUEEN ? attacks_bb<QUEEN>(s, pos.pieces() & ~pos.pieces(Us, QUEEN) 
+                  & ~(pos.pieces(Us, ROOK) & (RankBB[s] | FileBB[s])) & ~(pos.pieces(Us, BISHOP) & ~(RankBB[s] | FileBB[s])))
                          : pos.attacks_from<Pt>(s);
 
         if (pos.blockers_for_king(Us) & s)
