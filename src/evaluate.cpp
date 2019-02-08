d86542ea109423963c29dcadc15fadb47b356d7f
Vizvezdenec <Vizvezdenec@gmail.com>
2019-02-08 04:42:55 +0300

    twikk. bench 3893939

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -413,7 +413,7 @@ namespace {
     // Find the squares that opponent attacks in our king flank, and the squares
     // which are attacked twice in that flank.
     kingFlank = KingFlank[file_of(ksq)];
-    b1 = ((attackedBy[Them][ALL_PIECES] & ~attackedBy[Them][KING]) | attackedBy2[Them]) & kingFlank & Camp;
+    b1 = ((attackedBy[Them][ALL_PIECES] & ~attackedBy[Them][KING] & ~attackedBy[Them][QUEEN]) | attackedBy2[Them]) & kingFlank & Camp;
     b2 = b1 & attackedBy2[Them];
 
     int tropism = popcount(b1) + popcount(b2);
