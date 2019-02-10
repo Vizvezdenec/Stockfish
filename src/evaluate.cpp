bfd14e40c5d3f2a4dab1156d91fe553a47c0f407
Vizvezdenec <Vizvezdenec@gmail.com>
2019-02-10 17:54:09 +0300

    twikk. bench 3810726

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -759,7 +759,7 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    +  2 * abs(pe->pawn_rank(WHITE) - pe->pawn_rank(BLACK))
+                    +      abs(pe->pawn_rank(WHITE) - pe->pawn_rank(BLACK)) / 2
                     -121 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
