68c9c60e1609774086539afab882e392eed29925
Vizvezdenec <Vizvezdenec@gmail.com>
2019-02-12 15:22:41 +0300

    twikk. bench 3870830

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -131,10 +131,6 @@ namespace {
   constexpr Score ThreatByRook[PIECE_TYPE_NB] = {
     S(0, 0), S(0, 24), S(38, 71), S(38, 61), S(0, 38), S(51, 38)
   };
-  
-  constexpr Score ThreatBySafePawn[PIECE_TYPE_NB] = {
-    S(0, 0), S(0, 0), S(98, 108), S(104, 33), S(196, 110), S(290, 124)
-  };
 
   // PassedRank[Rank] contains a bonus according to the rank of a passed pawn
   constexpr Score PassedRank[RANK_NB] = {
@@ -163,6 +159,7 @@ namespace {
   constexpr Score ThreatByKing       = S( 24, 89);
   constexpr Score ThreatByPawnPush   = S( 48, 39);
   constexpr Score ThreatByRank       = S( 13,  0);
+  constexpr Score ThreatBySafePawn   = S(173, 94);
   constexpr Score TrappedRook        = S( 47,  4);
   constexpr Score WeakQueen          = S( 49, 15);
   constexpr Score WeakUnopposedPawn  = S( 12, 23);
@@ -267,7 +264,8 @@ namespace {
     else if (file_of(ksq) == FILE_A)
         kingRing[Us] |= shift<EAST>(kingRing[Us]);
 
-    kingAttackersCount[Them] = popcount(kingRing[Us] & pe->pawn_attacks(Them));
+    kingAttackersCount[Them] = popcount((kingRing[Us] & pe->pawn_attacks(Them)) 
+                               | (pawn_attacks_bb<Us>(kingRing[Us]) & pos.pieces(Them, PAWN)))/2;
     kingAttacksCount[Them] = kingAttackersWeight[Them] = 0;
 
     // Remove from kingRing[] the squares defended by two pawns
@@ -588,11 +586,7 @@ namespace {
     b = pos.pieces(Us, PAWN) & safe;
 
     b = pawn_attacks_bb<Us>(b) & nonPawnEnemies;
-    if (b)
-    while (b)
-        {
-	    score += ThreatBySafePawn[type_of(pos.piece_on(pop_lsb(&b)))];
-        };
+    score += ThreatBySafePawn * popcount(b);
 
     // Bonus for threats on the next moves against enemy queen
     if (pos.count<QUEEN>(Them) == 1)
