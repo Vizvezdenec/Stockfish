557322a988f258e5c856336c4f6eae86b4886a83
Vizvezdenec <Vizvezdenec@gmail.com>
2019-02-13 17:34:28 +0300

    twikk. bench 4218035

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -478,7 +478,7 @@ namespace {
                  + 185 * popcount(kingRing[Us] & weak)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
-                 - 853 * !pos.count<QUEEN>(Them)
+                 - 893 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
