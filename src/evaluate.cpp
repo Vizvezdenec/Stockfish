4ae96d87b99bd1e4830055df300bacd12507b8fd
Vizvezdenec <Vizvezdenec@gmail.com>
2019-02-15 10:04:02 +0300

    twikk. bench 3529892

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -607,8 +607,8 @@ namespace {
     while (b) 
     {
         Square s = pop_lsb(&b);
-        if (!(pos.attacks_from<BISHOP>(s) & ~attackedBy[Them][ALL_PIECES]))
-             score -= make_score(70, 70);
+        if (!(pos.attacks_from<BISHOP>(s) & (~((attackedBy[Them][ALL_PIECES] & ~attackedBy2[Us]) | attackedBy2[Them]))))
+             score -= make_score(50, 20);
     }
     if (T)
         Trace::add(THREAT, Us, score);
