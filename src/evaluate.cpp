20f9a084e4b327ab2d795e3706b6b90d2d59370c
Vizvezdenec <Vizvezdenec@gmail.com>
2019-02-17 01:35:39 +0300

    twikk. bench 3617561

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -603,8 +603,8 @@ namespace {
         score += SliderOnQueen * popcount(b & safe & attackedBy2[Us]);
     }
 
-    if (!(pos.pieces(Them) & shift<Up>(pos.pieces(Us, PAWN)) & CenterFiles))
-        score += make_score(5, 5) * (pos.count<BISHOP>(Us) + pos.count<QUEEN>(Us));
+        score += make_score(3, 3) * ((pos.count<BISHOP>(Us) + pos.count<QUEEN>(Us)) 
+                 * (8 - popcount(pos.pieces(PAWN) & CenterFiles)));
 
     if (T)
         Trace::add(THREAT, Us, score);
