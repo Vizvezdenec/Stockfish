79b50adb0db15f784117e1f7b54f6de427e4b8d4
Vizvezdenec <Vizvezdenec@gmail.com>
2019-02-21 21:42:04 +0300

    twikk. bench 3802066

--- src/search.cpp
+++ src/search.cpp
@@ -984,8 +984,7 @@ moves_loop: // When in check, search starts from here
               // Countermoves based pruning (~20 Elo)
               if (   lmrDepth < 3 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
-                  && ((*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
-                  || (*contHist[3])[movedPiece][to_sq(move)] < CounterMovePruneThreshold))
+                  && ((*contHist[1])[movedPiece][to_sq(move)] + (*contHist[3])[movedPiece][to_sq(move)] < 2 * CounterMovePruneThreshold))
                   continue;
 
               // Futility pruning: parent node (~2 Elo)
