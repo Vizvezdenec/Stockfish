b3bed2b141f66c268a301337b99b6a097a68c3fd
Vizvezdenec <Vizvezdenec@gmail.com>
2019-02-22 01:42:06 +0300

    twikk. bench 3241563

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -401,6 +401,7 @@ namespace {
     constexpr Color    Them = (Us == WHITE ? BLACK : WHITE);
     constexpr Bitboard Camp = (Us == WHITE ? AllSquares ^ Rank6BB ^ Rank7BB ^ Rank8BB
                                            : AllSquares ^ Rank1BB ^ Rank2BB ^ Rank3BB);
+    constexpr Direction Up       = (Us == WHITE ? NORTH   : SOUTH);
 
     Bitboard weak, b, b1, b2, safe, unsafeChecks = 0;
     int kingDanger = 0;
@@ -438,7 +439,16 @@ namespace {
                          & safe
                          & ~attackedBy[Us][QUEEN]
                          & ~RookCheck;
-
+    if (!QueenCheck && pos.count<QUEEN>(Them) == 1)
+    	{
+        Square s = pos.square<QUEEN>(Them);
+        if (
+            (attacks_bb<QUEEN>(s, pos.pieces(Us)) & ksq)
+            && (!more_than_one(pos.pieces(Them) & BetweenBB[s][ksq]))
+            && !(pos.pieces(Them) & BetweenBB[s][ksq] & (pos.blockers_for_king(Them) | (shift<Up>(pos.pieces()) & pos.pieces(Them, PAWN))))
+           )
+            QueenCheck = SquareBB[s];
+        }
     if (QueenCheck)
         kingDanger += QueenSafeCheck;
 
