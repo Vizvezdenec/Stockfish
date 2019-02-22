39dc18df3fe83fe9b20303b94cddb2260bdf00fe
Vizvezdenec <Vizvezdenec@gmail.com>
2019-02-22 11:08:48 +0300

    twikk. bench 3118241

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -401,6 +401,7 @@ namespace {
     constexpr Color    Them = (Us == WHITE ? BLACK : WHITE);
     constexpr Bitboard Camp = (Us == WHITE ? AllSquares ^ Rank6BB ^ Rank7BB ^ Rank8BB
                                            : AllSquares ^ Rank1BB ^ Rank2BB ^ Rank3BB);
+    //constexpr Direction Up       = (Us == WHITE ? NORTH   : SOUTH);
 
     Bitboard weak, b, b1, b2, safe, unsafeChecks = 0;
     int kingDanger = 0;
@@ -473,11 +474,24 @@ namespace {
 
     int kingFlankAttacks = popcount(b1) + popcount(b2);
 
+    b = pos.blockers_for_king(Us) & pos.pieces(Them);
+    if (b)
+         {
+         b1 = attacks_bb<ROOK  >(ksq, pos.pieces() ^ b);
+         b2 = attacks_bb<BISHOP>(ksq, pos.pieces() ^ b);
+         if (!RookCheck && (b1 & pos.pieces(Them, ROOK) & (~attackedBy[Us][ALL_PIECES] | weak)))
+              kingDanger += RookSafeCheck;
+         if (!QueenCheck && ((b1 | b2) & pos.pieces(Them, QUEEN) & ~attackedBy[Us][ALL_PIECES]))
+              kingDanger += QueenSafeCheck;
+         if (!BishopCheck && (b2 & pos.pieces(Them, BISHOP) & (~attackedBy[Us][ALL_PIECES] | weak)))
+              kingDanger += BishopSafeCheck;
+         }
+
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  +  69 * kingAttacksCount[Them]
                  + 185 * popcount(kingRing[Us] & weak)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
-                 + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
+                 + 150 * popcount((pos.blockers_for_king(Us) & pos.pieces(Us)) | unsafeChecks)
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
