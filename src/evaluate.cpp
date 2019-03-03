7608c1102787ae2cfb4badaf8b3a4a512e3f9e8a
Vizvezdenec <Vizvezdenec@gmail.com>
2019-03-03 18:25:54 +0300

    twikk. bench 3437854

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -484,7 +484,7 @@ namespace {
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
-                 +   2 * doubleAttKR * doubleAttKR / 3
+                 +   8 * doubleAttKR
                  -   25;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
