75ebcef996591c5f31951f6bf9d9d072fefd7da2
Vizvezdenec <Vizvezdenec@gmail.com>
2019-03-06 02:33:02 +0300

    tweack. bench 2966818

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -115,8 +115,7 @@ void MovePicker::score() {
                    + (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
-                   + (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)] / 2
-                   + (*continuationHistory[7])[pos.moved_piece(m)][to_sq(m)] / 4;
+                   + (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)] / 2;
 
       else // Type == EVASIONS
       {
