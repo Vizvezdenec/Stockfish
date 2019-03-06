9aa53eeb4b7dc149b120a8a0a71b62cb87064e14
Vizvezdenec <Vizvezdenec@gmail.com>
2019-03-06 04:16:19 +0300

    tweack. bench 3090372

--- src/search.cpp
+++ src/search.cpp
@@ -971,7 +971,7 @@ moves_loop: // When in check, search starts from here
               if (   lmrDepth < 3 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
-                  && ((*contHist[3])[movedPiece][to_sq(move)] + (*contHist[5])[movedPiece][to_sq(move)]< 5 * CounterMovePruneThreshold) /2)
+                  && ((*contHist[3])[movedPiece][to_sq(move)] + (*contHist[5])[movedPiece][to_sq(move)]< 5 * CounterMovePruneThreshold / 2))
                   continue;
 
               // Futility pruning: parent node (~2 Elo)
