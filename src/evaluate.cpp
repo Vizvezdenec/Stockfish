cb7576efa43859b15675543c3aa1e067ebdca16d
Vizvezdenec <Vizvezdenec@gmail.com>
2019-03-06 17:34:51 +0300

    tweack. bench 3158434

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -498,6 +498,7 @@ namespace {
     constexpr Color     Them     = (Us == WHITE ? BLACK   : WHITE);
     constexpr Direction Up       = (Us == WHITE ? NORTH   : SOUTH);
     constexpr Bitboard  TRank3BB = (Us == WHITE ? Rank3BB : Rank6BB);
+    constexpr Bitboard LowRanks = (Us == WHITE ? Rank2BB | Rank3BB: Rank7BB | Rank6BB);
 
     Bitboard b, weak, defended, nonPawnEnemies, stronglyProtected, safe;
     Score score = SCORE_ZERO;
@@ -562,6 +563,9 @@ namespace {
     // Find squares where our pawns can push on the next move
     b  = shift<Up>(pos.pieces(Us, PAWN)) & ~pos.pieces();
     b |= shift<Up>(b & TRank3BB) & ~pos.pieces();
+
+    score -= make_score(10, 10) * popcount(((attackedBy[Us][ALL_PIECES] & ~attackedBy[Us][PAWN]) 
+              | attackedBy2[Us]) & LowRanks & attackedBy[Them][PAWN] & ~b);
 
     // Keep only the squares which are relatively safe
     b &= ~attackedBy[Them][PAWN] & safe;
