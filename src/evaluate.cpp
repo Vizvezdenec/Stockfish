ce59960c18a975a0d5d872d96c6e7ee697a6b17c
Vizvezdenec <Vizvezdenec@gmail.com>
2019-03-12 18:57:48 +0300

    tweack. bench 3230496

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -325,9 +325,13 @@ namespace {
                 // Penalty according to number of pawns on the same color square as the
                 // bishop, bigger when the center files are blocked with pawns.
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
-
+                
+                if (mob<7)
                 score -= BishopPawns * pe->pawns_on_same_color_squares(Us, s)
                                      * (1 + popcount(blocked & CenterFiles));
+                else 
+                score -= BishopPawns * pe->pawns_on_same_color_squares(Us, s)
+                                     * (1 + popcount(blocked & CenterFiles)) / 3;
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
