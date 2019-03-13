013dd75f6da8dbbd65bc90ab3c42a6d165a8fb35
Vizvezdenec <Vizvezdenec@gmail.com>
2019-03-14 02:46:19 +0300

    tweack. bench 3105321

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -140,7 +140,7 @@ namespace {
   constexpr Score KingProtector      = S(  7,  8);
   constexpr Score KnightOnQueen      = S( 16, 12);
   constexpr Score LongDiagonalBishop = S( 45,  0);
-  constexpr Score MinorBehindPawn    = S( 18,  3);
+  constexpr Score MinorBehindPawn    = S( 21,  4);
   constexpr Score Outpost            = S(  9,  3);
   constexpr Score PawnlessFlank      = S( 17, 95);
   constexpr Score RestrictedPiece    = S(  7,  7);
