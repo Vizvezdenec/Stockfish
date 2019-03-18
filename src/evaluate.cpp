bb142296c59312b82e836800827a1ef81f0bfba3
Vizvezdenec <Vizvezdenec@gmail.com>
2019-03-18 10:53:42 +0300

    tweack. bench 3257863

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -465,7 +465,7 @@ namespace {
                  +  69 * kingAttacksCount[Them]
                  + 185 * popcount(kingRing[Us] & weak)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
-                 + 158 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
+                 + 143 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
