d224f40eca439b49cdfd31e1188a257811aafa3a
Vizvezdenec <Vizvezdenec@gmail.com>
2019-03-18 15:43:14 +0300

    tweack. bench 3341345

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -468,8 +468,8 @@ namespace {
                  + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
-                 + 105 * mg_value(mobility[Them] - mobility[Us])/ 100
-                 +   5 * kingFlankAttacks * kingFlankAttacks / 16
+                 +       mg_value(mobility[Them] - mobility[Us])
+                 +   19 * kingFlankAttacks * kingFlankAttacks / 64
                  -   25;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
