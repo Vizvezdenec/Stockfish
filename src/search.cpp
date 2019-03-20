e90221abbf52d9f71d2ce40506d5d5a52ad6623a
Vizvezdenec <Vizvezdenec@gmail.com>
2019-03-20 20:46:37 +0300

    tweack. bench 2545183

--- src/search.cpp
+++ src/search.cpp
@@ -1003,6 +1003,9 @@ moves_loop: // When in check, search starts from here
           if (ttPv)
               r -= ONE_PLY;
 
+          if (!improving)
+              r += ONE_PLY;
+
           // Decrease reduction if opponent's move count is high (~10 Elo)
           if ((ss-1)->moveCount > 15)
               r -= ONE_PLY;
