26ac2b529777ddcf95af51fd6321f1b0466e6b18
Vizvezdenec <Vizvezdenec@gmail.com>
2019-03-25 23:25:10 +0300

    tweack. bench 3369985

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -230,7 +230,7 @@ namespace {
     const Square ksq = pos.square<KING>(Us);
 
     // Find our pawns that are blocked or on the first two ranks
-    Bitboard b = pos.pieces(Us, PAWN) & (shift<Down>(pos.pieces()) | LowRanks);
+    Bitboard b = pos.pieces(Us, PAWN) & (shift<Down>(pos.pieces()) | LowRanks) & ~pawn_attacks_bb<Them>(pos.pieces(Them));
 
     // Squares occupied by those pawns, by our king or queen or controlled by
     // enemy pawns are excluded from the mobility area.
