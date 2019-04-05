b1c0b5e8fcf28f402b0331fab39a68a9b426b5da
Vizvezdenec <Vizvezdenec@gmail.com>
2019-04-05 14:35:35 +0300

    tweack. bench 3506898

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -710,15 +710,23 @@ namespace {
 
     // Find the available squares for our pieces inside the area defined by SpaceMask
     Bitboard safe =   SpaceMask
-                   & ~pos.pieces(Us, PAWN)
                    & ~attackedBy[Them][PAWN];
 
     // Find all squares which are at most three squares behind some friendly pawn
-    Bitboard behind = pos.pieces(Us, PAWN);
+    Bitboard behind = pos.pieces(Us, PAWN) & CenterFiles;
+    
+    int bonus = 0;
+
+    if (behind)
+    {
+    safe &= ~behind;
     behind |= shift<Down>(behind);
     behind |= shift<Down>(shift<Down>(behind));
+    bonus += popcount(behind & safe);
+    }
+    
+    bonus += popcount(safe);
 
-    int bonus = popcount(safe) + popcount(behind & safe);
     int weight =  pos.count<ALL_PIECES>(Us)
                 - 2 * popcount(pe->semiopenFiles[WHITE] & pe->semiopenFiles[BLACK]);
 
