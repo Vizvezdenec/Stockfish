eb527da2500054f62201ca11aa5bde9bc9250ab7
Vizvezdenec <Vizvezdenec@gmail.com>
2019-04-06 11:46:37 +0300

    tweack. bench 3446462

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -750,8 +750,9 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    + 36 * (bool(pos.count<KNIGHT>()))
-                    -112 ;
+                    + 27 * (pos.non_pawn_material(WHITE) == KnightValueMg 
+                            && pos.non_pawn_material(BLACK) == KnightValueMg)
+                    -103 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
