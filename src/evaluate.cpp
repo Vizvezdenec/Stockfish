bd758f5ff1394269e911f363782d92a87c07ba19
Vizvezdenec <Vizvezdenec@gmail.com>
2019-04-08 17:29:26 +0300

    tweack. bench 3064383

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -366,8 +366,7 @@ namespace {
             {
                 File kf = file_of(pos.square<KING>(Us));
                 if ((kf < FILE_E) == (file_of(s) < kf))
-                    score -= TrappedRook * (1 + !pos.castling_rights(Us)
-                             - bool(!(attackedBy[Us][KING] & s)));
+                    score -= TrappedRook * (1 + !pos.castling_rights(Us));
             }
         }
 
@@ -656,7 +655,10 @@ namespace {
                     defendedSquares &= attackedBy[Us][ALL_PIECES];
 
                 if (!(pos.pieces(Them) & bb))
-                    unsafeSquares &= attackedBy[Them][ALL_PIECES] | pos.pieces(Them);
+                    unsafeSquares &= ((attackedBy[Them][ALL_PIECES] 
+                    & ~(attackedBy[Us][PAWN] & ~attackedBy2[Them])) | pos.pieces(Them));
+                else 
+                    unsafeSquares &= ~(attackedBy[Us][PAWN] & ~attackedBy[Them][ALL_PIECES]) | pos.pieces(Them);
 
                 // If there aren't any enemy attacks, assign a big bonus. Otherwise
                 // assign a smaller bonus if the block square isn't attacked.
