98539d292a2580716b47d05a004de8321abdc02c
Vizvezdenec <Vizvezdenec@gmail.com>
2019-04-14 16:35:35 +0300

    tweack. bench 3298439

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -374,8 +374,9 @@ namespace {
         {
             // Penalty if any relative pin or discovered attack against the queen
             Bitboard queenPinners;
-            if (pos.slider_blockers(pos.pieces(Them, ROOK, BISHOP), s, queenPinners))
-                score -= WeakQueen;
+            b = pos.slider_blockers(pos.pieces(Them, ROOK, BISHOP), s, queenPinners);
+            if (b)
+                score -= WeakQueen * (1 + bool(LineBB[s][pos.square<KING>(Us)] & b));
         }
     }
     if (T)
