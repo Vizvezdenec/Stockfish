ad5c6cceb073229d4fa0950c885addbd36495f81
Vizvezdenec <Vizvezdenec@gmail.com>
2019-05-07 08:12:19 +0300

    tweik. bench 3380381

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -406,7 +406,7 @@ namespace {
           & (~attackedBy[Us][ALL_PIECES] | attackedBy[Us][KING] | attackedBy[Us][QUEEN]);
 
     // Analyse the safe enemy's checks which are possible on next move
-    safe  = ~pos.pieces(Them);
+    safe  = ~(pos.pieces(Them) & ~pos.blockers_for_king(Us));
     safe &= ~attackedBy[Us][ALL_PIECES] | (weak & attackedBy2[Them]);
 
     b1 = attacks_bb<ROOK  >(ksq, pos.pieces() ^ pos.pieces(Us, QUEEN));
