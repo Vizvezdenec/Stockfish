bcfe1c18e1e7f7e1a93e1f97360d07028a4eb1b8
Vizvezdenec <Vizvezdenec@gmail.com>
2019-05-07 15:10:26 +0300

    tweik. bench 3254155

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -136,7 +136,7 @@ namespace {
         else if (backward)
             score -= Backward, e->weakUnopposed[Us] += !opposed;
 
-        if (doubled && !neighbours)
+        if (doubled && !(neighbours & pos.attacks_from<KING>(s)))
             score -= Doubled;
     }
 
