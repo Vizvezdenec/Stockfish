8d61cc8c1684fef6dc373d66bd115d73832c97d9
Vizvezdenec <Vizvezdenec@gmail.com>
2019-05-09 14:32:32 +0300

    tweik. bench 3328789

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -461,6 +461,7 @@ namespace {
     b2 = b1 & attackedBy2[Them];
 
     int kingFlankAttacks = popcount(b1) + popcount(b2);
+    int kingFlankDef = popcount(attackedBy[Us][ALL_PIECES] & KingFlank[file_of(ksq)] & Camp);
 
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  +  69 * kingAttacksCount[Them]
@@ -472,7 +473,8 @@ namespace {
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
-                 -   7;
+                 -       kingFlankDef * kingFlankDef
+                 +   133;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
