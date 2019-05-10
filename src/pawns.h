6d6c39e5f11208ad755ebca5e12f15d3b21d20c5
Vizvezdenec <Vizvezdenec@gmail.com>
2019-05-10 04:26:49 +0300

    tweik. bench 3582208

--- src/pawns.h
+++ src/pawns.h
@@ -37,7 +37,6 @@ struct Entry {
   Bitboard pawn_attacks(Color c) const { return pawnAttacks[c]; }
   Bitboard passed_pawns(Color c) const { return passedPawns[c]; }
   Bitboard pawn_attacks_span(Color c) const { return pawnAttacksSpan[c]; }
-  Bitboard pawn_double_att(Color c) const { return pawnDoubleAtt[c]; }
   int weak_unopposed(Color c) const { return weakUnopposed[c]; }
   int passed_count() const { return popcount(passedPawns[WHITE] | passedPawns[BLACK]); }
 
@@ -58,7 +57,6 @@ struct Entry {
   Bitboard passedPawns[COLOR_NB];
   Bitboard pawnAttacks[COLOR_NB];
   Bitboard pawnAttacksSpan[COLOR_NB];
-  Bitboard pawnDoubleAtt[COLOR_NB];
   Square kingSquares[COLOR_NB];
   Score kingSafety[COLOR_NB];
   int weakUnopposed[COLOR_NB];
