3f6e08a9374241b60557800854960056aa5d5bd3
Vizvezdenec <Vizvezdenec@gmail.com>
2019-05-11 05:22:13 +0300

    tweik. bench 3392317

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -652,7 +652,7 @@ namespace {
                 bb = forward_file_bb(Them, s) & pos.pieces(ROOK, QUEEN) & pos.attacks_from<ROOK>(s);
 
                 if (!(pos.pieces(Us) & bb))
-                    defendedSquares &= attackedBy[Us][ALL_PIECES];
+                    defendedSquares &= (attackedBy[Us][ALL_PIECES] | pos.pieces(Us));
 
                 if (!(pos.pieces(Them) & bb))
                     unsafeSquares &= attackedBy[Them][ALL_PIECES] | pos.pieces(Them);
