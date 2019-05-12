cf365376d8469424d13379c9474e1a4bc766dc52
Vizvezdenec <Vizvezdenec@gmail.com>
2019-05-12 17:03:52 +0300

    tweik. bench 3204502

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -473,7 +473,7 @@ namespace {
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
-                 +   2 * noKFmobility * noKFmobility
+                 +       noKFmobility * noKFmobility / 2
                  -   7;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
