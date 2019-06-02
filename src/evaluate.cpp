ac7bf26cf211cf651b2a79e7a91b75b5a1e13b3b
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-02 05:13:32 +0300

    tviig. bench 3618177

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -312,7 +312,7 @@ namespace {
 
             else if (bb &= b & ~pos.pieces(Us))
                 score += Outpost * (Pt == KNIGHT ? 2 : 1)
-                                 * ((attackedBy[Us][PAWN] & bb) ? 2 : 1) * 2 / (2 + !(bb & ~forward_ranks_bb(Us, s)));
+                                 * ((attackedBy[Us][PAWN] & bb) ? 2 : 1) * 2 / (2 + !(bb & forward_ranks_bb(Us, s)));
 
             // Knight and Bishop bonus for being right behind a pawn
             if (shift<Down>(pos.pieces(PAWN)) & s)
