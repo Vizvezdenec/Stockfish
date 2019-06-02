3e4481a0f355155afcf5fb821f293e4382b42893
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-02 18:02:13 +0300

    tviig. bench 3655932

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -598,7 +598,7 @@ namespace {
         {
         Bitboard queenColor = bool(DarkSquares & s) ? DarkSquares : ~DarkSquares;
         if (pos.pieces(Us, BISHOP) & queenColor)
-            score += make_score(12, 8);
+            score += make_score(7, 4);
         }
     }
 
