68252e56a57f1e3d741b588329dbdf2355284315
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-04 00:23:09 +0300

    tviig. bench 3666125

--- src/search.cpp
+++ src/search.cpp
@@ -957,8 +957,7 @@ moves_loop: // When in check, search starts from here
           moveCountPruning = moveCount >= futility_move_count(improving, depth / ONE_PLY);
 
           if (   !captureOrPromotion
-              && !givesCheck
-              && (!pos.advanced_pawn_push(move) || pos.non_pawn_material(~pos.side_to_move()) >= Value(1000)))
+              && !givesCheck)
           {
               // Move count based pruning (~30 Elo)
               if (moveCountPruning)
