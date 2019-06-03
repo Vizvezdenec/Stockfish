7752d630a2677bcc5e5b9af26b94eaa322555ca7
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-03 13:13:04 +0300

    tviig. bench 3513982

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -591,7 +591,15 @@ namespace {
         b =  (attackedBy[Us][BISHOP] & pos.attacks_from<BISHOP>(s))
            | (attackedBy[Us][ROOK  ] & pos.attacks_from<ROOK  >(s));
 
-        score += SliderOnQueen * popcount(b & safe & attackedBy2[Us]);
+        b &= safe & attackedBy2[Us];
+        score += SliderOnQueen * popcount(b);
+
+        if (!(b & pos.attacks_from<BISHOP>(s)))
+        {
+        Bitboard queenColor = bool(DarkSquares & s) ? DarkSquares : ~DarkSquares;
+        if (pos.pieces(Us, BISHOP) & queenColor)
+            score += make_score(0, 10);
+        }
     }
 
     if (T)
