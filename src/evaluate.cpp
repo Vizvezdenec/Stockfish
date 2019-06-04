f86d5c5ecf4a4a8542ae85169b8de56ee9b4af8f
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-05 01:54:49 +0300

    tveack. bench 3632955

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -751,8 +751,8 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    - 18 * noAdvPawns
-                    -100 ;
+                    - 12 * noAdvPawns
+                    -103 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
