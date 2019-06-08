5db31de5f41f7bbb8327ccebfc79be22f3fcfe9c
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-09 02:14:47 +0300

    tveack. bench 3387554

--- src/search.cpp
+++ src/search.cpp
@@ -1011,7 +1011,9 @@ moves_loop: // When in check, search starts from here
           &&  moveCount > 1 + 3 * rootNode
           && (  !captureOrPromotion
               || moveCountPruning
-              || (ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha && !rootNode)))
+              || ss->staticEval 
+          + (PieceValue[MG][pos.captured_piece()] * int(pos.non_pawn_material())
+            + PieceValue[EG][pos.captured_piece()] * int(MidgameLimit - pos.non_pawn_material())) / int(MidgameLimit) <= alpha))
       {
           Depth r = reduction(improving, depth, moveCount);
 
