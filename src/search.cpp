14458b54faa3c35c739786b6522da6936e3568d9
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-09 03:25:03 +0300

    tveack. bench 3788515

--- src/search.cpp
+++ src/search.cpp
@@ -1007,12 +1007,11 @@ moves_loop: // When in check, search starts from here
 
       // Step 16. Reduced depth search (LMR). If the move fails high it will be
       // re-searched at full depth.
-      if (    depth >= 3 * ONE_PLY
+      if (    depth >= (3 - ((ss-2)->moveCount == 1)) * ONE_PLY
           &&  moveCount > 1 + 3 * rootNode
           && (  !captureOrPromotion
               || moveCountPruning
-              || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
-              || cutNode))
+              || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha))
       {
           Depth r = reduction(improving, depth, moveCount);
 
