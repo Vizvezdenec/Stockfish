4a4937231b53a184400cde3c4c57b83a02115e39
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-09 03:54:31 +0300

    tveack. bench 4001702

--- src/search.cpp
+++ src/search.cpp
@@ -1007,7 +1007,7 @@ moves_loop: // When in check, search starts from here
 
       // Step 16. Reduced depth search (LMR). If the move fails high it will be
       // re-searched at full depth.
-      if (    depth >= (3 + ((ss-2)->moveCount == 1)) * ONE_PLY
+      if (    depth >= 3 * ONE_PLY
           &&  moveCount > 1 + 3 * rootNode
           && (  !captureOrPromotion
               || moveCountPruning
