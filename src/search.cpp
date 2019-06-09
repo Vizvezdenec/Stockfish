2c0f48085feb8f04a72b516c951a7309b72844c8
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-09 21:30:53 +0300

    tveack. bench 3716005

--- src/search.cpp
+++ src/search.cpp
@@ -1025,7 +1025,7 @@ moves_loop: // When in check, search starts from here
 
           if (!captureOrPromotion)
           {
-              if (abs(eval) < 100 && (abs(pos.non_pawn_material(WHITE) - pos.non_pawn_material(BLACK)) >= KnightValueMg))
+              if (abs(eval) < 20 && (abs(pos.non_pawn_material(WHITE) - pos.non_pawn_material(BLACK)) >= KnightValueMg))
                   r -= ONE_PLY;
 
 
