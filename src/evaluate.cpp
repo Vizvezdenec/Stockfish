15e2df22fb46f64b09c0848859cb199684481747
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-11 17:48:10 +0300

    tveack. bench 4022343

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -621,7 +621,7 @@ namespace {
 
         if (r > RANK_3)
         {
-            int w = (r-2) * (r-2) + 2 + 3 * bool(pe->passed_pawns(Us) & pos.attacks_from<KING>(s));
+            int w = (r-2) * (r-2) + 2;
             Square blockSq = s + Up;
 
             // Adjust bonus based on the king's proximity
--- src/search.cpp
+++ src/search.cpp
@@ -954,7 +954,8 @@ moves_loop: // When in check, search starts from here
 
           if (   !captureOrPromotion
               && !givesCheck
-              && (!pos.advanced_pawn_push(move) || pos.non_pawn_material(~us) > BishopValueMg))
+              && (!pos.advanced_pawn_push(move) || (pos.non_pawn_material(~us) > BishopValueMg 
+                      && pos.non_pawn_material() < 6000)))
           {
               // Move count based pruning (~30 Elo)
               if (moveCountPruning)
