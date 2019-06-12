8a90ef1a81edda347b076c781186b5c64d48deb4
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-12 11:20:52 +0300

    tveack. bench 3636349

--- src/search.cpp
+++ src/search.cpp
@@ -954,8 +954,7 @@ moves_loop: // When in check, search starts from here
 
           if (   !captureOrPromotion
               && !givesCheck
-              && (!pos.advanced_pawn_push(move) || (pos.non_pawn_material(~us) > BishopValueMg 
-                      && pos.non_pawn_material() < 4500)))
+              && (!pos.advanced_pawn_push(move) || pos.non_pawn_material(~us) > BishopValueMg))
           {
               // Move count based pruning (~30 Elo)
               if (moveCountPruning)
