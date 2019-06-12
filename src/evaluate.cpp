0801c3950944a9d4eff58af1b97916e237d04485
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-13 00:57:14 +0300

    tveack. bench 3605959

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -468,7 +468,7 @@ namespace {
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
-                 -   8 * (spaceWeight[Us] - spaceWeight[Them])
+                 -   2 * (spaceWeight[Us] - spaceWeight[Them])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
                  -   7;
 
