b7127df7f87c50bc378aa8f069257e8232080d3f
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-12 12:50:43 +0300

    tveack. bench 3718263

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -323,7 +323,8 @@ namespace {
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
 
                 score -= BishopPawns * pos.pawns_on_same_color_squares(Us, s)
-                                     * (1 + popcount(blocked & CenterFiles));
+                                     * (1 + popcount(blocked & CenterFiles))
+                         * (2 + (pos.count<ALL_PIECES>(Us) - pos.count<PAWN>(Us) < 4)) / 2;
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
