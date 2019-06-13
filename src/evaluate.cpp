a4dc344f4de509dc50d42c465c35a83abe1e831f
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-13 14:54:26 +0300

    tveack. bench 3368401

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -386,6 +386,7 @@ namespace {
     constexpr Color    Them = (Us == WHITE ? BLACK : WHITE);
     constexpr Bitboard Camp = (Us == WHITE ? AllSquares ^ Rank6BB ^ Rank7BB ^ Rank8BB
                                            : AllSquares ^ Rank1BB ^ Rank2BB ^ Rank3BB);
+    constexpr Direction Up = (Us == WHITE ? NORTH : SOUTH);
 
     Bitboard weak, b1, b2, safe, unsafeChecks = 0;
     Bitboard rookChecks, queenChecks, bishopChecks, knightChecks;
@@ -456,12 +457,14 @@ namespace {
     b2 = b1 & attackedBy2[Them];
 
     int kingFlankAttacks = popcount(b1) + popcount(b2);
+ 
+    Bitboard kingUp = attackedBy[Us][KING] | shift<Up>(attackedBy[Us][KING]);
 
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  +  69 * kingAttacksCount[Them]
                  + 185 * popcount(kingRing[Us] & weak)
-                 - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
-                 -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
+                 -  80 * bool(attackedBy[Us][KNIGHT] & kingUp)
+                 -  30 * bool(attackedBy[Us][BISHOP] & kingUp)
                  + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
