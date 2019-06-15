bdca62a8cdf5752364ef74a429bfd688bcb5ad7b
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-15 22:40:54 +0300

    tveack. bench 3676509

--- src/search.cpp
+++ src/search.cpp
@@ -945,7 +945,7 @@ moves_loop: // When in check, search starts from here
       newDepth = depth - ONE_PLY + extension;
 
       // Step 14. Pruning at shallow depth (~170 Elo)
-      if (  !(rootNode || (PvNode && depth < 2 * ONE_PLY))
+      if (  !(rootNode || (PvNode && (depth < 2 * ONE_PLY) && (ss->ply < 4)))
           && pos.non_pawn_material(us)
           && bestValue > VALUE_MATED_IN_MAX_PLY)
       {
