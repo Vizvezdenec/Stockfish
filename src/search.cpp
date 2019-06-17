46b58b0d386c71efd3259d72c34047c5b81fe82e
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-17 09:49:28 +0300

    tveack. bench 3472539

--- src/search.cpp
+++ src/search.cpp
@@ -917,7 +917,7 @@ moves_loop: // When in check, search starts from here
           // the hard beta bound.
           else if (   eval >= beta
                    && singularBeta >= beta)
-              return (singularBeta + beta) / 2;
+              return (singularBeta + eval) / 2;
       }
 
       // Check extension (~2 Elo)
