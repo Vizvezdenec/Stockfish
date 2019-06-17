00b4fcc26fc7fd0650165d1a09febb43d98c47e2
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-17 19:20:36 +0300

    tveack. bench 3742522

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -457,15 +457,11 @@ namespace {
 
     int kingFlankAttacks = popcount(b1) + popcount(b2);
 
-    b1 = attackedBy[Us][KNIGHT] & attackedBy[Us][KING];
-    b2 = attackedBy[Us][BISHOP] & attackedBy[Us][KING];
-
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  +  69 * kingAttacksCount[Them]
                  + 185 * popcount(kingRing[Us] & weak)
-                 - 100 * bool(b1)
-                 -  35 * bool(b2)
-                 -  16 * bool(b1 & b2)
+                 - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
+                 -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
                  + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
