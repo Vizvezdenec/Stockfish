76b6038c3313525c2c7cd1a25acaced08598b4c5
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-17 08:15:54 +0300

    tveack. bench 3909509

--- src/search.cpp
+++ src/search.cpp
@@ -895,7 +895,7 @@ moves_loop: // When in check, search starts from here
           &&  tte->depth() >= depth - 3 * ONE_PLY
           &&  pos.legal(move))
       {
-          Value singularBeta = ttValue - 7 * depth / 4 / ONE_PLY;
+          Value singularBeta = ttValue - 2 * depth / ONE_PLY;
           Depth halfDepth = depth / (2 * ONE_PLY) * ONE_PLY; // ONE_PLY invariant
           ss->excludedMove = move;
           value = search<NonPV>(pos, ss, singularBeta - 1, singularBeta, halfDepth, cutNode);
@@ -917,7 +917,7 @@ moves_loop: // When in check, search starts from here
           // the hard beta bound.
           else if (   eval >= beta
                    && singularBeta >= beta)
-              return singularBeta;
+              return (singularBeta + beta) / 2;
       }
 
       // Check extension (~2 Elo)
