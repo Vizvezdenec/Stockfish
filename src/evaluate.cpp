7f3238be85f2507fa52aeff92717010929c0dbd5
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-21 11:01:01 +0300

    tviick. bench 3338310

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -460,15 +460,14 @@ namespace {
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  +  69 * kingAttacksCount[Them]
                  + 185 * popcount(kingRing[Us] & weak)
-                 -  83 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
+                 - 120 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
-                 -  20 * bool(attackedBy[Us][KNIGHT] & KingFlank[file_of(ksq)])
                  + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
-                 -   4;
+                 -   3;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
