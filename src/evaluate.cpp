71d065b5de3b98654b70b4c19cd7b0312f2aaaf0
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-27 08:23:18 +0300

    tviigg. bench 3555371

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -769,7 +769,8 @@ namespace {
             && pos.non_pawn_material() == 2 * BishopValueMg)
             sf = 16 + 4 * pe->passed_count();
         else
-            sf = std::min(40 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide), sf);
+            sf = std::min(40 + (pos.opposite_bishops() ? 1 : 6) * pos.count<PAWN>(strongSide)
+                          + 2 * pe->passed_count_color(strongSide), sf);
 
     }
 
