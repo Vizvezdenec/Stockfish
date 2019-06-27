f7c73023e42f7e967ef8d1b251095a0590712fed
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-27 08:04:12 +0300

    tviigg. bench 3635443

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -457,9 +457,6 @@ namespace {
 
     int kingFlankAttacks = popcount(b1) + popcount(b2);
 
-    if (!(attackedBy[Them][ALL_PIECES] & kingRing[Us] & ~attackedBy[Us][PAWN]))
-    	kingDanger -= 40;
-
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  +  69 * kingAttacksCount[Them]
                  + 185 * popcount(kingRing[Us] & weak)
@@ -734,13 +731,16 @@ namespace {
     bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
                             && (pos.pieces(PAWN) & KingSide);
 
+    bool noMinors = !(pos.count<KNIGHT>() + pos.count<BISHOP>());
+
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    -103 ;
+                    -  9 * noMinors
+                    -101 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
