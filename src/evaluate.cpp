21e19540e294e9d61d951b58f937c04632e89561
Vizvezdenec <Vizvezdenec@gmail.com>
2019-06-30 02:10:27 +0300

    tviigg. bench 3351675

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -463,13 +463,13 @@ namespace {
                  + 185 * popcount(kingRing[Us] & weak)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
-                 +  27 * !(attackedBy[Us][KING] & attackedBy2[Us])
+                 +  40 * !(attackedBy[Us][KING] & attackedBy2[Us])
                  + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
-                 -   7;
+                 -  10;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
