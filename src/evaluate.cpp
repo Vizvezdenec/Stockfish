fdb5168fe675a05c9b10897e0662eb256bc5f90c
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-01 13:43:29 +0300

    tviigg. bench 3538107

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -734,12 +734,15 @@ namespace {
     bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
                             && (pos.pieces(PAWN) & KingSide);
 
+    Color strongSide = eg > VALUE_DRAW ? WHITE : BLACK;
+
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
+                    - 18 * !(pe->not_opposed_pawns(strongSide))
                     -103 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
