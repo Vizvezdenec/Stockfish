43dd02dba8709d39c2aa79a9cd14d17e714aef6d
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-03 06:16:46 +0300

    tviigg. bench 3660096

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -133,8 +133,8 @@ namespace {
   };
 
   constexpr Score ThreatByRank[RANK_NB]      = {
-    S( 0,  0), S(  8,  0), S( 20,  0), S( 36,  0),
-    S( 56,  0), S( 80,  0), S( 108,  0), S( 150,  0)
+    S( 0,  0), S( 10,  0), S( 23,  0), S( 36,  0),
+    S( 54,  0), S( 72,  0), S( 90,  0), S( 108,  0)
   };
 
   // Assorted bonuses and penalties
