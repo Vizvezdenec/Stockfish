6d55564efc603cef163e1205744d6d59a1a1e29c
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-05 01:55:08 +0300

    tviigg. bench 3699124

--- src/search.cpp
+++ src/search.cpp
@@ -1018,7 +1018,8 @@ moves_loop: // When in check, search starts from here
                   continue;
 
               // Reduced depth of the next LMR search
-              int lmrDepth = std::min(8, std::max(newDepth - reduction(improving, depth, moveCount), DEPTH_ZERO) / ONE_PLY);
+              int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount), DEPTH_ZERO);
+              lmrDepth /= ONE_PLY;
 
               // Countermoves based pruning (~20 Elo)
               if (   lmrDepth < 3 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
