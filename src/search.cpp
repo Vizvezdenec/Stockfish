fd3fde3f6e077756cb592a4657071ec25bd020ae
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-04 15:00:09 +0300

    tviigg. bench 3797256

--- src/search.cpp
+++ src/search.cpp
@@ -1067,7 +1067,7 @@ moves_loop: // When in check, search starts from here
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha))
       {
-          Depth r = reduction(improving, depth, moveCount - 3 * rootNode);
+          Depth r = reduction(improving, depth, moveCount - rootNode);
 
           // Reduction if other threads are searching this position.
 	  if (th.marked())
