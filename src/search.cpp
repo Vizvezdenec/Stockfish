929fd93e30593ef5fa937395d46cca4960ed92fc
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-05 23:40:24 +0300

    tviigg. bench 3397414

--- src/search.cpp
+++ src/search.cpp
@@ -64,7 +64,7 @@ namespace {
   // Razor and futility margins
   constexpr int RazorMargin = 600;
   Value futility_margin(Depth d, bool improving) {
-    return Value((175 - 50 * improving) * d / ONE_PLY);
+    return Value((150 - 50 * improving) * d / ONE_PLY);
   }
 
   // Reductions lookup table, initialized at startup
