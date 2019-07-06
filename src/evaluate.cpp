f889189d6ac8642241ec98d504a2305ca9e6f9d9
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-06 20:14:46 +0300

    tviigg. bench 3452187

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -712,8 +712,8 @@ namespace {
     int weight = pos.count<ALL_PIECES>(Us) - 1;
     Score score = make_score(bonus * weight * weight / 16, 0);
 
-    score -= AttacksOnSpaceArea * (popcount(attackedBy[Them][ALL_PIECES] & behind & safe) 
-                + popcount((attackedBy[Them][QUEEN]) & behind & safe));
+    score -= AttacksOnSpaceArea * popcount(attackedBy[Them][ALL_PIECES] & behind & safe); 
+    score -= make_score(12, 0) * popcount((attackedBy[Them][QUEEN]) & behind & safe);
 
     if (T)
         Trace::add(SPACE, Us, score);
