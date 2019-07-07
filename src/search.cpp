77df98fead0afccab72f7d5b16c3763473de59be
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-08 02:55:32 +0300

    tviigg. bench 3596270

--- src/search.cpp
+++ src/search.cpp
@@ -1022,7 +1022,7 @@ moves_loop: // When in check, search starts from here
               lmrDepth /= ONE_PLY;
 
               // Countermoves based pruning (~20 Elo)
-              if (   (th.marked() || (lmrDepth < 3 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)))
+              if (   lmrDepth < 3 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1) + 2 * th.marked()
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
