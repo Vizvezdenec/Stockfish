3fc7aac7bc59893c0b2bf390f0e0d3b778fd668a
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-09 14:32:11 +0300

    tviigg. bench 3624856

--- src/search.cpp
+++ src/search.cpp
@@ -1026,7 +1026,7 @@ moves_loop: // When in check, search starts from here
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
                   && (((*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   || ((*contHist[1])[movedPiece][to_sq(move)] <= CounterMovePruneThreshold
-                  && (*contHist[3])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)))
+                  && (*contHist[3])[movedPiece][to_sq(move)] < CounterMovePruneThreshold - 300)))
                   continue;
 
               // Futility pruning: parent node (~2 Elo)
