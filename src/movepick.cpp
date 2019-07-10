bdc911f517fc1586fcff034fd50592f354161d14
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-10 17:13:01 +0300

    tviigg. bench 3774566

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -111,11 +111,15 @@ void MovePicker::score() {
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if (Type == QUIETS)
+          {
           m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
                    + (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)] / 2;
+          if (type_of(pos.moved_piece(m)) == KNIGHT)
+              m.value = m.value * 12 / 10;
+          }
 
       else // Type == EVASIONS
       {
