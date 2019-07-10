1e0ab5228b975891808bf4c6f306e7902c56771c
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-10 03:36:50 +0300

    tviigg. bench 4006662

--- src/search.cpp
+++ src/search.cpp
@@ -1034,7 +1034,7 @@ moves_loop: // When in check, search starts from here
                   continue;
 
               // Prune moves with negative SEE (~10 Elo)
-              if (lmrDepth < 7 && !pos.see_ge(move, Value(-29 * lmrDepth * lmrDepth)))
+              if (!pos.see_ge(move, Value(-29 * lmrDepth * lmrDepth - lmrDepth * lmrDepth * lmrDepth)))
                   continue;
           }
           else if ((!givesCheck || !extension)
