6c8177c420effccdb09e025e71a4a4d5b787108b
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-15 05:56:39 +0300

    tviigg. bench 3498095

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -654,7 +654,7 @@ namespace {
 
                 // Assign a larger bonus if the block square is defended
                 if ((pos.pieces(Us) & bb) || (attackedBy[Us][ALL_PIECES] & blockSq))
-                    k += 5;
+                    k += 5 * !(squaresToQueen & ~unsafeSquares);
 
                 bonus += make_score(k * w, k * w);
             }
