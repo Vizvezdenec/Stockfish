ae8f8b88895d779cc167d7ea8613886759572b05
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-15 06:37:04 +0300

    tviigg. bench 3854607

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -647,8 +647,8 @@ namespace {
                 // If there are no enemy attacks on passed pawn span, assign a big bonus.
                 // Otherwise assign a smaller bonus if the path to queen is not attacked
                 // and even smaller bonus if it is attacked but block square is not.
-                int k = (!unsafeSquares && (pos.count<ALL_PIECES>(Them) - pos.count<KNIGHT>(Them) - pos.count<PAWN>(Them) == 1)) ? 55 :
-                        !unsafeSquares                    ? 35 :!unsafeSquares                    ? 35 :
+                int k = (!unsafeSquares && (pos.count<ALL_PIECES>(Them) - pos.count<KNIGHT>(Them) - pos.count<PAWN>(Them) == 1)) ? 40 :
+                        !unsafeSquares                    ? 30 :
                         !(unsafeSquares & squaresToQueen) ? 20 :
                         !(unsafeSquares & blockSq)        ?  9 :
                                                              0 ;
