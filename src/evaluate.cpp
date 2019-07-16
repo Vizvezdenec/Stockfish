e36076e7460f5b40f75f3a3640f2fb9139ae8dbf
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-16 16:32:39 +0300

    tvicc. bench 3276801

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -133,6 +133,7 @@ namespace {
   };
 
   // Assorted bonuses and penalties
+  constexpr Score AttacksOnSpaceArea = S(  4,  0);
   constexpr Score BishopPawns        = S(  3,  7);
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score FlankAttacks       = S(  8,  0);
@@ -704,9 +705,11 @@ namespace {
     behind |= shift<Down>(behind);
     behind |= shift<Down+Down>(behind);
 
-    int bonus = popcount(safe) + popcount(behind & safe & ~attackedBy[Them][ALL_PIECES]);
-    int weight = pos.count<ALL_PIECES>(Us) - pos.count<PAWN>(Us);
-    Score score = make_score(bonus * weight * weight / 4, 0);
+    int bonus = popcount(safe) + popcount(behind & safe);
+    int weight = pos.count<ALL_PIECES>(Us) - 1;
+    Score score = make_score(bonus * weight * weight / 16, 0);
+
+    score -= AttacksOnSpaceArea * popcount(attackedBy[Them][ALL_PIECES] & behind & safe);
 
     if (T)
         Trace::add(SPACE, Us, score);
