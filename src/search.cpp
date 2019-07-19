109947b5b469296529f1b2270aed128d718a4073
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-19 05:36:29 +0300

    tvicc. bench 3198104

--- src/search.cpp
+++ src/search.cpp
@@ -1018,8 +1018,7 @@ moves_loop: // When in check, search starts from here
 
           if (   !captureOrPromotion
               && !givesCheck
-              && (!pos.advanced_pawn_push(move) || pos.non_pawn_material(~us) > BishopValueMg)
-              && move != ss->killers[0])
+              && (!pos.advanced_pawn_push(move) || pos.non_pawn_material(~us) > BishopValueMg))
           {
               // Move count based pruning
               if (moveCountPruning)
@@ -1032,7 +1031,8 @@ moves_loop: // When in check, search starts from here
               // Countermoves based pruning (~20 Elo)
               if (   lmrDepth < 3 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
-                  && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
+                  && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
+                  && !(move == ss->killers[0]))
                   continue;
 
               // Futility pruning: parent node (~2 Elo)
