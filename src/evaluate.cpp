047bb960a070b35442598664c1a6a163e4bac889
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-19 17:20:46 +0300

    tvicc. bench 3494243

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -470,7 +470,7 @@ namespace {
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
                  -   7;
 
-    int KDbonus = std::max(kingDanger - 1024, 0) / 64;
+    int KDbonus = std::max(kingDanger - 1024, 0) / 128;
     kingDanger += KDbonus * KDbonus;
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
