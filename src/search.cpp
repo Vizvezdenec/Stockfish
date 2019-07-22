067a21b570b2dc872a33c282a09e18e2e29acec4
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-22 21:06:29 +0300

    tvicc. bench 3509542

--- src/search.cpp
+++ src/search.cpp
@@ -410,7 +410,7 @@ void Thread::search() {
               beta  = std::min(previousScore + delta, VALUE_INFINITE);
 
               // Adjust contempt based on root move's previousScore (dynamic contempt)
-              int dct = ct + 176 * previousScore / (abs(previousScore) + 200);
+              int dct = ct + 176 * previousScore / (abs(previousScore) + 400);
 
               contempt = (us == WHITE ?  make_score(dct, dct / 2)
                                       : -make_score(dct, dct / 2));
