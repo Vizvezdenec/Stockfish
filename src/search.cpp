e211427be6ec5e13c1999a7648eb40ff57ad6199
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-26 01:01:29 +0300

    tweecg. bench 3265411

--- src/search.cpp
+++ src/search.cpp
@@ -1028,11 +1028,11 @@ moves_loop: // When in check, search starts from here
               int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount), DEPTH_ZERO);
               lmrDepth /= ONE_PLY;
 
+              int lmrMargin = 3 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1) - lmrDepth;
               // Countermoves based pruning (~20 Elo)
-              if (   lmrDepth < 5 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
-                  && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
+              if ((*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
-                  && (*contHist[3])[movedPiece][to_sq(move)] < 20000 - lmrDepth * lmrDepth * lmrDepth * 200)
+                  && thisThread->mainHistory[us][from_to(move)] < lmrMargin * 16000 + 8000)
                   continue;
 
               // Futility pruning: parent node (~2 Elo)
