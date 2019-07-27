feda92d1fa2ba0e99e8150d6a2bcb46592e1dbc5
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-27 14:49:10 +0300

    tweecg. bench 3379723

--- src/search.cpp
+++ src/search.cpp
@@ -1137,9 +1137,6 @@ moves_loop: // When in check, search starts from here
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
               r -= ss->statScore / 16384 * ONE_PLY;
-
-	      if (ss->staticEval + 512 + 400 * depth < alpha)
-              	  r += ONE_PLY;
           }
 
           Depth d = clamp(newDepth - r, ONE_PLY, newDepth);
