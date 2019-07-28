7a2f6f7067bd9935c95a1269eff62fe5f1b27a84
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-28 07:22:43 +0300

    tweecg. bench 3401930

--- src/search.cpp
+++ src/search.cpp
@@ -1164,7 +1164,7 @@ moves_loop: // When in check, search starts from here
               if (!captureOrPromotion)
               	  update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
               else
-              	  update_capture_stats(pos, bestMove, capturesSearched, captureCount, bonus / 4);
+              	  update_capture_stats(pos, bestMove, capturesSearched, captureCount, bonus / 2);
           }
       }
 
