cc881955c0c44df69d7336b5cba4022df70e239b
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-28 23:42:19 +0300

    tweecg. bench 3587006

--- src/search.cpp
+++ src/search.cpp
@@ -973,7 +973,7 @@ moves_loop: // When in check, search starts from here
 
           if (value < singularBeta)
           {
-              int bonus = stat_bonus(depth + ONE_PLY);
+              int bonus = stat_bonus(depth + ONE_PLY) / 4;
               extension = ONE_PLY;
               singularLMR++;
 
