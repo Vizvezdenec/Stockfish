4ae0c9815dcd7c2c2274f8136b155f613c83bc57
Vizvezdenec <Vizvezdenec@gmail.com>
2019-07-29 03:23:24 +0300

    tweecg. bench 3638023

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -564,8 +564,6 @@ namespace {
     b = pawn_attacks_bb<Us>(b) & nonPawnEnemies;
     score += ThreatBySafePawn * popcount(b);
 
-    score -= make_score(7, 0) * popcount(stronglyProtected & Center);
-
     // Bonus for threats on the next moves against enemy queen
     if (pos.count<QUEEN>(Them) == 1)
     {
