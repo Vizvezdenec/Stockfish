85f130f6cf81435d25bfc6950241e8b8d0f9ca76
Vizvezdenec <Vizvezdenec@gmail.com>
2019-08-03 04:23:31 +0300

    tweecg. bench 4128619

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -272,9 +272,8 @@ namespace {
     for (Square s = *pl; s != SQ_NONE; s = *++pl)
     {
         // Find attacked squares, including x-ray attacks for bishops and rooks
-        b = Pt == BISHOP ? attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(QUEEN))
-          : Pt ==   ROOK ? attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(QUEEN) ^ pos.pieces(Us, ROOK))
-          : Pt ==  QUEEN ? attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(Us, QUEEN) ^ pos.pieces(Us, ROOK)) | pos.attacks_from<BISHOP>(s)
+        b = Pt == BISHOP ? attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(QUEEN) ^ pos.pieces(Them, KING))
+          : Pt ==   ROOK ? attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(QUEEN) ^ pos.pieces(Us, ROOK) ^ pos.pieces(Them, KING))
                          : pos.attacks_from<Pt>(s);
 
         if (pos.blockers_for_king(Us) & s)
