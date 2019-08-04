97e6967b23b23fdb338ee0cd6612e5a8f10f8d04
Vizvezdenec <Vizvezdenec@gmail.com>
2019-08-04 04:04:00 +0300

    tweecg. bench 4158790

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -621,8 +621,8 @@ namespace {
             Square blockSq = s + Up;
 
             // Adjust bonus based on the king's proximity
-            bonus += make_score(0, std::max((  king_proximity(Them, blockSq) * 5
-                                    - king_proximity(Us,   blockSq) * 2) * w , 0));
+            bonus += make_score(0, (  king_proximity(Them, blockSq) * 5
+                                    - king_proximity(Us,   blockSq) * 2) * w);
 
             // If blockSq is not the queening square then consider also a second push
             if (r != RANK_7)
