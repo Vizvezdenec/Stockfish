9ed24cc07a1806c5108b7779f348cf35818a9dba
Vizvezdenec <Vizvezdenec@gmail.com>
2019-08-07 11:23:00 +0300

    tweecg. bench 4192724

--- src/search.cpp
+++ src/search.cpp
@@ -796,9 +796,9 @@ namespace {
     // Step 9. Null move search with verification search (~40 Elo)
     if (   !PvNode
         && (ss-1)->currentMove != MOVE_NULL
-        && (ss-1)->statScore < 22661
+        && (ss-1)->statScore < 10000 + 15000 * improving
         &&  eval >= beta
-        &&  ss->staticEval >= beta - 33 * depth / ONE_PLY + 330 - improving * 75
+        &&  ss->staticEval >= beta - 33 * depth / ONE_PLY + 299
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))
