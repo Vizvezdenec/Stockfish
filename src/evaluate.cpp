7b7530b58a82064bd9984b4577c9b4ff201a8a46
Vizvezdenec <Vizvezdenec@gmail.com>
2019-08-10 03:12:11 +0300

    tweecg. bench 3963727

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -81,10 +81,10 @@ namespace {
   constexpr int KingAttackWeights[PIECE_TYPE_NB] = { 0, 0, 77, 55, 44, 10 };
 
   // Penalties for enemy's safe checks
-  constexpr int QueenSafeCheck  = 760;
-  constexpr int RookSafeCheck   = 1060;
-  constexpr int BishopSafeCheck = 615;
-  constexpr int KnightSafeCheck = 770;
+  constexpr int QueenSafeCheck  = 780;
+  constexpr int RookSafeCheck   = 1080;
+  constexpr int BishopSafeCheck = 635;
+  constexpr int KnightSafeCheck = 790;
 
 #define S(mg, eg) make_score(mg, eg)
 
@@ -402,13 +402,11 @@ namespace {
     b1 = attacks_bb<ROOK  >(ksq, pos.pieces() ^ pos.pieces(Us, QUEEN));
     b2 = attacks_bb<BISHOP>(ksq, pos.pieces() ^ pos.pieces(Us, QUEEN));
 
-    int r = relative_rank(Us, ksq) * 10;
-
     // Enemy rooks checks
     rookChecks = b1 & safe & attackedBy[Them][ROOK];
 
     if (rookChecks)
-        kingDanger += RookSafeCheck + r;
+        kingDanger += RookSafeCheck;
     else
         unsafeChecks |= b1 & attackedBy[Them][ROOK];
 
@@ -421,7 +419,7 @@ namespace {
                  & ~rookChecks;
 
     if (queenChecks)
-        kingDanger += QueenSafeCheck + r;
+        kingDanger += QueenSafeCheck;
 
     // Enemy bishops checks: we count them only if they are from squares from
     // which we can't give a queen check, because queen checks are more valuable.
@@ -431,7 +429,7 @@ namespace {
                   & ~queenChecks;
 
     if (bishopChecks)
-        kingDanger += BishopSafeCheck + r;
+        kingDanger += BishopSafeCheck;
     else
         unsafeChecks |= b2 & attackedBy[Them][BISHOP];
 
@@ -439,7 +437,7 @@ namespace {
     knightChecks = pos.attacks_from<KNIGHT>(ksq) & attackedBy[Them][KNIGHT];
 
     if (knightChecks & safe)
-        kingDanger += KnightSafeCheck + r;
+        kingDanger += KnightSafeCheck;
     else
         unsafeChecks |= knightChecks;
 
--- src/search.cpp
+++ src/search.cpp
@@ -1019,7 +1019,7 @@ moves_loop: // When in check, search starts from here
 
           if (   !captureOrPromotion
               && !givesCheck
-              && (!pos.advanced_pawn_push(move) || pos.non_pawn_material(~us) > BishopValueMg))
+              && (!pos.advanced_pawn_push(move) || (pos.non_pawn_material(~us) > BishopValueMg && !extension)))
           {
               // Move count based pruning
               if (moveCountPruning)
