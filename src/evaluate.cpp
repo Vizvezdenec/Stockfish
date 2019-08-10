c7db7a5cbf9ddabcb08d127b5ff056dc77d43d88
Vizvezdenec <Vizvezdenec@gmail.com>
2019-08-10 04:35:12 +0300

    tweecg. bench 3927616

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -381,6 +381,7 @@ namespace {
     constexpr Color    Them = (Us == WHITE ? BLACK : WHITE);
     constexpr Bitboard Camp = (Us == WHITE ? AllSquares ^ Rank6BB ^ Rank7BB ^ Rank8BB
                                            : AllSquares ^ Rank1BB ^ Rank2BB ^ Rank3BB);
+    constexpr int weakRing[9] = {0, 150, 330, 540, 780, 1050, 1350, 1680, 2040};
 
     Bitboard weak, b1, b2, safe, unsafeChecks = 0;
     Bitboard rookChecks, queenChecks, bishopChecks, knightChecks;
@@ -452,10 +453,10 @@ namespace {
 
     int kingFlankAttacks = popcount(b1) + popcount(b2);
 
+    kingDanger +=  weakRing[popcount(kingRing[Us] & weak)];
+
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  +  69 * kingAttacksCount[Them]
-                 + 185 * popcount(kingRing[Us] & weak)
-                 + 300 * !(kingRing[Us] & ~weak)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
                  + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
