f5fb420ed54b01d35f7b3d6f9dfdd204b7ae1c3b
Vizvezdenec <Vizvezdenec@gmail.com>
2019-08-11 20:31:49 +0300

    tweecg. bench 4266090

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -164,7 +164,7 @@ namespace {
     template<Color Us> void initialize();
     template<Color Us, PieceType Pt> Score pieces();
     template<Color Us> Score king() const;
-    template<Color Us> Score threats() const;
+    template<Color Us> Score threats();
     template<Color Us> Score passed() const;
     template<Color Us> Score space() const;
     ScaleFactor scale_factor(Value eg) const;
@@ -205,6 +205,7 @@ namespace {
     // a white knight on g5 and black's king is on g8, this white knight adds 2
     // to kingAttacksCount[WHITE].
     int kingAttacksCount[COLOR_NB];
+    int restrictedCount[COLOR_NB];
   };
 
 
@@ -248,6 +249,7 @@ namespace {
 
     kingAttackersCount[Them] = popcount(kingRing[Us] & pe->pawn_attacks(Them));
     kingAttacksCount[Them] = kingAttackersWeight[Them] = 0;
+    restrictedCount[Us] = 0;
 
     // Remove from kingRing[] the squares defended by two pawns
     kingRing[Us] &= ~dblAttackByPawn;
@@ -461,6 +463,7 @@ namespace {
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
+                 +       (restrictedCount[Us] - restrictedCount[Them]) * abs(restrictedCount[Us] - restrictedCount[Them]) / 4
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
                  -   7;
 
@@ -485,7 +488,7 @@ namespace {
   // Evaluation::threats() assigns bonuses according to the types of the
   // attacking and the attacked pieces.
   template<Tracing T> template<Color Us>
-  Score Evaluation<T>::threats() const {
+  Score Evaluation<T>::threats() {
 
     constexpr Color     Them     = (Us == WHITE ? BLACK   : WHITE);
     constexpr Direction Up       = (Us == WHITE ? NORTH   : SOUTH);
@@ -545,7 +548,8 @@ namespace {
        & ~stronglyProtected
        &  attackedBy[Us][ALL_PIECES];
 
-    score += RestrictedPiece * popcount(b);
+    restrictedCount[Us] = popcount(b);
+    score += RestrictedPiece * restrictedCount[Us];
 
     // Find squares where our pawns can push on the next move
     b  = shift<Up>(pos.pieces(Us, PAWN)) & ~pos.pieces();
@@ -811,8 +815,9 @@ namespace {
 
     score += mobility[WHITE] - mobility[BLACK];
 
+    score += threats<WHITE>() - threats<BLACK>();
+
     score +=  king<   WHITE>() - king<   BLACK>()
-            + threats<WHITE>() - threats<BLACK>()
             + passed< WHITE>() - passed< BLACK>()
             + space<  WHITE>() - space<  BLACK>();
 
