060532bbb991cc5a6fda50085404452cd88d44b9
Vizvezdenec <Vizvezdenec@gmail.com>
2019-08-14 23:29:03 +0300

    tweecg. bench 4278660

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -421,7 +421,6 @@ namespace {
     if (queenChecks)
         kingDanger += QueenSafeCheck;
 
-    safe |= ~attackedBy2[Us] & attackedBy2[Them] & attackedBy[Us][ROOK];
     // Enemy bishops checks: we count them only if they are from squares from
     // which we can't give a queen check, because queen checks are more valuable.
     bishopChecks =  b2
@@ -455,7 +454,8 @@ namespace {
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
                  + 148 * popcount(unsafeChecks)
-                 +  98 * popcount(pos.blockers_for_king(Us))
+                 +  78 * popcount(pos.blockers_for_king(Us) & pos.pieces(Us))
+                 + 128 * popcount(pos.blockers_for_king(Us) & pos.pieces(Them))
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
