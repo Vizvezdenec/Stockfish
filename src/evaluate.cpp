9d46f326b132558506163b1634dbd442b454d6bf
Vizvezdenec <Vizvezdenec@gmail.com>
2019-08-14 23:18:08 +0300

    tweecg. bench 4219994

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -420,6 +420,9 @@ namespace {
 
     if (queenChecks)
         kingDanger += QueenSafeCheck;
+    else 
+        unsafeChecks |= (b2 | b1) & attackedBy[Them][QUEEN] & ~(attackedBy[Us][PAWN]
+                       | (attackedBy2[Us] & ~attackedBy2[Them]));
 
     // Enemy bishops checks: we count them only if they are from squares from
     // which we can't give a queen check, because queen checks are more valuable.
