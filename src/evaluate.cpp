b1a8129bec4789bb12e259199e3b9ab7d090a9d5
Vizvezdenec <Vizvezdenec@gmail.com>
2019-08-14 23:35:07 +0300

    tweecg. bench 4027545

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -455,6 +455,7 @@ namespace {
                  -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
                  + 148 * popcount(unsafeChecks)
                  +  98 * popcount(pos.blockers_for_king(Us))
+                 +  40 * popcount(pos.blockers_for_king(Us) & pos.pieces(Us) & weak)
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
