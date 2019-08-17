b01af4ac8f0cedca93d3177f1c826dad70de5948
Vizvezdenec <Vizvezdenec@gmail.com>
2019-08-17 19:31:19 +0300

    tweegk. bench 4399040

--- src/search.cpp
+++ src/search.cpp
@@ -1074,7 +1074,7 @@ moves_loop: // When in check, search starts from here
           &&  moveCount > 1 + 3 * rootNode
           && (  !captureOrPromotion
               || moveCountPruning
-              || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha))
+              || (ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha && move != ss->killers[0])))
       {
           Depth r = reduction(improving, depth, moveCount);
 
