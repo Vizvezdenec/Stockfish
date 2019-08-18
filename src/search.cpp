1dba7b625525121f4d7cc9f2482e4d80df82c9d3
Vizvezdenec <Vizvezdenec@gmail.com>
2019-08-18 03:59:32 +0300

    tweegk. bench 4110647

--- src/search.cpp
+++ src/search.cpp
@@ -850,7 +850,7 @@ namespace {
         &&  depth >= 5 * ONE_PLY
         &&  abs(beta) < VALUE_MATE_IN_MAX_PLY)
     {
-        Value raisedBeta = std::min(beta + 141 - 46 * improving + 6 * depth / ONE_PLY, VALUE_INFINITE);
+        Value raisedBeta = std::min(beta + 91 - 46 * improving + 12 * depth / ONE_PLY, VALUE_INFINITE);
         MovePicker mp(pos, ttMove, raisedBeta - ss->staticEval, &thisThread->captureHistory);
         int probCutCount = 0;
 
