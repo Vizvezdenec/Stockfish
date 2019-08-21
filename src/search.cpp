2edd31ddd1b0d5451c134ea9b6994efe0e3034c1
Vizvezdenec <Vizvezdenec@gmail.com>
2019-08-21 16:42:15 +0300

    twiikg. bench 3971679

--- src/search.cpp
+++ src/search.cpp
@@ -1094,15 +1094,15 @@ moves_loop: // When in check, search starts from here
           // Decrease reduction if move has been singularly extended
           r -= singularLMR * ONE_PLY;
 
+          // Increase reduction for cut nodes (~5 Elo)
+          if (cutNode)
+              r += 2 * ONE_PLY;
+
           if (!captureOrPromotion)
           {
               // Increase reduction if ttMove is a capture (~0 Elo)
               if (ttCapture)
                   r += ONE_PLY;
-
-              // Increase reduction for cut nodes (~5 Elo)
-              if (cutNode)
-                  r += 2 * ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
