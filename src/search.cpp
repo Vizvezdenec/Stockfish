ebdf6687ee49c5099c2110b45d144edef2535dac
Vizvezdenec <Vizvezdenec@gmail.com>
2019-08-22 05:26:57 +0300

    twiikg. bench 4000907

--- src/search.cpp
+++ src/search.cpp
@@ -1075,7 +1075,8 @@ moves_loop: // When in check, search starts from here
           && (  !captureOrPromotion
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
-              || cutNode))
+              || cutNode
+              || thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] <= 0))
       {
           Depth r = reduction(improving, depth, moveCount);
 
@@ -1094,9 +1095,6 @@ moves_loop: // When in check, search starts from here
           // Decrease reduction if move has been singularly extended
           r -= singularLMR * ONE_PLY;
 
-          if (cutNode)
-               r += ONE_PLY;
-
           if (!captureOrPromotion)
           {
               // Increase reduction if ttMove is a capture (~0 Elo)
@@ -1105,7 +1103,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
-                  r += ONE_PLY;
+                  r += 2 * ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
