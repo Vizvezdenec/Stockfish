485f27be13a010ca6e40e587118f9b0214adb797
Vizvezdenec <Vizvezdenec@gmail.com>
2019-08-27 21:26:17 +0300

    twiikg. bench 3748056

--- src/search.cpp
+++ src/search.cpp
@@ -1051,7 +1051,7 @@ moves_loop: // When in check, search starts from here
                   continue;
           }
           else if (  (!givesCheck || !extension)
-                   && !pos.see_ge(move, Value(-199) * (depth / ONE_PLY))) // (~20 Elo)
+                   && !pos.see_ge(move, Value(-250) * (depth / ONE_PLY - 1))) // (~20 Elo)
                   continue;
       }
 
