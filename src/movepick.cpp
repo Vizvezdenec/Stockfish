4182fb0728621dcad1adc8ae017284744261e327
Vizvezdenec <Vizvezdenec@gmail.com>
2019-08-29 12:19:15 +0300

    twiikg. bench 3782118

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -107,8 +107,18 @@ void MovePicker::score() {
 
   for (auto& m : *this)
       if (Type == CAPTURES)
-          m.value =  int(PieceValue[MG][pos.piece_on(to_sq(m))]) * 6
+          {
+          if (pos.non_pawn_material() < MidgameLimit && pos.non_pawn_material() > EndgameLimit)
+          	m.value =  int(PieceValue[MG][pos.piece_on(to_sq(m))] * int(pos.non_pawn_material())
+                       + PieceValue[EG][pos.piece_on(to_sq(m))] * int(MidgameLimit - pos.non_pawn_material())) * 6 / int(MidgameLimit)
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
+          else if (pos.non_pawn_material() >= MidgameLimit)
+          	m.value =  int(PieceValue[MG][pos.piece_on(to_sq(m))]) * 6 
+                   + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
+          else  
+                m.value =  int(PieceValue[EG][pos.piece_on(to_sq(m))]) * 6 
+                   + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
+          }
 
       else if (Type == QUIETS)
           m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
