47af76b940b8ceb384ff3b96a1f26c46d83048a5
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-05 03:54:00 +0300

    twiikg. bench 3904128

--- src/search.cpp
+++ src/search.cpp
@@ -787,9 +787,9 @@ namespace {
         &&  eval <= alpha - RazorMargin)
         return qsearch<NT>(pos, ss, alpha, beta);
 
-    improving =   ss->staticEval >= (ss-2)->staticEval
+    improving =   ss->staticEval > (ss-2)->staticEval
                || (ss-2)->staticEval == VALUE_NONE
-               || ((ss-1)->staticEval != VALUE_NONE && ss->staticEval > -(ss-1)->staticEval + 2 * Eval::Tempo);
+               || (ss->staticEval == (ss-2)->staticEval && (ss-1)->staticEval != VALUE_NONE && ss->staticEval > -(ss-1)->staticEval + 2 * Eval::Tempo);
 
     // Step 8. Futility pruning: child node (~30 Elo)
     if (   !PvNode
