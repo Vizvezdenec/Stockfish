6cabcace871ecaba271fcd74a7bfa6b9b5b4f5f8
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-06 02:36:21 +0300

    twiikg. bench 3568234

--- src/search.cpp
+++ src/search.cpp
@@ -1049,7 +1049,7 @@ moves_loop: // When in check, search starts from here
                   continue;
 
               // Prune moves with negative SEE (~10 Elo)
-              if (!pos.see_ge(move, Value(-(31 + 5 * futilityPruning - std::min(lmrDepth, 18)) * lmrDepth * lmrDepth)))
+              if (!pos.see_ge(move, Value(-(31 - !futilityPruning * std::min(lmrDepth, 18)) * lmrDepth * lmrDepth)))
                   continue;
           }
           else if (  (!givesCheck || !extension)
