57bfdedb2c97e941d2d271612aa9483c7fe6dcd2
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-06 21:15:39 +0300

    twiikg. bench 4323587

--- src/search.cpp
+++ src/search.cpp
@@ -1093,8 +1093,8 @@ moves_loop: // When in check, search starts from here
               r -= 2 * ONE_PLY;
 
           // Decrease reduction if opponent's move count is high (~10 Elo)
-          if ((ss-1)->moveCount > 15)
-              r -= (1 + (moveCount > 15) ) * ONE_PLY;
+          if ((ss-1)->moveCount > 15 || (ss-1)->moveCount + moveCount > 29)
+              r -= ONE_PLY;
 
           // Decrease reduction if move has been singularly extended
           r -= singularLMR * ONE_PLY;
