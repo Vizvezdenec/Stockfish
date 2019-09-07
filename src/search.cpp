daa4727af382fde8916eee4581a8bc63dcdbd602
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-07 14:57:14 +0300

    twiikg. bench 3549537

--- src/search.cpp
+++ src/search.cpp
@@ -964,7 +964,7 @@ moves_loop: // When in check, search starts from here
           &&  tte->depth() >= depth - 3 * ONE_PLY
           &&  pos.legal(move))
       {
-          Value singularBeta = ttValue - (2 - ttCapture) * depth / ONE_PLY;
+          Value singularBeta = ttValue - (3 - 2 * ttCapture) * depth / ONE_PLY;
           Depth halfDepth = depth / (2 * ONE_PLY) * ONE_PLY; // ONE_PLY invariant
           ss->excludedMove = move;
           value = search<NonPV>(pos, ss, singularBeta - 1, singularBeta, halfDepth, cutNode);
