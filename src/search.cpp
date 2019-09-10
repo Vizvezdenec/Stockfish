d193a0af240f284f83720042030cd8b282c47d5f
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-10 10:16:17 +0300

    twiikg. bench 3777598

--- src/search.cpp
+++ src/search.cpp
@@ -787,8 +787,8 @@ namespace {
         &&  eval <= alpha - RazorMargin)
         return qsearch<NT>(pos, ss, alpha, beta);
 
-    improving =   ss->staticEval >= (ss-2)->staticEval
-               || (ss-2)->staticEval == VALUE_NONE;
+    improving =   (ss->staticEval >= (ss-2)->staticEval
+               || (ss-2)->staticEval == VALUE_NONE) || ss->staticEval > beta + RookValueMg;
 
     // Step 8. Futility pruning: child node (~30 Elo)
     if (   !PvNode
@@ -1037,7 +1037,7 @@ moves_loop: // When in check, search starts from here
               // Countermoves based pruning (~20 Elo)
               if (   lmrDepth < 4 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
-                  && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold + th.marked())
+                  && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
 
               // Futility pruning: parent node (~2 Elo)
