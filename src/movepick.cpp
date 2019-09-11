4f9b0e5a1f39c45955e08516fe2aef1cd1f524f7
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-11 19:58:18 +0300

    twiikg. bench 4068500

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -125,6 +125,7 @@ void MovePicker::score() {
           else
               m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
                        + (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
+                       + (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)] / 4
                        - (1 << 28);
       }
 }
