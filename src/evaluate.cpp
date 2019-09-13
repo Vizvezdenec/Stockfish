24cdb4aa4fceb29c2b04fcd29c117e0f801fde5b
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-13 07:02:11 +0300

    twwekk. bench 3546173

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -457,7 +457,7 @@ namespace {
 
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  +  69 * kingAttacksCount[Them]
-                 + 200 * popcount(kingRing[Us] & weak)
+                 + 190 * popcount(kingRing[Us] & weak)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
                  + 148 * popcount(unsafeChecks)
