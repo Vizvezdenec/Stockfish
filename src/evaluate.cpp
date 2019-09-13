9641d9ab5b2a3fc4fa01423dbfce2dff9c637d4d
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-14 01:58:35 +0300

    twwekk. bench 3671492

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -133,7 +133,7 @@ namespace {
   constexpr Score Hanging            = S( 69, 36);
   constexpr Score KingProtector      = S(  7,  8);
   constexpr Score KnightOnQueen      = S( 16, 12);
-  constexpr Score LongDiagonalBishop = S( 45,  0);
+  constexpr Score LongDiagonalBishop = S( 52,  0);
   constexpr Score MinorBehindPawn    = S( 18,  3);
   constexpr Score Outpost            = S( 18,  6);
   constexpr Score PassedFile         = S( 11,  8);
@@ -321,9 +321,10 @@ namespace {
                 score -= BishopPawns * pos.pawns_on_same_color_squares(Us, s)
                                      * (1 + popcount(blocked & CenterFiles));
 
-                bb = LineBB[SQ_A1][SQ_H8] | LineBB[SQ_A8][SQ_H1];
+                bb = attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center;
+
                 // Bonus for bishop on a long diagonal which can "see" both center squares
-                if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center) && more_than_one(b & bb & mobilityArea[Us]))
+                if (more_than_one(bb) && (bb & ~attackedBy[Them][PAWN]))
                     score += LongDiagonalBishop;
             }
 
