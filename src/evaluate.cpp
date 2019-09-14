4e4168ddfb094e76f019c6ec1d44b63d8488e615
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-14 05:50:06 +0300

    twwekk. bench 3601395

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -140,7 +140,7 @@ namespace {
   constexpr Score PawnlessFlank      = S( 17, 95);
   constexpr Score RestrictedPiece    = S(  7,  7);
   constexpr Score RookOnPawn         = S( 10, 32);
-  constexpr Score RookOnQueenFile    = S( 10,  4);
+  constexpr Score RookOnQueenFile    = S( 11,  4);
   constexpr Score SliderOnQueen      = S( 59, 18);
   constexpr Score ThreatByKing       = S( 24, 89);
   constexpr Score ThreatByPawnPush   = S( 48, 39);
@@ -319,7 +319,7 @@ namespace {
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
 
                 score -= BishopPawns * pos.pawns_on_same_color_squares(Us, s)
-                                     * (1 + popcount(blocked & CenterFiles));
+                                     * (1 + popcount(blocked & CenterFiles) + popcount(pos.pieces(Us, PAWN) & b));
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
@@ -348,7 +348,7 @@ namespace {
                 score += RookOnPawn * popcount(pos.pieces(Them, PAWN) & PseudoAttacks[ROOK][s]);
 
             // Bonus for rook on same file as their queen
-            if (file_bb(s) & (pos.pieces(Them, QUEEN) | pos.pieces(Them, KING)))
+            if (file_bb(s) & pos.pieces(Them, QUEEN))
                 score += RookOnQueenFile;
 
             // Bonus for rook on an open or semi-open file
