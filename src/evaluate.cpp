f6665a0bf3b23da2628ac81b737f03d76efca006
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-16 02:47:27 +0300

    twwykk. bench 3791367

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -741,14 +741,10 @@ namespace {
                     - 36 * almostUnwinnable
                     -103 ;
 
-    int maxEval = int(std::max(abs(mg), abs(eg)));
-
-    complexity = std::min(50 + maxEval, complexity);
-
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
     // so that the midgame and endgame scores do not change sign after the bonus.
-    int u = ((mg > 0) - (mg < 0)) * std::max(std::min(complexity + 50, 0), -abs(mg));
+    int u = ((mg > 0) - (mg < 0)) * std::max(2 * std::min(complexity + 50, 0), -abs(mg));
     int v = ((eg > 0) - (eg < 0)) * std::max(complexity, -abs(eg));
 
     if (T)
