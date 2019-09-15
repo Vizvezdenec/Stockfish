22164fb11dbd7cccf53bb5a787f1bdba87378ee6
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-16 02:56:32 +0300

    twwykk. bench 3643982

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -741,13 +741,11 @@ namespace {
                     - 36 * almostUnwinnable
                     -103 ;
 
-    complexity += -std::min(0, complexity + 100) * (100 + complexity) / 16;
-
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
     // so that the midgame and endgame scores do not change sign after the bonus.
     int u = ((mg > 0) - (mg < 0)) * std::max(std::min(complexity + 50, 0), -abs(mg));
-    int v = ((eg > 0) - (eg < 0)) * std::max(complexity, -abs(eg));
+    int v = ((eg > 0) - (eg < 0)) * std::max(complexity + std::min(0, complexity + 100), -abs(eg));
 
     if (T)
         Trace::add(INITIATIVE, make_score(u, v));
