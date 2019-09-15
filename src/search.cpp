29251aca5919853cbffe2c4f84dc7818e10b9433
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-15 20:45:31 +0300

    twwykk. bench 3813983

--- src/search.cpp
+++ src/search.cpp
@@ -1035,7 +1035,7 @@ moves_loop: // When in check, search starts from here
               lmrDepth /= ONE_PLY;
 
               // Countermoves based pruning (~20 Elo)
-              if (   lmrDepth < 4 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
+              if (   lmrDepth < 4 + (((ss-1)->statScore > 0 || (ss-1)->moveCount == 1) + (ss-1)->statScore / 32768)
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
@@ -1076,7 +1076,7 @@ moves_loop: // When in check, search starts from here
       // re-searched at full depth.
       if (    depth >= 3 * ONE_PLY
           &&  moveCount > 1 + 2 * rootNode
-          && (!PvNode || thisThread->best_move_count(move) <= 4 * ss->ply)
+          && (!rootNode || thisThread->best_move_count(move) == 0)
           && (  !captureOrPromotion
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
