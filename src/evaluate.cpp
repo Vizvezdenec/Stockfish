b46355a5486fc3c3f1b2b9a87564f7c6e18a3caa
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-16 01:21:55 +0300

    twwykk. bench 4219697

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -290,8 +290,6 @@ namespace {
             kingAttackersWeight[Us] += KingAttackWeights[Pt];
             kingAttacksCount[Us] += popcount(b & attackedBy[Them][KING]);
         }
-        else if (PseudoAttacks[Pt][s] & pos.pieces(Them, KING))
-           kingAttackersWeight[Us]++;
 
         int mob = popcount(b & mobilityArea[Us]);
 
@@ -746,7 +744,7 @@ namespace {
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
     // so that the midgame and endgame scores do not change sign after the bonus.
-    int u = ((mg > 0) - (mg < 0)) * std::max(std::min(complexity + 50, 0), -abs(mg));
+    int u = ((mg > 0) - (mg < 0)) * std::max(std::min(complexity + 50, 0), -abs(mg) + 1);
     int v = ((eg > 0) - (eg < 0)) * std::max(complexity, -abs(eg));
 
     if (T)
