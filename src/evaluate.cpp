bfb66a992f879365ac3fe69d30f111c8a3466948
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-16 21:00:27 +0300

    twwykk. bench 4039599

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -735,7 +735,7 @@ namespace {
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
-                    +  9 * outflanking
+                    +  5 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     - 36 * almostUnwinnable
@@ -745,7 +745,7 @@ namespace {
     // sign of the midgame or endgame values, and that we carefully cap the bonus
     // so that the midgame and endgame scores do not change sign after the bonus.
     int u = ((mg > 0) - (mg < 0)) * std::max(std::min(complexity + 50, 0), -abs(mg));
-    int v = ((eg > 0) - (eg < 0)) * std::max(complexity, -abs(eg));
+    int v = ((eg > 0) - (eg < 0)) * std::max(complexity + 6 * outflanking, -abs(eg));
 
     if (T)
         Trace::add(INITIATIVE, make_score(u, v));
