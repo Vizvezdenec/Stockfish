87aa08332e282dfdaf55584fea061d4b02d9bc5c
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-17 02:19:36 +0300

    twwykk. bench 3644827

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -638,12 +638,12 @@ namespace {
                 bb = forward_file_bb(Them, s) & pos.pieces(ROOK, QUEEN);
 
                 if (!(pos.pieces(Them) & bb))
-                    unsafeSquares &= attackedBy[Them][ALL_PIECES] & (~attackedBy[Us][PAWN] | PawnAttacks[Us][s]);
+                    unsafeSquares &= (attackedBy[Them][ALL_PIECES] & ~attackedBy[Them][PAWN]) | attackedBy2[Them];
 
                 // If there are no enemy attacks on passed pawn span, assign a big bonus.
                 // Otherwise assign a smaller bonus if the path to queen is not attacked
                 // and even smaller bonus if it is attacked but block square is not.
-                int k = !unsafeSquares                    ? 35 :
+                int k = !unsafeSquares                    ? 32 :
                         !(unsafeSquares & squaresToQueen) ? 20 :
                         !(unsafeSquares & blockSq)        ?  9 :
                                                              0 ;
