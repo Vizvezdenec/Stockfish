695d2803180a0fd4797500379b83f1eaa9baa8d2
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-21 04:59:02 +0300

    tweecg. bench 3649593

--- src/search.cpp
+++ src/search.cpp
@@ -1024,11 +1024,7 @@ moves_loop: // When in check, search starts from here
 
           if (   !captureOrPromotion
               && !givesCheck
-              && (!pos.advanced_pawn_push(move) || pos.non_pawn_material(~us) > BishopValueMg)
-              && !(type_of(movedPiece) == PAWN 
-                   && ((PawnAttacks[~us][to_sq(move)] & pos.pieces(us, PAWN))
-                       || !(PawnAttacks[us][to_sq(move)] & pos.pieces(~us, PAWN)))
-                   && (PawnAttacks[us][to_sq(move)] & pos.pieces(~us) & ~pos.pieces(~us, PAWN))))
+              && (!pos.advanced_pawn_push(move) || pos.non_pawn_material(~us) > BishopValueMg))
           {
               // Move count based pruning
               if (moveCountPruning)
