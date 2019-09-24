c033aebef226dac6b17843f9e8a96b5afab11c76
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-24 23:16:03 +0300

    tweecg. bench 3933448

--- src/search.cpp
+++ src/search.cpp
@@ -1054,7 +1054,7 @@ moves_loop: // When in check, search starts from here
                   continue;
           }
           else if (  !(givesCheck && extension)
-                   && !pos.see_ge(move, Value(-199) * (depth / ONE_PLY))) // (~20 Elo)
+                   && !pos.see_ge(move, Value(-199) * (depth / ONE_PLY + (type_of((ss-1)->currentMove) != NORMAL)))) // (~20 Elo)
                   continue;
       }
 
