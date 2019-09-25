5b7617ab11eecc54e0ff5dc94193eb89882a4f63
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-25 06:45:20 +0300

    tweecg. bench 3700155

--- src/search.cpp
+++ src/search.cpp
@@ -1619,7 +1619,7 @@ moves_loop: // When in check, search starts from here
     for (int i = 0; i < quietCount; ++i)
     {
         thisThread->mainHistory[us][from_to(quiets[i])] << -bonus;
-        if (to_sq(quiets[i]) != to_sq(move))
+        if (to_sq(quiets[i]) != to_sq(move) && from_sq(quiets[i]) != from_sq(move))
             update_continuation_histories(ss, pos.moved_piece(quiets[i]), to_sq(quiets[i]), -bonus);
         else 
             update_continuation_histories(ss, pos.moved_piece(quiets[i]), to_sq(quiets[i]), -bonus * 5 / 4);
