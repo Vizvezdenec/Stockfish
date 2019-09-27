05537b38cd3fae09f624d77c3f6d9bc2f6e71b36
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-28 00:25:51 +0300

    tweak. bench 3680797

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -732,7 +732,7 @@ namespace {
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
-                    + (49 - 18 * almostUnwinnable) * !pos.non_pawn_material()
+                    + (48 - 24 * almostUnwinnable) * !pos.non_pawn_material()
                     - 36 * almostUnwinnable
                     -103 ;
 
