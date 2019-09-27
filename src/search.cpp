6acaf35c5f5ecba73d970bd04140997d94eca2b7
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-27 07:01:56 +0300

    tweecg. bench 3468370

--- src/search.cpp
+++ src/search.cpp
@@ -1081,9 +1081,9 @@ moves_loop: // When in check, search starts from here
           &&  moveCount > 1 + 2 * rootNode
           && (!rootNode || thisThread->best_move_count(move) == 0)
           && (  !captureOrPromotion
-              || ((moveCountPruning
+              || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
-              || cutNode) && !rootNode)))
+              || cutNode))
       {
           Depth r = reduction(improving, depth, moveCount);
 
@@ -1164,6 +1164,9 @@ moves_loop: // When in check, search starts from here
 
               if (move == ss->killers[0])
                   bonus += bonus / 4;
+
+              if (bonus < 0 && ss->statScore > 0)
+                  ss->statScore = 0;
 
               update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
           }
