876410fa9442b22fd54dd1a81d4b716302750ca4
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-27 05:47:09 +0300

    tweecg. bench 3973627

--- src/search.cpp
+++ src/search.cpp
@@ -1054,7 +1054,7 @@ moves_loop: // When in check, search starts from here
                   continue;
           }
           else if (  !(givesCheck && extension)
-                   && !pos.see_ge(move, Value(-199) * (newDepth / ONE_PLY))) // (~20 Elo)
+                   && !pos.see_ge(move, Value(-179) * (newDepth / ONE_PLY))) // (~20 Elo)
                   continue;
       }
 
