630c78de3f171095f9c833a68de70e62087e371d
Vizvezdenec <Vizvezdenec@gmail.com>
2019-10-06 06:51:19 +0300

    tweak. bench 4270650

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -245,11 +245,12 @@ namespace {
     else if (file_of(ksq) == FILE_A)
         kingRing[Us] |= shift<EAST>(kingRing[Us]);
 
-    kingAttackersCount[Them] = popcount(kingRing[Us] & pe->pawn_attacks(Them));
-    kingAttacksCount[Them] = kingAttackersWeight[Them] = 0;
-
     // Remove from kingRing[] the squares defended by two pawns
     kingRing[Us] &= ~dblAttackByPawn;
+
+    kingAttacksCount[Them] = popcount(kingRing[Us] & pe->pawn_attacks(Them));
+    kingAttackersCount[Them] = kingAttackersWeight[Them] = 0;
+
   }
 
 
