e22cd97affc52ff09586a3f97640cb58a4315c94
Vizvezdenec <Vizvezdenec@gmail.com>
2019-10-08 01:10:16 +0300

    tweak. bench 3730247

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -725,6 +725,9 @@ namespace {
                     - 36 * almostUnwinnable
                     -103 ;
 
+    if (eg * int(mg) < 0)
+    	complexity -= (abs(mg) + abs(eg)) / 128;
+
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
     // so that the midgame and endgame scores do not change sign after the bonus.
