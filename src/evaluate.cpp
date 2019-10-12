2f4c5d5cde2403825c1d352c052a3ffd08d67d67
Vizvezdenec <Vizvezdenec@gmail.com>
2019-10-12 06:18:13 +0300

    tweak. bench 3985885

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -452,7 +452,7 @@ namespace {
 
     int kingFlankAttacks = popcount(b1) + popcount(b2);
 
-    int kfad = std::min((kfd - kingFlankAttacks), 0) * abs(kfd - kingFlankAttacks);
+    int kfad = std::max((kfd - kingFlankAttacks), 0) * abs(kfd - kingFlankAttacks);
 
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  +  69 * kingAttacksCount[Them]
