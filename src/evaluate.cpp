45508e3349bad4ea2e39d5ca0095dd350ed4b25f
Vizvezdenec <Vizvezdenec@gmail.com>
2019-10-14 00:20:10 +0300

    tweak. bench 4023386

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -461,7 +461,7 @@ namespace {
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   3 * kingFlankAttacks * kingFlankAttacks / 8
-                 +   7;
+                 +   3;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
