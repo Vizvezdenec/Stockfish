d6e296b5c31344d67bc1c01a8fb358ed98f3ff18
Vizvezdenec <Vizvezdenec@gmail.com>
2019-10-18 05:11:41 +0300

    tweak. bench 4590210

--- src/search.cpp
+++ src/search.cpp
@@ -70,6 +70,8 @@ namespace {
   // Reductions lookup table, initialized at startup
   int Reductions[MAX_MOVES]; // [depth or moveNumber]
 
+  float logThreads;
+
   Depth reduction(bool i, Depth d, int mn) {
     int r = Reductions[d] * Reductions[mn];
     return (r + 520) / 1024 + (!i && r > 999);
@@ -189,8 +191,10 @@ namespace {
 
 void Search::init() {
 
+  float logThreads = std::log(Threads.size());
+
   for (int i = 1; i < MAX_MOVES; ++i)
-      Reductions[i] = int((23.4 + std::log(Threads.size()) / 2) * std::log(i));
+      Reductions[i] = int((23.4 + logThreads / 2) * std::log(i));
 }
 
 
@@ -1052,7 +1056,7 @@ moves_loop: // When in check, search starts from here
                   continue;
           }
           else if (  !(givesCheck && extension)
-                   && !pos.see_ge(move, Value(-199) * depth)) // (~20 Elo)
+                   && !pos.see_ge(move, Value(-199 + logThreads * 5) * depth)) // (~20 Elo)
                   continue;
       }
 
