af23196b0b65e2b19628523b8ce8789a93325ce2
Vizvezdenec <Vizvezdenec@gmail.com>
2019-10-21 02:11:28 +0300

    tweak. bench 4441818

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -134,7 +134,7 @@ namespace {
   constexpr Score KingProtector      = S(  7,  8);
   constexpr Score KnightOnQueen      = S( 16, 12);
   constexpr Score LongDiagonalBishop = S( 45,  0);
-  constexpr Score MinorBehindPawn    = S( 18,  3);
+  constexpr Score MinorBehindPawn    = S( 36,  6);
   constexpr Score Outpost            = S( 32, 10);
   constexpr Score PassedFile         = S( 11,  8);
   constexpr Score PawnlessFlank      = S( 17, 95);
@@ -304,8 +304,8 @@ namespace {
                 score += Outpost;
 
             // Knight and Bishop bonus for being right behind a pawn
-            if (shift<Down>(pos.pieces(PAWN)) & s)
-                score += MinorBehindPawn * (1 + 2 * !(pe->pawn_attacks_span(Them) & s));
+            if ((shift<Down>(pos.pieces(PAWN)) & ~pe->pawn_attacks_span(Them)) & s)
+                score += MinorBehindPawn;
 
             // Penalty if the piece is far from the king
             score -= KingProtector * distance(s, pos.square<KING>(Us));
