ca63c0522bfc5a488cd9932ec4bb671547a4db5b
Vizvezdenec <Vizvezdenec@gmail.com>
2019-10-21 04:17:43 +0300

    tweak. bench 4836129

--- src/search.cpp
+++ src/search.cpp
@@ -1138,6 +1138,9 @@ moves_loop: // When in check, search starts from here
                   && thisThread->mainHistory[us][from_to(move)] >= 0)
                   ss->statScore = 0;
 
+              if (move == ss->killers[0])
+                  ss->statScore += ss->statScore / 4;
+
               // Decrease/increase reduction by comparing opponent's stat score (~10 Elo)
               if (ss->statScore >= -99 && (ss-1)->statScore < -116)
                   r--;
