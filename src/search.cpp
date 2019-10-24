be56f14e3b2b364666de2d8892a35c80feb4452a
Vizvezdenec <Vizvezdenec@gmail.com>
2019-10-25 01:06:29 +0300

    tweak. bench 4764701

--- src/search.cpp
+++ src/search.cpp
@@ -981,8 +981,6 @@ moves_loop: // When in check, search starts from here
           {
               extension = 1;
               singularLMR = true;
-              if (!captureOrPromotion)
-                  update_continuation_histories(ss, movedPiece, to_sq(move), stat_bonus((depth + 1) / 2));
           }
 
           // Multi-cut pruning
@@ -993,6 +991,9 @@ moves_loop: // When in check, search starts from here
           else if (   eval >= beta
                    && singularBeta >= beta)
               return singularBeta;
+          else if (value > singularBeta
+                   && eval < beta)
+              extension = -1;
       }
 
       // Check extension (~2 Elo)
