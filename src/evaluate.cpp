b642f463be854574ab52a47c9f258dd2f5dfe4b7
Vizvezdenec <Vizvezdenec@gmail.com>
2019-10-27 19:32:55 +0300

    tweak. bench 4321424

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -455,7 +455,7 @@ namespace {
                  +   3 * kingFlankAttacks * kingFlankAttacks / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  - 873 * !pos.count<QUEEN>(Them)
-                 -  90 * !(pos.count<QUEEN>(Them) + pos.count<ROOK>(Them))
+                 -  30 * !(pos.count<QUEEN>(Them) + pos.count<ROOK>(Them))
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
