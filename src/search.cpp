1b23f43fc4977d246678cbe9df024ac870e11f3f
Vizvezdenec <Vizvezdenec@gmail.com>
2019-10-29 02:04:32 +0300

    tweak. bench 4809672

--- src/search.cpp
+++ src/search.cpp
@@ -1085,9 +1085,9 @@ moves_loop: // When in check, search starts from here
           &&  moveCount > 1 + 2 * rootNode
           && (!rootNode || thisThread->best_move_count(move) == 0)
           && (  !captureOrPromotion
-              || ((moveCountPruning
+              || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
-              || cutNode) && (!rootNode || captureCount > 1))))
+              || cutNode))
       {
           Depth r = reduction(improving, depth, moveCount);
 
@@ -1102,6 +1102,9 @@ moves_loop: // When in check, search starts from here
           // Decrease reduction if opponent's move count is high (~10 Elo)
           if ((ss-1)->moveCount > 15)
               r--;
+
+          if (captureCount >= futility_move_count(improving, depth) / 2)
+              r++;
 
           // Decrease reduction if ttMove has been singularly extended
           if (singularLMR)
