71dd46baf8264527edf969451e24430f04b4800b
Vizvezdenec <Vizvezdenec@gmail.com>
2019-11-03 18:09:01 +0300

    tweak. bench 4618116

--- src/search.cpp
+++ src/search.cpp
@@ -1047,7 +1047,7 @@ moves_loop: // When in check, search starts from here
               // Futility pruning: parent node (~2 Elo)
               if (   lmrDepth < 6
                   && !inCheck
-                  && ss->staticEval + 250 + 211 * lmrDepth <= alpha)
+                  && ss->staticEval + 250 + 249 * (lmrDepth - improving) <= alpha)
                   continue;
 
               // Prune moves with negative SEE (~10 Elo)
