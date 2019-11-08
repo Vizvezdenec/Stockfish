2fbd649eab41d155d7bcb2381d86adaa3beb16de
Vizvezdenec <Vizvezdenec@gmail.com>
2019-11-08 10:12:31 +0300

    tweak. bench 4496400

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -441,6 +441,8 @@ namespace {
 
     int kingFlankAttacks = popcount(b1) + popcount(b2);
 
+    Bitboard stronglyProtected = attackedBy[Us][PAWN] | (attackedBy2[Us] & ~attackedBy2[Them]);
+
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  + 185 * popcount(kingRing[Us] & weak)
                  + 148 * popcount(unsafeChecks)
@@ -452,8 +454,8 @@ namespace {
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
-                 - 450 * !(pos.non_pawn_material(Them))
-                 -   7;
+                 -  20 * popcount(stronglyProtected & kingRing[Us])
+                 +  93;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
