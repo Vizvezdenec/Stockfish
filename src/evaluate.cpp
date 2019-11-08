1782529524cc01e194d3e0461b52f62d3cadf57c
Vizvezdenec <Vizvezdenec@gmail.com>
2019-11-08 03:55:06 +0300

    tweak. bench 4308328

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -452,7 +452,7 @@ namespace {
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
-                 - 150 * !(pos.non_pawn_material(Them))
+                 - 450 * !(pos.non_pawn_material(Them))
                  -   7;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
