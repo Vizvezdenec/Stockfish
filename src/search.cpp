608460c3950fa6b660472bc20d5de6334c43f580
Vizvezdenec <Vizvezdenec@gmail.com>
2019-11-12 01:50:32 +0300

    tweak. bench 4442893

--- src/search.cpp
+++ src/search.cpp
@@ -1047,7 +1047,7 @@ moves_loop: // When in check, search starts from here
               if (!pos.see_ge(move, Value(-(31 - std::min(lmrDepth, 18)) * lmrDepth * lmrDepth)))
                   continue;
           }
-          else if (depth < 5 && !givesCheck && ss->staticEval + PieceValue[MG][type_of(pos.piece_on(to_sq(move)))] + 1000 + 300 * depth < alpha)
+          else if (depth > 5 && !givesCheck && ss->staticEval + PieceValue[MG][type_of(pos.piece_on(to_sq(move)))] + 1000 + 300 * depth < alpha)
                   continue;
 
           else if (  !(givesCheck && extension)
