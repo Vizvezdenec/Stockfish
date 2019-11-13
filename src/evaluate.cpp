04b422b9f41cdaa553af32643902774d9ad8c52a
Vizvezdenec <Vizvezdenec@gmail.com>
2019-11-13 21:48:08 +0300

    tweak. bench 4962078

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -377,11 +377,12 @@ namespace {
   Score Evaluation<T>::king() const {
 
     constexpr Color    Them = (Us == WHITE ? BLACK : WHITE);
+    constexpr Direction Up       = pawn_push(Us);
     constexpr Bitboard Camp = (Us == WHITE ? AllSquares ^ Rank6BB ^ Rank7BB ^ Rank8BB
                                            : AllSquares ^ Rank1BB ^ Rank2BB ^ Rank3BB);
 
     Bitboard weak, b1, b2, safe, unsafeChecks = 0;
-    Bitboard rookChecks, queenChecks, bishopChecks, knightChecks;
+    Bitboard rookChecks, queenChecks, bishopChecks, knightChecks, Ccamp;
     int kingDanger = 0;
     const Square ksq = pos.square<KING>(Us);
 
@@ -441,7 +442,9 @@ namespace {
 
     // Find the squares that opponent attacks in our king flank, and the squares
     // which are attacked twice in that flank.
-    b1 = attackedBy[Them][ALL_PIECES] & KingFlank[file_of(ksq)] & Camp;
+
+    Ccamp = relative_rank(Us, ksq) < RANK_5 ? Camp : Camp | shift<Up>(Camp);
+    b1 = attackedBy[Them][ALL_PIECES] & KingFlank[file_of(ksq)] & Ccamp;
     b2 = b1 & attackedBy2[Them];
 
     int kingFlankAttacks = popcount(b1) + popcount(b2);
