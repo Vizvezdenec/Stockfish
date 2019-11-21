0a707b5b494009c5fd3c188a00d66b4a05347a55
Vizvezdenec <Vizvezdenec@gmail.com>
2019-11-21 03:36:40 +0300

    tweak. bench 4759769

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -455,13 +455,13 @@ namespace {
                  +  69 * kingAttacksCount[Them]
                  +   4 * (kingFlankAttack - kingFlankDefense)
                  +   3 * kingFlankAttack * kingFlankAttack / 8
-                 -       kingFlankDefense * kingFlankDefense / 8
+                 -       kingFlankDefense * kingFlankDefense / 4
                  +       mg_value(mobility[Them] - mobility[Us])
                  - 873 * !pos.count<QUEEN>(Them)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
-                 +   3;
+                 +  13;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
