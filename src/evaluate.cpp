95a6a9ad8dd6fa182f4c23d0d0689ac71bf31e28
Vizvezdenec <Vizvezdenec@gmail.com>
2019-11-24 04:08:51 +0300

    tweak. bench 4819085

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -749,7 +749,7 @@ namespace {
     {
         if (   pos.opposite_bishops()
             && pos.non_pawn_material() == 2 * BishopValueMg)
-            sf = 12 + 9 * pe->passed_count(strongSide);
+            sf = 16 + 6 * pe->passed_count(strongSide);
         else
             sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
 
