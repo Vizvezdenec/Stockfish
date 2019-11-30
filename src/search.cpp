e57756601769035ce1588bc9ba349cc827b4b521
Vizvezdenec <Vizvezdenec@gmail.com>
2019-11-30 03:46:33 +0300

    tweak. bench 5701221

--- src/search.cpp
+++ src/search.cpp
@@ -1099,6 +1099,10 @@ moves_loop: // When in check, search starts from here
           if (thisThread->ttHitAverage > 544 * ttHitAverageResolution * ttHitAverageWindow / 1024)
               r--;
 
+          if ((captureOrPromotion || type_of(movedPiece) == PAWN)
+              && thisThread->ttHitAverage > 640 * ttHitAverageResolution * ttHitAverageWindow / 1024)
+              r--;
+
           // Reduction if other threads are searching this position.
           if (th.marked())
               r++;
@@ -1115,10 +1119,6 @@ moves_loop: // When in check, search starts from here
           if (singularLMR)
               r -= 2;
 
-          if (cutNode && (!captureOrPromotion
-              || thisThread->ttHitAverage < 448 * ttHitAverageResolution * ttHitAverageWindow / 1024))
-              r++;
-
           if (!captureOrPromotion)
           {
               // Increase reduction if ttMove is a capture (~0 Elo)
@@ -1127,7 +1127,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
-                  r++;
+                  r += 2;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
