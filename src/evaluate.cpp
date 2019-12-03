bb548b7bd4cec9ed84b010f1967a2f9261c3d2d1
Vizvezdenec <Vizvezdenec@gmail.com>
2019-12-03 05:51:43 +0300

    tweak. bench 5107787

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -827,7 +827,7 @@ namespace {
     }
 
     return  (pos.side_to_move() == WHITE ? v : -v) // Side to move point of view
-           + Eval::Tempo + 20 * !pos.non_pawn_material();
+           + Eval::Tempo + 10 * !pos.non_pawn_material();
   }
 
 } // namespace
