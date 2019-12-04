8713f6632ab24f75a57c201f2d5754a80b0e49bb
Vizvezdenec <Vizvezdenec@gmail.com>
2019-12-04 09:58:59 +0300

    tweak. bench 4867368

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -459,8 +459,8 @@ namespace {
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
                  -   4 * kingFlankDefense
-                 -       kfsd * kfsd / 2
-                 +  43;
+                 -       kfsd * kfsd
+                 +  47;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
