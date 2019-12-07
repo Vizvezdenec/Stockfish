dab030c5b5774d110076d4b14d605dba8c8aa116
Vizvezdenec <Vizvezdenec@gmail.com>
2019-12-07 22:16:51 +0300

    tweak. bench 5559370

--- src/search.cpp
+++ src/search.cpp
@@ -1051,7 +1051,7 @@ moves_loop: // When in check, search starts from here
 
       // Last captures extension
       else if (   PieceValue[EG][pos.captured_piece()] > PawnValueEg
-               && pos.non_pawn_material() <= 2 * BishopValueMg)
+               && pos.count<ALL_PIECES>(~us) - pos.count<PAWN>(~us) < 3)
           extension = 1;
 
       // Castling extension
