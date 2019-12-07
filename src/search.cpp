11f35c8c87c7a76fd5c1c7939612af60e699bfcc
Vizvezdenec <Vizvezdenec@gmail.com>
2019-12-07 07:36:21 +0300

    tweak. bench 5466693

--- src/search.cpp
+++ src/search.cpp
@@ -1117,7 +1117,7 @@ moves_loop: // When in check, search starts from here
               r -= 2;
 
           if (lastCapture)
-              r--;
+              r -= 2;
 
           if (!captureOrPromotion)
           {
