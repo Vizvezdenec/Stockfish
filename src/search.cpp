6795c567085b23756b65898276327b58af484a53
Vizvezdenec <Vizvezdenec@gmail.com>
2019-12-10 01:41:33 +0300

    tweak. bench 5077035

--- src/search.cpp
+++ src/search.cpp
@@ -1168,6 +1168,8 @@ moves_loop: // When in check, search starts from here
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
               r -= ss->statScore / 16384;
           }
+          else if (givesCheck && !cutNode)
+              r--;
 
           Depth d = clamp(newDepth - r, 1, newDepth);
 
