ea31e666093319fb7b85b63c565f0bf34859a3fc
Vizvezdenec <Vizvezdenec@gmail.com>
2019-12-09 03:39:30 +0300

    tweak. bench 5612508

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -723,7 +723,7 @@ namespace {
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
                     - 95 
-                    - 18 * pureOcb;
+                    - 14 * pureOcb;
 
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
