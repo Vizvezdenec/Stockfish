deb109f252311b3d047a325208410ad7227bf6a3
Vizvezdenec <Vizvezdenec@gmail.com>
2019-12-14 01:58:26 +0300

    tweak. bench 4991434

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -626,8 +626,8 @@ namespace {
                 // Otherwise assign a smaller bonus if the path to queen is not attacked
                 // and even smaller bonus if it is attacked but block square is not.
                 int k = !unsafeSquares                    ? 35 :
-                        !((~stronglyDefended | squaresToQueen) & unsafeSquares) ? 27 :
-                        !(unsafeSquares & squaresToQueen) ? 20 :
+                        !((~stronglyDefended | squaresToQueen) & unsafeSquares) ? 26 :
+                        !(unsafeSquares & squaresToQueen) ? 18 :
                         !(unsafeSquares & blockSq)        ?  9 :
                                                              0 ;
 
