9ba7b91ddf5cc84d8bf9022721fba3cc55c37983
Vizvezdenec <Vizvezdenec@gmail.com>
2019-12-15 22:58:01 +0300

    tweak. bench 5306340

--- src/search.cpp
+++ src/search.cpp
@@ -884,7 +884,7 @@ namespace {
         int probCutCount = 0;
 
         while (  (move = mp.next_move()) != MOVE_NONE
-               && (probCutCount < 3 || cutNode))
+               && (probCutCount < 2 || cutNode))
             if (move != excludedMove && pos.legal(move))
             {
                 assert(pos.capture_or_promotion(move));
