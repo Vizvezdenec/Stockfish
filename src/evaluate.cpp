ba3c095a1684461f9c424aa5a0787b71a0d93039
Vizvezdenec <Vizvezdenec@gmail.com>
2019-12-20 02:43:05 +0300

    tweak. bench 5332449

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -443,7 +443,6 @@ namespace {
 
     int kingFlankAttack = popcount(b1) + popcount(b2);
     int kingFlankDefense = popcount(b3);
-    kingDanger -= 3 * popcount(b3 & attackedBy[Us][KNIGHT]);
 
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  + 185 * popcount(kingRing[Us] & weak)
@@ -455,7 +454,7 @@ namespace {
                  - 873 * !pos.count<QUEEN>(Them)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
-                 -   3 * kingFlankDefense
+                 -   4 * kingFlankDefense
                  +  37;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
