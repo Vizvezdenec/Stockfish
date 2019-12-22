a7a9790811a1ea526bb087324d006b346f8124d0
Vizvezdenec <Vizvezdenec@gmail.com>
2019-12-22 05:10:20 +0300

    tweak. bench 5095899

--- src/search.cpp
+++ src/search.cpp
@@ -1064,7 +1064,7 @@ moves_loop: // When in check, search starts from here
           extension = 1;
 
       // Last captures extension
-      else if (   PieceValue[EG][pos.captured_piece()] > PawnValueEg
+      if (   PieceValue[EG][pos.captured_piece()] > PawnValueEg
                && pos.non_pawn_material() <= 2 * RookValueMg)
           extension = 1;
 
