38090f702cc9764dfde4286fc22f7886f7ccb34c
Vizvezdenec <Vizvezdenec@gmail.com>
2019-12-24 00:43:58 +0300

    tweak. bench 5371271

--- src/search.cpp
+++ src/search.cpp
@@ -1048,7 +1048,7 @@ moves_loop: // When in check, search starts from here
           // search without the ttMove. So we assume this expected Cut-node is not singular,
           // that multiple moves fail high, and we can prune the whole subtree by returning
           // a soft bound.
-          else if (singularBeta >= beta && !th.marked())
+          else if (singularBeta >= beta + th.marked())
               return singularBeta;
       }
 
