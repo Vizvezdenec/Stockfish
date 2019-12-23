fe5a4440d0e6223419577e3325d0f9e34ad0345d
Vizvezdenec <Vizvezdenec@gmail.com>
2019-12-23 04:18:15 +0300

    tweak. bench 5234020

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -314,6 +314,8 @@ namespace {
                 score -= BishopPawns * pos.pawns_on_same_color_squares(Us, s)
                                      * (1 + popcount(blocked & CenterFiles));
 
+                score -= make_score(0, 20) * !pos.pawns_on_same_color_squares(Them, s);
+
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
                     score += LongDiagonalBishop;
