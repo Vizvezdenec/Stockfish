5fb7159073001551427642c3675908ef4ab7c1b0
Vizvezdenec <Vizvezdenec@gmail.com>
2019-12-24 21:09:13 +0300

    tweak. bench 5395597

--- src/search.cpp
+++ src/search.cpp
@@ -1098,7 +1098,7 @@ moves_loop: // When in check, search starts from here
       // Step 16. Reduced depth search (LMR). If the move fails high it will be
       // re-searched at full depth.
       if (    depth >= 3
-          &&  moveCount > 1 + rootNode + (PvNode && ss->ply < 2)
+          &&  moveCount > 1 + 2 * rootNode
           && (!rootNode || thisThread->best_move_count(move) == 0)
           && (  !captureOrPromotion
               || moveCountPruning
