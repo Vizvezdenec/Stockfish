7b5943c1175910a4fe504454aaa582de69b73576
Vizvezdenec <Vizvezdenec@gmail.com>
2019-12-24 04:24:36 +0300

    tweak. bench 5320903

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -205,6 +205,7 @@ namespace {
     // a white knight on g5 and black's king is on g8, this white knight adds 2
     // to kingAttacksCount[WHITE].
     int kingAttacksCount[COLOR_NB];
+    int pawnAttacksCount[COLOR_NB];
   };
 
 
@@ -241,7 +242,7 @@ namespace {
     kingRing[Us] = PseudoAttacks[KING][s] | s;
 
     kingAttackersCount[Them] = popcount(kingRing[Us] & pe->pawn_attacks(Them));
-    kingAttackersCount[Them] += popcount(kingRing[Us] & pawn_double_attacks_bb<Them>(pos.pieces(Them, PAWN)));
+    pawnAttacksCount[Them] = kingAttackersCount[Them];
     kingAttacksCount[Them] = kingAttackersWeight[Them] = 0;
 
     // Remove from kingRing[] the squares defended by two pawns
@@ -450,13 +451,14 @@ namespace {
                  + 148 * popcount(unsafeChecks)
                  +  98 * popcount(pos.blockers_for_king(Us))
                  +  69 * kingAttacksCount[Them]
+                 +       pawnAttacksCount[Them] * pawnAttacksCount[Them] * kingAttacksCount[Them]
                  +   3 * kingFlankAttack * kingFlankAttack / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  - 873 * !pos.count<QUEEN>(Them)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
                  -   4 * kingFlankDefense
-                 +  37;
+                 +  17;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
