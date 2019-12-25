ef547a6c5c8d4206ca56747407a31e5fadf10373
Vizvezdenec <Vizvezdenec@gmail.com>
2019-12-25 03:42:47 +0300

    tweak. bench 4865236

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -114,11 +114,11 @@ namespace {
   // which piece type attacks which one. Attacks on lesser pieces which are
   // pawn-defended are not considered.
   constexpr Score ThreatByMinor[PIECE_TYPE_NB] = {
-    S(0, 0), S(6, 32), S(59, 41), S(79, 56), S(90, 119), S(76, 155)
+    S(0, 0), S(6, 32), S(59, 41), S(79, 56), S(90, 119), S(74, 152)
   };
 
   constexpr Score ThreatByRook[PIECE_TYPE_NB] = {
-    S(0, 0), S(3, 44), S(38, 71), S(38, 61), S(0, 38), S(49, 36)
+    S(0, 0), S(3, 44), S(38, 71), S(38, 61), S(0, 38), S(48, 35)
   };
 
   // PassedRank[Rank] contains a bonus according to the rank of a passed pawn
