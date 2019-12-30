9227e11890ae11da39fea9ca2f27aaf1cf137165
Vizvezdenec <Vizvezdenec@gmail.com>
2019-12-31 02:44:55 +0300

    tweak. bench 4773357

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -244,7 +244,7 @@ namespace {
     kingAttacksCount[Them] = kingAttackersWeight[Them] = 0;
 
     // Remove from kingRing[] the squares defended by two pawns
-    kingRing[Us] &= ~dblAttackByPawn | (pos.blockers_for_king(Us) & ~pos.pieces(Us, PAWN));
+    kingRing[Us] &= ~(dblAttackByPawn & pos.pieces(Us));
   }
 
 
