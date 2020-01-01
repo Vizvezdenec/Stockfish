b8b810232506271945a4157457c565dc651c0c15
Vizvezdenec <Vizvezdenec@gmail.com>
2020-01-01 10:05:53 +0300

    tweak. bench 4652893

--- src/search.cpp
+++ src/search.cpp
@@ -705,9 +705,13 @@ namespace {
                     update_quiet_stats(pos, ss, ttMove, stat_bonus(depth));
 
                 // Extra penalty for early quiet moves of the previous ply
-                if ((ss-1)->moveCount <= 3 && !priorCapture)
-                    update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, 
-                                                 -stat_bonus(depth + 1) / (1 + std::max(0, (ss-1)->moveCount - 2)));
+                if (!priorCapture)
+                {
+                    int bonus = stat_bonus(depth + 1);
+                    if ((ss-1)->moveCount > 2)
+                    	bonus /= ((ss-1)->moveCount - 2) * ((ss-1)->moveCount - 1);
+                    update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -bonus);
+                }
             }
             // Penalty for a quiet ttMove that fails low
             else if (!pos.capture_or_promotion(ttMove))
