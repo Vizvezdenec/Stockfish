26a4a6ec0fe2ce8428a0ffd89ea546b30b8d15e2
Vizvezdenec <Vizvezdenec@gmail.com>
2020-01-03 14:13:46 +0300

    tweak. bench 5019676

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -721,9 +721,9 @@ namespace {
                     +  9 * outflanking
                     + 21 * pawnsOnBothFlanks
                     + 51 * !pos.non_pawn_material()
-                    + 24 * kingInfiltration
+                    + 18 * kingInfiltration
                     - 43 * almostUnwinnable
-                    - 110 ;
+                    - 105 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
