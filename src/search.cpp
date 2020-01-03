efba09354b6ec173b60e60e891b930057e117a83
Vizvezdenec <Vizvezdenec@gmail.com>
2020-01-03 12:12:27 +0300

    tweak. bench 5305974

--- src/search.cpp
+++ src/search.cpp
@@ -1174,9 +1174,6 @@ moves_loop: // When in check, search starts from here
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
           doFullDepthSearch = (value > alpha && d != newDepth), didLMR = true;
-
-          if (singularLMR && !doFullDepthSearch && d != newDepth && !captureOrPromotion)
-              update_continuation_histories(ss, movedPiece, to_sq(move), -stat_bonus(d));
       }
       else
           doFullDepthSearch = !PvNode || moveCount > 1, didLMR = false;
