0af60404a353452b9e21c4d1725956092c16269a
Vizvezdenec <Vizvezdenec@gmail.com>
2020-01-08 03:25:04 +0300

    tweak. bench 5159252

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -723,7 +723,6 @@ namespace {
                     + 21 * pawnsOnBothFlanks
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
-                    - 12 * (almostUnwinnable && !infiltration)
                     - 100 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting the
