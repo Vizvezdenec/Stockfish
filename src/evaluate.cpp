a28e5a1bd99814086f0026d7e2306040f705b78b
Vizvezdenec <Vizvezdenec@gmail.com>
2020-01-13 22:30:08 +0300

    tweak. bench 4766094

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -722,11 +722,11 @@ namespace {
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 12 * infiltration
-                    + 15 * pawnsOnBothFlanks
-                    + 15 * winnableStructure
+                    + 18 * pawnsOnBothFlanks
+                    + 12 * winnableStructure
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
-                    - 103 ;
+                    - 104 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
