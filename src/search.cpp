dbacc7e555fedded5caf8da75ecc6ba240944516
Vizvezdenec <Vizvezdenec@gmail.com>
2020-01-16 21:56:01 +0300

    tweak. bench 4844301

--- src/search.cpp
+++ src/search.cpp
@@ -1013,21 +1013,21 @@ moves_loop: // When in check, search starts from here
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
 
-              bool badHist = thisThread->mainHistory[us][from_to(move)]
+              int history = thisThread->mainHistory[us][from_to(move)]
                     + (*contHist[0])[movedPiece][to_sq(move)]
                     + (*contHist[1])[movedPiece][to_sq(move)]
-                    + (*contHist[3])[movedPiece][to_sq(move)] < 25000;
+                    + (*contHist[3])[movedPiece][to_sq(move)];
 
               // Futility pruning: parent node (~5 Elo)
               if (   lmrDepth < 6
                   && !inCheck
                   && ss->staticEval + 235 + 172 * lmrDepth <= alpha
-                  && badHist)
+                  && history < 25000)
                   continue;
 
               int seeMargin = (32 - std::min(lmrDepth, 18)) * lmrDepth * lmrDepth;
               // Prune moves with negative SEE (~20 Elo)
-              if ((seeMargin >= PawnValueEg || badHist) &&
+              if ((seeMargin >= PawnValueEg || history < 30000) &&
                   !pos.see_ge(move, Value(-seeMargin)))
                   continue;
           }
