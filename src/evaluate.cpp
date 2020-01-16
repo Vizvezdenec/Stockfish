217cfd20e9a767dce62a91243ded6a682d1d6047
Vizvezdenec <Vizvezdenec@gmail.com>
2020-01-16 03:03:50 +0300

    tweak. bench 4985931

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -129,7 +129,7 @@ namespace {
   // Assorted bonuses and penalties
   constexpr Score BishopPawns        = S(  3,  7);
   constexpr Score CorneredBishop     = S( 50, 50);
-  constexpr Score FlankAttacks       = S(  8,  0);
+  constexpr Score FlankAttacks       = S(  7,  0);
   constexpr Score Hanging            = S( 69, 36);
   constexpr Score KingProtector      = S(  7,  8);
   constexpr Score KnightOnQueen      = S( 16, 12);
@@ -466,6 +466,8 @@ namespace {
 
     // Penalty if king flank is under attack, potentially moving toward the king
     score -= FlankAttacks * kingFlankAttack;
+
+    score -= make_score(6, 0) * popcount(kingRing[Us] & attackedBy2[Them]);
 
     if (T)
         Trace::add(KING, Us, score);
