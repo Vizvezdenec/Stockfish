19f8cb9db0838ebfc2ce6828c212d391269b57cd
Vizvezdenec <Vizvezdenec@gmail.com>
2020-01-20 15:29:29 +0300

    tweak. bench 4602701

--- src/search.cpp
+++ src/search.cpp
@@ -1013,15 +1013,14 @@ moves_loop: // When in check, search starts from here
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
 
-              int history = thisThread->mainHistory[us][from_to(move)]
-                    + (*contHist[0])[movedPiece][to_sq(move)]
-                    + (*contHist[1])[movedPiece][to_sq(move)]
-                    + (*contHist[3])[movedPiece][to_sq(move)];
               // Futility pruning: parent node (~5 Elo)
-              if (   lmrDepth < 6 - history / 16384
+              if (   lmrDepth < 6
                   && !inCheck
                   && ss->staticEval + 235 + 172 * lmrDepth <= alpha
-                  && history < 25000)
+                  &&  thisThread->mainHistory[us][from_to(move)]
+                    + (*contHist[0])[movedPiece][to_sq(move)]
+                    + (*contHist[1])[movedPiece][to_sq(move)]
+                    + (*contHist[3])[movedPiece][to_sq(move)] < 25000)
                   continue;
 
               // Prune moves with negative SEE (~20 Elo)
