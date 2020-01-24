6a34d374ac449cf33f86066717d50c4a9efae339
Vizvezdenec <Vizvezdenec@gmail.com>
2020-01-24 04:26:13 +0300

    tweak. bench 5064350

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -446,7 +446,7 @@ namespace {
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  + 185 * popcount(kingRing[Us] & weak)
                  + 148 * popcount(unsafeChecks)
-                 +  18 * popcount(unsafeChecks & kingRing[Us])
+                 +  30 * popcount(unsafeChecks & kingRing[Us])
                  +  98 * popcount(pos.blockers_for_king(Us))
                  +  69 * kingAttacksCount[Them]
                  +   3 * kingFlankAttack * kingFlankAttack / 8
