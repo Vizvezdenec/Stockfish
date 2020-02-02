2c57a71d8332482c5172769f69572b463306ab5f
Vizvezdenec <Vizvezdenec@gmail.com>
2020-02-02 22:16:50 +0300

    tweak. bench 4869669

--- src/search.cpp
+++ src/search.cpp
@@ -1020,7 +1020,7 @@ moves_loop: // When in check, search starts from here
                   &&  thisThread->mainHistory[us][from_to(move)]
                     + (*contHist[0])[movedPiece][to_sq(move)]
                     + (*contHist[1])[movedPiece][to_sq(move)]
-                    + (*contHist[3])[movedPiece][to_sq(move)] < 25000 - (ss-1)->statScore / 4)
+                    + (*contHist[3])[movedPiece][to_sq(move)] < 25000)
                   continue;
 
               // Prune moves with negative SEE (~20 Elo)
