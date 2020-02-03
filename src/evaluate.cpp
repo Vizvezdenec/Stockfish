822db8d8a2dc177a762dc5ed62e2bad93ef7a536
Vizvezdenec <Vizvezdenec@gmail.com>
2020-02-03 05:21:04 +0300

    tweak. bench 5067271

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -708,18 +708,17 @@ namespace {
                            &&  outflanking < 0
                            && !pawnsOnBothFlanks;
 
-    int infiltration =  clamp(-3, rank_of(pos.square<KING>(WHITE)) -
-                        rank_of(pos.square<KING>(BLACK)), 3);
+    bool infiltration = rank_of(pos.square<KING>(WHITE)) > RANK_4
+                     || rank_of(pos.square<KING>(BLACK)) < RANK_5;
 
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 21 * pawnsOnBothFlanks
-                    +  6 * infiltration
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
-                    -110 ;
+                    - 95 ;
 
     // Give more importance to non-material score
     Value mg = mg_value(score);
@@ -729,6 +728,8 @@ namespace {
     // sign of the midgame or endgame values, and that we carefully cap the bonus
     // so that the midgame and endgame scores do not change sign after the bonus.
     int u = ((mg > 0) - (mg < 0)) * std::max(std::min(complexity + 50, 0), -abs(mg));
+
+    complexity += 24 * infiltration - 15;
     int v = ((eg > 0) - (eg < 0)) * std::max(complexity, -abs(eg));
 
     if (T)
