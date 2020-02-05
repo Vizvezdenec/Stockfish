9f846580f06c50e713f791ef5e1c838acc8edf57
Vizvezdenec <Vizvezdenec@gmail.com>
2020-02-05 06:30:50 +0300

    tweak. bench 4884313

--- src/search.cpp
+++ src/search.cpp
@@ -1016,7 +1016,7 @@ moves_loop: // When in check, search starts from here
               // Futility pruning: parent node (~5 Elo)
               if (   lmrDepth < 6
                   && !inCheck
-                  && ss->staticEval + 235 + 172 * lmrDepth + 10 * cutNode * lmrDepth <= alpha
+                  && ss->staticEval + 235 + 172 * lmrDepth + 40 * cutNode * lmrDepth <= alpha
                   &&  thisThread->mainHistory[us][from_to(move)]
                     + (*contHist[0])[movedPiece][to_sq(move)]
                     + (*contHist[1])[movedPiece][to_sq(move)]
