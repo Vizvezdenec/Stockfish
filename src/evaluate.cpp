81167651f21ebb5bee92be203718396f94c1c4c6
Vizvezdenec <Vizvezdenec@gmail.com>
2020-02-07 10:42:08 +0300

    tweak. bench 4692580

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -558,11 +558,6 @@ namespace {
            | (attackedBy[Us][ROOK  ] & pos.attacks_from<ROOK  >(s));
 
         score += SliderOnQueen * popcount(b & safe & attackedBy2[Us]);
-
-        b = nonPawnEnemies & (attackedBy2[Us] | (attackedBy[Us][ALL_PIECES] & ~attackedBy[Us][QUEEN]));
-
-        if (more_than_one(attackedBy[Them][QUEEN] & b & ~attackedBy2[Them]))
-            score += make_score(30, 24);
     }
 
     if (T)
