39045933023614989b21ada1be5af1c62c60c42c
Vizvezdenec <Vizvezdenec@gmail.com>
2020-02-07 12:04:01 +0300

    tweak. bench 4626513

--- src/search.cpp
+++ src/search.cpp
@@ -1027,7 +1027,7 @@ moves_loop: // When in check, search starts from here
               if (!pos.see_ge(move, Value(-(32 - std::min(lmrDepth, 18)) * lmrDepth * lmrDepth)))
                   continue;
           }
-          else if (!pos.see_ge(move, Value(-194 - 20 * ttPv) * depth)) // (~25 Elo)
+          else if (!pos.see_ge(move, Value(-194 + 20 * ttPv) * depth)) // (~25 Elo)
               continue;
       }
 
