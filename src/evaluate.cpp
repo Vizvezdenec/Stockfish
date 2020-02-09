413e705fc26b4d953039be22e185a0956b9c757c
Vizvezdenec <Vizvezdenec@gmail.com>
2020-02-10 02:00:48 +0300

    tweak. bench 4612654

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -728,7 +728,7 @@ namespace {
     // so that the midgame and endgame scores do not change sign after the bonus.
     int u = ((mg > 0) - (mg < 0)) * std::max(std::min(complexity + 50, 0), -abs(mg));
 
-    complexity += std::min(complexity + 65, 0);
+    complexity += std::min(complexity + 55, 0);
 
     int v = ((eg > 0) - (eg < 0)) * std::max(complexity, -abs(eg));
 
