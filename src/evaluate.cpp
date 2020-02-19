e0eaf0be216af789ed6169599d81baf7f127baf7
Vizvezdenec <Vizvezdenec@gmail.com>
2020-02-19 04:27:17 +0300

    tweak. bench 4965170

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -359,7 +359,7 @@ namespace {
             if (pos.slider_blockers(pos.pieces(Them, ROOK, BISHOP), s, queenPinners))
                 score -= WeakQueen;
 
-            score += make_score(2, 3) * std::min((std::max(mob - 14, 0) * std::max(8 - pos.count<PAWN>(), 0)), 20);
+            score += make_score(3, 5) * (std::max(mob - 20, 0) * std::max(6 - pos.count<PAWN>(), 0));
         }
     }
     if (T)
