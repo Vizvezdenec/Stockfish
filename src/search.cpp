6690b86a9cd0d25c11bf7986eacaeddb5c5b2f25
Vizvezdenec <Vizvezdenec@gmail.com>
2020-03-03 00:16:27 +0300

    tweak. bench 5190186

--- src/search.cpp
+++ src/search.cpp
@@ -1122,7 +1122,8 @@ moves_loop: // When in check, search starts from here
       // Step 16. Reduced depth search (LMR, ~200 Elo). If the move fails high it will be
       // re-searched at full depth.
       if (    depth >= 3
-          &&  moveCount > 1 + 2 * rootNode + !(pos.non_pawn_material(us))
+          && (pos.non_pawn_material(us) || depth > 11)
+          &&  moveCount > 1 + 2 * rootNode
           && (!rootNode || thisThread->best_move_count(move) == 0)
           && (  !captureOrPromotion
               || moveCountPruning
