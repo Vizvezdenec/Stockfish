8b2f61d5e83cb1541beaf9da40b3c6aab62a2068
Vizvezdenec <Vizvezdenec@gmail.com>
2020-03-04 04:28:08 +0300

    tweak. bench 4625707

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -756,6 +756,10 @@ namespace {
         else
             sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
 
+        if (pos.count<QUEEN>(strongSide) == 1 
+         && pos.count<QUEEN>(~strongSide) == 0)
+            sf -= 10 * !pe->passed_pawns(strongSide);
+
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4);
     }
 
