eeb563b7b54a4eba51e8e8d0171231f2f74d19c0
Vizvezdenec <Vizvezdenec@gmail.com>
2020-03-07 18:33:41 +0300

    tweak. bench 5346279

--- src/search.cpp
+++ src/search.cpp
@@ -1122,7 +1122,7 @@ moves_loop: // When in check, search starts from here
       // Step 16. Reduced depth search (LMR, ~200 Elo). If the move fails high it will be
       // re-searched at full depth.
       if (    depth >= 3
-          &&  moveCount > 1 + 2 * rootNode
+          &&  moveCount > 1 + 2 * rootNode + bool(ttMove)
           && (!rootNode || thisThread->best_move_count(move) == 0)
           && (  !captureOrPromotion
               || moveCountPruning
@@ -1160,7 +1160,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode)
-                  r += 2 - bool(excludedMove);
+                  r += 2;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
