98c67f91eacb29b01dd219e8513e33d8103ff49d
Vizvezdenec <Vizvezdenec@gmail.com>
2020-03-26 20:38:12 +0300

    tweak. bench 4578913

--- src/search.cpp
+++ src/search.cpp
@@ -1141,7 +1141,7 @@ moves_loop: // When in check, search starts from here
 
           // Decrease reduction if position is or has been on the PV (~10 Elo)
           if (ttPv)
-              r -= 2 + !PvNode;
+              r -= 1 + 2 * !PvNode;
 
           // Decrease reduction if opponent's move count is high (~5 Elo)
           if ((ss-1)->moveCount > 14)
