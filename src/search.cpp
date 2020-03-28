cb62f024b3c148aedcc6028cabc48dd3705d741f
Vizvezdenec <Vizvezdenec@gmail.com>
2020-03-29 00:08:20 +0300

    tweak. bench 5110104

--- src/search.cpp
+++ src/search.cpp
@@ -622,7 +622,7 @@ namespace {
     StateInfo st;
     TTEntry* tte;
     Key posKey;
-    Move ttMove, move, excludedMove, bestMove;
+    Move ttMove, move, excludedMove, bestMove, probcutMove[4];
     Depth extension, newDepth;
     Value bestValue, value, ttValue, eval, maxValue;
     bool ttHit, ttPv, inCheck, givesCheck, improving, didLMR, priorCapture;
@@ -638,6 +638,10 @@ namespace {
     moveCount = captureCount = quietCount = ss->moveCount = 0;
     bestValue = -VALUE_INFINITE;
     maxValue = VALUE_INFINITE;
+    probcutMove[0] = MOVE_NONE;
+    probcutMove[1] = MOVE_NONE;
+    probcutMove[2] = MOVE_NONE;
+    probcutMove[3] = MOVE_NONE;
 
     // Check for the available remaining time
     if (thisThread == Threads.main())
@@ -930,6 +934,9 @@ namespace {
 
                 if (value >= raisedBeta)
                     return value;
+
+                else if (!cutNode && ss->staticEval >= raisedBeta)
+                    probcutMove[probCutCount - 1] = move;
             }
     }
 
@@ -1220,14 +1227,16 @@ moves_loop: // When in check, search starts from here
               update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
           }
 
-          else if (didLMR && captureOrPromotion)
+          else if (didLMR && value <= alpha && 
+                  (move == probcutMove[0]
+                || move == probcutMove[1] 
+                || move == probcutMove[2]
+                || move == probcutMove[3]))
           {
-          
               CapturePieceToHistory& captureHistory = thisThread->captureHistory;
-              if (value > alpha)
-                  captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] << stat_bonus(newDepth);
-
+              captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] << - stat_bonus(newDepth);
           }
+              
       }
 
       // For PV nodes only, do a full PV search on the first move or after a fail
