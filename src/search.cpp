4eb0991eb15900d1503eaebd663226b331bb4de3
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-02 07:28:07 +0300

    tweak. bench 4809164

--- src/search.cpp
+++ src/search.cpp
@@ -1196,7 +1196,7 @@ moves_loop: // When in check, search starts from here
               r++;
 
           if (ss->staticEval + PieceValue[EG][pos.captured_piece()] + 200 * depth <= alpha)
-              r+= 1 - 2 * givesCheck;
+              r+= 1 - 3 * givesCheck;
           }
 
           Depth d = Utility::clamp(newDepth - r, 1, newDepth);
