636ec3699dbcee911467d634ee3a201c32fcf8b8
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-02 13:04:59 +0300

    tweak. bench 5318806

--- src/search.cpp
+++ src/search.cpp
@@ -1197,7 +1197,7 @@ moves_loop: // When in check, search starts from here
 
           if (!givesCheck && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 200 * depth <= alpha)
               r++;
-          else if (givesCheck && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 100 * depth <= alpha)
+          else if (givesCheck && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 400 * depth <= alpha)
               r--;
           }
 
