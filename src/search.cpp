b5551df8e1b25d681b5f95edca4bc3e4d39ca84d
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-02 06:51:37 +0300

    tweak. bench 4692175

--- src/search.cpp
+++ src/search.cpp
@@ -1187,9 +1187,6 @@ moves_loop: // When in check, search starts from here
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
               r -= ss->statScore / 16434;
-
-              if (!givesCheck && ss->staticEval + 200 * depth <= alpha)
-                  r++;
           }
 
           // Increase reduction for captures/promotions if late move and at low depth
@@ -1198,8 +1195,8 @@ moves_loop: // When in check, search starts from here
           if (depth < 8 && moveCount > 2)
               r++;
 
-          if (!givesCheck && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 200 * depth <= alpha)
-              r++;
+          if (ss->staticEval + PieceValue[EG][pos.captured_piece()] + 200 * depth <= alpha)
+              r+= 1 - 2 * givesCheck;
           }
 
           Depth d = Utility::clamp(newDepth - r, 1, newDepth);
