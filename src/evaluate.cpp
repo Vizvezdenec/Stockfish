99f80822e06cad88530ed8488d1af7f4ac34c52a
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-03 00:15:55 +0300

    tweak. bench 4771307

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -205,6 +205,7 @@ namespace {
     // a white knight on g5 and black's king is on g8, this white knight adds 2
     // to kingAttacksCount[WHITE].
     int kingAttacksCount[COLOR_NB];
+    Square pseudoKing[COLOR_NB];
   };
 
 
@@ -236,15 +237,15 @@ namespace {
     attackedBy2[Us] = dblAttackByPawn | (attackedBy[Us][KING] & attackedBy[Us][PAWN]);
 
     // Init our king safety tables
-    Square s = make_square(Utility::clamp(file_of(ksq), FILE_B, FILE_G),
+    pseudoKing[Us] = make_square(Utility::clamp(file_of(ksq), FILE_B, FILE_G),
                            Utility::clamp(rank_of(ksq), RANK_2, RANK_7));
-    kingRing[Us] = PseudoAttacks[KING][s] | s;
+    kingRing[Us] = PseudoAttacks[KING][pseudoKing[Us]] | pseudoKing[Us];
 
     kingAttackersCount[Them] = popcount(kingRing[Us] & pe->pawn_attacks(Them));
     kingAttacksCount[Them] = kingAttackersWeight[Them] = 0;
 
     // Remove from kingRing[] the squares defended by two pawns
-    kingRing[Us] &= ~dblAttackByPawn | pe->pawn_attacks(Them);
+    kingRing[Us] &= ~dblAttackByPawn;
   }
 
 
@@ -443,6 +444,9 @@ namespace {
 
     int kingFlankAttack = popcount(b1) + popcount(b2);
     int kingFlankDefense = popcount(b3);
+
+    if ((attackedBy[Them][QUEEN] & attackedBy2[Them]) & pseudoKing[Us])
+        kingDanger += 50;
 
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  + 185 * popcount(kingRing[Us] & weak)
