b0ba3e663c1bb152df3cd275969b05d599e53088
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-05 00:51:06 +0300

    tweak. bench 4465472

--- src/search.cpp
+++ src/search.cpp
@@ -1128,7 +1128,7 @@ moves_loop: // When in check, search starts from here
       // Step 16. Reduced depth search (LMR, ~200 Elo). If the move fails high it will be
       // re-searched at full depth.
       if (    depth >= 3
-          &&  moveCount > 1 + 2 * rootNode
+          && (moveCount > 1 + 2 * rootNode || (moveCount == 1 && failedExt))
           && (!rootNode || thisThread->best_move_count(move) == 0)
           && (  !captureOrPromotion
               || moveCountPruning
@@ -1232,9 +1232,6 @@ moves_loop: // When in check, search starts from here
                                         : -stat_bonus(newDepth);
 
               if (move == ss->killers[0])
-                  bonus += bonus / 4;
-
-              if (value > alpha && failedExt)
                   bonus += bonus / 4;
 
               update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
