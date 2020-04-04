8b3599bd0d548df38a7456612ad6dc2b1eb81835
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-04 21:36:35 +0300

    tweak. bench 4742598

--- src/search.cpp
+++ src/search.cpp
@@ -194,7 +194,7 @@ namespace {
 void Search::init() {
 
   for (int i = 1; i < MAX_MOVES; ++i)
-      Reductions[i] = int((24.8 + 3 * std::log(Threads.size()) / 2) * std::log(i));
+      Reductions[i] = int((24.8 + 5 * std::log(Threads.size()) / 4) * std::log(i));
 }
 
 
