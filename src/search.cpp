32e8717d6783f53aa73322408561218e50818221
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-06 05:45:21 +0300

    tweak. bench 4757761

--- src/search.cpp
+++ src/search.cpp
@@ -1160,8 +1160,7 @@ moves_loop: // When in check, search starts from here
           {
               // Increase reduction if ttMove is a capture (~5 Elo)
               if (ttCapture)
-                  r+= 1 + (!givesCheck
-                && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 200 * depth <= alpha);
+                  r++;
 
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode)
@@ -1199,7 +1198,7 @@ moves_loop: // When in check, search starts from here
             // Unless giving check, this capture is likely bad
             if (   !givesCheck
                 && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 200 * depth <= alpha)
-                r++;
+                r+= 1 + cutNode;
           }
 
           Depth d = Utility::clamp(newDepth - r, 1, newDepth);
