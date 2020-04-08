4f755793aff322b062306a321ebb11d4e67dec3e
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-08 18:24:26 +0300

    tweak. bench 4740497

--- src/search.cpp
+++ src/search.cpp
@@ -1103,6 +1103,12 @@ moves_loop: // When in check, search starts from here
                && pos.non_pawn_material() <= 2 * RookValueMg)
           extension = 1;
 
+      else if (   captureOrPromotion
+               && type_of(pos.piece_on(to_sq(move))) != PAWN
+               && pos.count<ALL_PIECES>(~us) - pos.count<PAWN>(~us) == 2
+               && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] >= 0)
+          extension = 1;
+
       // Castling extension
       if (type_of(move) == CASTLING)
           extension = 1;
