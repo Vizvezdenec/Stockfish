649fff0808449722f05d681275a7970e8f350ae6
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-08 15:46:09 +0300

    tweak. bench 4775118

--- src/search.cpp
+++ src/search.cpp
@@ -731,7 +731,7 @@ namespace {
                 thisThread->mainHistory[us][from_to(ttMove)] << penalty;
                 update_continuation_histories(ss, pos.moved_piece(ttMove), to_sq(ttMove), penalty);
             }
-            else if (ttValue <= alpha)
+            else if (ttValue < beta - PieceValue[EG][pos.piece_on(to_sq(ttMove))])
             {
                 CapturePieceToHistory& captureHistory = thisThread->captureHistory;
                 captureHistory[pos.moved_piece(ttMove)][to_sq(ttMove)][type_of(pos.piece_on(to_sq(ttMove)))] << -stat_bonus(depth);
