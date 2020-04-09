f2e59a9646fe72c54fe9b5ddbef385f59d5d4e04
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-09 23:10:25 +0300

    tweak. bench 5198185

--- src/search.cpp
+++ src/search.cpp
@@ -1041,7 +1041,7 @@ moves_loop: // When in check, search starts from here
           else
           {
               if (   !givesCheck
-                  && lmrDepth < 1
+                  && lmrDepth < 1 + ttCheck
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
 
@@ -1140,8 +1140,7 @@ moves_loop: // When in check, search starts from here
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
               || cutNode
-              || thisThread->ttHitAverage < 375 * ttHitAverageResolution * ttHitAverageWindow / 1024
-              || (!givesCheck && ttCheck)))
+              || thisThread->ttHitAverage < 375 * ttHitAverageResolution * ttHitAverageWindow / 1024))
       {
           Depth r = reduction(improving, depth, moveCount);
 
