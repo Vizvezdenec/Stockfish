a050f3abba31e2eece14129b7cee27b30881acbd
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-09 22:16:06 +0300

    tweak. bench 4795939

--- src/search.cpp
+++ src/search.cpp
@@ -627,7 +627,7 @@ namespace {
     Depth extension, newDepth;
     Value bestValue, value, ttValue, eval, maxValue;
     bool ttHit, ttPv, inCheck, givesCheck, improving, didLMR, priorCapture;
-    bool captureOrPromotion, doFullDepthSearch, moveCountPruning, ttCapture, singularLMR;
+    bool captureOrPromotion, doFullDepthSearch, moveCountPruning, ttCapture, ttCheck, singularLMR;
     Piece movedPiece;
     int moveCount, captureCount, quietCount;
 
@@ -965,6 +965,7 @@ moves_loop: // When in check, search starts from here
     value = bestValue;
     singularLMR = moveCountPruning = false;
     ttCapture = ttMove && pos.capture_or_promotion(ttMove);
+    ttCheck = ttMove && pos.gives_check(ttMove);
     bool formerPv = ttPv && !PvNode;
 
     // Mark this node as being searched
@@ -1170,7 +1171,7 @@ moves_loop: // When in check, search starts from here
           {
               // Increase reduction if ttMove is a capture (~5 Elo)
               if (ttCapture)
-                  r+= 1 + (pos.gives_check(ttMove) && !givesCheck);
+                  r+= 1 + (ttCheck && !givesCheck);
 
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode)
