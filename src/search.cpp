0b8bd192699ed509131997344f2597e7a015b504
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-09 20:29:01 +0300

    tweak. bench 4956637

--- src/search.cpp
+++ src/search.cpp
@@ -737,6 +737,8 @@ namespace {
                 thisThread->mainHistory[us][from_to(ttMove)] << penalty;
                 update_continuation_histories(ss, pos.moved_piece(ttMove), to_sq(ttMove), penalty);
             }
+            else
+                captureHistory[pos.moved_piece(ttMove)][to_sq(ttMove)][type_of(pos.piece_on(to_sq(ttMove)))] << -stat_bonus(depth);
         }
 
         if (pos.rule50_count() < 90)
