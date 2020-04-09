d1e19578e85a51bca50ad89dd1281b90c9ebd3e5
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-10 01:15:31 +0300

    tweak. bench 4810215

--- src/search.cpp
+++ src/search.cpp
@@ -1210,7 +1210,9 @@ moves_loop: // When in check, search starts from here
                 && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 200 * depth <= alpha)
                 r++;
 
-            if (captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0 && (ss-1)->statScore > 0)
+            if (   !givesCheck 
+                 && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0 
+                 && (ss-1)->statScore > 0)
                 r++;
           }
 
