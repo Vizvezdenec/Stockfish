01efc77d22c6f355100853843dbe96b13deb4185
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-09 16:41:10 +0300

    tweak. bench 4701119

--- src/search.cpp
+++ src/search.cpp
@@ -1040,7 +1040,7 @@ moves_loop: // When in check, search starts from here
           else
           {
               if (   !givesCheck
-                  && lmrDepth < 1 + ((ss-1)->statScore > 25000)
+                  && lmrDepth < 1 + ((ss-1)->statScore > 15000)
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
 
