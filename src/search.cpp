381f69e62bd603a244a1316aec5309b0a54510b0
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-13 07:20:05 +0300

    tweak. bench 4719435

--- src/search.cpp
+++ src/search.cpp
@@ -1066,7 +1066,7 @@ moves_loop: // When in check, search starts from here
        /* &&  ttValue != VALUE_NONE Already implicit in the next condition */
           &&  abs(ttValue) < VALUE_KNOWN_WIN
           && (tte->bound() & BOUND_LOWER)
-          &&  tte->depth() >= depth - 3
+          &&  tte->depth() >= depth / 2
           &&  pos.legal(move))
       {
           Value singularBeta = ttValue - ((formerPv + 4) * depth) / 2;
