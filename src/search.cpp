ce59c55207614d50ba675c54e26447639444c72a
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-13 08:13:35 +0300

    tweak. bench 4614529

--- src/search.cpp
+++ src/search.cpp
@@ -1219,7 +1219,7 @@ moves_loop: // When in check, search starts from here
                 && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 200 * depth <= alpha)
                 r++;
 
-            if (!(PvNode || cutNode))
+            if (givesCheck && !(PvNode || cutNode))
                 r--;
           }
 
