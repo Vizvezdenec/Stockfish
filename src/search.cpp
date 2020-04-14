178cdc9136c00119dfa0e642d928afbc98b2a7ea
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-14 08:56:23 +0300

    tweak. bench 4975749

--- src/search.cpp
+++ src/search.cpp
@@ -1042,7 +1042,7 @@ moves_loop: // When in check, search starts from here
           {
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
-                  && lmrDepth < 1
+                  && lmrDepth < 1 + !(PvNode || cutNode)
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
 
@@ -1218,9 +1218,6 @@ moves_loop: // When in check, search starts from here
             if (   !givesCheck
                 && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 200 * depth <= alpha)
                 r++;
-
-            if (!(PvNode || cutNode))
-                r-= 2 - !givesCheck;
           }
 
           Depth d = Utility::clamp(newDepth - r, 1, newDepth);
