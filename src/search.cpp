3af3de24fc18eca1c664f3beeb96061e627ede39
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-16 02:39:32 +0300

    tweak. bench 4917988

--- src/search.cpp
+++ src/search.cpp
@@ -1051,7 +1051,7 @@ moves_loop: // When in check, search starts from here
           {
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
-                  && lmrDepth < 1 + ttGoodQuiet
+                  && lmrDepth < 1 + (ttGoodQuiet && (ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha))
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
 
