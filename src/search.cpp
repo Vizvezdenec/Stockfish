1fbc4de53b6d5df7d98bcb1f82179e269ac5d921
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-17 01:16:30 +0300

    tweak. bench 5238635

--- src/search.cpp
+++ src/search.cpp
@@ -1052,7 +1052,7 @@ moves_loop: // When in check, search starts from here
               bool goodCheckCapt = captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] > 0 
                                 || givesCheck;
               // See based pruning
-              if (!pos.see_ge(move, Value(-194 - 25 * goodCheckCapt) * depth)) // (~25 Elo)
+              if (!pos.see_ge(move, Value(-190 - 30 * goodCheckCapt) * depth)) // (~25 Elo)
                   continue;
           }
       }
