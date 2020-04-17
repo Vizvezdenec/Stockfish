26d5bc0876a10f76b126142cf54fedc4f5063ff5
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-17 21:01:57 +0300

    tweak. bench 4953221

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -428,7 +428,7 @@ namespace {
     else
     {
         if (bishopChecks & almostSafe)
-            kingDanger += BishopSafeCheck / 8;
+            kingDanger += BishopSafeCheck / 2;
         unsafeChecks |= b2 & attackedBy[Them][BISHOP];
     }
 
@@ -440,7 +440,7 @@ namespace {
     else 
     {
         if (knightChecks & almostSafe)
-            kingDanger += KnightSafeCheck / 8;
+            kingDanger += KnightSafeCheck / 2;
         unsafeChecks |= knightChecks;
     }
 
