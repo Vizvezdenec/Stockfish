befeb1b45df122022437e7ab38a39d3c89cd6bb1
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-19 01:43:44 +0300

    tweak. bench 4914110

--- src/search.cpp
+++ src/search.cpp
@@ -1103,10 +1103,10 @@ moves_loop: // When in check, search starts from here
               if (value >= beta)
                   return beta;
           }
-          else if (ttValue < alpha)
+          else if (ttValue <= alpha)
           {
               pos.do_move(move, st, givesCheck);
-              value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, depth - 3, true);
+              value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, depth - 3, cutNode);
               pos.undo_move(move);
               if (value <= alpha)
                   return alpha;
