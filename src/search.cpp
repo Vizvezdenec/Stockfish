fef1fe5ca4accb9651d338fb36d0e2733f1373fd
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-18 19:28:04 +0300

    tweak. bench 4777771

--- src/search.cpp
+++ src/search.cpp
@@ -1097,7 +1097,7 @@ moves_loop: // When in check, search starts from here
           else if (ttValue >= beta)
           {
               ss->excludedMove = move;
-              value = search<NonPV>(pos, ss, beta - 1, beta, (depth - 2 * cutNode + 4) / 2, cutNode);
+              value = search<NonPV>(pos, ss, beta - 1, beta, (depth + 3) / 2, cutNode);
               ss->excludedMove = MOVE_NONE;
 
               if (value >= beta)
