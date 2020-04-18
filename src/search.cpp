61b4beef2b3c4afeea5a9c24fae998d5e501e607
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-18 03:01:00 +0300

    tweak. bench 3896334

--- src/search.cpp
+++ src/search.cpp
@@ -842,7 +842,11 @@ namespace {
         &&  depth < 6
         &&  eval - futility_margin(depth, improving) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
+    {
+        if (!priorCapture && depth > 1)
+            update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -stat_bonus(depth + 1));
         return eval;
+    }
 
     // Step 9. Null move search with verification search (~40 Elo)
     if (   !PvNode
@@ -876,16 +880,7 @@ namespace {
                 nullValue = beta;
 
             if (thisThread->nmpMinPly || (abs(beta) < VALUE_KNOWN_WIN && depth < 13))
-            {
-                if (ttHit
-                 && tte->depth() >= depth
-                 && ttValue != VALUE_NONE
-                 && ttMove
-                 && (ss-1)->moveCount <= 2 
-                 && !priorCapture)
-                    update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -stat_bonus(depth + 1));
                 return nullValue;
-            }
 
             assert(!thisThread->nmpMinPly); // Recursive verification is not allowed
 
