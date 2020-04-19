1b76851a48d4d8014bcb4c8d179cc4799d36dfc9
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-19 06:22:47 +0300

    tweak. bench 4344244

--- src/search.cpp
+++ src/search.cpp
@@ -1102,6 +1102,8 @@ moves_loop: // When in check, search starts from here
 
               if (value >= beta)
                   return beta;
+              else if (cutNode)
+                  singularLMR = true;
           }
       }
 
@@ -1378,9 +1380,7 @@ moves_loop: // When in check, search starts from here
     // Bonus for prior countermove that caused the fail low
     else if (   (depth >= 3 || PvNode)
              && !priorCapture)
-        update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth + ((ss-1)->moveCount > 7)));
-
-
+        update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth));
 
     if (PvNode)
         bestValue = std::min(bestValue, maxValue);
