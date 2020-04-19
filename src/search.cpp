905e32a6627167c293aefcb14438f5120e638e08
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-20 00:31:35 +0300

    tweak. bench 4422238

--- src/search.cpp
+++ src/search.cpp
@@ -1030,7 +1030,7 @@ moves_loop: // When in check, search starts from here
 
               if (   lmrDepth < 1
                   && type_of(pos.moved_piece(move)) != PAWN
-                  && thisThread->mainHistory[us][from_to(reverse_move(move))] > 6000)
+                  && thisThread->mainHistory[us][from_to(reverse_move(move))] > 0)
                   continue;
 
               // Futility pruning: parent node (~5 Elo)
