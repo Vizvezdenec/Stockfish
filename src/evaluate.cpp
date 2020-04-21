76c4b466c39b3f2e8cf7e3be8b1728196f7a9bcb
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-21 13:30:36 +0300

    tweak. bench 4640983

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -649,7 +649,7 @@ namespace {
 
                 // Assign a larger bonus if the block square is defended
                 if ((pos.pieces(Us) & bb) || (attackedBy[Us][ALL_PIECES] & blockSq))
-                    k += 5;
+                    k += 4 + 3 * (r == RANK_7);
 
                 bonus += make_score(k * w, k * w);
             }
