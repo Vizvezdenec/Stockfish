1c85de084678fba143bf9094764eeeef049ca72c
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-21 13:39:40 +0300

    tweak. bench 4952161

--- src/search.cpp
+++ src/search.cpp
@@ -1159,7 +1159,7 @@ moves_loop: // When in check, search starts from here
       if (    depth >= 3
           &&  moveCount > 1 + 2 * rootNode
           && (!rootNode || thisThread->best_move_count(move) == 0)
-          && pos.non_pawn_material(us)
+          && (thisThread->best_move_count(move) <= pos.non_pawn_material(us) / PawnValueEg)
           && (  !captureOrPromotion
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
