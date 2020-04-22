4e40e1a6055cc1fce3d130021125e7b14de6f47a
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-22 09:41:26 +0300

    tweak. bench 4540615

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -110,6 +110,7 @@ void MovePicker::score() {
                    + 2 * (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + 2 * (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
+                   +     (*continuationHistory[7])[pos.moved_piece(m)][to_sq(m)] / 2
                    + (ply < MAX_LPH ?  4 * (*lowPlyHistory)[ply][from_to(m)] : 0);
 
       else // Type == EVASIONS
