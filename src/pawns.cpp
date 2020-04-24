e78b891b1bf2d00b99ff3a13c453ad54b0ea1693
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-24 06:29:38 +0300

    tweak. bench 4915569

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -106,9 +106,7 @@ namespace {
         phalanx    = neighbours & rank_bb(s);
         support    = neighbours & rank_bb(s - Up);
 
-        e->blockedCount[Us] += (blocked 
-                            || (more_than_one(leverPush) & !(ourPawns & (s + Up))) 
-                           || ((ourPawns & (s + Up)) && !leverPush)) && !lever;
+        e->blockedCount[Us] += ((pos.pieces(PAWN) & (s + Up)) || more_than_one(leverPush)) && !lever;
 
         // A pawn is backward when it is behind all pawns of the same color on
         // the adjacent files and cannot safely advance.
