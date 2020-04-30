29ddbf7ca69bfa8b9bcede2d816b5952a96f314f
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-30 20:08:06 +0300

    tweak. bench 4385589

--- src/search.cpp
+++ src/search.cpp
@@ -1046,6 +1046,7 @@ moves_loop: // When in check, search starts from here
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
                   && lmrDepth < 1
+                  && !(pos.blockers_for_king(~us) & to_sq(move))
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
 
@@ -1053,7 +1054,7 @@ moves_loop: // When in check, search starts from here
               if (   !givesCheck
                   && lmrDepth < 6
                   && !ss->inCheck
-                  && ss->staticEval + 270 + (384 - 170 * cutNode) * lmrDepth + PieceValue[MG][type_of(pos.piece_on(to_sq(move)))] <= alpha)
+                  && ss->staticEval + 270 + 384 * lmrDepth + PieceValue[MG][type_of(pos.piece_on(to_sq(move)))] <= alpha)
                   continue;
 
               // See based pruning
