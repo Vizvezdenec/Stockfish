7539ba7ede7785a70b0a24f87cfe62080e2ba02b
Vizvezdenec <Vizvezdenec@gmail.com>
2020-04-30 18:51:43 +0300

    tweak. bench 4854768

--- src/search.cpp
+++ src/search.cpp
@@ -1051,9 +1051,10 @@ moves_loop: // When in check, search starts from here
 
               // Futility pruning for captures
               if (   !givesCheck
+                  && lmrDepth < 6
                   && !ss->inCheck
                   && ss->staticEval + 270 + 384 * lmrDepth + PieceValue[MG][type_of(pos.piece_on(to_sq(move)))] <= alpha
-                  && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 10000 - 2000 * lmrDepth)
+                  && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 9500 - 400 * lmrDepth)
                   continue;
 
               // See based pruning
