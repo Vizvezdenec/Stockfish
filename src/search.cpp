1d72ec43d04b9c5339207e14230f7164de48a443
Vizvezdenec <Vizvezdenec@gmail.com>
2020-05-02 17:54:49 +0300

    tweak. bench 4682952

--- src/search.cpp
+++ src/search.cpp
@@ -1233,7 +1233,7 @@ moves_loop: // When in check, search starts from here
           else
           {
             // Increase reduction for captures/promotions if late move and at low depth
-            if (depth < 8 && moveCount > 2 && (PvNode || cutNode))
+            if (depth < 8 && moveCount > 2 && (PvNode || cutNode || !givesCheck))
                 r++;
 
             // Unless giving check, this capture is likely bad
