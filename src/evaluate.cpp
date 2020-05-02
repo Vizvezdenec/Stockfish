8c3bebad1d23c2f48246dda47158cac919298db4
Vizvezdenec <Vizvezdenec@gmail.com>
2020-05-02 14:57:51 +0300

    tweak. bench 4557492

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -765,7 +765,7 @@ namespace {
         {
             if (   pos.non_pawn_material(WHITE) == BishopValueMg
                 && pos.non_pawn_material(BLACK) == BishopValueMg)
-                sf = 14 + 4 * popcount(pe->passed_pawns(strongSide));
+                sf = 16 + 4 * popcount(pe->passed_pawns(strongSide));
             else
                 sf = 22 + 3 * pos.count<ALL_PIECES>(strongSide);
         }
