ce6f1fb1eac98a7a46b36c638fbb3a4453c6174e
Vizvezdenec <Vizvezdenec@gmail.com>
2020-05-03 03:14:02 +0300

    tweak. bench 4455468

--- src/search.cpp
+++ src/search.cpp
@@ -722,8 +722,6 @@ namespace {
             {
                 if (!pos.capture_or_promotion(ttMove))
                     update_quiet_stats(pos, ss, ttMove, stat_bonus(depth), depth);
-                else if (!ss->inCheck)
-                    captureHistory[pos.moved_piece(ttMove)][to_sq(ttMove)][type_of(pos.piece_on(to_sq(ttMove)))] << stat_bonus(depth);
 
                 // Extra penalty for early quiet moves of the previous ply
                 if ((ss-1)->moveCount <= 2 && !priorCapture)
@@ -736,6 +734,8 @@ namespace {
                 thisThread->mainHistory[us][from_to(ttMove)] << penalty;
                 update_continuation_histories(ss, pos.moved_piece(ttMove), to_sq(ttMove), penalty);
             }
+            else if (!ss->inCheck)
+                captureHistory[pos.moved_piece(ttMove)][to_sq(ttMove)][type_of(pos.piece_on(to_sq(ttMove)))] << -stat_bonus(depth);
         }
 
         if (pos.rule50_count() < 90)
