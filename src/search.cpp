dbc060c984f3f8460b43a9d26d2f4bb5346423ec
Vizvezdenec <Vizvezdenec@gmail.com>
2020-05-08 00:39:43 +0300

    tweak. bench 4811449

--- src/search.cpp
+++ src/search.cpp
@@ -839,10 +839,18 @@ namespace {
 
     // Step 8. Futility pruning: child node (~50 Elo)
     if (   !PvNode
-        &&  depth < 6
         &&  eval - futility_margin(depth, improving) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
-        return eval;
+    {
+        if (depth < 6)
+            return eval;
+        else
+        {
+            value = search<NonPV>(pos, ss, eval - 1, eval, depth - 5, cutNode);
+            if (value >= eval)
+                return value;
+        }
+    }
 
     // Step 9. Null move search with verification search (~40 Elo)
     if (   !PvNode
@@ -1057,7 +1065,7 @@ moves_loop: // When in check, search starts from here
                   continue;
 
               // See based pruning
-              if (!pos.see_ge(move, Value(-196 + lmrDepth) * depth)) // (~25 Elo)
+              if (!pos.see_ge(move, Value(-194) * depth)) // (~25 Elo)
                   continue;
           }
       }
