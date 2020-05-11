bce3b1c6db1a9f2758275d7eeb3ce4e83eac4273
Vizvezdenec <Vizvezdenec@gmail.com>
2020-05-11 15:21:44 +0300

    tweak. bench 4286900

--- src/search.cpp
+++ src/search.cpp
@@ -729,8 +729,7 @@ namespace {
             else if (!pos.capture_or_promotion(ttMove))
             {
                 int penalty = -stat_bonus(depth);
-                if (!ss->inCheck)
-                    thisThread->mainHistory[us][from_to(ttMove)] << penalty;
+                thisThread->mainHistory[us][from_to(ttMove)] << penalty;
                 update_continuation_histories(ss, pos.moved_piece(ttMove), to_sq(ttMove), penalty);
             }
         }
@@ -1739,7 +1738,8 @@ moves_loop: // When in check, search starts from here
 
     Color us = pos.side_to_move();
     Thread* thisThread = pos.this_thread();
-    thisThread->mainHistory[us][from_to(move)] << bonus;
+    if (thisThread->mainHistory[us][from_to(move)] < bonus)
+        thisThread->mainHistory[us][from_to(move)] << bonus;
     update_continuation_histories(ss, pos.moved_piece(move), to_sq(move), bonus);
 
     if (type_of(pos.moved_piece(move)) != PAWN)
