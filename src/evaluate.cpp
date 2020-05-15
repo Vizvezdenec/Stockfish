9d3e34b1d7001f8381ca752ecbb4f5014a0814be
Vizvezdenec <Vizvezdenec@gmail.com>
2020-05-15 22:44:59 +0300

    tweak. bench 5039729

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -248,7 +248,7 @@ namespace {
     kingAttacksCount[Them] = kingAttackersWeight[Them] = 0;
 
     // Remove from kingRing[] the squares defended by two pawns
-    kingRing[Us] &= ~dblAttackByPawn;
+    kingRing[Us] &= ~(dblAttackByPawn & shift<Up>(pos.pieces(Us)));
   }
 
 
--- src/search.cpp
+++ src/search.cpp
@@ -1385,7 +1385,7 @@ moves_loop: // When in check, search starts from here
     // Bonus for prior countermove that caused the fail low
     else if (   (depth >= 3 || PvNode)
              && !priorCapture)
-        update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth + (bestValue < alpha - PawnValueEg)));
+        update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth));
 
     if (PvNode)
         bestValue = std::min(bestValue, maxValue);
