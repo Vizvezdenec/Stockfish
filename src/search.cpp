575012d60db6c64f3a7fab5f25ce14df60758cca
Vizvezdenec <Vizvezdenec@gmail.com>
2020-05-18 01:53:02 +0300

    tweak. bench 4616419

--- src/search.cpp
+++ src/search.cpp
@@ -838,7 +838,7 @@ namespace {
               || (ss-4)->staticEval == VALUE_NONE) : ss->staticEval > (ss-2)->staticEval;
 
     // Step 8. Futility pruning: child node (~50 Elo)
-    if (   !PvNode
+    if (   (!PvNode || (!rootNode && ttHit && (tte->bound() & BOUND_EXACT)))
         &&  depth < 6
         &&  eval - futility_margin(depth, improving) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
