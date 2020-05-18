08026f278d6de4e4228fdb90a9717f081f352700
Vizvezdenec <Vizvezdenec@gmail.com>
2020-05-18 04:30:14 +0300

    tweak. bench 4681405

--- src/search.cpp
+++ src/search.cpp
@@ -1690,9 +1690,6 @@ moves_loop: // When in check, search starts from here
         for (int i = 0; i < quietCount; ++i)
         {
             thisThread->mainHistory[us][from_to(quietsSearched[i])] << -bonus2;
-            if (type_of(pos.moved_piece(quietsSearched[i])) != PAWN 
-             && thisThread->mainHistory[us][from_to(reverse_move(quietsSearched[i]))] >= 0)
-                thisThread->mainHistory[us][from_to(reverse_move(quietsSearched[i]))] << bonus2;
             update_continuation_histories(ss, pos.moved_piece(quietsSearched[i]), to_sq(quietsSearched[i]), -bonus2);
         }
     }
