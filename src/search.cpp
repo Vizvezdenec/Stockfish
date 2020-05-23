951dbd13837791d522c7578546230c7b3a85d8d4
Vizvezdenec <Vizvezdenec@gmail.com>
2020-05-23 15:50:37 +0300

    tweak. bench 4122771

--- src/search.cpp
+++ src/search.cpp
@@ -825,7 +825,7 @@ namespace {
         else
             ss->staticEval = eval = -(ss-1)->staticEval + 2 * Tempo;
 
-        int comp = std::max(int(thisThread->ttHitAverage * 1024 / (TtHitAverageResolution * TtHitAverageWindow) - 700), 0) / 16;
+        int comp = std::max(int(thisThread->ttHitAverage * 1024 / (TtHitAverageResolution * TtHitAverageWindow) - 600), 0) / 32;
         ss->staticEval = eval = eval - comp * comp * ((ss->staticEval > 0) - (ss->staticEval < 0));
 
         tte->save(posKey, VALUE_NONE, ttPv, BOUND_NONE, DEPTH_NONE, MOVE_NONE, eval);
