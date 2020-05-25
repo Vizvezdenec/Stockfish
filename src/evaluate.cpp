089ad6852848f982a459636b1452e1702d97980e
Vizvezdenec <Vizvezdenec@gmail.com>
2020-05-26 02:03:09 +0300

    tweak. bench 4663220

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -328,9 +328,6 @@ namespace {
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
                     score += LongDiagonalBishop;
-                else if (!(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center)
-                      && (PseudoAttacks[BISHOP][s] & Center))
-                    score -= make_score(15, 15);
 
                 // An important Chess960 pattern: a cornered bishop blocked by a friendly
                 // pawn diagonally in front of it is a very serious problem, especially
--- src/search.cpp
+++ src/search.cpp
@@ -1170,6 +1170,7 @@ moves_loop: // When in check, search starts from here
           &&  moveCount > 1 + 2 * rootNode
           && (!rootNode || thisThread->best_move_count(move) == 0)
           && (  !captureOrPromotion
+              || th.marked()
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
               || cutNode
