d6a64839e91f0b76419455446dc008530ac9c9f1
Vizvezdenec <Vizvezdenec@gmail.com>
2020-05-26 00:05:33 +0300

    tweak. bench 4968241

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -290,8 +290,8 @@ namespace {
         }
         else if (Pt == ROOK && (file_bb(s) & kingRing[Them]))
             score += RookOnKingRing;
-        else if (Pt == QUEEN && (file_bb(s) & kingRing[Them]))
-            score += make_score(32, 0);
+        else if (Pt == BISHOP && (PseudoAttacks[BISHOP][s] & kingRing[Them]))
+            score += make_score(24, 0);
 
         int mob = popcount(b & mobilityArea[Us]);
 
