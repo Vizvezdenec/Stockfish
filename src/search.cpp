c3e648f35be7d0a8fcba794d0f47a4c9862b7f46
Vizvezdenec <Vizvezdenec@gmail.com>
2020-06-07 02:25:07 +0300

    tweak. bench 4324759

--- src/search.cpp
+++ src/search.cpp
@@ -905,7 +905,7 @@ namespace {
     // much above beta, we can (almost) safely prune the previous move.
     if (   !PvNode
         &&  depth >= 5
-        &&  (cutNode || eval >= beta - 50 * depth)
+        &&  (cutNode || eval >= beta - 100 * depth)
         &&  abs(beta) < VALUE_TB_WIN_IN_MAX_PLY)
     {
         Value raisedBeta = beta + 189 - 45 * improving;
