8cdb07bab98a25c1efe1704f8f58efa2a4dfdd42
Vizvezdenec <Vizvezdenec@gmail.com>
2020-06-17 10:43:02 +0300

    tweak. bench 4463037

--- src/search.cpp
+++ src/search.cpp
@@ -1088,7 +1088,7 @@ moves_loop: // When in check, search starts from here
 
       // Check extension (~2 Elo)
       else if (    givesCheck
-               && (pos.is_discovery_check_on_king(~us, move) || pos.see_ge(move, -BishopValueMg + KnightValueMg)))
+               && (pos.is_discovery_check_on_king(~us, move) || pos.see_ge(move)))
           extension = 1;
 
       // Passed pawn extension
@@ -1214,6 +1214,10 @@ moves_loop: // When in check, search starts from here
             if (   !givesCheck
                 && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 211 * depth <= alpha)
                 r++;
+
+            if (   type_of(pos.captured_piece()) == BISHOP && pos.count<BISHOP>(~us) == 1 
+                && pos.count<BISHOP>(us) < 2)
+                r--;
           }
 
           Depth d = Utility::clamp(newDepth - r, 1, newDepth);
