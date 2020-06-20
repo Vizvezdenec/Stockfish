b51469aca7bc5b0ddad6d7f48d8d6123342ff46b
Vizvezdenec <Vizvezdenec@gmail.com>
2020-06-20 10:27:34 +0300

    tweak. bench 4251190

--- src/search.cpp
+++ src/search.cpp
@@ -1019,7 +1019,6 @@ moves_loop: // When in check, search starts from here
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
                   && lmrDepth < 1
-                  && !(type_of(movedPiece) == PAWN && pos.advanced_pawn_push(move))
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
 
@@ -1027,6 +1026,7 @@ moves_loop: // When in check, search starts from here
               if (   !givesCheck
                   && lmrDepth < 6
                   && !(PvNode && abs(bestValue) < 2)
+                  && !(type_of(movedPiece) == PAWN && pos.advanced_pawn_push(move))
                   && !ss->inCheck
                   && ss->staticEval + 267 + 391 * lmrDepth + PieceValue[MG][type_of(pos.piece_on(to_sq(move)))] <= alpha)
                   continue;
