e06857c3ff2189957c8426652298eb744b753141
Vizvezdenec <Vizvezdenec@gmail.com>
2020-06-24 06:27:01 +0300

    tweak. bench 4291700

--- src/search.cpp
+++ src/search.cpp
@@ -704,7 +704,7 @@ namespace {
                 thisThread->mainHistory[us][from_to(ttMove)] << penalty;
                 update_continuation_histories(ss, pos.moved_piece(ttMove), to_sq(ttMove), penalty);
 
-                if (cutNode && (ss-1)->moveCount > 3 && !priorCapture)
+                if (cutNode && (ss-1)->moveCount > 2 && !priorCapture)
                     update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth + 1));
             }
         }
