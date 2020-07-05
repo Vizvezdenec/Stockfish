d6472bd8362e5a9f75eb0c6f144170a0dc6272dd
Vizvezdenec <Vizvezdenec@gmail.com>
2020-07-05 09:53:37 +0300

    tweak. bench 4941706

--- src/search.cpp
+++ src/search.cpp
@@ -1183,7 +1183,7 @@ moves_loop: // When in check, search starts from here
               if (type_of(movedPiece) == PAWN && pos.non_pawn_material() > 10000)
               {
               Bitboard blocked = us == WHITE ? shift<SOUTH>(pos.pieces(BLACK, PAWN)) : shift<NORTH>(pos.pieces(WHITE, PAWN));
-              if (blocked & to_sq(move))
+              if (blocked & CenterFiles & to_sq(move))
                   r--;
               }
 
