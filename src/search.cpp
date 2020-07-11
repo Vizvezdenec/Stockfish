6ef4f091ef68ab7d5864b8c8c86cf97dc8949c86
Vizvezdenec <Vizvezdenec@gmail.com>
2020-07-11 16:25:01 +0300

    tweak. bench 4616651

--- src/search.cpp
+++ src/search.cpp
@@ -928,7 +928,7 @@ namespace {
                     tte->save(posKey, value_to_tt(value, ss->ply), ttPv,
                         BOUND_LOWER,
                         depth - 3, move, ss->staticEval);
-                    thisThread->captureHistory[pos.moved_piece(move)][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] << stat_bonus(depth - 3);
+                    thisThread->captureHistory[pos.moved_piece(move)][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] << stat_bonus(depth - 2);
                     return value;
                 }
             }
