b75229b5793cba18bd7af43db919dc1d12bcf49f
Vizvezdenec <Vizvezdenec@gmail.com>
2020-07-13 00:49:40 +0300

    tweak. bench 4011094

--- src/search.cpp
+++ src/search.cpp
@@ -1038,8 +1038,8 @@ moves_loop: // When in check, search starts from here
           {
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
-                  && lmrDepth < 1 + ((ss-1)->currentMove == MOVE_NULL)
-                  && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
+                  && lmrDepth < 1
+                  && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < ((ss-1)->currentMove == MOVE_NULL))
                   continue;
 
               // Futility pruning for captures
