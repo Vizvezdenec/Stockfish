6e2a79f9c67cc42e3a038ab1b0a7fbcb800901d5
Vizvezdenec <Vizvezdenec@gmail.com>
2020-07-13 22:43:18 +0300

    tweak. bench 4101096

--- src/search.cpp
+++ src/search.cpp
@@ -1073,7 +1073,7 @@ moves_loop: // When in check, search starts from here
           &&  abs(ttValue) < VALUE_KNOWN_WIN
           && (tte->bound() & BOUND_LOWER)
           &&  tte->depth() >= depth - 3
-          &&  ttValue >= alpha - 100 * depth
+          &&  ttValue >= ss->staticEval - 50 * depth
           &&  pos.legal(move))
       {
           Value singularBeta = ttValue - ((formerPv + 4) * depth) / 2;
