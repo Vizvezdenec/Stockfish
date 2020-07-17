2359c293f9bc64de5e9d2f0d0037a433f7ce4d46
Vizvezdenec <Vizvezdenec@gmail.com>
2020-07-18 02:22:18 +0300

    tweak. bench 4213668

--- src/search.cpp
+++ src/search.cpp
@@ -931,7 +931,7 @@ namespace {
                         tte->save(posKey, value_to_tt(value, ss->ply), ttPv,
                             BOUND_LOWER,
                             depth - 3, move, ss->staticEval);
-                    else if (!priorCapture && (ss-1)->moveCount < 3)
+                    else if (!priorCapture && ((ss-1)->moveCount < 2 || (ss-1)->currentMove == (ss-1)->killers[0]))
                         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -stat_bonus(depth - 2));
                     return value;
                 }
