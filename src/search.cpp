f7d533d4617e8bdc9c76fc122f1568478d752fe8
Vizvezdenec <Vizvezdenec@gmail.com>
2020-08-01 16:30:16 +0300

    tweak. bench 4645909

--- src/search.cpp
+++ src/search.cpp
@@ -1169,10 +1169,11 @@ moves_loop: // When in check, search starts from here
 
           // Decrease reduction at non-check cut nodes for second move at low depths
           if (   cutNode
+              && captureOrPromotion
               && depth <= 10
               && moveCount <= 2
               && !ss->inCheck)
-              r -= 1 + captureOrPromotion;
+              r -= 2;
 
           // Decrease reduction if the ttHit running average is large
           if (thisThread->ttHitAverage > 473 * TtHitAverageResolution * TtHitAverageWindow / 1024)
