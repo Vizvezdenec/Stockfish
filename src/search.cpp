e0ec35be882bc299285bb651fcd59b7f038b5b15
Vizvezdenec <Vizvezdenec@gmail.com>
2020-08-06 10:15:28 +0300

    tweak. bench 4188349

--- src/search.cpp
+++ src/search.cpp
@@ -1240,6 +1240,10 @@ moves_loop: // When in check, search starts from here
             if (   !givesCheck
                 && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 211 * depth <= alpha)
                 r++;
+
+            if (   !PvNode && !cutNode
+                && moveCount < 4)
+               r--;
           }
 
           Depth d = Utility::clamp(newDepth - r, 1, newDepth);
