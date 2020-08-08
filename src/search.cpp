9d2ade06833b0f76a617d4996a0835bbf729156a
Vizvezdenec <Vizvezdenec@gmail.com>
2020-08-09 01:01:09 +0300

    tweak. bench 4270825

--- src/search.cpp
+++ src/search.cpp
@@ -1081,7 +1081,7 @@ moves_loop: // When in check, search starts from here
           && (tte->bound() & BOUND_LOWER)
           &&  tte->depth() >= depth - 3)
       {
-          Value singularBeta = ttValue - ((formerPv + 4) * depth) / 2;
+          Value singularBeta = ttValue - ((formerPv + 3) * depth) / 2;
           Depth singularDepth = (depth - 1 + 3 * formerPv) / 2;
           ss->excludedMove = move;
           value = search<NonPV>(pos, ss, singularBeta - 1, singularBeta, singularDepth, cutNode);
