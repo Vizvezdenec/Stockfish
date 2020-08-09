5129aab83a50ed5e19f071a4e227b5f49ff92920
Vizvezdenec <Vizvezdenec@gmail.com>
2020-08-09 04:06:21 +0300

    tweak. bench 4084753

--- src/search.cpp
+++ src/search.cpp
@@ -1240,7 +1240,7 @@ moves_loop: // When in check, search starts from here
 
             // Unless giving check, this capture is likely bad
             if (   !givesCheck
-                && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 161 * depth <= alpha)
+                && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 211 * depth <= alpha)
                 r++;
           }
 
