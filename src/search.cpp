351647574edb3c00762d44bf3feea7a345eb5391
Vizvezdenec <Vizvezdenec@gmail.com>
2020-08-10 10:41:01 +0300

    tweak. bench 4112839

--- src/search.cpp
+++ src/search.cpp
@@ -773,6 +773,8 @@ namespace {
     {
         // Skip early pruning when in check
         ss->staticEval = eval = VALUE_NONE;
+        if (!(ss-2)->inCheck)
+            ss->staticEval = -(ss-1)->staticEval - PieceValue[MG][pos.captured_piece()];
         improving = false;
         goto moves_loop;
     }
