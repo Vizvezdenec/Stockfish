a6acc4d7a0051525d1a9865ad44e05514f1e1e2a
Vizvezdenec <Vizvezdenec@gmail.com>
2020-08-10 03:21:16 +0300

    tweak. bench 4097641

--- src/search.cpp
+++ src/search.cpp
@@ -897,7 +897,7 @@ namespace {
         int probCutCount = 0;
 
         while (   (move = mp.next_move()) != MOVE_NONE
-               && probCutCount < 2 + 2 * cutNode)
+               && probCutCount < 2 + 2 * cutNode + (ss->staticEval >= beta))
             if (move != excludedMove && pos.legal(move))
             {
                 assert(pos.capture_or_promotion(move));
