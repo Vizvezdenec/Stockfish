d83f85527758f518da413c575a019085739c0715
Vizvezdenec <Vizvezdenec@gmail.com>
2020-08-11 02:33:45 +0300

    tweak. bench 4447803

--- src/search.cpp
+++ src/search.cpp
@@ -1208,7 +1208,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode)
-                  r += 2 + (!givesCheck && moveCount > 4 && ss->staticEval < alpha);
+                  r += 2;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
@@ -1242,7 +1242,7 @@ moves_loop: // When in check, search starts from here
             // Unless giving check, this capture is likely bad
             if (   !givesCheck
                 && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 213 * depth <= alpha)
-                r++;
+                r += cutNode;
           }
 
           Depth d = Utility::clamp(newDepth - r, 1, newDepth);
