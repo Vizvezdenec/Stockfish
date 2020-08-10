94d528ca1a6acdca60e5d9a5b244e447ab743997
Vizvezdenec <Vizvezdenec@gmail.com>
2020-08-10 20:06:49 +0300

    tweak. bench 4169601

--- src/search.cpp
+++ src/search.cpp
@@ -1132,7 +1132,7 @@ moves_loop: // When in check, search starts from here
 
       // Castling extension
       if (   type_of(move) == CASTLING
-          && popcount(pos.pieces(us) & ~pos.pieces(PAWN) & (to_sq(move) & KingSide ? KingSide : QueenSide)) <= 3)
+          && popcount(pos.pieces(us) & ~pos.pieces(PAWN, KING) & (to_sq(move) & KingSide ? KingSide : QueenSide)) <= 2)
           extension = 1;
 
       // Late irreversible move extension
@@ -1232,9 +1232,6 @@ moves_loop: // When in check, search starts from here
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
               r -= ss->statScore / 14615;
-
-              if (!ss->inCheck && !(ss-1)->inCheck && !priorCapture && ss->staticEval < -(ss-1)->staticEval - Tempo)
-                  r++;
           }
           else
           {
