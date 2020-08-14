8c3f3821b6c4a4d26581c0c92f0e11f91d82a63e
Vizvezdenec <Vizvezdenec@gmail.com>
2020-08-15 02:18:36 +0300

    tweak. bench 4262029

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -939,9 +939,9 @@ make_v:
 Value Eval::evaluate(const Position& pos) {
 
   bool classical = !Eval::useNNUE
-                ||  (abs(eg_value(pos.psq_score())) >= NNUEThreshold && !pos.captured_piece());
+                ||  abs(eg_value(pos.psq_score())) >= NNUEThreshold;
   Value v = classical ? Evaluation<NO_TRACE>(pos).value()
-                      : NNUE::evaluate(pos) * 5 / 4 + Tempo;
+                      : NNUE::evaluate(pos) * 3 / 2 + Tempo;
 
   // Damp down the evaluation linearly when shuffling
   v = v * (100 - pos.rule50_count()) / 100;
