f6a47aefc878b6ae23faeeceda1dbb1604d1a881
Vizvezdenec <Vizvezdenec@gmail.com>
2020-08-15 15:52:23 +0300

    tweak. bench 4028464

--- src/search.cpp
+++ src/search.cpp
@@ -1200,7 +1200,7 @@ moves_loop: // When in check, search starts from here
               r++;
 
           // Decrease reduction if opponent's move count is high (~5 Elo)
-          if ((ss-1)->moveCount > 13)
+          if ((ss-1)->moveCount > 8 + moveCount / 2)
               r--;
 
           // Decrease reduction if ttMove has been singularly extended (~3 Elo)
