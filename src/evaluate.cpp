9878e9c2e173864ddb6c4712b29ffec6439e347a
Vizvezdenec <Vizvezdenec@gmail.com>
2020-08-16 12:19:33 +0300

    tweak. bench 4353932

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -939,7 +939,10 @@ make_v:
 Value Eval::evaluate(const Position& pos) {
 
   bool classical = !Eval::useNNUE
-                ||  abs(eg_value(pos.psq_score())) >= NNUEThreshold * (20 + pos.rule50_count()) / 20;
+                ||  abs(eg_value(pos.psq_score())) >= NNUEThreshold * (16 + pos.rule50_count()) / 16;
+  if (classical && Eval::useNNUE)
+      classical &= popcount(   (pos.pieces(WHITE, PAWN) & shift<SOUTH>(pos.pieces()))
+                             | (pos.pieces(BLACK, PAWN) & shift<NORTH>(pos.pieces()))) < 10;
   Value v = classical ? Evaluation<NO_TRACE>(pos).value()
                       : NNUE::evaluate(pos) * 5 / 4 + Tempo;
 
