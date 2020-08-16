2c8e4a2e349b7043d4239b1bb1c09d7a8ca0bfb8
Vizvezdenec <Vizvezdenec@gmail.com>
2020-08-16 05:40:08 +0300

    tweak. bench 4919744

--- src/search.cpp
+++ src/search.cpp
@@ -1186,7 +1186,7 @@ moves_loop: // When in check, search starts from here
 
           // Decrease reduction if the ttHit running average is large
           if (thisThread->ttHitAverage > 509 * TtHitAverageResolution * TtHitAverageWindow / 1024)
-              r--;
+              r -= 1 + (ss->staticEval > 2);
 
           // Reduction if other threads are searching this position
           if (th.marked())
