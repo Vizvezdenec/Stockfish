69d62d1af91679c11ce19dafb0b0398a1a59a228
Vizvezdenec <Vizvezdenec@gmail.com>
2020-08-16 06:59:26 +0300

    tweak. bench 4397758

--- src/search.cpp
+++ src/search.cpp
@@ -71,13 +71,13 @@ namespace {
   // Reductions lookup table, initialized at startup
   int Reductions[MAX_MOVES]; // [depth or moveNumber]
 
-  Depth reduction(bool i, Depth d, int mn) {
+  Depth reduction(bool i, bool j, Depth d, int mn) {
     int r = Reductions[d] * Reductions[mn];
-    return (r + 509) / 1024 + (!i && r > 894);
+    return (r + 509) / 1024 + (!i && r > 894 * (1 + j));
   }
 
-  constexpr int futility_move_count(bool improving, bool sImproving, Depth depth) {
-    return (6 + 2 * depth * depth) / (4 - 2 * improving - sImproving);
+  constexpr int futility_move_count(bool improving, Depth depth) {
+    return (3 + depth * depth) / (2 - improving);
   }
 
   // History and stats update bonus, based on depth
@@ -1022,10 +1022,10 @@ moves_loop: // When in check, search starts from here
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
-          moveCountPruning = moveCount >= futility_move_count(improving, sImproving, depth);
+          moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
-          int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount), 0);
+          int lmrDepth = std::max(newDepth - reduction(improving, sImproving, depth, moveCount), 0);
 
           if (   !captureOrPromotion
               && !givesCheck)
@@ -1177,7 +1177,7 @@ moves_loop: // When in check, search starts from here
               || cutNode
               || thisThread->ttHitAverage < 427 * TtHitAverageResolution * TtHitAverageWindow / 1024))
       {
-          Depth r = reduction(improving, depth, moveCount);
+          Depth r = reduction(improving, sImproving, depth, moveCount);
 
           // Decrease reduction at non-check cut nodes for second move at low depths
           if (   cutNode
