d92727d0b431ecde25617d511d6cea7c462d5ec8
Vizvezdenec <Vizvezdenec@gmail.com>
2020-08-17 04:11:46 +0300

    tweak. bench 4316406

--- src/search.cpp
+++ src/search.cpp
@@ -871,7 +871,7 @@ namespace {
         }
     }
 
-    probCutBeta = beta + 176 - 49 * improving;
+    probCutBeta = beta + 176 - 49 * improving + 147;
 
     // Step 10. ProbCut (~10 Elo)
     // If we have a good enough capture and a reduced search returns a value
@@ -903,7 +903,7 @@ namespace {
         int probCutCount = 0;
 
         while (   (move = mp.next_move()) != MOVE_NONE
-               && probCutCount < 2 + 2 * cutNode + (ss->staticEval >= beta + 18 * depth))
+               && probCutCount < 2 + 2 * cutNode)
             if (move != excludedMove && pos.legal(move))
             {
                 assert(pos.capture_or_promotion(move));
