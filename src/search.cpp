210c457f78ecb17d7375c2a1847629eae1eacb18
Vizvezdenec <Vizvezdenec@gmail.com>
2020-08-22 02:24:38 +0300

    tweak. bench 3574896

--- src/search.cpp
+++ src/search.cpp
@@ -1530,6 +1530,9 @@ moves_loop: // When in check, search starts from here
       {
           assert(type_of(move) != ENPASSANT); // Due to !pos.advanced_pawn_push
 
+          if (moveCount > abs(depth) + 1)
+              continue;
+
           futilityValue = futilityBase + PieceValue[EG][pos.piece_on(to_sq(move))];
 
           if (futilityValue <= alpha)
