9e5ef858c98d3ecd72df74dee1c9ea475f2a4c43
Vizvezdenec <Vizvezdenec@gmail.com>
2020-08-22 04:29:00 +0300

    tweak. bench 3775875

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -952,7 +952,7 @@ Value Eval::evaluate(const Position& pos) {
   {
       Bitboard b = pos.pieces(~pos.side_to_move(),QUEEN);
       Square s = pop_lsb(&b);
-      if (pos.pieces(pos.side_to_move(), KNIGHT) & attacks_bb<KNIGHT>(s))
+      if (pos.pieces(pos.side_to_move(), KNIGHT) & attacks_bb<KNIGHT>(s) & ~pos.blockers_for_king(pos.side_to_move()))
           v += Tempo;
   }
 
