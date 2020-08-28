623f6255e6b7c02ef58b970ae4143ab55554765b
Vizvezdenec <Vizvezdenec@gmail.com>
2020-08-28 04:56:18 +0300

    tweak. bench 3797148

--- src/search.cpp
+++ src/search.cpp
@@ -1192,7 +1192,7 @@ moves_loop: // When in check, search starts from here
           {
               // Increase reduction if ttMove is a capture (~5 Elo)
               if (ttCapture && !givesCheck)
-                  r++;
+                  r += 2;
 
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode)
