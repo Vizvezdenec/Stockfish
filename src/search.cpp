ce287b0cef039425d23475725f79e957cc532bc0
Vizvezdenec <Vizvezdenec@gmail.com>
2020-08-30 22:52:46 +0300

    tweak. bench 3680270

--- src/search.cpp
+++ src/search.cpp
@@ -1028,6 +1028,7 @@ moves_loop: // When in check, search starts from here
           {
               // Countermoves based pruning (~20 Elo)
               if (   lmrDepth < 4 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
+                  && !(pos.advanced_pawn_push(move) && pos.non_pawn_material() <= 2 * QueenValueMg)
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
@@ -1043,8 +1044,7 @@ moves_loop: // When in check, search starts from here
                   continue;
 
               // Prune moves with negative SEE (~20 Elo)
-              if (!(pos.advanced_pawn_push(move) && pos.non_pawn_material() <= 2 * QueenValueMg) && 
-                  !pos.see_ge(move, Value(-(29 - std::min(lmrDepth, 18)) * lmrDepth * lmrDepth)))
+              if (!pos.see_ge(move, Value(-(29 - std::min(lmrDepth, 18)) * lmrDepth * lmrDepth)))
                   continue;
           }
           else
