473c8d14d09b1c67c151fc014b1065ea124e1acb
Vizvezdenec <Vizvezdenec@gmail.com>
2020-09-04 17:17:14 +0300

    tweak. bench 3832716

--- src/search.cpp
+++ src/search.cpp
@@ -1051,7 +1051,7 @@ moves_loop: // When in check, search starts from here
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
                   && lmrDepth < 1
-                  && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
+                  && captureHistory[us][movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
 
               // Futility pruning for captures
@@ -1709,7 +1709,7 @@ moves_loop: // When in check, search starts from here
         }
     }
     else
-        captureHistory[moved_piece][to_sq(bestMove)][captured] << bonus1;
+        captureHistory[us][moved_piece][to_sq(bestMove)][captured] << bonus1;
 
     // Extra penalty for a quiet TT or main killer move in previous ply when it gets refuted
     if (   ((ss-1)->moveCount == 1 || ((ss-1)->currentMove == (ss-1)->killers[0]))
@@ -1721,7 +1721,7 @@ moves_loop: // When in check, search starts from here
     {
         moved_piece = pos.moved_piece(capturesSearched[i]);
         captured = type_of(pos.piece_on(to_sq(capturesSearched[i])));
-        captureHistory[moved_piece][to_sq(capturesSearched[i])][captured] << -bonus1;
+        captureHistory[us][moved_piece][to_sq(capturesSearched[i])][captured] << -bonus1;
     }
   }
 
