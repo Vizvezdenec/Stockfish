331fd2dc7188b25bb97ff7a1c694275fea03a233
Vizvezdenec <Vizvezdenec@gmail.com>
2020-09-07 18:37:46 +0300

    tweak. bench 3821971

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -108,7 +108,7 @@ void MovePicker::score() {
                    + 2 * (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + 2 * (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
-                   + (ply < MAX_LPH ? std::min(4, depth / 3) * std::min(4, 7 - ply) * (*lowPlyHistory)[ply][from_to(m)] : 0) / 4;
+                   + (ply < MAX_LPH ? std::min(4, depth / 3) * std::min(4, 7 - ply) * (*lowPlyHistory)[ply][from_to(m)] : 0) / 4 / (1 + (ply > 3));
 
       else // Type == EVASIONS
       {
