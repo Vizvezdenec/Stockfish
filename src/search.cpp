ffef609edfe30a96572d8a0731f2ec841f21dd40
Vizvezdenec <Vizvezdenec@gmail.com>
2020-09-09 23:49:53 +0300

    tweak. bench 4368804

--- src/search.cpp
+++ src/search.cpp
@@ -1233,7 +1233,7 @@ moves_loop: // When in check, search starts from here
                 r++;
 
             ss->statScore = int(PieceValue[MG][pos.captured_piece()]) * 6
-                          + captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] - 2500;
+                          + captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] - 5200;
           }
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
