650dbe02be9b90bfd2355febf9406d3823cbdd9b
Vizvezdenec <Vizvezdenec@gmail.com>
2020-09-18 01:24:43 +0300

    tweak. bench 4034258

--- src/search.cpp
+++ src/search.cpp
@@ -1084,8 +1084,8 @@ moves_loop: // When in check, search starts from here
           &&  tte->depth() >= depth - 3
           && !(ttValue < alpha 
            && !captureOrPromotion
-           && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
-           && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold))
+           && (*contHist[0])[movedPiece][to_sq(move)] < -5000
+           && (*contHist[1])[movedPiece][to_sq(move)] < -5000))
       {
           Value singularBeta = ttValue - ((formerPv + 4) * depth) / 2;
           Depth singularDepth = (depth - 1 + 3 * formerPv) / 2;
