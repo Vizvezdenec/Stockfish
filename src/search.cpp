3ea5a4143634e41d698abbceb4f101f9162cd6c4
Vizvezdenec <Vizvezdenec@gmail.com>
2020-09-19 22:10:17 +0300

    tweak. bench 3233941

--- src/search.cpp
+++ src/search.cpp
@@ -1523,7 +1523,7 @@ moves_loop: // When in check, search starts from here
 
       moveCount++;
 
-      if (captureOrPromotion && !givesCheck && moveCount > 2
+      if (captureOrPromotion && !givesCheck
        && captureHistory[pos.moved_piece(move)][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
           continue;
 
