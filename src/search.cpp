88274e4d30852f17bf34191d276eca87003274da
Vizvezdenec <Vizvezdenec@gmail.com>
2020-09-21 20:04:35 +0300

    tweak. bench 3886020

--- src/search.cpp
+++ src/search.cpp
@@ -1045,8 +1045,7 @@ moves_loop: // When in check, search starts from here
           {
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
-                  && lmrDepth < 1 
-                  + (PieceValue[MG][type_of(movedPiece)] - PieceValue[MG][type_of(pos.piece_on(to_sq(move)))] > RookValueMg)
+                  && lmrDepth < 1
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
 
