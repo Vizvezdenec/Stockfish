d32b733ebcfe5e995f62bc741bf71634ed302a33
Vizvezdenec <Vizvezdenec@gmail.com>
2020-09-29 01:41:42 +0300

    tweak. bench 3786959

--- src/search.cpp
+++ src/search.cpp
@@ -1202,7 +1202,7 @@ moves_loop: // When in check, search starts from here
                              + (*contHist[1])[movedPiece][to_sq(move)]
                              + (*contHist[3])[movedPiece][to_sq(move)]
                              + (*contHist[5])[movedPiece][to_sq(move)] / 2
-                             - 5687;
+                             - 4887;
 
               // Decrease/increase reduction by comparing opponent's stat score (~10 Elo)
               if (ss->statScore >= -106 && (ss-1)->statScore < -104)
