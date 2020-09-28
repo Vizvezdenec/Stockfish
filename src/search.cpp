35ad44cdedb376a342ee981eaa361cc851e74507
Vizvezdenec <Vizvezdenec@gmail.com>
2020-09-29 02:29:59 +0300

    tweak. bench 3679640

--- src/search.cpp
+++ src/search.cpp
@@ -1171,7 +1171,7 @@ moves_loop: // When in check, search starts from here
           if (ss->ttPv)
               r -= 2;
 
-          if (moveCountPruning && !formerPv)
+          if (captLMR && !formerPv)
               r++;
 
           // Decrease reduction if opponent's move count is high (~5 Elo)
