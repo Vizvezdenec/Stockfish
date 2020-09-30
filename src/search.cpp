a8891a85601d7d4bdffe62b73396cff5df7bdd8c
Vizvezdenec <Vizvezdenec@gmail.com>
2020-09-30 07:04:09 +0300

    tweak. bench 3680079

--- src/search.cpp
+++ src/search.cpp
@@ -942,9 +942,8 @@ namespace {
 
     // Step 11. If the position is not in TT, decrease depth by 2
     if (   PvNode
-        && depth >= 6
         && !ttMove)
-        depth -= 2 + (depth > 12);
+        depth -= depth / 4;
 
 moves_loop: // When in check, search starts from here
 
