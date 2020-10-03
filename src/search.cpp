d24d8c854a0b46666c90a26f81cb3decfbeb90bf
Vizvezdenec <Vizvezdenec@gmail.com>
2020-10-03 11:51:58 +0300

    tweak. bench 3398474

--- src/search.cpp
+++ src/search.cpp
@@ -1225,7 +1225,7 @@ moves_loop: // When in check, search starts from here
                   && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 213 * depth <= alpha)
                   r++;
 
-              r -= (captQ > 0);
+              r += (captQ > 1);
           }
 
           Depth d = std::clamp(newDepth - r, 1, newDepth);
