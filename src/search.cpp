49410f995d31825fa105b82837fa9cf2e1460419
Vizvezdenec <Vizvezdenec@gmail.com>
2020-10-11 21:40:31 +0300

    tweak. bench 3901022

--- src/search.cpp
+++ src/search.cpp
@@ -964,7 +964,8 @@ moves_loop: // When in check, search starts from here
 
     value = bestValue;
     singularQuietLMR = moveCountPruning = false;
-    ttCapture = ttMove && pos.capture_or_promotion(ttMove);
+    ttCapture = ttMove && pos.capture_or_promotion(ttMove) 
+             && captureHistory[pos.moved_piece(ttMove)][to_sq(ttMove)][type_of(pos.piece_on(to_sq(ttMove)))] > -6000;
 
     // Mark this node as being searched
     ThreadHolding th(thisThread, posKey, ss->ply);
