b26c7b4708bc71bbf8724a3895900a52a08bc3f0
Vizvezdenec <Vizvezdenec@gmail.com>
2020-11-04 05:52:42 +0300

    tweak. bench 3520576

--- src/search.cpp
+++ src/search.cpp
@@ -1139,7 +1139,7 @@ moves_loop: // When in check, search starts from here
 
       // Step 16. Reduced depth search (LMR, ~200 Elo). If the move fails high it will be
       // re-searched at full depth.
-      if (    depth >= (rootNode ? 4 : cutNode ? 2 : 3)
+      if (    depth >= 3 + rootNode
           &&  moveCount > 1 + 2 * rootNode
           && (  !captureOrPromotion
               || moveCountPruning
