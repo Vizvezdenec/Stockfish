cb1cd327ad5ebc8785299bfc62e37a83ca1f73b6
Vizvezdenec <Vizvezdenec@gmail.com>
2020-11-12 07:11:13 +0300

    tweak. bench 3719934

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1035,6 +1035,9 @@ Value Eval::evaluate(const Position& pos) {
       bool  largePsq = psq * 16 > (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50;
       bool  classical = largePsq || (psq > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB));
 
+      if (pos.count<QUEEN>() == 1 && pos.non_pawn_material() < 5000)
+          classical = false;
+
       v = classical ? Evaluation<NO_TRACE>(pos).value() : adjusted_NNUE();
 
       // If the classical eval is small and imbalance large, use NNUE nevertheless.
--- src/search.cpp
+++ src/search.cpp
@@ -1148,7 +1148,7 @@ moves_loop: // When in check, search starts from here
       // Step 16. Reduced depth search (LMR, ~200 Elo). If the move fails high it will be
       // re-searched at full depth.
       if (    depth >= 3
-          &&  moveCount > 1 + (2 - (thisThread->bestMoveChanges <= 2 && depth > 10)) * rootNode
+          &&  moveCount > 1 + 2 * rootNode
           && (  !captureOrPromotion
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
