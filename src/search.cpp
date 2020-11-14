cb7a75322f797000d1049619b343b3d197e178da
Vizvezdenec <Vizvezdenec@gmail.com>
2020-11-14 09:57:13 +0300

    tweak. bench 3828974

--- src/search.cpp
+++ src/search.cpp
@@ -518,6 +518,7 @@ void Thread::search() {
           {
               totBestMoveChanges += th->bestMoveChanges;
               th->bestMoveChanges = 0;
+              th->bigBMC = 0;
           }
           double bestMoveInstability = 1 + 2 * totBestMoveChanges / Threads.size();
 
@@ -1170,8 +1171,11 @@ moves_loop: // When in check, search starts from here
               r -= 2;
 
           // Increase reduction at root and non-PV nodes when the best move does not change frequently
-          if ((rootNode || !PvNode) && depth > 10 && thisThread->bestMoveChanges <= 2 + (depth > 15))
+          if ((rootNode || !PvNode) && depth > 10 && thisThread->bestMoveChanges <= 2)
               r++;
+
+          if (rootNode && thisThread->bigBMC > 3 && moveCount < 12)
+              r--;
 
           if (moveCountPruning && !formerPv)
               r++;
@@ -1310,6 +1314,8 @@ moves_loop: // When in check, search starts from here
               // the best move changes frequently, we allocate some more time.
               if (moveCount > 1)
                   ++thisThread->bestMoveChanges;
+              if (moveCount > 5)
+                  ++thisThread->bigBMC;
           }
           else
               // All other moves but the PV are set to the lowest value: this
--- src/thread.h
+++ src/thread.h
@@ -61,7 +61,7 @@ public:
   uint64_t ttHitAverage;
   int selDepth, nmpMinPly;
   Color nmpColor;
-  std::atomic<uint64_t> nodes, tbHits, bestMoveChanges;
+  std::atomic<uint64_t> nodes, tbHits, bestMoveChanges, bigBMC;
 
   Position rootPos;
   StateInfo rootState;
