4b893dfbef6d8a3bd7c723f1979ae6553dda1665
Vizvezdenec <Vizvezdenec@gmail.com>
2020-12-01 21:24:22 +0300

    tweak. bench 3744207

--- src/search.cpp
+++ src/search.cpp
@@ -1155,7 +1155,6 @@ moves_loop: // When in check, search starts from here
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
               || cutNode
-              || ttQueenWin
               || thisThread->ttHitAverage < 432 * TtHitAverageResolution * TtHitAverageWindow / 1024))
       {
           Depth r = reduction(improving, depth, moveCount);
@@ -1191,7 +1190,7 @@ moves_loop: // When in check, search starts from here
           {
               // Increase reduction if ttMove is a capture (~5 Elo)
               if (ttCapture)
-                  r++;
+                  r += 1 + ttQueenWin;
 
               // Increase reduction at root if failing high
               r += rootNode ? thisThread->failedHighCnt * thisThread->failedHighCnt * moveCount / 512 : 0;
