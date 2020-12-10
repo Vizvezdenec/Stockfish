25e4aab192cb8e9e0199a24a660e218416c24c6a
Vizvezdenec <Vizvezdenec@gmail.com>
2020-12-10 11:01:13 +0300

    tweak. bench 4122488

--- src/search.cpp
+++ src/search.cpp
@@ -1746,12 +1746,15 @@ moves_loop: // When in check, search starts from here
         && !pos.captured_piece())
             update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -bonus1);
 
+    if (!ss->inCheck)
+    {
     // Decrease stats for all non-best capture moves
     for (int i = 0; i < captureCount; ++i)
     {
         moved_piece = pos.moved_piece(capturesSearched[i]);
         captured = type_of(pos.piece_on(to_sq(capturesSearched[i])));
         captureHistory[moved_piece][to_sq(capturesSearched[i])][captured] << -bonus1;
+    }
     }
   }
 
