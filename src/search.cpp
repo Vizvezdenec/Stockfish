272d6018d4b3288f1ae0127a7f6748926bdee8a4
Vizvezdenec <Vizvezdenec@gmail.com>
2020-12-11 08:23:11 +0300

    tweak. bench 3849656

--- src/search.cpp
+++ src/search.cpp
@@ -1059,7 +1059,7 @@ moves_loop: // When in check, search starts from here
                   continue;
 
               if (   lmrDepth < 1 
-                  && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
+                  && (*contHist[1])[movedPiece][to_sq(move)] + (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
 
               // Futility pruning: parent node (~5 Elo)
