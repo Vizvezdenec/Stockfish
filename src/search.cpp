8df4c117f338f0c6cea353f82654a0fd22ad6946
Vizvezdenec <Vizvezdenec@gmail.com>
2020-12-11 12:26:25 +0300

    tweak. bench 3842919

--- src/search.cpp
+++ src/search.cpp
@@ -1058,10 +1058,6 @@ moves_loop: // When in check, search starts from here
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
 
-              if (   lmrDepth < 1
-                  && (*contHist[0])[movedPiece][to_sq(move)] + (*contHist[1])[movedPiece][to_sq(move)] < -15000)
-                  continue;
-
               // Futility pruning: parent node (~5 Elo)
               if (   lmrDepth < 7
                   && !ss->inCheck
@@ -1215,6 +1211,9 @@ moves_loop: // When in check, search starts from here
           {
               // Increase reduction if ttMove is a capture (~5 Elo)
               if (ttCapture)
+                  r++;
+
+              if (ss->inCheck && type_of(movedPiece) == KING)
                   r++;
 
               // Increase reduction at root if failing high
