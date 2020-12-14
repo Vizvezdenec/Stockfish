ccf2bb1fe0474bccc3de11bff28883f4debc1c55
Vizvezdenec <Vizvezdenec@gmail.com>
2020-12-14 20:45:19 +0300

    tweak. bench 3984628

--- src/search.cpp
+++ src/search.cpp
@@ -1190,7 +1190,7 @@ moves_loop: // When in check, search starts from here
           // Increase reduction at root and non-PV nodes when the best move does not change frequently
           if ((rootNode || !PvNode) && thisThread->rootDepth > 10 && thisThread->bestMoveChanges <= 2)
               r++;
-          else if (rootNode && int(thisThread->bestMoveChanges) > 5)
+          else if (rootNode && int(thisThread->bestMoveChanges) > 6)
               r--;
 
           // More reductions for late moves if position was not in previous PV
