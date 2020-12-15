6dc78e30e593785e0990a81fbdcebe5b6665f029
Vizvezdenec <Vizvezdenec@gmail.com>
2020-12-15 21:28:09 +0300

    tweak. bench 4239612

--- src/search.cpp
+++ src/search.cpp
@@ -1169,7 +1169,7 @@ moves_loop: // When in check, search starts from here
       // Step 16. Reduced depth search (LMR, ~200 Elo). If the move fails high it will be
       // re-searched at full depth.
       if (    depth >= 3
-          &&  moveCount > 1 + std::max(0, 3 - thisThread->iterationCnt) * rootNode
+          &&  moveCount > 1 + std::max(0, 4 - thisThread->iterationCnt) * rootNode
           && (  !captureOrPromotion
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
