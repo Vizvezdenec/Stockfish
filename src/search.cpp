f2ce1d7d1411fce33e24b3f33601ead0db3313fd
Vizvezdenec <Vizvezdenec@gmail.com>
2020-12-17 21:42:26 +0300

    tweak. bench 3824406

--- src/search.cpp
+++ src/search.cpp
@@ -1061,7 +1061,7 @@ moves_loop: // When in check, search starts from here
                   &&  (*contHist[0])[movedPiece][to_sq(move)]
                     + (*contHist[1])[movedPiece][to_sq(move)]
                     + (*contHist[3])[movedPiece][to_sq(move)]
-                    + (*contHist[5])[movedPiece][to_sq(move)] / 2 < 27376)
+                    + (*contHist[5])[movedPiece][to_sq(move)] / 2 < 27376 - 2000 * (move == countermove))
                   continue;
 
               // Prune moves with negative SEE (~20 Elo)
@@ -1759,7 +1759,7 @@ moves_loop: // When in check, search starts from here
     for (int i : {1, 2, 4, 6})
     {
         // Only update first 2 continuation histories if we are in check
-        if (ss->inCheck && i > 1)
+        if (ss->inCheck && i > 2)
             break;
         if (is_ok((ss-i)->currentMove))
             (*(ss-i)->continuationHistory)[pc][to] << bonus;
