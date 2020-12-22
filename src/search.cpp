e29c472c11ba2c489d956ff9a9335322ad088e7f
Vizvezdenec <Vizvezdenec@gmail.com>
2020-12-22 03:10:52 +0300

    tweak. bench 4084332

--- src/search.cpp
+++ src/search.cpp
@@ -1172,6 +1172,7 @@ moves_loop: // When in check, search starts from here
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
               || cutNode
+              || (ss->statScore < -30000 && (ss-1)->statScore > 0)
               || thisThread->ttHitAverage < 432 * TtHitAverageResolution * TtHitAverageWindow / 1024))
       {
           Depth r = reduction(improving, depth, moveCount);
@@ -1224,16 +1225,11 @@ moves_loop: // When in check, search starts from here
                        && !pos.see_ge(reverse_move(move)))
                   r -= 2 + ss->ttPv - (type_of(movedPiece) == PAWN);
 
-              int hist = thisThread->mainHistory[us][from_to(move)]
-                       + (*contHist[0])[movedPiece][to_sq(move)]
-                       + (*contHist[1])[movedPiece][to_sq(move)]
-                       + (*contHist[3])[movedPiece][to_sq(move)]
-                       - 5287;
-
-              if (hist - ss->statScore < -30000)
-                  r++;
-
-              ss->statScore = hist;
+              ss->statScore =  thisThread->mainHistory[us][from_to(move)]
+                             + (*contHist[0])[movedPiece][to_sq(move)]
+                             + (*contHist[1])[movedPiece][to_sq(move)]
+                             + (*contHist[3])[movedPiece][to_sq(move)]
+                             - 5287;
 
               // Decrease/increase reduction by comparing opponent's stat score (~10 Elo)
               if (ss->statScore >= -105 && (ss-1)->statScore < -103)
