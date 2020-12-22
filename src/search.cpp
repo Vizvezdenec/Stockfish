685a256f495353bd5fcb80b785ea17bce73a9176
Vizvezdenec <Vizvezdenec@gmail.com>
2020-12-22 03:09:28 +0300

    tweak. bench 3855191

--- src/search.cpp
+++ src/search.cpp
@@ -1224,11 +1224,16 @@ moves_loop: // When in check, search starts from here
                        && !pos.see_ge(reverse_move(move)))
                   r -= 2 + ss->ttPv - (type_of(movedPiece) == PAWN);
 
-              ss->statScore =  thisThread->mainHistory[us][from_to(move)]
-                             + (*contHist[0])[movedPiece][to_sq(move)]
-                             + (*contHist[1])[movedPiece][to_sq(move)]
-                             + (*contHist[3])[movedPiece][to_sq(move)]
-                             - 5287;
+              int hist = thisThread->mainHistory[us][from_to(move)]
+                       + (*contHist[0])[movedPiece][to_sq(move)]
+                       + (*contHist[1])[movedPiece][to_sq(move)]
+                       + (*contHist[3])[movedPiece][to_sq(move)]
+                       - 5287;
+
+              if (hist - ss->statScore < -30000)
+                  r++;
+
+              ss->statScore = hist;
 
               // Decrease/increase reduction by comparing opponent's stat score (~10 Elo)
               if (ss->statScore >= -105 && (ss-1)->statScore < -103)
