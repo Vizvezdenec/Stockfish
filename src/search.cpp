72559500fdd4fe85418a626b52f44b001c3f005d
Vizvezdenec <Vizvezdenec@gmail.com>
2020-12-26 09:19:42 +0300

    tweak. bench 3938407

--- src/search.cpp
+++ src/search.cpp
@@ -1055,6 +1055,11 @@ moves_loop: // When in check, search starts from here
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
 
+              if (   lmrDepth < 1 && !ss->ttPv
+                  && (*contHist[0])[movedPiece][to_sq(move)] <= CounterMovePruneThreshold
+                  && (*contHist[1])[movedPiece][to_sq(move)] <= CounterMovePruneThreshold)
+                  continue;
+
               // Futility pruning: parent node (~5 Elo)
               if (   lmrDepth < 7
                   && !ss->inCheck
@@ -1216,7 +1221,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode)
-                  r += 3 - 2 * formerPv;
+                  r += 2;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
