23fd4ffa5955ff36fb98a4a0458676c5a07ff4cc
Vizvezdenec <Vizvezdenec@gmail.com>
2020-12-26 13:16:48 +0300

    tweak. bench 4077489

--- src/search.cpp
+++ src/search.cpp
@@ -1203,7 +1203,7 @@ moves_loop: // When in check, search starts from here
 
           // Decrease reduction if ttMove has been singularly extended (~3 Elo)
           if (singularQuietLMR)
-              r -= 1 + (PvNode);
+              r -= 1 + tte->is_pv();
 
           if (!captureOrPromotion)
           {
