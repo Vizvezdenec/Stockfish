51581fbd3bbc629d025d7c01c480b4f458516016
Vizvezdenec <Vizvezdenec@gmail.com>
2020-12-27 18:58:16 +0300

    tweak. bench 4310412

--- src/search.cpp
+++ src/search.cpp
@@ -993,6 +993,7 @@ moves_loop: // When in check, search starts from here
     value = bestValue;
     singularQuietLMR = moveCountPruning = false;
     ttCapture = ttMove && pos.capture_or_promotion(ttMove);
+    bool bestQuiet = false;
 
     // Mark this node as being searched
     ThreadHolding th(thisThread, posKey, ss->ply);
@@ -1167,7 +1168,7 @@ moves_loop: // When in check, search starts from here
       // Step 16. Reduced depth search (LMR, ~200 Elo). If the move fails high it will be
       // re-searched at full depth.
       if (    depth >= 3
-          &&  moveCount > 1 + 2 * rootNode
+          &&  moveCount > 1 + 2 * rootNode + 2 * (PvNode && bestQuiet && std::abs(bestValue) < 2)
           && (  !captureOrPromotion
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
@@ -1203,6 +1204,9 @@ moves_loop: // When in check, search starts from here
 
           // Decrease reduction if ttMove has been singularly extended (~3 Elo)
           if (singularQuietLMR)
+              r--;
+
+          if (PvNode && bestQuiet && std::abs(bestValue) < 2)
               r--;
 
           if (!captureOrPromotion)
@@ -1351,6 +1355,7 @@ moves_loop: // When in check, search starts from here
                   ss->statScore = 0;
                   break;
               }
+              bestQuiet = !captureOrPromotion && !givesCheck;
           }
       }
 
