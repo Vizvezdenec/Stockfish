d8eac251c8e66554c583086dc90c52552fd3188c
Vizvezdenec <Vizvezdenec@gmail.com>
2020-12-28 04:25:28 +0300

    tweak. bench 3733349

--- src/search.cpp
+++ src/search.cpp
@@ -1056,7 +1056,7 @@ moves_loop: // When in check, search starts from here
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
 
-              if (   lmrDepth < 2 + ss->ttPv
+              if (   lmrDepth < 4 - ss->ttPv
                   && ss->inCheck
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
