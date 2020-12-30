42fccf16d1768625253a959297a7c1c90e134ab9
Vizvezdenec <Vizvezdenec@gmail.com>
2020-12-30 03:57:51 +0300

    tweak. bench 4274604

--- src/search.cpp
+++ src/search.cpp
@@ -1221,9 +1221,9 @@ moves_loop: // When in check, search starts from here
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
               // hence break make_move(). (~2 Elo)
-              else if (    type_of(move) == NORMAL
+              else if (    type_of(move) == NORMAL && (PvNode || formerPv || type_of(movedPiece) == QUEEN)
                        && !pos.see_ge(reverse_move(move)))
-                  r -= 2 + ss->ttPv - !(PvNode || formerPv) - (type_of(movedPiece) == PAWN);
+                  r -= 2 + ss->ttPv - (type_of(movedPiece) == PAWN);
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
