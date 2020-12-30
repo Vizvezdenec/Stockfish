d7e6fd7538fe21350002f160fcc4eb1f447f6a18
Vizvezdenec <Vizvezdenec@gmail.com>
2020-12-30 14:21:37 +0300

    tweak. bench 4114593

--- src/search.cpp
+++ src/search.cpp
@@ -1216,7 +1216,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode || formerPv)
-                  r += 2 - formerPv;
+                  r += cutNode ? 2 : !formerPv ? 1 : 0;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
