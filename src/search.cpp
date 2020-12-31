fce3c2905ea92dd46f0ae9e1a16a8a230b6e28a3
Vizvezdenec <Vizvezdenec@gmail.com>
2020-12-31 05:09:36 +0300

    tweak. bench 4283583

--- src/search.cpp
+++ src/search.cpp
@@ -460,7 +460,7 @@ void Thread::search() {
               }
               else if (bestValue >= beta)
               {
-                  alpha += delta / 4;
+                  alpha = (alpha + beta) / 2;
                   beta = std::min(bestValue + delta, VALUE_INFINITE);
                   ++failedHighCnt;
               }
