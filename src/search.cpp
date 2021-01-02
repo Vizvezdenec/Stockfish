6eb77652ae392d45796ce2bd52896fceb3a7a1b0
Vizvezdenec <Vizvezdenec@gmail.com>
2021-01-02 22:41:31 +0300

    tweak. bench 4097959

--- src/search.cpp
+++ src/search.cpp
@@ -893,7 +893,6 @@ namespace {
     // much above beta, we can (almost) safely prune the previous move.
     if (   !PvNode
         &&  depth > 4
-        && (cutNode || !formerPv)
         &&  abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         // if value from transposition table is lower than probCutBeta, don't attempt probCut
         // there and in further interactions with transposition table cutoff depth is set to depth - 3
@@ -921,7 +920,7 @@ namespace {
         ss->ttPv = false;
 
         while (   (move = mp.next_move()) != MOVE_NONE
-               && probCutCount < 2 + 2 * cutNode)
+               && probCutCount < 2 + 2 * cutNode - (!cutNode && formerPv))
             if (move != excludedMove && pos.legal(move))
             {
                 assert(pos.capture_or_promotion(move));
