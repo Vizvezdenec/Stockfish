1ab7715a12887b4060614208172b01c57244ae70
Vizvezdenec <Vizvezdenec@gmail.com>
2021-01-14 23:37:43 +0300

    tweak. bench 4031496

--- src/search.cpp
+++ src/search.cpp
@@ -1158,8 +1158,7 @@ moves_loop: // When in check, search starts from here
           && (  !captureOrPromotion
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
-              || cutNode
-              || (!PvNode && !formerPv && captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 4506)
+              || (!PvNode && !formerPv)
               || thisThread->ttHitAverage < 432 * TtHitAverageResolution * TtHitAverageWindow / 1024))
       {
           Depth r = reduction(improving, depth, moveCount);
