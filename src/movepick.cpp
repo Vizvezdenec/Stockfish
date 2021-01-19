3f0a88ecdb3e8ae58616b13562fb2c17d7c2d527
Vizvezdenec <Vizvezdenec@gmail.com>
2021-01-20 01:35:02 +0300

    tweak. bench 4150413

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -168,7 +168,7 @@ top:
 
   case GOOD_CAPTURE:
       if (select<Best>([&](){
-                       return pos.see_ge(*cur, Value(-69 * cur->value / 1024)) ?
+                       return pos.see_ge(*cur, Value(-77 * cur->value / 1024)) ?
                               // Move losing capture to endBadCaptures to be tried later
                               true : (*endBadCaptures++ = *cur, false); }))
           return *(cur - 1);
@@ -200,7 +200,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -3000 * depth);
+          partial_insertion_sort(cur, endMoves, -3242 * depth);
       }
 
       ++stage;
