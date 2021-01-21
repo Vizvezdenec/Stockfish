3bb86ec1da216e386f693089a35d2efe72464b13
Vizvezdenec <Vizvezdenec@gmail.com>
2021-01-21 18:36:39 +0300

    tweak. bench 3923086

--- src/search.cpp
+++ src/search.cpp
@@ -1198,6 +1198,9 @@ moves_loop: // When in check, search starts from here
               if (   !givesCheck
                   && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 210 * depth <= alpha)
                   r++;
+
+              if (cutNode && !formerPv && captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 0)
+                  r++;
           }
           else
           {
