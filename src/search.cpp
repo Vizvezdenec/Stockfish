c123822f9e8d55fa8ebf1bf32fd0f084b39455a6
Vizvezdenec <Vizvezdenec@gmail.com>
2021-01-25 23:18:58 +0300

    tweak. bench 4835536

--- src/search.cpp
+++ src/search.cpp
@@ -1732,9 +1732,9 @@ moves_loop: // When in check, search starts from here
 
     // Extra penalty for a quiet early move that was not a TT move or
     // main killer move in previous ply when it gets refuted.
-    if (   bestValue >= beta && ((ss-1)->moveCount == 1 + (ss-1)->ttHit || ((ss-1)->currentMove == (ss-1)->killers[0]))
+    if (   ((ss-1)->moveCount == 1 + (ss-1)->ttHit || ((ss-1)->currentMove == (ss-1)->killers[0]))
         && !pos.captured_piece())
-            update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -bonus1);
+            update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -stat_bonus(depth + (bestValue >= beta)));
 
     // Decrease stats for all non-best capture moves
     for (int i = 0; i < captureCount; ++i)
