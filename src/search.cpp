568c9be458d40bbee53235777be330158ff34fc2
Vizvezdenec <Vizvezdenec@gmail.com>
2021-01-28 16:41:02 +0300

    tweak. bench 4168932

--- src/search.cpp
+++ src/search.cpp
@@ -852,7 +852,7 @@ namespace {
         // Null move dynamic reduction based on depth and value
         Depth R = (1015 + 85 * depth) / 256 + std::min(int(eval - beta) / 191, 3);
 
-        if ((ss-1)->currentMove == (ss-1)->killers[0] && (ss-1)->statScore > 0)
+        if ((ss-1)->currentMove == (ss-1)->killers[0])
             R--;
 
         ss->currentMove = MOVE_NULL;
