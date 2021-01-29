c68b9825750c2204115df78b2251cc6817c4a03f
Vizvezdenec <Vizvezdenec@gmail.com>
2021-01-29 12:54:40 +0300

    tweak. bench 4153355

--- src/search.cpp
+++ src/search.cpp
@@ -940,8 +940,8 @@ namespace {
                 // Perform a preliminary qsearch to verify that the move holds
                 value = -qsearch<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1);
 
-                if (depth > 10 && value >= probCutBeta)
-                    value = -search<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1, depth - 10, !cutNode);
+                if (depth > 8 && value >= probCutBeta)
+                    value = -search<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1, depth - 8, !cutNode);
 
                 // If the qsearch held, perform the regular search
                 if (value >= probCutBeta)
