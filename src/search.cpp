1ae06b80db006c2579a45bd70b787458a90fae14
Vizvezdenec <Vizvezdenec@gmail.com>
2021-01-29 20:42:17 +0300

    tweak. bench 3724277

--- src/search.cpp
+++ src/search.cpp
@@ -1211,8 +1211,6 @@ moves_loop: // When in check, search starts from here
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode)
                   r += 2;
-              else if (!PvNode && !formerPv)
-                  r++;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
@@ -1220,6 +1218,9 @@ moves_loop: // When in check, search starts from here
               else if (    type_of(move) == NORMAL
                        && !pos.see_ge(reverse_move(move)))
                   r -= 2 + ss->ttPv - (type_of(movedPiece) == PAWN);
+
+              if (!PvNode && !formerPv)
+                  r++;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
