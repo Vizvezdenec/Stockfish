14d22965f413b4cd01bfad31aa919c861447e369
Vizvezdenec <Vizvezdenec@gmail.com>
2021-01-30 13:25:50 +0300

    tweak. bench 4548164

--- src/search.cpp
+++ src/search.cpp
@@ -965,7 +965,7 @@ namespace {
     if (   PvNode
         && depth >= 6
         && !ttMove)
-        depth = std::max(5, depth - 3);
+        depth = std::max(5, depth - 4);
 
 moves_loop: // When in check, search starts from here
 
