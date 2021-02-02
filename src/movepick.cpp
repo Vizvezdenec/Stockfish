8ef26259591ce4f5618be7d14c436579304ebc04
Vizvezdenec <Vizvezdenec@gmail.com>
2021-02-03 02:41:04 +0300

    tweak. bench 4142509

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -115,7 +115,7 @@ void MovePicker::score() {
       {
           if (pos.capture(m))
               m.value =  PieceValue[MG][pos.piece_on(to_sq(m))]
-                       - Value(type_of(pos.moved_piece(m))) - !pos.see_ge(m, Value(-1)) * (1 << 28);
+                       - Value(type_of(pos.moved_piece(m)));
           else
               m.value =      (*mainHistory)[pos.side_to_move()][from_to(m)]
                        + 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
