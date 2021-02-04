5149d1b3cb703b6152fcf3f6bcae3df6e58d40b6
Vizvezdenec <Vizvezdenec@gmail.com>
2021-02-04 17:44:41 +0300

    tweak. bench 3917995

--- src/search.cpp
+++ src/search.cpp
@@ -953,11 +953,11 @@ namespace {
                 {
                     // if transposition table doesn't have equal or more deep info write probCut data into it
                     if ( !(ss->ttHit
-                       && tte->depth() >= depth - 3
+                       && tte->depth() >= probcutDepth + 1
                        && ttValue != VALUE_NONE))
                         tte->save(posKey, value_to_tt(value, ss->ply), ttPv,
                             BOUND_LOWER,
-                            depth - 3, move, ss->staticEval);
+                            probcutDepth + 1, move, ss->staticEval);
                     return value;
                 }
             }
