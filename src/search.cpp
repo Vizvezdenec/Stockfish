7c4ac60dcff6d39a1926598753db1ffe398a338e
Vizvezdenec <Vizvezdenec@gmail.com>
2021-02-10 12:14:36 +0300

    tweak. bench 4085132

--- src/search.cpp
+++ src/search.cpp
@@ -1219,11 +1219,15 @@ moves_loop: // When in check, search starts from here
                        && !pos.see_ge(reverse_move(move)))
                   r -= 2 + ss->ttPv - (type_of(movedPiece) == PAWN);
 
-              ss->statScore =  thisThread->mainHistory[us][from_to(move)]
-                             + (*contHist[0])[movedPiece][to_sq(move)]
-                             + (*contHist[1])[movedPiece][to_sq(move)]
-                             + (*contHist[3])[movedPiece][to_sq(move)]
-                             - 5337;
+              if (ss->inCheck)
+                  ss->statScore = thisThread->mainHistory[us][from_to(move)]
+                              + (*contHist[0])[movedPiece][to_sq(move)] - 4341;
+              else
+                  ss->statScore =  thisThread->mainHistory[us][from_to(move)]
+                              + (*contHist[0])[movedPiece][to_sq(move)]
+                              + (*contHist[1])[movedPiece][to_sq(move)]
+                              + (*contHist[3])[movedPiece][to_sq(move)]
+                              - 5337;
 
               // Decrease/increase reduction by comparing opponent's stat score (~10 Elo)
               if (ss->statScore >= -89 && (ss-1)->statScore < -116)
@@ -1235,11 +1239,7 @@ moves_loop: // When in check, search starts from here
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
               // If we are not in check use statScore, if we are in check
               // use sum of main history and first continuation history with an offset
-              if (ss->inCheck)
-                  r -= (thisThread->mainHistory[us][from_to(move)]
-                     + (*contHist[0])[movedPiece][to_sq(move)] - 4341) / 16384;
-              else
-                  r -= ss->statScore / 14382;
+              r -= ss->statScore / 14382;
           }
 
           Depth d = std::clamp(newDepth - r, 1, newDepth);
