f089b390dfd521dee62c4abed72f85024ae88f68
Vizvezdenec <Vizvezdenec@gmail.com>
2021-02-11 11:36:44 +0300

    tweak. bench 3971588

--- src/search.cpp
+++ src/search.cpp
@@ -975,9 +975,9 @@ moves_loop: // When in check, search starts from here
 
     Move countermove = thisThread->counterMoves[pos.piece_on(prevSq)][prevSq];
 
-    if (   (*contHist[1])[pos.moved_piece(countermove)][to_sq(countermove)] < -20000
-        && (*contHist[3])[pos.moved_piece(countermove)][to_sq(countermove)] < -20000
-        && (*contHist[5])[pos.moved_piece(countermove)][to_sq(countermove)] < -20000)
+    if (   (*contHist[1])[pos.moved_piece(countermove)][to_sq(countermove)] < -10000
+        && (*contHist[3])[pos.moved_piece(countermove)][to_sq(countermove)] < -10000
+        && (*contHist[5])[pos.moved_piece(countermove)][to_sq(countermove)] < -10000)
         countermove = MOVE_NONE;
 
     MovePicker mp(pos, ttMove, depth, &thisThread->mainHistory,
