cc2a8df04afc882a794a90f39b50be01abb8b3fb
Vizvezdenec <Vizvezdenec@gmail.com>
2021-02-15 12:25:12 +0300

    tweak. bench 3502609

--- src/search.cpp
+++ src/search.cpp
@@ -1068,7 +1068,7 @@ moves_loop: // When in check, search starts from here
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
 
-              if (   lmrDepth < 3
+              if (   lmrDepth < 2
                   && ( ((*contHist[0])[movedPiece][to_sq(move)] == CounterMovePruneThreshold
                     && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                     || ((*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
