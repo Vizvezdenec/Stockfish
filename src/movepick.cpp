d84f60a1da590b0a23f6b0c122bdbc700a8976b5
Vizvezdenec <Vizvezdenec@gmail.com>
2021-02-19 13:41:13 +0300

    tweak. bench 3900703

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -68,7 +68,7 @@ MovePicker::MovePicker(const Position& p, Move ttm, Depth d, const ButterflyHist
 /// MovePicker constructor for quiescence search
 MovePicker::MovePicker(const Position& p, Move ttm, Depth d, const ButterflyHistory* mh,
                        const CapturePieceToHistory* cph, const PieceToHistory** ch, Square rs, bool pc)
-           : pos(p), mainHistory(mh), captureHistory(cph), continuationHistory(ch), ttMove(ttm), recaptureSquare(rs), probcut(pc) , depth(d) {
+           : pos(p), mainHistory(mh), captureHistory(cph), continuationHistory(ch), ttMove(ttm), recaptureSquare(rs), depth(d) , probcut(pc) {
 
   assert(d <= 0);
 
@@ -100,7 +100,7 @@ void MovePicker::score() {
 
   for (auto& m : *this)
       if constexpr (Type == CAPTURES)
-          m.value =  int(PieceValue[MG][pos.piece_on(to_sq(m))]) * (6 + 2 * (probcut && to_sq(m) == recaptureSquare))
+          m.value =  int(PieceValue[MG][pos.piece_on(to_sq(m))]) * (6 + (probcut && to_sq(m) == recaptureSquare))
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if constexpr (Type == QUIETS)
