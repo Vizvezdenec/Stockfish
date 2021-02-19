fea161050f9cf93db0dc85210258d9a8da827e0f
Vizvezdenec <Vizvezdenec@gmail.com>
2021-02-19 22:19:21 +0300

    tweak. bench 3653317

--- src/search.cpp
+++ src/search.cpp
@@ -1208,7 +1208,7 @@ moves_loop: // When in check, search starts from here
                   && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 210 * depth <= alpha)
                   r++;
 
-              r -= ss->statScore / 16384;
+              r -= (ss->statScore + 6 * PieceValue[MG][pos.captured_piece()]) / 32768;
           }
           else
           {
