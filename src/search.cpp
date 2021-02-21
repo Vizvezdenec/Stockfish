5e2d67fc23681e8cb86c903931fdaf3bc604becb
Vizvezdenec <Vizvezdenec@gmail.com>
2021-02-21 13:21:50 +0300

    tweak. bench 3713388

--- src/search.cpp
+++ src/search.cpp
@@ -1753,7 +1753,7 @@ moves_loop: // When in check, search starts from here
     }
     else
     {
-        bool goodCapt = ss->staticEval + PieceValue[EG][captured] + 300 < bestValue;
+        bool goodCapt = ss->staticEval + PieceValue[EG][captured] * 2 < bestValue;
         // Increase stats for the best move in case it was a capture move
         captureHistory[moved_piece][to_sq(bestMove)][captured] << (goodCapt ? std::max(stat_bonus(depth + 2), bonus1) : bonus1);
     }
