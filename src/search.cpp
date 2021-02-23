5b7ba5d0644850e0cde0842bbb2d765cd4237b6e
Vizvezdenec <Vizvezdenec@gmail.com>
2021-02-23 03:41:12 +0300

    tweak. bench 3704948

--- src/search.cpp
+++ src/search.cpp
@@ -1060,6 +1060,9 @@ moves_loop: // When in check, search starts from here
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
           moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
+          if (captureOrPromotion && !givesCheck && captureCount >= futility_move_count(improving, depth))
+              continue;
+
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount), 0);
 
