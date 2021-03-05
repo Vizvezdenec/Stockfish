585e01fba9ce7c4f7f98c3d7bf57a66f6d265a0e
Vizvezdenec <Vizvezdenec@gmail.com>
2021-03-06 01:59:53 +0300

    tweak. bench 4690576

--- src/search.cpp
+++ src/search.cpp
@@ -1184,7 +1184,7 @@ moves_loop: // When in check, search starts from here
       // cases where we extend a son if it has good chances to be "interesting".
       if (    depth >= 3
           &&  moveCount > 1 + 2 * rootNode
-          && bestValue > VALUE_TB_LOSS_IN_MAX_PLY
+          && (bestValue > VALUE_TB_LOSS_IN_MAX_PLY || ss->staticEval < 0)
           && (  !captureOrPromotion
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
