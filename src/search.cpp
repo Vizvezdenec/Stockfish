79a9ba70d8cf5210eeb6db734cea9b8464fd3e98
Vizvezdenec <Vizvezdenec@gmail.com>
2021-03-07 22:36:15 +0300

    tweak. bench 4775393

--- src/search.cpp
+++ src/search.cpp
@@ -1191,7 +1191,7 @@ moves_loop: // When in check, search starts from here
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
               || cutNode
               || (!PvNode && !formerPv && captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 3678)
-              || (PvNode && ss->distanceFromRoot >= ss->ply * 24 && captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 0)
+              || (PvNode && ss->distanceFromRoot > ss->ply * 24)
               || thisThread->ttHitAverage < 432 * TtHitAverageResolution * TtHitAverageWindow / 1024))
       {
           Depth r = reduction(improving, depth, moveCount);
