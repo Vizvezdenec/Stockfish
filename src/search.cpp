45cdc9bdb84dbdab5369431821682e5a4e303052
Vizvezdenec <Vizvezdenec@gmail.com>
2021-03-07 04:07:42 +0300

    tweak. bench 5109200

--- src/search.cpp
+++ src/search.cpp
@@ -851,7 +851,7 @@ namespace {
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and value
-        Depth R = (1112 + 68 * depth - 200 * formerPv) / 256 + std::min(int(eval - beta) / 190, 3);
+        Depth R = (1062 + 68 * depth - 100 * formerPv) / 256 + std::min(int(eval - beta) / 190, 3);
 
         ss->currentMove = MOVE_NULL;
         ss->continuationHistory = &thisThread->continuationHistory[0][0][NO_PIECE][0];
