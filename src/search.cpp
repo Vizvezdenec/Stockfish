06ecd509f5cd5e78b4a9c1d511450385c8458e0d
Vizvezdenec <Vizvezdenec@gmail.com>
2021-03-13 16:53:23 +0300

    tweak. bench 4799665

--- src/search.cpp
+++ src/search.cpp
@@ -1369,7 +1369,7 @@ moves_loop: // When in check, search starts from here
           {
               bestMove = move;
 
-              gbv = value > alpha + PawnValueMg;
+              gbv = value > alpha + PawnValueMg && value >= beta;
 
               if (PvNode && !rootNode) // Update pv even in fail-high case
                   update_pv(ss->pv, move, (ss+1)->pv);
