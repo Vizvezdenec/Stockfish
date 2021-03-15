3ab27d085edbdf631f1fb089c0c7c005a2d193c4
Vizvezdenec <Vizvezdenec@gmail.com>
2021-03-16 00:17:25 +0300

    tweak. bench 4476047

--- src/search.cpp
+++ src/search.cpp
@@ -1178,7 +1178,7 @@ moves_loop: // When in check, search starts from here
       // Step 15. Make the move
       pos.do_move(move, st, givesCheck);
 
-      (ss+1)->distanceFromPv = ss->distanceFromPv + moveCount - 1;
+      (ss+1)->distanceFromPv = ss->distanceFromPv + moveCount - bool(ttMove);
 
       // Step 16. Late moves reduction / extension (LMR, ~200 Elo)
       // We use various heuristics for the sons of a node after the first son has
@@ -1277,9 +1277,7 @@ moves_loop: // When in check, search starts from here
           // In general we want to cap the LMR depth search at newDepth. But for nodes
           // close to the principal variation the cap is at (newDepth + 1), which will
           // allow these nodes to be searched deeper than the pv (up to 4 plies deeper).
-          Depth d = std::clamp(newDepth - r, 1, newDepth 
-                             + ((ss+1)->distanceFromPv <= 4 + (captureOrPromotion 
-                                                            && captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] > 8000)));
+          Depth d = std::clamp(newDepth - r, 1, newDepth + ((ss+1)->distanceFromPv <= 6));
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
