e5c66c84764cf00a73e7ad33540ca7bddf71c90b
Vizvezdenec <Vizvezdenec@gmail.com>
2021-03-16 00:44:46 +0300

    tweak. bench 4677095

--- src/search.cpp
+++ src/search.cpp
@@ -834,7 +834,7 @@ namespace {
 
     // Step 7. Futility pruning: child node (~50 Elo)
     if (   !PvNode
-        &&  depth < 9 + tte->depth()
+        &&  depth < 9 + tte->depth() / 2
         &&  eval - futility_margin(depth, improving) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;
