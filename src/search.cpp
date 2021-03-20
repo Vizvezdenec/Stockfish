4572ca9fee396dcba2ed87ce46d0932d1b350870
Vizvezdenec <Vizvezdenec@gmail.com>
2021-03-20 18:58:29 +0300

    tweak. bench 4856477

--- src/search.cpp
+++ src/search.cpp
@@ -1225,7 +1225,7 @@ moves_loop: // When in check, search starts from here
           if (singularQuietLMR)
               r--;
 
-          if (rootNode && thisThread->bestMoveMc > 4 * depth)
+          if (rootNode && thisThread->bestMoveMc > 12 * depth)
               r--;
 
           if (captureOrPromotion)
