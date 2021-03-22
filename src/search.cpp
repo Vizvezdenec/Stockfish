4b810a00129a7d3e6b0e55e824d574f2d122c0af
Vizvezdenec <Vizvezdenec@gmail.com>
2021-03-22 14:23:22 +0300

    tweak. bench 4790826

--- src/search.cpp
+++ src/search.cpp
@@ -1264,6 +1264,8 @@ moves_loop: // When in check, search starts from here
               else if ((ss-1)->statScore >= -112 && ss->statScore < -100)
                   r++;
 
+              r -= (ss->statScore - (ss-1)->statScore) / 65536;
+
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
               // If we are not in check use statScore, but if we are in check we use
               // the sum of main history and first continuation history with an offset.
