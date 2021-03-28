775cdd5c59467be0fc2ca63303260dd1f74a9acf
Vizvezdenec <Vizvezdenec@gmail.com>
2021-03-28 12:42:45 +0300

    tweak. bench 4389035

--- src/search.cpp
+++ src/search.cpp
@@ -619,6 +619,8 @@ namespace {
     bestValue          = -VALUE_INFINITE;
     maxValue           = VALUE_INFINITE;
     ss->distanceFromPv = (PvNode ? 0 : ss->distanceFromPv);
+    if (rootNode)
+        ss->distanceFromMainPv = 0;
 
     // Check for the available remaining time
     if (thisThread == Threads.main())
@@ -1155,9 +1157,7 @@ moves_loop: // When in check, search starts from here
       }
 
       // Check extension (~2 Elo)
-      else if (!cutNode || formerPv)
-      {
-      if (    givesCheck
+      else if (    givesCheck
                && (pos.is_discovered_check_on_king(~us, move) || pos.see_ge(move)))
           extension = 1;
 
@@ -1165,7 +1165,6 @@ moves_loop: // When in check, search starts from here
       else if (   PieceValue[EG][pos.captured_piece()] > PawnValueEg
                && pos.non_pawn_material() <= 2 * RookValueMg)
           extension = 1;
-      }
 
       // Add extension to new depth
       newDepth += extension;
@@ -1184,6 +1183,7 @@ moves_loop: // When in check, search starts from here
       pos.do_move(move, st, givesCheck);
 
       (ss+1)->distanceFromPv = ss->distanceFromPv + moveCount - 1;
+      (ss+1)->distanceFromMainPv = ss->distanceFromMainPv + moveCount - 1;
 
       // Step 16. Late moves reduction / extension (LMR, ~200 Elo)
       // We use various heuristics for the sons of a node after the first son has
@@ -1286,7 +1286,7 @@ moves_loop: // When in check, search starts from here
           // In general we want to cap the LMR depth search at newDepth. But for nodes
           // close to the principal variation the cap is at (newDepth + 1), which will
           // allow these nodes to be searched deeper than the pv (up to 4 plies deeper).
-          Depth d = std::clamp(newDepth - r, 1, newDepth + ((ss+1)->distanceFromPv <= 4));
+          Depth d = std::clamp(newDepth - r, 1, newDepth + ((ss+1)->distanceFromPv <= 4 || (PvNode && (ss+1)->distanceFromMainPv < 7)));
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
--- src/search.h
+++ src/search.h
@@ -50,6 +50,7 @@ struct Stack {
   int statScore;
   int moveCount;
   int distanceFromPv;
+  int distanceFromMainPv;
   bool inCheck;
   bool ttPv;
   bool ttHit;
