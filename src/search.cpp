69c34de06d086a4f8a52a5847989e1d29826150d
Vizvezdenec <Vizvezdenec@gmail.com>
2021-04-01 16:47:27 +0300

    tweak. bench 4457830

--- src/search.cpp
+++ src/search.cpp
@@ -1770,7 +1770,7 @@ moves_loop: // When in check, search starts from here
     }
     else
         // Increase stats for the best move in case it was a capture move
-        captureHistory[moved_piece][to_sq(bestMove)][captured] << bonus1 / (1 + !bcStatsU);
+        captureHistory[moved_piece][to_sq(bestMove)][captured] << (bcStatsU ? bonus1 : bonus2);
 
     // Extra penalty for a quiet early move that was not a TT move or
     // main killer move in previous ply when it gets refuted.
