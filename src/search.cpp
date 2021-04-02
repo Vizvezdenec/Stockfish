98c2e59682ab9933051648e92a3de84ce5a539e1
Vizvezdenec <Vizvezdenec@gmail.com>
2021-04-02 05:35:48 +0300

    tweak. bench 4465954

--- src/search.cpp
+++ src/search.cpp
@@ -834,7 +834,7 @@ namespace {
 
     if (   !rootNode
         && eval < ss->staticEval
-        && ss->staticEval < alpha - 2000
+        && ss->staticEval < alpha - 3000
         && depth > 1)
         depth--;
 
