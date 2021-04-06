526d9f82d845fd8e5d0fd1a890110405a669ab35
Vizvezdenec <Vizvezdenec@gmail.com>
2021-04-06 18:38:11 +0300

    tweak. bench 4714496

--- src/search.cpp
+++ src/search.cpp
@@ -1090,7 +1090,6 @@ moves_loop: // When in check, search starts from here
 
               // Futility pruning: parent node (~5 Elo)
               if (   lmrDepth < 7
-                  && lmrDepth < newDepth
                   && !ss->inCheck
                   && ss->staticEval + 174 + 157 * lmrDepth <= alpha
                   &&  (*contHist[0])[movedPiece][to_sq(move)]
@@ -1249,7 +1248,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode)
-                  r += 2;
+                  r += 2 + (bestValue <= alpha - PawnValueMg);
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
