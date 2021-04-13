c9c0ccaa713da2d940a97c263c648e700f3aeb50
Vizvezdenec <Vizvezdenec@gmail.com>
2021-04-13 10:21:40 +0300

    tweak. bench 4653762

--- src/search.cpp
+++ src/search.cpp
@@ -1290,7 +1290,7 @@ moves_loop: // When in check, search starts from here
       }
       else
       {
-          doFullDepthSearch = !PvNode || moveCount > 1 + 2 * rootNode;
+          doFullDepthSearch = !PvNode || moveCount > 1;
           didLMR = false;
       }
 
@@ -1312,7 +1312,7 @@ moves_loop: // When in check, search starts from here
       // For PV nodes only, do a full PV search on the first move or after a fail
       // high (in the latter case search only if value < beta), otherwise let the
       // parent node fail low with value <= alpha and try another move.
-      if (PvNode && (moveCount <= 1 + 2 * rootNode || (value > alpha && (rootNode || value < beta))))
+      if (PvNode && (moveCount == 1 || (value > alpha && (ss->ply < 5 || value < beta))))
       {
           (ss+1)->pv = pv;
           (ss+1)->pv[0] = MOVE_NONE;
