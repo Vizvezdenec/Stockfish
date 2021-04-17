619a3d3aec119a7b64c72f93543c0f7a991e0136
Vizvezdenec <Vizvezdenec@gmail.com>
2021-04-17 17:49:00 +0300

    tweak. bench 4511354

--- src/search.cpp
+++ src/search.cpp
@@ -1091,10 +1091,10 @@ moves_loop: // When in check, search starts from here
               if (   lmrDepth < 7
                   && !ss->inCheck
                   && ss->staticEval + 174 + 157 * lmrDepth <= alpha
-                  &&  ((*contHist[0])[movedPiece][to_sq(move)]
+                  && ((*contHist[0])[movedPiece][to_sq(move)]
                     + (*contHist[1])[movedPiece][to_sq(move)]
                     + (*contHist[3])[movedPiece][to_sq(move)]
-                    + (*contHist[5])[movedPiece][to_sq(move)] / 3 < 28255 || !pos.see_ge(move, -PieceValue[MG][movedPiece] + Value(1))))
+                    + (*contHist[5])[movedPiece][to_sq(move)] / 3 < 28255 || (*contHist[0])[movedPiece][to_sq(move)] < 0))
                   continue;
 
               // Prune moves with negative SEE (~20 Elo)
