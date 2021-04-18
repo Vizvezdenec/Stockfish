13dbb74191b7062ee3e4474e71e33e24ecb92813
Vizvezdenec <Vizvezdenec@gmail.com>
2021-04-18 18:34:14 +0300

    tweak. bench 4564613

--- src/search.cpp
+++ src/search.cpp
@@ -1077,7 +1077,7 @@ moves_loop: // When in check, search starts from here
 
               if (   !givesCheck
                   && depth < 2
-                  && ss->staticEval + PieceValue[EG][pos.piece_on(to_sq(move))] + 782 < alpha)
+                  && ss->staticEval + PieceValue[EG][pos.piece_on(to_sq(move))] + 1082 < alpha)
                   continue;
 
               // SEE based pruning
