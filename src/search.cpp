b7d2271b6ccf260dbbf5a78de02731406ced042e
Vizvezdenec <Vizvezdenec@gmail.com>
2021-04-19 07:37:02 +0300

    tweak. bench 4430089

--- src/search.cpp
+++ src/search.cpp
@@ -1222,9 +1222,6 @@ moves_loop: // When in check, search starts from here
           if (singularQuietLMR)
               r--;
 
-          if (!pos.non_pawn_material(~us))
-              r--;
-
           if (captureOrPromotion)
           {
               // Increase reduction for non-checking captures likely to be bad
@@ -1243,7 +1240,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode)
-                  r += 2;
+                  r += 3 - 2 * formerPv;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
