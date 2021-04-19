f9d93180e423022d377cd2f3decf733676f7c6ff
Vizvezdenec <Vizvezdenec@gmail.com>
2021-04-20 02:08:43 +0300

    tweak. bench 4503918

--- src/search.cpp
+++ src/search.cpp
@@ -1082,7 +1082,7 @@ moves_loop: // When in check, search starts from here
           else
           {
               // Countermoves based pruning (~20 Elo)
-              if (   lmrDepth < 4 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
+              if (   lmrDepth + th.marked() < 4 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
