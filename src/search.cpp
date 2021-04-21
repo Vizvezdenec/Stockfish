43ff56f5656b6c6d05bf75aa17ebfd572e7f9786
Vizvezdenec <Vizvezdenec@gmail.com>
2021-04-21 23:56:34 +0300

    tweak. bench 4291506

--- src/search.cpp
+++ src/search.cpp
@@ -1005,6 +1005,7 @@ moves_loop: // When in check, search starts from here
 
     value = bestValue;
     singularQuietLMR = moveCountPruning = false;
+    int extcnt = 0;
 
     // Mark this node as being searched
     ThreadHolding th(thisThread, posKey, ss->ply);
@@ -1151,8 +1152,6 @@ moves_loop: // When in check, search starts from here
               if (value >= beta)
                   return beta;
           }
-          else if (!captureOrPromotion && singularBeta >= alpha)
-              singularQuietLMR = true;
       }
 
       // Check extension (~2 Elo)
@@ -1277,12 +1276,14 @@ moves_loop: // When in check, search starts from here
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
           // to be searched deeper than the first move.
-          Depth d = std::clamp(newDepth - r, 1, newDepth + (r < -1 && moveCount <= 5));
+          Depth d = std::clamp(newDepth - r, 1, newDepth + (r < -1 && moveCount <= 5 && extcnt == 0));
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
           // If the son is reduced and fails high it will be re-searched at full depth
           doFullDepthSearch = value > alpha && d < newDepth;
+          if (d > newDepth)
+              extcnt++;
           didLMR = true;
       }
       else
