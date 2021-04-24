dbc6a5c0ffdfcb818db3f9d8a81a087785800acf
Vizvezdenec <Vizvezdenec@gmail.com>
2021-04-24 19:45:32 +0300

    tweak. bench 4409369

--- src/search.cpp
+++ src/search.cpp
@@ -609,7 +609,6 @@ namespace {
          ttCapture, singularQuietLMR;
     Piece movedPiece;
     int moveCount, captureCount, quietCount;
-    ss->pivi = PvNode;
 
     // Step 1. Initialize node
     Thread* thisThread = pos.this_thread();
@@ -853,7 +852,7 @@ namespace {
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and value
-        Depth R = (1062 + 68 * depth) / 256 + std::min(int(eval - beta) / 190, 3);
+        Depth R = (1062 + 68 * depth) / 256 + std::min(int(eval - beta) / 190, 3 + cutNode);
 
         ss->currentMove = MOVE_NULL;
         ss->continuationHistory = &thisThread->continuationHistory[0][0][NO_PIECE][0];
@@ -1241,7 +1240,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode)
-                  r += 2 - 2 * (ss-1)->pivi;
+                  r += 2;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
