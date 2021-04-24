ddd2778177dddfbdd1e5b4bf84bdf523d92ba306
Vizvezdenec <Vizvezdenec@gmail.com>
2021-04-24 17:49:12 +0300

    tweak. bench 4182318

--- src/search.cpp
+++ src/search.cpp
@@ -609,6 +609,7 @@ namespace {
          ttCapture, singularQuietLMR;
     Piece movedPiece;
     int moveCount, captureCount, quietCount;
+    ss->pivi = PvNode;
 
     // Step 1. Initialize node
     Thread* thisThread = pos.this_thread();
@@ -1240,7 +1241,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode)
-                  r += 2;
+                  r += 2 - (ss-1)->pivi;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
