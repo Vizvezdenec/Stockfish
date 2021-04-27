e40da5759277e16244dc5218b29083bf4b467a59
Vizvezdenec <Vizvezdenec@gmail.com>
2021-04-28 01:25:08 +0300

    tweak. bench 4953272

--- src/search.cpp
+++ src/search.cpp
@@ -964,12 +964,13 @@ namespace {
          ss->ttPv = ttPv;
     }
 
-moves_loop: // When in check, search starts from here
-
     // Step 10. If the position is not in TT, decrease depth by 2
     if (   PvNode
+        && depth >= 6
         && !ttMove)
-        depth -= depth >= 8 ? 2 : depth >= 2 ? 1 : 0;
+        depth -= depth > 11 ? 3 : 2;
+
+moves_loop: // When in check, search starts from here
 
     ttCapture = ttMove && pos.capture_or_promotion(ttMove);
 
