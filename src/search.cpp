5a944e5da8fe76c756f9f575ecea861c2932438d
Vizvezdenec <Vizvezdenec@gmail.com>
2021-05-02 07:49:59 +0300

    tweak. bench 4540591

--- src/search.cpp
+++ src/search.cpp
@@ -1282,7 +1282,7 @@ moves_loop: // When in check, search starts from here
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
-          if (d > newDepth && value > alpha && r < -5)
+          if (d > newDepth && value > alpha && r < -6)
               newDepth = d;
 
           // If the son is reduced and fails high it will be re-searched at full depth
