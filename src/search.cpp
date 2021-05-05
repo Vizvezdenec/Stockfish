f34bab0025f92241043a5081585ef7a54eed58e6
Vizvezdenec <Vizvezdenec@gmail.com>
2021-05-05 17:18:31 +0300

    tweak. bench 3945757

--- src/search.cpp
+++ src/search.cpp
@@ -1183,7 +1183,7 @@ moves_loop: // When in check, search starts from here
               || cutNode
               || (!PvNode && !formerPv && captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 3678)
               || thisThread->ttHitAverage < 432 * TtHitAverageResolution * TtHitAverageWindow / 1024)
-          && (!PvNode || ss->ply > 1 + (thisThread->id() % 8 == 7) || thisThread->id() % 4 != 3))
+          && (!PvNode || ss->ply > 1 || thisThread->id() % 4 != 3))
       {
           Depth r = reduction(improving, depth, moveCount);
 
@@ -1226,6 +1226,9 @@ moves_loop: // When in check, search starts from here
               if (   !givesCheck
                   && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 210 * depth <= alpha)
                   r++;
+
+          if (givesCheck && type_of(movedPiece) == QUEEN)
+              r--;
           }
           else
           {
