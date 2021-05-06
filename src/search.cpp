5c9b636a9c87035e9b788b18b00cdffba134333d
Vizvezdenec <Vizvezdenec@gmail.com>
2021-05-06 18:46:54 +0300

    tweak. bench 3927646

--- src/search.cpp
+++ src/search.cpp
@@ -1414,7 +1414,7 @@ moves_loop: // When in check, search starts from here
                          quietsSearched, quietCount, capturesSearched, captureCount, depth);
 
     // Bonus for prior countermove that caused the fail low
-    else if (   (depth >= 3 || PvNode || (ss-1)->moveCount > 7 * (3 - depth))
+    else if (   (PvNode || (ss-1)->moveCount > 8 * (4 - depth))
              && !priorCapture)
         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth));
 
