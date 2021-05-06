6e584694c13c7b9170aef4fb66e423b059845a9b
Vizvezdenec <Vizvezdenec@gmail.com>
2021-05-06 21:56:23 +0300

    tweak. bench 3832859

--- src/search.cpp
+++ src/search.cpp
@@ -717,8 +717,8 @@ namespace {
                 thisThread->mainHistory[us][from_to(ttMove)] << penalty;
                 update_continuation_histories(ss, pos.moved_piece(ttMove), to_sq(ttMove), penalty);
 
-                if (!priorCapture && depth >= 3)
-                    update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth));
+                if (!priorCapture && depth >= 6)
+                    update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth - 5));
             }
         }
 
