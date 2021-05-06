330d4cb57e21acf662999039cf0e46f2b6454f56
Vizvezdenec <Vizvezdenec@gmail.com>
2021-05-06 06:05:00 +0300

    tweak. bench 4007926

--- src/search.cpp
+++ src/search.cpp
@@ -1404,14 +1404,9 @@ moves_loop: // When in check, search starts from here
     assert(moveCount || !ss->inCheck || excludedMove || !MoveList<LEGAL>(pos).size());
 
     if (!moveCount)
-    {
         bestValue = excludedMove ? alpha :
                     ss->inCheck  ? mated_in(ss->ply)
                                  : VALUE_DRAW;
-        if (bestValue <= alpha && (depth >= 3 || PvNode)
-             && !priorCapture)
-            update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth));
-    }
 
     // If there is a move which produces search value greater than alpha we update stats of searched moves
     else if (bestMove)
@@ -1420,7 +1415,7 @@ moves_loop: // When in check, search starts from here
 
     // Bonus for prior countermove that caused the fail low
     else if (   (depth >= 3 || PvNode)
-             && !priorCapture)
+             && !priorCapture && !excludedMove)
         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth));
 
     if (PvNode)
