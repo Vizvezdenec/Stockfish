651808087791e8061189fdabcdffad3ce91714e8
Vizvezdenec <Vizvezdenec@gmail.com>
2021-05-10 13:44:02 +0300

    tweak. bench 3511014

--- src/search.cpp
+++ src/search.cpp
@@ -1090,10 +1090,6 @@ moves_loop: // When in check, search starts from here
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
 
-              if (   lmrDepth < 3 && icNoCapt 
-                  && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
-                  continue;
-
               // Futility pruning: parent node (~5 Elo)
               if (   lmrDepth < 7
                   && !ss->inCheck
@@ -1226,6 +1222,9 @@ moves_loop: // When in check, search starts from here
           // Decrease reduction if ttMove has been singularly extended (~3 Elo)
           if (singularQuietLMR)
               r--;
+
+          if (icNoCapt)
+              r += 2;
 
           if (captureOrPromotion)
           {
