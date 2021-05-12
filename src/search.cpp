62f345e4bd6daf0dc44240ee5f7edbc9cb18191e
Vizvezdenec <Vizvezdenec@gmail.com>
2021-05-13 01:37:28 +0300

    tweak. bench 3713156

--- src/search.cpp
+++ src/search.cpp
@@ -1083,7 +1083,7 @@ moves_loop: // When in check, search starts from here
           {
               extension = 1;
               singularQuietLMR = !ttCapture;
-              if (!PvNode && value < singularBeta - 140)
+              if (!PvNode && value < singularBeta - PawnValueMg)
                   extension = 2;
           }
 
