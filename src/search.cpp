9bbfdd3e9f1663f005c7401e83edba1a15fab1c7
Vizvezdenec <Vizvezdenec@gmail.com>
2021-05-19 23:06:19 +0300

    tweak. bench 3986160

--- src/search.cpp
+++ src/search.cpp
@@ -1120,7 +1120,7 @@ moves_loop: // When in check, search starts from here
       // cases where we extend a son if it has good chances to be "interesting".
       if (    depth >= 3
           &&  moveCount > 1 + 2 * rootNode
-          && !(PvNode && ss->inCheck && (*contHist[0])[movedPiece][to_sq(move)] > 21000)
+          && !(PvNode && ss->inCheck && (*contHist[0])[movedPiece][to_sq(move)] > 24000)
           && (  !captureOrPromotion
               || cutNode
               || (!PvNode && !formerPv))
