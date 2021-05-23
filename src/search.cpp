6a1ee7bc5b98db9d40999369427fa5fb3037d749
Vizvezdenec <Vizvezdenec@gmail.com>
2021-05-24 02:02:52 +0300

    tweak. bench 3539038

--- src/search.cpp
+++ src/search.cpp
@@ -1160,7 +1160,10 @@ moves_loop: // When in check, search starts from here
           {
               // Increase reduction if ttMove is a capture (~3 Elo)
               if (ttCapture)
-                  r += 1 + ttCheck;
+                  r++;
+
+              if (ttCheck && !givesCheck)
+                  r++;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
