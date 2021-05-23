6aaae74bf8142932f7df33b3c4357e74b9588574
Vizvezdenec <Vizvezdenec@gmail.com>
2021-05-23 22:43:09 +0300

    tweak. bench 4166540

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -177,7 +177,7 @@ namespace Stockfish::Eval::NNUE {
 
       int sum = (A * materialist + B * positional) / 128;
 
-      if (sum * positional < 0 && std::abs(sum) <= std::abs(positional) / 16)
+      if (sum * positional < 0 && std::abs(sum) <= std::abs(positional) / 8)
           sum = 0;
 
       return static_cast<Value>( sum / OutputScale );
