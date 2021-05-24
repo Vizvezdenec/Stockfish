c953e168204f4b8e221eb529283171ee84bd2204
Vizvezdenec <Vizvezdenec@gmail.com>
2021-05-24 09:03:34 +0300

    tweak. bench 4417920

--- src/search.cpp
+++ src/search.cpp
@@ -1176,7 +1176,7 @@ moves_loop: // When in check, search starts from here
                   r -= ss->statScore / 14721;
 
               if (rootNode)
-                  r += thisThread->failedHighCnt * moveCount / 64;
+                  r += thisThread->failedHighCnt * moveCount / 128;
           }
 
           // In general we want to cap the LMR depth search at newDepth. But if
