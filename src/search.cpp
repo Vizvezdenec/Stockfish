a429e07ce14662f46aea2b18ba105bb9b8f71048
Vizvezdenec <Vizvezdenec@gmail.com>
2021-05-30 07:21:01 +0300

    tweak. bench 5224293

--- src/search.cpp
+++ src/search.cpp
@@ -1077,7 +1077,7 @@ moves_loop: // When in check, search starts from here
           {
               extension = 1;
               singularQuietLMR = !ttCapture;
-              ssHardExt = PvNode && value < singularBeta - 191;
+              ssHardExt = PvNode && value < singularBeta - 93;
 
               // Avoid search explosion by limiting the number of double extensions to at most 3
               if (   !PvNode
