332c6d4d9f59e236c08a03fdaa16ea68fb3f39ed
Vizvezdenec <Vizvezdenec@gmail.com>
2021-06-14 19:45:30 +0300

    tweak. bench 4973294

--- src/search.cpp
+++ src/search.cpp
@@ -778,7 +778,7 @@ namespace {
     // Use static evaluation difference to improve quiet move ordering
     if (is_ok((ss-1)->currentMove) && !(ss-1)->inCheck && !priorCapture)
     {
-        int bonus = std::clamp(-depth * 4 * int((ss-1)->staticEval + ss->staticEval), -1000, 1000);
+        int bonus = std::clamp(-depth * 4 * int((ss-1)->staticEval + ss->staticEval), -2000, 2000);
         thisThread->mainHistory[~us][from_to((ss-1)->currentMove)] << bonus;
     }
 
