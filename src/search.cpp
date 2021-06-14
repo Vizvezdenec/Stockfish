03f022c0a87974656adffbf0dfbead0a5ea52a4c
Vizvezdenec <Vizvezdenec@gmail.com>
2021-06-14 23:46:22 +0300

    tweak. bench 4294131

--- src/search.cpp
+++ src/search.cpp
@@ -780,7 +780,7 @@ namespace {
     {
         int bonus = std::clamp(-depth * 4 * int((ss-1)->staticEval + ss->staticEval), -1000, 1000);
         thisThread->mainHistory[~us][from_to((ss-1)->currentMove)] << bonus;
-        bonus = std::clamp(-int((ss-1)->staticEval + ss->staticEval), -50, 50);
+        bonus = std::clamp(- 4 * int((ss-1)->staticEval + ss->staticEval), -50, 50);
         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, bonus);
     }
 
