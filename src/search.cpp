8f8ab0f76dae84f7b718c193972a5db5e1e6ba23
Vizvezdenec <Vizvezdenec@gmail.com>
2021-06-18 14:31:41 +0300

    tweak. bench 4957245

--- src/search.cpp
+++ src/search.cpp
@@ -653,9 +653,8 @@ namespace {
         && ss->ttHit
         && tte->depth() >= depth
         && ttValue != VALUE_NONE // Possible in case of TT access race
-        && ((ttValue >= beta ? (tte->bound() & BOUND_LOWER)
-                            : (tte->bound() & BOUND_UPPER)) 
-            || (std::abs(ttValue - alpha > 800) && (tte->bound() & BOUND_EXACT))))
+        && (ttValue >= beta ? (tte->bound() & BOUND_LOWER)
+                            : (tte->bound() & BOUND_UPPER)))
     {
         // If ttMove is quiet, update move sorting heuristics on TT hit
         if (ttMove)
@@ -1080,7 +1079,7 @@ moves_loop: // When in check, search starts from here
 
               // Avoid search explosion by limiting the number of double extensions to at most 3
               if (   !PvNode
-                  && value < singularBeta - 93
+                  && value < singularBeta - 81 + 2 * singularDepth
                   && ss->doubleExtensions < 3)
               {
                   extension = 2;
