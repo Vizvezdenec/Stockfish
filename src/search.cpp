9c49814ab85d736cfe5cb7471c61b75c575d9713
Vizvezdenec <Vizvezdenec@gmail.com>
2021-06-19 16:17:11 +0300

    tweak. bench 4172760

--- src/search.cpp
+++ src/search.cpp
@@ -1144,7 +1144,7 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction(improving, depth, moveCount);
 
           if (reducedPN)
-              r--;
+              r -= 2;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
