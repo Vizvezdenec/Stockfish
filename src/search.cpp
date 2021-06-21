ba92f4f96a19d0663595bef513b1e6995afbc363
Vizvezdenec <Vizvezdenec@gmail.com>
2021-06-21 23:38:20 +0300

    tweak. bench 4973418

--- src/search.cpp
+++ src/search.cpp
@@ -1189,8 +1189,8 @@ moves_loop: // When in check, search starts from here
                   r -= ss->statScore / 14721;
           }
 
-          if (r < 0 && captureOrPromotion && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] > 6322)
-              r--;
+          if (r < 0 && captureOrPromotion && captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < -1000)
+              r = 0;
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
