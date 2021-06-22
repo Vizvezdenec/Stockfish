a14c86d3af0e5c57c167d0392ad6690e5c519d37
Vizvezdenec <Vizvezdenec@gmail.com>
2021-06-22 21:00:27 +0300

    tweak. bench 5628656

--- src/search.cpp
+++ src/search.cpp
@@ -789,7 +789,7 @@ namespace {
         {
             pos.do_null_move(st);
 
-            Value pseudobeta = (eval * 3 + beta) / 4;
+            Value pseudobeta = (eval + beta * 3) / 4;
 
             Value nullValue = -qsearch<NonPV>(pos, ss+1, -pseudobeta, -pseudobeta +1);
 
