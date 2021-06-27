517bc6ae853610782c69719d74e67e31c0648982
Vizvezdenec <Vizvezdenec@gmail.com>
2021-06-27 21:36:35 +0300

    tweak. bench 5436291

--- src/search.cpp
+++ src/search.cpp
@@ -1485,7 +1485,7 @@ moves_loop: // When in check, search starts from here
           &&  type_of(move) != PROMOTION)
       {
 
-          if (moveCount > 3 - cutNode)
+          if (moveCount > 3 - 2 * cutNode)
               continue;
 
           futilityValue = futilityBase + PieceValue[EG][pos.piece_on(to_sq(move))];
