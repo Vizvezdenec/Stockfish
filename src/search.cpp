a1f0cc75ecb4e3f24906ab41d846c553e96efdfe
Vizvezdenec <Vizvezdenec@gmail.com>
2021-06-27 13:39:55 +0300

    tweak. bench 5324674

--- src/search.cpp
+++ src/search.cpp
@@ -770,6 +770,8 @@ namespace {
     {
         int bonus = std::clamp(-depth * 4 * int((ss-1)->staticEval + ss->staticEval), -1000, 1000);
         thisThread->mainHistory[~us][from_to((ss-1)->currentMove)] << bonus;
+        thisThread->pieceSquareH[pos.moved_piece((ss-1)->currentMove)][to_sq((ss-1)->currentMove)] << bonus;
+        thisThread->pieceSquareH[pos.moved_piece((ss-1)->currentMove)][from_sq((ss-1)->currentMove)] << -bonus;
     }
 
     // Set up improving flag that is used in various pruning heuristics
