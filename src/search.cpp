b98d6a5430e80b5227f663fd97b3c57a36b491a5
Vizvezdenec <Vizvezdenec@gmail.com>
2021-07-04 02:27:43 +0300

    tweak. bench 5310042

--- src/search.cpp
+++ src/search.cpp
@@ -1022,7 +1022,7 @@ moves_loop: // When in check, search starts from here
                   continue;
 
               // Futility pruning: parent node (~5 Elo)
-              if (   lmrDepth < 7
+              if (   lmrDepth < 8
                   && !ss->inCheck
                   && ss->staticEval + 174 + 157 * lmrDepth <= alpha
                   &&  (*contHist[0])[movedPiece][to_sq(move)]
