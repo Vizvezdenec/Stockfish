d4bcdaa78f3299109a57b2d72b9c6fdcdccc7ceb
Vizvezdenec <Vizvezdenec@gmail.com>
2021-07-11 22:54:36 +0300

    tweak. bench 5311123

--- src/search.cpp
+++ src/search.cpp
@@ -1126,6 +1126,7 @@ moves_loop: // When in check, search starts from here
           && (  !captureOrPromotion
               || (cutNode && (ss-1)->moveCount > 1)
               || !ss->ttPv)
+          && !(pos.captured_piece() && type_of(pos.captured_piece()) != PAWN && pos.count<ALL_PIECES>() - pos.count<PAWN>() < 4)
           && (!PvNode || ss->ply > 1 || thisThread->id() % 4 != 3))
       {
           Depth r = reduction(improving, depth, moveCount);
@@ -1165,9 +1166,6 @@ moves_loop: // When in check, search starts from here
               // Increase reduction if ttMove is a capture (~3 Elo)
               if (ttCapture)
                   r++;
-
-              if (move == (ss-4)->currentMove)
-                  r += 2;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
