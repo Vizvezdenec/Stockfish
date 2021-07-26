1ad0ccf4786597d7530bf2affc4fe6574e00ea9d
Vizvezdenec <Vizvezdenec@gmail.com>
2021-07-26 10:28:16 +0300

    tweak. bench 5369935

--- src/search.cpp
+++ src/search.cpp
@@ -1160,7 +1160,7 @@ moves_loop: // When in check, search starts from here
               r += 2;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
-          if (ttCapture && (!captureOrPromotion || !PvNode))
+          if (ttCapture && move != ss->killers[0])
               r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
