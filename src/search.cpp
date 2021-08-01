6b00ef3fd3084857634488cd914b3f75c2b064d8
Vizvezdenec <Vizvezdenec@gmail.com>
2021-08-01 12:51:33 +0300

    tweak. bench 5303184

--- src/search.cpp
+++ src/search.cpp
@@ -66,7 +66,7 @@ namespace {
 
   // Futility margin
   Value futility_margin(Depth d, bool improving) {
-    return Value(209 * (d - improving));
+    return Value(194 * (d - improving));
   }
 
   // Reductions lookup table, initialized at startup
