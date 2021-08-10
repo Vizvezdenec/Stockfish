ffd07f85261363aeb560d44aa2fbdc3d0bf43954
Vizvezdenec <Vizvezdenec@gmail.com>
2021-08-10 18:15:19 +0300

    tweak. bench 4664859

--- src/search.cpp
+++ src/search.cpp
@@ -1164,9 +1164,6 @@ moves_loop: // When in check, search starts here
           if (ttCapture)
               r++;
 
-          if ((ss-1)->dext && (ss-1)->moveCount != 1 && !doubleExtension)
-              r--;
-
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
@@ -1179,7 +1176,7 @@ moves_loop: // When in check, search starts here
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
           // to be searched deeper than the first move, unless ttMove was extended by 2.
-          Depth d = std::clamp(newDepth - r, 1, newDepth + (r < -1 && moveCount <= 5 && !doubleExtension));
+          Depth d = std::clamp(newDepth - r, 1, newDepth + (r < -1 && (moveCount <= 5 || ((ss-1)->dext && (ss-1)->moveCount != 1)) && !doubleExtension));
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
