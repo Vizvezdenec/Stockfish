69e1cc3481d4796981a4d2eb0d43678cfba01047
Vizvezdenec <Vizvezdenec@gmail.com>
2021-08-10 18:08:24 +0300

    tweak. bench 6067657

--- src/search.cpp
+++ src/search.cpp
@@ -1165,7 +1165,7 @@ moves_loop: // When in check, search starts here
               r++;
 
           if ((ss-1)->dext && !doubleExtension)
-              r--;
+              r -= 2;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
