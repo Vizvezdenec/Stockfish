9dfeea0386f1bb07eca14e188f0540ee498cb41b
Michael Chaly <Vizvezdenec@gmail.com>
2021-08-20 01:15:36 +0300

    bench 5110337

--- src/search.cpp
+++ src/search.cpp
@@ -1206,12 +1206,12 @@ moves_loop: // When in check, search starts here
       // For PV nodes only, do a full PV search on the first move or after a fail
       // high (in the latter case search only if value < beta), otherwise let the
       // parent node fail low with value <= alpha and try another move.
-      if (PvNode && (moveCount == 1 || (value > alpha && (rootNode || value < beta))))
+      if (PvNode && (moveCount == 1 || (value > alpha && (ss->ply <= 1 || value < beta))))
       {
           (ss+1)->pv = pv;
           (ss+1)->pv[0] = MOVE_NONE;
 
-          value = -search<PV>(pos, ss+1, -beta, rootNode ? -alpha : -(value + alpha) / 2,
+          value = -search<PV>(pos, ss+1, -beta, -alpha,
                               std::min(maxNextDepth, newDepth), false);
       }
 
