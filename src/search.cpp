5c8574c9b2378cd3c936780c4f96be46ddcc19a8
Michael Chaly <Vizvezdenec@gmail.com>
2021-08-19 21:00:25 +0300

    bench 4634611

--- src/search.cpp
+++ src/search.cpp
@@ -1652,7 +1652,7 @@ moves_loop: // When in check, search starts here
             update_continuation_histories(ss, pos.moved_piece(quietsSearched[i]), to_sq(quietsSearched[i]), -bonus2);
         }
 
-        if (bestValue > beta + PieceValue[EG][captured])
+        if (bestValue > beta + PieceValue[EG][captured] + 422)
             for (int i = 0; i < quietCount; ++i)
             {
             thisThread->mainHistory[us][from_to(quietsSearched[i])] << -bonus2;
