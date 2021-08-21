45d883b109a7ce165fb17bca677e634b7fcbfd34
Michael Chaly <Vizvezdenec@gmail.com>
2021-08-21 20:36:17 +0300

    bench 4696515

--- src/search.cpp
+++ src/search.cpp
@@ -1023,7 +1023,7 @@ moves_loop: // When in check, search starts here
                   && (*contHist[1])[movedPiece][to_sq(move)] < 23 - 23 * depth * depth)
                   continue;
 
-              lmrDepth = std::max(0, lmrDepth - 2 * (!ss->ttPv && thisThread->bestMoveChanges <= 1));
+              lmrDepth = std::max(0, lmrDepth - (!PvNode && thisThread->bestMoveChanges <= 1 + !ss->ttPv));
 
               // Futility pruning: parent node (~5 Elo)
               if (   !ss->inCheck
