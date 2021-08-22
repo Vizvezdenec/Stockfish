b2562a8b77cec2e082b0be9b0c115c976d549eac
Michael Chaly <Vizvezdenec@gmail.com>
2021-08-22 06:37:36 +0300

    bench  4725493

--- src/search.cpp
+++ src/search.cpp
@@ -1155,12 +1155,12 @@ moves_loop: // When in check, search starts here
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
-          if (cutNode && move != ss->killers[0] && !(move == ss->killers[1] && move == countermove))
+          if (cutNode && move != ss->killers[0])
               r += 2;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
-              r++;
+              r += 1 + (tte->depth() >= depth && !captureOrPromotion);
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
