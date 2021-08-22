4b7f99db6e926263f11c37bdda9732989c4fc224
Michael Chaly <Vizvezdenec@gmail.com>
2021-08-23 02:37:00 +0300

    bench 4878288

--- src/search.cpp
+++ src/search.cpp
@@ -1124,6 +1124,7 @@ moves_loop: // When in check, search starts here
           && (  !captureOrPromotion
               || (cutNode && (ss-1)->moveCount > 1)
               || !ss->ttPv)
+          && !(givesCheck && type_of(movedPiece) == BISHOP && (attacks_bb<KING>(to_sq(move)) & pos.square<KING>(~us)))
           && (!PvNode || ss->ply > 1 || thisThread->id() % 4 != 3))
       {
           Depth r = reduction(improving, depth, moveCount);
@@ -1174,7 +1175,7 @@ moves_loop: // When in check, search starts here
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
           // to be searched deeper than the first move in specific cases.
-          Depth d = std::clamp(newDepth - r, 1, newDepth + (r < -1 && (moveCount <= 5 || ((depth > 6 || !ttMove) && PvNode)) && !doubleExtension));
+          Depth d = std::clamp(newDepth - r, 1, newDepth + (r < -1 && (moveCount <= 5 || (depth > 6 && PvNode)) && !doubleExtension));
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
