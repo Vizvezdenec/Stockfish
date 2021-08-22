0f3e79d289757ef3926aff8760558384b68946a3
Michael Chaly <Vizvezdenec@gmail.com>
2021-08-22 14:49:34 +0300

    bench 5200406

--- src/search.cpp
+++ src/search.cpp
@@ -801,7 +801,7 @@ namespace {
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and value
-        Depth R = std::min(int(eval - beta) / 205, 3) + depth / 3 + 4;
+        Depth R = (1090 + 81 * depth) / 256 + std::min(int(eval - beta) / 205, 3);
 
         ss->currentMove = MOVE_NULL;
         ss->continuationHistory = &thisThread->continuationHistory[0][0][NO_PIECE][0];
@@ -1020,10 +1020,10 @@ moves_loop: // When in check, search starts here
               // Continuation history based pruning (~20 Elo)
               if (   lmrDepth < 5
                   && (*contHist[0])[movedPiece][to_sq(move)] < 23 - 23 * depth * depth
-                  && (*contHist[1])[movedPiece][to_sq(move)] < 23 - 23 * depth * depth
-                  && ( (*contHist[3])[movedPiece][to_sq(move)] < 5000 + 300 * depth * depth
-                    || (*contHist[5])[movedPiece][to_sq(move)] < 5000 + 300 * depth * depth))
+                  && (*contHist[1])[movedPiece][to_sq(move)] < 23 - 23 * depth * depth)
                   continue;
+
+              lmrDepth = std::max(0, lmrDepth - (!PvNode && thisThread->bestMoveChanges <= 1));
 
               // Futility pruning: parent node (~5 Elo)
               if (   !ss->inCheck
@@ -1175,8 +1175,8 @@ moves_loop: // When in check, search starts here
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
-          // to be searched deeper than the first move in specific cases.
-          Depth d = std::clamp(newDepth - r, 1, newDepth + (r < -1 && (moveCount <= 5 || (depth > 6 && PvNode)) && !doubleExtension));
+          // to be searched deeper than the first move, unless ttMove was extended by 2.
+          Depth d = std::clamp(newDepth - r, 1, newDepth + (r < -1 && moveCount <= 5 && !doubleExtension));
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
