bfe247f2ea8d4126e0aed79bcf1f2e210b905ef1
Michael Chaly <Vizvezdenec@gmail.com>
2021-08-24 01:37:37 +0300

    bench 5254004

--- src/search.cpp
+++ src/search.cpp
@@ -1161,6 +1161,9 @@ moves_loop: // When in check, search starts here
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
               r++;
+        
+          if (doubleExtenstion && bestValue >= ttValue && bestMove != ttMove)
+              r--;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
