59a92c7acc6dc8a0c2f9adc730f4a777302532ad
Michael Chaly <Vizvezdenec@gmail.com>
2021-08-24 11:35:24 +0300

    bench 4965346

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -168,8 +168,7 @@ namespace Stockfish::Eval::NNUE {
     int positional  = output[0];
 
     int delta_npm = abs(pos.non_pawn_material(WHITE) - pos.non_pawn_material(BLACK));
-    int entertainment = adjusted * (delta_npm <= BishopValueMg - KnightValueMg ? 7 
-                                  : delta_npm <= RookValueMg - KnightValueMg   ? 3
+    int entertainment = adjusted * (delta_npm <= 2 * (BishopValueMg - KnightValueMg) ? 7 
                                                                                : 0);
 
     int A = 128 - entertainment;
