ac51a2e988a7f0be1a63897454cbb3498de87c07
Michael Chaly <Vizvezdenec@gmail.com>
2021-08-27 16:46:25 +0300

    bench  5255239

--- src/search.cpp
+++ src/search.cpp
@@ -1013,8 +1013,9 @@ moves_loop: // When in check, search starts here
 
               if (    !givesCheck
                    && !ss->inCheck
-                   && lmrDepth < 1
-                   && ss->staticEval + PieceValue[EG][pos.piece_on(to_sq(move))] <= alpha)
+                   && !ss->ttPv
+                   && lmrDepth < 8
+                   && ss->staticEval + 592 + 410 * lmrDepth + PieceValue[MG][pos.piece_on(to_sq(move))] <= alpha)
                    continue;
 
               // SEE based pruning
