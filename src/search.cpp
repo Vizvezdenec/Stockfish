34e396ece6236fa2ccf4eac1dc2b201685f814f5
Michael Chaly <Vizvezdenec@gmail.com>
2021-08-29 01:25:42 +0300

    bench 5286713

--- src/search.cpp
+++ src/search.cpp
@@ -1681,10 +1681,10 @@ moves_loop: // When in check, search starts here
     for (int i : {1, 2, 4, 6})
     {
         // Only update first 2 continuation histories if we are in check
-        if (ss->inCheck && i > 1)
+        if (ss->inCheck && i > 2)
             break;
         if (is_ok((ss-i)->currentMove))
-            (*(ss-i)->continuationHistory)[pc][to] << bonus * (1 + ss->inCheck);
+            (*(ss-i)->continuationHistory)[pc][to] << bonus;
     }
   }
 
