f06cd7df32d285a8c4b3617c55122471645cc6bd
Michael Chaly <Vizvezdenec@gmail.com>
2021-08-28 20:10:48 +0300

    bench 5307229

--- src/search.cpp
+++ src/search.cpp
@@ -663,6 +663,8 @@ namespace {
                 int penalty = -stat_bonus(depth);
                 thisThread->mainHistory[us][from_to(ttMove)] << penalty;
                 update_continuation_histories(ss, pos.moved_piece(ttMove), to_sq(ttMove), penalty);
+                if (depth > 11)
+                    thisThread->lowPlyHistory[ss->ply][from_to(ttMove)] << -stat_bonus(depth - 7);
             }
         }
 
