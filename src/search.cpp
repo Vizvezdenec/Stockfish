b5f721c8f5118c9ab90e687cf9fd30d69dc25aac
Michael Chaly <Vizvezdenec@gmail.com>
2021-09-01 11:04:59 +0300

    bench 5122638

--- src/search.cpp
+++ src/search.cpp
@@ -1098,7 +1098,8 @@ moves_loop: // When in check, search starts here
                && depth > 6
                && abs(ss->staticEval) > Value(100))
           extension = 1;
-      else if (   PvNode && captureOrPromotion && moveCount != 1 && !likelyFailLow && depth >= 7 && givesCheck)
+      else if (   PvNode && captureOrPromotion && moveCount != 1 && !likelyFailLow && depth >= 7 && givesCheck
+               && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] > -5000)
           extension = 1;
 
       // Add extension to new depth
