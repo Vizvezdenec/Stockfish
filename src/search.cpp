a62dafc17c05e3c1d676e32a3a0c5947c13aaa10
Michael Chaly <Vizvezdenec@gmail.com>
2021-09-04 05:24:29 +0300

    bench 5197287

--- src/search.cpp
+++ src/search.cpp
@@ -819,7 +819,13 @@ namespace {
                 nullValue = beta;
 
             if (thisThread->nmpMinPly || (abs(beta) < VALUE_KNOWN_WIN && depth < 14))
+            {
+                if (!ss->ttHit)
+                    tte->save(posKey, value_to_tt(nullValue, ss->ply), ss->ttPv,
+                                BOUND_LOWER,
+                                DEPTH_NONE, MOVE_NONE, ss->staticEval);
                 return nullValue;
+            }
 
             assert(!thisThread->nmpMinPly); // Recursive verification is not allowed
 
