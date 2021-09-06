52e8739759da1677b1feaae343c5863965c1d387
Michael Chaly <Vizvezdenec@gmail.com>
2021-09-06 03:31:06 +0300

    bench 4884880

--- src/search.cpp
+++ src/search.cpp
@@ -1165,7 +1165,7 @@ moves_loop: // When in check, search starts here
           if (ttCapture)
               r++;
 
-          if (movesFS >= 8)
+          if (movesFS >= 16)
               r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
