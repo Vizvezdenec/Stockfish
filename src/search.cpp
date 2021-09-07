930485081134deb0a6a86434a88fd5f565bf531a
Michael Chaly <Vizvezdenec@gmail.com>
2021-09-08 01:53:43 +0300

    bench 7145258

--- src/search.cpp
+++ src/search.cpp
@@ -1096,8 +1096,7 @@ moves_loop: // When in check, search starts here
       }
 
       // Capture extensions for PvNodes and cutNodes
-      else if (   (PvNode || cutNode) 
-               && moveCount < 2 * depth + 8
+      else if (   (ss->ttPv || cutNode) 
                && captureOrPromotion 
                && moveCount != 1)
           extension = 1;
