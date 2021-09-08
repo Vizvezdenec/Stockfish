afaa67ff6b3ce733023fb9e1fadc604f0b17cfdb
Michael Chaly <Vizvezdenec@gmail.com>
2021-09-08 12:44:15 +0300

    bench 6238347

--- src/search.cpp
+++ src/search.cpp
@@ -1172,6 +1172,7 @@ moves_loop: // When in check, search starts here
               r++;
 
           if (!captureOrPromotion)
+          {
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
@@ -1180,6 +1181,7 @@ moves_loop: // When in check, search starts here
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 14721;
+          }
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
