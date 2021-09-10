153171a4b22e91495ebe553c6ab47075dd136656
Michael Chaly <Vizvezdenec@gmail.com>
2021-09-10 20:59:41 +0300

    bench 6440105

--- src/search.cpp
+++ src/search.cpp
@@ -1071,7 +1071,7 @@ moves_loop: // When in check, search starts here
 
               // Avoid search explosion by limiting the number of double extensions to at most 3
               if (   !PvNode
-                  && value < singularBeta - 73 - 40 * cutNode
+                  && value < singularBeta - 73 - 40 * !cutNode
                   && ss->doubleExtensions < 3)
               {
                   extension = 2;
