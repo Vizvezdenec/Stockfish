250c3565a31378ba3dfb29f43f895e0fc314285d
Michael Chaly <Vizvezdenec@gmail.com>
2021-09-13 19:50:25 +0300

    bench 6581919

--- src/search.cpp
+++ src/search.cpp
@@ -1142,8 +1142,8 @@ moves_loop: // When in check, search starts here
       {
           Depth r = reduction(improving, depth, moveCount);
 
-          if (PvNode)
-              r -= 1 + (ss->ply < 2);
+          if (PvNode && (ss->ply + depth) > thisThread->rootDepth / 2)
+              r--;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
