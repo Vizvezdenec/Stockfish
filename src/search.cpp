341d5190a898de75d20d6baa4fddda76f2c8a170
Michael Chaly <Vizvezdenec@gmail.com>
2021-09-18 04:50:48 +0300

    bench 6117411

--- src/search.cpp
+++ src/search.cpp
@@ -797,7 +797,7 @@ namespace {
         && (ss-1)->statScore < 23767
         &&  eval >= beta
         &&  eval >= ss->staticEval
-        &&  ss->staticEval >= beta - 20 * depth - 22 * improving + 168 * ss->ttPv + 177 - 100 * !ss->isNmp
+        &&  ss->staticEval >= beta - 20 * depth - 22 * improving + 168 * ss->ttPv + 177 - 25 * !ss->isNmp
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))
