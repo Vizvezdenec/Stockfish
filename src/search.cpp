83f2a67b6ee4f41a3bb3c14a1d9f1d73570d7e4e
Michael Chaly <Vizvezdenec@gmail.com>
2021-09-19 04:32:37 +0300

    bench 5772787

--- src/search.cpp
+++ src/search.cpp
@@ -909,6 +909,11 @@ namespace {
         && !ttMove)
         depth -= 2;
 
+    else if (   PvNode
+        && depth > 2
+        && !ss->ttHit)
+        depth--;
+
     if (   cutNode
         && depth >= 9
         && !ttMove)
@@ -1071,7 +1076,7 @@ moves_loop: // When in check, search starts here
 
               // Avoid search explosion by limiting the number of double extensions to at most 3
               if (   !PvNode
-                  && value < singularBeta - 62 - 3 * ss->ply
+                  && value < singularBeta - 93
                   && ss->doubleExtensions < 3)
               {
                   extension = 2;
