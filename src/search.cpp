275c0f9e0814750bb8545512a18054be5065c400
Michael Chaly <Vizvezdenec@gmail.com>
2021-09-22 00:49:31 +0300

    bench 5870710

--- src/search.cpp
+++ src/search.cpp
@@ -1161,7 +1161,7 @@ moves_loop: // When in check, search starts here
               r++;
 
           // Decrease reduction if opponent's move count is high (~1 Elo)
-          if ((ss-1)->moveCount > 13)
+          if ((ss-1)->moveCount > 13 && (cutNode || PvNode))
               r--;
 
           // Decrease reduction if ttMove has been singularly extended (~1 Elo)
