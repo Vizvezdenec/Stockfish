3df45b1a6f552574d7be61218da0694a47ffde47
Michael Chaly <Vizvezdenec@gmail.com>
2021-09-21 09:50:40 +0300

    bench 6021674

--- src/search.cpp
+++ src/search.cpp
@@ -1102,7 +1102,7 @@ moves_loop: // When in check, search starts here
 
       // Capture extensions for PvNodes and cutNodes
       else if (   (PvNode || cutNode)
-               && (captureOrPromotion || (ss->ply <= 7 && (givesCheck || ss->inCheck)))
+               && captureOrPromotion
                && moveCount != 1)
           extension = 1;
 
@@ -1188,7 +1188,7 @@ moves_loop: // When in check, search starts here
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
           // to be searched deeper than the first move in specific cases.
-          Depth d = std::clamp(newDepth - r, 1, newDepth + (r < -1 && (moveCount <= 5 || (depth > 6 && PvNode)) && !doubleExtension));
+          Depth d = std::clamp(newDepth - r, 1, newDepth + (r < -1 && (moveCount <= 5 || (depth > 6 && PvNode) || givesCheck) && !doubleExtension));
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
