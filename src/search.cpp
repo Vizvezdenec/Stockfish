e2926f871671083429baf8b6fc849398e95e56df
Michael Chaly <Vizvezdenec@gmail.com>
2021-09-25 02:32:46 +0300

    bench 5226751

--- src/search.cpp
+++ src/search.cpp
@@ -1153,7 +1153,8 @@ moves_loop: // When in check, search starts here
                && depth >= 7
                && move == ttMove 
                && move == ss->killers[0]
-               && (*contHist[0])[movedPiece][to_sq(move)] >= 19000)
+               && (*contHist[0])[movedPiece][to_sq(move)] >= 10000
+               && (*contHist[1])[movedPiece][to_sq(move)] >= 10000)
           extension = 1;
 
       // Add extension to new depth
