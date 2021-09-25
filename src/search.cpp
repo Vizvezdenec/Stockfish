06b2663decef63e2b9c2396cd943b10fb12b206a
Michael Chaly <Vizvezdenec@gmail.com>
2021-09-25 12:31:11 +0300

    bench 5921427

--- src/search.cpp
+++ src/search.cpp
@@ -1152,8 +1152,8 @@ moves_loop: // When in check, search starts here
       else if (   PvNode
                && move == ttMove 
                && move == ss->killers[0]
-               && (*contHist[0])[movedPiece][to_sq(move)] >= 10000
-               && (*contHist[1])[movedPiece][to_sq(move)] >= 10000)
+               && (*contHist[0])[movedPiece][to_sq(move)] >= 18000
+               && (*contHist[1])[movedPiece][to_sq(move)] >= 18000)
           extension = 1;
 
       // Add extension to new depth
