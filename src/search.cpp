af0af5e74f5555d5c6ce7433a25e343c8775fade
Michael Chaly <Vizvezdenec@gmail.com>
2021-09-27 17:20:30 +0300

    bench 6364872

--- src/search.cpp
+++ src/search.cpp
@@ -1235,7 +1235,7 @@ moves_loop: // When in check, search starts here
           // deeper than the first move (this may lead to hidden double extensions if
           // newDepth got its own extension before).
           int deeper =   r >= -1               ? 0
-                       : noLMRExtension        ? 0
+                       : (noLMRExtension && ss->ply > 1)        ? 0
                        : moveCount <= 5        ? 1
                        : (depth > 6 && PvNode) ? 1
                        :                         0;
