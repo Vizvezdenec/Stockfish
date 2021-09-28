3dbc2ec671de80e6220004b7ba2c753ab30e2bca
Michael Chaly <Vizvezdenec@gmail.com>
2021-09-28 20:45:49 +0300

    bench 5654011

--- src/search.cpp
+++ src/search.cpp
@@ -838,7 +838,7 @@ namespace {
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and value
-        Depth R = std::min(int(eval - beta) / 205, 3) + (depth + improving - 2) / 3 + 4;
+        Depth R = std::min(int(eval - beta) / 205, 3) + depth / 3 + 3 + 2 * improving;
 
         ss->currentMove = MOVE_NULL;
         ss->continuationHistory = &thisThread->continuationHistory[0][0][NO_PIECE][0];
