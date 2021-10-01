90aba7ed26cad9f6c103c52c15acd428d242adaf
Michael Chaly <Vizvezdenec@gmail.com>
2021-10-01 21:15:58 +0300

    bench 5714575

--- src/search.cpp
+++ src/search.cpp
@@ -173,7 +173,7 @@ namespace {
 void Search::init() {
 
   for (int i = 1; i < MAX_MOVES; ++i)
-      Reductions[i] = int(21.9 * std::log(i));
+      Reductions[i] = int((21.9 + std::log(Threads.size()) / 2)* std::log(i));
 }
 
 
@@ -1152,9 +1152,9 @@ moves_loop: // When in check, search starts here
 
       // Quiet ttMove extensions
       else if (   PvNode
-               && moveCount == 1
+               && move == ttMove
                && move == ss->killers[0]
-               && (*contHist[0])[movedPiece][to_sq(move)] >= 10000 + 5000 * !ttMove)
+               && (*contHist[0])[movedPiece][to_sq(move)] >= 10000)
           extension = 1;
 
       // Add extension to new depth
