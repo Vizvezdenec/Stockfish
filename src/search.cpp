cce00aa0e4c32b6dd43735a8481e7081e4de32b2
Michael Chaly <Vizvezdenec@gmail.com>
2021-10-09 04:31:52 +0300

    bench 6628721

--- src/search.cpp
+++ src/search.cpp
@@ -1221,7 +1221,7 @@ moves_loop: // When in check, search starts here
           if (ttCapture)
               r++;
 
-          if (!PvNode && moveCount == 2)
+          if (!PvNode && moveCount <= 3)
               r--;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
