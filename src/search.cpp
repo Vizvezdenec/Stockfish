cad038843f518adf03f5e3d6e216e9f4cd3fd5aa
Michael Chaly <Vizvezdenec@gmail.com>
2021-10-10 14:36:28 +0300

    bench 6151508

--- src/search.cpp
+++ src/search.cpp
@@ -80,7 +80,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return std::min((6 * d + 219) * d - 215 , 2000);
+    return std::min((6 * d + 239) * d - 215 , 2000);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
