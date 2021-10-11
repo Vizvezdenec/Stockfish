137dc5432cd13cc75efa87ad1cded54fab51d741
Michael Chaly <Vizvezdenec@gmail.com>
2021-10-11 12:06:12 +0300

    bench 5832275

--- src/search.cpp
+++ src/search.cpp
@@ -1558,7 +1558,7 @@ moves_loop: // When in check, search starts here
           &&  type_of(move) != PROMOTION)
       {
 
-          if (moveCount > 2 + PvNode)
+          if (moveCount > 2)
               continue;
 
           futilityValue = futilityBase + PieceValue[EG][pos.piece_on(to_sq(move))];
@@ -1593,8 +1593,8 @@ moves_loop: // When in check, search starts here
       // Continuation history based pruning
       if (  !captureOrPromotion
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY
-          && (*contHist[0])[pos.moved_piece(move)][to_sq(move)] < CounterMovePruneThreshold
-          && (*contHist[1])[pos.moved_piece(move)][to_sq(move)] < CounterMovePruneThreshold)
+          && (*contHist[0])[pos.moved_piece(move)][to_sq(move)] < CounterMovePruneThreshold - 2000 * PvNode
+          && (*contHist[1])[pos.moved_piece(move)][to_sq(move)] < CounterMovePruneThreshold - 2000 * PvNode)
           continue;
 
       // Make and search the move
