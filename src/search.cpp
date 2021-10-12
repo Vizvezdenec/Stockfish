2f0d44de0ad104b76be4b28170a57844a44ef9b4
Michael Chaly <Vizvezdenec@gmail.com>
2021-10-13 02:32:00 +0300

    bench 6074913

--- src/search.cpp
+++ src/search.cpp
@@ -1188,7 +1188,7 @@ moves_loop: // When in check, search starts here
 
           // Decrease reduction if on the PV (~2 Elo)
           if (   PvNode
-              && (bestMoveCount <= 3 || ttMove))
+              && (bestMoveCount <= 2 || ttMove))
               r--;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
