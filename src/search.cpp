c6eb510f454631b53ee274de23ac33d610be7b40
Michael Chaly <Vizvezdenec@gmail.com>
2021-10-12 09:52:02 +0300

    bench 5989310

--- src/search.cpp
+++ src/search.cpp
@@ -1731,7 +1731,7 @@ moves_loop: // When in check, search starts here
                                             : stat_bonus(depth);   // smaller bonus
 
     if (depth <= 0)
-        bonus1 = bonus2 = 7;
+        bonus1 = bonus2 = 6;
 
     if (!pos.capture_or_promotion(bestMove))
     {
