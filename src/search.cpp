5d8530b68aa5d25b8cbac3b1e85d642cd5bd124c
Michael Chaly <Vizvezdenec@gmail.com>
2021-10-14 01:47:49 +0300

    bench 5615058

--- src/search.cpp
+++ src/search.cpp
@@ -1223,8 +1223,7 @@ moves_loop: // When in check, search starts here
                          + (*contHist[3])[movedPiece][to_sq(move)]
                          - 4923;
 
-          if (extensionCount > 12)
-              r++;
+          r += extensionCount / 16;
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 14721;
