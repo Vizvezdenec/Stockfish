306a28e775fb9afa2ad7e48e62384be974c0093a
Michael Chaly <Vizvezdenec@gmail.com>
2021-10-27 19:43:48 +0300

    bench 6721108

--- src/search.cpp
+++ src/search.cpp
@@ -1114,7 +1114,7 @@ moves_loop: // When in check, search starts here
 
           // If the eval of ttMove is greater than beta, we reduce it (negative extension)
           else if (ttValue >= beta)
-              extension = -2;
+              extension = -2 + (move == ss->killers[0]);
       }
 
       // Capture extensions for PvNodes and cutNodes
