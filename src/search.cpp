f812441f78718643081f48cf269cc5f6c2fe8601
Michael Chaly <Vizvezdenec@gmail.com>
2021-10-31 13:51:25 +0300

    bench 7548578

--- src/search.cpp
+++ src/search.cpp
@@ -1113,7 +1113,7 @@ moves_loop: // When in check, search starts here
               return singularBeta;
 
           // If the eval of ttMove is greater than beta, we reduce it (negative extension)
-          else if (ttValue >= beta || value >= beta)
+          else if (value >= beta)
               extension = -2;
       }
 
