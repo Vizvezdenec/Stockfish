d21f85ab025473a52acc6faf5551020f3cc8a57f
Michael Chaly <Vizvezdenec@gmail.com>
2021-11-05 18:34:59 +0300

    bench 7068652

--- src/search.cpp
+++ src/search.cpp
@@ -1107,7 +1107,7 @@ moves_loop: // When in check, search starts here
           // that multiple moves fail high, and we can prune the whole subtree by returning
           // a soft bound.
           else if (singularBeta >= beta)
-              return singularBeta;
+              return std::min(value, ttValue);
 
           // If the eval of ttMove is greater than beta, we reduce it (negative extension)
           else if (ttValue >= beta)
@@ -1127,9 +1127,10 @@ moves_loop: // When in check, search starts here
           extension = 1;
 
       // Quiet ttMove extensions
-      else if (   move == ttMove
+      else if (   PvNode
+               && move == ttMove
                && move == ss->killers[0]
-               && (*contHist[0])[movedPiece][to_sq(move)] >= 10000 + 5000 * !PvNode)
+               && (*contHist[0])[movedPiece][to_sq(move)] >= 10000)
           extension = 1;
 
       // Add extension to new depth
