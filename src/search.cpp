a40a8607d554256f3ef9524ae9a1fab26645104a
Michael Chaly <Vizvezdenec@gmail.com>
2021-11-08 20:39:27 +0300

    bench 6147428

--- src/search.cpp
+++ src/search.cpp
@@ -1170,7 +1170,7 @@ moves_loop: // When in check, search starts here
               r--;
 
           // Increases reduction for PvNodes that have small window
-          if (PvNode && beta - alpha < thisThread->rootDelta / 4)
+          if (PvNode && beta - alpha < thisThread->rootDelta / 2)
               r++;
 
           // Decrease reduction if position is or has been on the PV
@@ -1689,7 +1689,7 @@ moves_loop: // When in check, search starts here
     PieceType captured = type_of(pos.piece_on(to_sq(bestMove)));
 
     bonus1 = stat_bonus(depth + 1);
-    bonus2 = bestValue > beta + 150 ? bonus1               // larger bonus
+    bonus2 = bestValue > beta + PawnValueMg ? bonus1               // larger bonus
                                             : stat_bonus(depth);   // smaller bonus
 
     if (!pos.capture_or_promotion(bestMove))
