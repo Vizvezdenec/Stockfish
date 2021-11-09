54bffa6431fd00b074559e1d49b258fcfb26eef8
Michael Chaly <Vizvezdenec@gmail.com>
2021-11-09 03:00:50 +0300

    bench 7242556

--- src/search.cpp
+++ src/search.cpp
@@ -1130,7 +1130,6 @@ moves_loop: // When in check, search starts here
 
       // Quiet ttMove extensions
       else if (   PvNode
-               && !likelyFailLow
                && move == ttMove
                && move == ss->killers[0]
                && (*contHist[0])[movedPiece][to_sq(move)] >= 10000)
@@ -1214,6 +1213,7 @@ moves_loop: // When in check, search starts here
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1                   ? 0
+                       : ss->inCheck && !captureOrPromotion && (*contHist[0])[movedPiece][to_sq(move)] < -10000 ? 0
                        : moveCount <= 5            ? 2
                        : PvNode && depth > 6       ? 1
                        : cutNode && moveCount <= 7 ? 1
