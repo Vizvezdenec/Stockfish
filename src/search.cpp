a1b6204c5245af2b233f47dff30e5207b05ec031
Michael Chaly <Vizvezdenec@gmail.com>
2021-11-09 11:10:08 +0300

    bench 7204777

--- src/search.cpp
+++ src/search.cpp
@@ -1213,7 +1213,8 @@ moves_loop: // When in check, search starts here
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1                   ? 0
-                       : moveCount <= 5            ? 2 - (r + 3)
+                       : moveCount == 2 && r < -4  ? 3
+                       : moveCount <= 5            ? 2
                        : PvNode && depth > 6       ? 1
                        : cutNode && moveCount <= 7 ? 1
                        :                             0;
