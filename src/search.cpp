fe2d9b865f5f4fb1ec7ecee810474519c5b4dc6b
Michael Chaly <Vizvezdenec@gmail.com>
2021-11-12 08:35:02 +0300

    bench 6886096

--- src/search.cpp
+++ src/search.cpp
@@ -1689,7 +1689,7 @@ moves_loop: // When in check, search starts here
     PieceType captured = type_of(pos.piece_on(to_sq(bestMove)));
 
     bonus1 = stat_bonus(depth + 1);
-    bonus2 = bestValue > beta + thisThread->rootDelta * 3 / 2 ? bonus1               // larger bonus
+    bonus2 = bestValue > beta + thisThread->rootDelta * 3 / 4 ? bonus1               // larger bonus
                                             : stat_bonus(depth);   // smaller bonus
 
     if (!pos.capture_or_promotion(bestMove))
