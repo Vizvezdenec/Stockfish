1b2c2fc8e56442e64f8ca63f247d5f24c65b565f
Michael Chaly <Vizvezdenec@gmail.com>
2021-11-18 15:07:12 +0300

    bench 6339548

--- src/search.cpp
+++ src/search.cpp
@@ -986,7 +986,7 @@ moves_loop: // When in check, search starts here
                          && (tte->bound() & BOUND_UPPER)
                          && tte->depth() >= depth;
 
-    bool onlySing = ss->nonSingExtensions > 10;
+    bool onlySing = ss->nonSingExtensions > 13;
 
     // Step 12. Loop through all pseudo-legal moves until no moves remain
     // or a beta cutoff occurs.
