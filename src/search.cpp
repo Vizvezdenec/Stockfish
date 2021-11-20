29181b3a1fe59e97abcbb801b2ae5dca237c8dab
Michael Chaly <Vizvezdenec@gmail.com>
2021-11-20 10:56:03 +0300

    bench 7418134

--- src/search.cpp
+++ src/search.cpp
@@ -1114,7 +1114,7 @@ moves_loop: // When in check, search starts here
           // If the eval of ttMove is greater than beta, we reduce it (negative extension)
           else if (ttValue >= beta)
           {
-              if (tte->depth() >= depth - 1)
+              if (tte->depth() >= depth - 0)
                   return beta;
               extension = -2;
           }
