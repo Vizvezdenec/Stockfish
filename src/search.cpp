5cbfe67d771afd2eb9c09ad3f68fdc686757b588
Michael Chaly <Vizvezdenec@gmail.com>
2021-11-24 21:31:32 +0300

    bench 6774489

--- src/search.cpp
+++ src/search.cpp
@@ -824,7 +824,7 @@ namespace {
         return eval;
 
     // Step 8. Null move search with verification search (~40 Elo)
-    if (   !ss->ttPv
+    if (   !PvNode
         && (ss-1)->currentMove != MOVE_NULL
         && (ss-1)->statScore < 23767
         &&  eval >= beta
@@ -878,7 +878,7 @@ namespace {
     // Step 9. ProbCut (~4 Elo)
     // If we have a good enough capture and a reduced search returns a value
     // much above beta, we can (almost) safely prune the previous move.
-    if (   !PvNode
+    if (   !ss->ttPv
         &&  depth > 4
         &&  abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         // if value from transposition table is lower than probCutBeta, don't attempt probCut
