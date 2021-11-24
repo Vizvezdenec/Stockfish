2584e9f046fb0bfc931539620963eae7ce04a04a
Michael Chaly <Vizvezdenec@gmail.com>
2021-11-24 21:23:59 +0300

    bench 6886220

--- src/search.cpp
+++ src/search.cpp
@@ -1066,7 +1066,7 @@ moves_loop: // When in check, search starts here
               // Futility pruning: parent node (~5 Elo)
               if (   !ss->inCheck
                   && lmrDepth < 8
-                  && ss->staticEval + 172 + (145 + history / 2048) * lmrDepth + history / 256 <= alpha)
+                  && ss->staticEval + 162 + 145 * lmrDepth + history / 256 <= alpha)
                   continue;
 
               // Prune moves with negative SEE (~20 Elo)
