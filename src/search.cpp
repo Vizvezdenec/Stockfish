33405c74cdddb5d2da998cce3646358cd5c1fdf8
Michael Chaly <Vizvezdenec@gmail.com>
2021-11-30 04:53:12 +0300

    bench 6328683

--- src/search.cpp
+++ src/search.cpp
@@ -1175,6 +1175,7 @@ moves_loop: // When in check, search starts here
           &&  moveCount > 1 + 2 * rootNode
           && (   !ss->ttPv
               || !captureOrPromotion
+              || ss->isProbcut
               || (cutNode && (ss-1)->moveCount > 1)))
       {
           Depth r = reduction(improving, depth, moveCount, rangeReduction > 2);
@@ -1211,9 +1212,6 @@ moves_loop: // When in check, search starts here
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
               r++;
-
-          if (ss->isProbcut)
-              r += 2;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
