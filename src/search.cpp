b645831e55d73d82015b231b6f17c8d4ff7ba929
Michael Chaly <Vizvezdenec@gmail.com>
2021-11-30 04:52:02 +0300

    bench 6977475

--- src/search.cpp
+++ src/search.cpp
@@ -1213,7 +1213,7 @@ moves_loop: // When in check, search starts here
               r++;
 
           if (ss->isProbcut)
-              r++;
+              r += 2;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
