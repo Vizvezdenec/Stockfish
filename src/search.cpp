600bf38c42ee07475fcf8ba29092804523c15c33
Michael Chaly <Vizvezdenec@gmail.com>
2021-12-01 16:46:01 +0300

    bench 6684405

--- src/search.cpp
+++ src/search.cpp
@@ -1107,7 +1107,9 @@ moves_loop: // When in check, search starts here
 
               // Avoid search explosion by limiting the number of double extensions
               if (   !PvNode
-                  && value < singularBeta - 75 + 40 * (move == ss->killers[0])
+                  && (value < singularBeta - 75 
+                      || (   move == ss->killers[0]
+                          && (*contHist[0])[movedPiece][to_sq(move)] >= 10000))
                   && ss->doubleExtensions <= 6)
                   extension = 2;
           }
