2aed54598f9849a4a6f3805e0e668073b3bc7bcf
Michael Chaly <Vizvezdenec@gmail.com>
2021-12-03 00:12:05 +0300

    bench 4952192

--- src/search.cpp
+++ src/search.cpp
@@ -1050,7 +1050,7 @@ moves_loop: // When in check, search starts here
 
               if (  !givesCheck
                   && depth == 1
-                  && captureCount > 3
+                  && captureCount > 4
                   && ss->staticEval > -VALUE_KNOWN_WIN
                   && type_of(move) != PROMOTION)
                   continue;
