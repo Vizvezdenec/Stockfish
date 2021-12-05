332016ad1ccc0b73b25cc2e004a08c7cd75a6f78
Michael Chaly <Vizvezdenec@gmail.com>
2021-12-06 02:54:33 +0300

    bench 4646607

--- src/search.cpp
+++ src/search.cpp
@@ -1052,7 +1052,7 @@ moves_loop: // When in check, search starts here
                   continue;
 
               // SEE based pruning
-              if (!pos.see_ge(move, Value(-198 - captHist / 128) * depth)) // (~25 Elo)
+              if (!pos.see_ge(move, Value(-213 - captHist / 512) * depth)) // (~25 Elo)
                   continue;
           }
           else
