b638fa8a0f70aafd5ef6dddfd97265623331deb0
Michael Chaly <Vizvezdenec@gmail.com>
2021-12-08 04:51:29 +0300

    bench 4532518

--- src/search.cpp
+++ src/search.cpp
@@ -1067,7 +1067,7 @@ moves_loop: // When in check, search starts here
 
               history += thisThread->mainHistory[us][from_to(move)];
 
-              lmrDepth = std::max(0, lmrDepth - (beta - alpha < thisThread->rootDelta / 4) - (!PvNode && move != ss->killers[0]));
+              lmrDepth = std::max(0, lmrDepth - (beta - alpha < thisThread->rootDelta / 4) - (cutNode && move != ss->killers[0]));
 
               // Futility pruning: parent node (~5 Elo)
               if (   !ss->inCheck
