fdbeac62d5a0821e3dca54ecbf6b9ec07edf8331
Michael Chaly <Vizvezdenec@gmail.com>
2021-12-09 18:43:34 +0300

    bench 4648292

--- src/search.cpp
+++ src/search.cpp
@@ -1239,9 +1239,9 @@ moves_loop: // When in check, search starts here
 
           // If the son is reduced and fails high it will be re-searched at full depth
           doFullDepthSearch = value > alpha && d < newDepth;
-          doDeeperSearch = value <= alpha + 88 ? 0
-                         : moveCount == 2      ? 2
-                         :                       1;
+          doDeeperSearch = value <= alpha + 88 ?           0
+                         : PvNode && moveCount == 2      ? 2
+                         :                                 1;
           didLMR = true;
       }
       else
