f3a55eb9067cc503ed8e74b2b77e869ef45940e5
Michael Chaly <Vizvezdenec@gmail.com>
2021-12-10 12:45:27 +0300

    bench 4644235

--- src/search.cpp
+++ src/search.cpp
@@ -1130,7 +1130,7 @@ moves_loop: // When in check, search starts here
       }
 
       // Capture extensions for PvNodes and cutNodes
-      else if (   (PvNode || cutNode)
+      else if (   (ss->ttPv || cutNode)
                && captureOrPromotion
                && moveCount != 1)
           extension = 1;
@@ -1251,7 +1251,7 @@ moves_loop: // When in check, search starts here
       // Step 17. Full depth search when LMR is skipped or fails high
       if (doFullDepthSearch)
       {
-          value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth + (1 + PvNode) * doDeeperSearch, !cutNode);
+          value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth + doDeeperSearch, !cutNode);
 
           // If the move passed LMR update its stats
           if (didLMR && !captureOrPromotion)
