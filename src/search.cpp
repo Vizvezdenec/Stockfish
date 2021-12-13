50e282eb3a034ac54a6ba7aedc7b1aefa02fa8b0
Michael Chaly <Vizvezdenec@gmail.com>
2021-12-13 13:28:14 +0300

    bench 4609282

--- src/search.cpp
+++ src/search.cpp
@@ -887,7 +887,7 @@ namespace {
         // because probCut search has depth set to depth - 4 but we also do a move before it
         // so effective depth is equal to depth - 3
         && !(   ss->ttHit
-             && tte->depth() >= depth - 3
+             && tte->depth() >= depth - 2
              && ttValue != VALUE_NONE
              && ttValue < probCutBeta))
     {
