e3878bab10adba1287fad8b7bc7d049306e4007c
Michael Chaly <Vizvezdenec@gmail.com>
2021-12-16 05:11:24 +0300

    bench 5047923

--- src/search.cpp
+++ src/search.cpp
@@ -1555,7 +1555,7 @@ moves_loop: // When in check, search starts here
           &&  type_of(move) != PROMOTION)
       {
 
-          if (moveCount > 2 - (beta - alpha < thisThread->rootDelta / 4))
+          if (moveCount > 2 - (PvNode && beta - alpha < thisThread->rootDelta / 4))
               continue;
 
           futilityValue = futilityBase + PieceValue[EG][pos.piece_on(to_sq(move))];
