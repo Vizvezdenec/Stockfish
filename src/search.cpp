42d6ae2c5b053212840ef681bf8723465e0016fe
Michael Chaly <Vizvezdenec@gmail.com>
2021-12-18 10:43:45 +0300

    bench 4484388

--- src/search.cpp
+++ src/search.cpp
@@ -1038,10 +1038,10 @@ moves_loop: // When in check, search starts here
               if (   !pos.empty(to_sq(move))
                   && !givesCheck
                   && !PvNode
-                  && lmrDepth < 6
+                  && lmrDepth < 4
                   && !ss->inCheck
-                  && ss->staticEval + 342 + 238 * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))] 
-                   + captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] / 8 < alpha)
+                  && ss->staticEval + 252 + 238 * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))] 
+                   + captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] / 16 < alpha)
                   continue;
 
               // SEE based pruning
