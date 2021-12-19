2256d4ed7b554a3a89a25f8535f051fd2c2036fb
Michael Chaly <Vizvezdenec@gmail.com>
2021-12-20 00:09:24 +0300

    bench 4847460

--- src/search.cpp
+++ src/search.cpp
@@ -141,7 +141,7 @@ namespace {
   Value value_from_tt(Value v, int ply, int r50c);
   void update_pv(Move* pv, Move move, Move* childPv);
   void update_continuation_histories(Stack* ss, Piece pc, Square to, int bonus);
-  void update_quiet_stats(const Position& pos, Stack* ss, Move move, int bonus, bool killUpd);
+  void update_quiet_stats(const Position& pos, Stack* ss, Move move, int bonus, Value kv);
   void update_all_stats(const Position& pos, Stack* ss, Move bestMove, Value bestValue, Value beta, Square prevSq,
                         Move* quietsSearched, int quietCount, Move* capturesSearched, int captureCount, Depth depth);
 
@@ -678,7 +678,7 @@ namespace {
             {
                 // Bonus for a quiet ttMove that fails high
                 if (!ttCapture)
-                    update_quiet_stats(pos, ss, ttMove, stat_bonus(depth), true);
+                    update_quiet_stats(pos, ss, ttMove, stat_bonus(depth), ttValue);
 
                 // Extra penalty for early quiet moves of the previous ply
                 if ((ss-1)->moveCount <= 2 && !priorCapture)
@@ -1700,7 +1700,7 @@ moves_loop: // When in check, search starts here
     if (!pos.capture_or_promotion(bestMove))
     {
         // Increase stats for the best move in case it was a quiet move
-        update_quiet_stats(pos, ss, bestMove, bonus2, bestValue >= beta);
+        update_quiet_stats(pos, ss, bestMove, bonus2, bestValue);
 
         // Decrease stats for all non-best quiet moves
         for (int i = 0; i < quietCount; ++i)
@@ -1747,13 +1747,19 @@ moves_loop: // When in check, search starts here
 
   // update_quiet_stats() updates move sorting heuristics
 
-  void update_quiet_stats(const Position& pos, Stack* ss, Move move, int bonus, bool killUpd) {
+  void update_quiet_stats(const Position& pos, Stack* ss, Move move, int bonus, Value kv) {
 
     // Update killers
-    if (ss->killers[0] != move && (killUpd || !ss->killers[0]))
+    if (ss->killers[0] != move)
     {
-        ss->killers[1] = ss->killers[0];
-        ss->killers[0] = move;
+        if (!ss->killers[0] || kv <= ss->killerValue)
+        {
+            ss->killers[1] = ss->killers[0];
+            ss->killers[0] = move;
+            ss->killerValue = kv;
+        }
+        else 
+            ss->killers[1] = move;
     }
 
     Color us = pos.side_to_move();
--- src/search.h
+++ src/search.h
@@ -46,6 +46,7 @@ struct Stack {
   Move currentMove;
   Move excludedMove;
   Move killers[2];
+  Value killerValue;
   Value staticEval;
   Depth depth;
   int statScore;
