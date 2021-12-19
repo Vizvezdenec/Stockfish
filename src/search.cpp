9a569fb4f95e449371bbe8db87637db6ca99c8d2
Michael Chaly <Vizvezdenec@gmail.com>
2021-12-19 06:07:52 +0300

    bench 4658497

--- src/search.cpp
+++ src/search.cpp
@@ -1246,7 +1246,7 @@ moves_loop: // When in check, search starts here
           (ss+1)->pv = pv;
           (ss+1)->pv[0] = MOVE_NONE;
 
-          if (rootNode && value >= beta && doDeeperSearch)
+          if (rootNode && value > alpha + 122 && doDeeperSearch)
               newDepth++;
 
           value = -search<PV>(pos, ss+1, -beta, -alpha,
