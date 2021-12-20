b933b4b404d7c065876ace63f44e64e35f120b64
Michael Chaly <Vizvezdenec@gmail.com>
2021-12-20 13:16:06 +0300

    bench 4705043

--- src/search.cpp
+++ src/search.cpp
@@ -1210,7 +1210,8 @@ moves_loop: // When in check, search starts here
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1                   ? 0
                        : moveCount <= 5            ? 2
-                       : PvNode || cutNode         ? 1
+                       : PvNode && depth > 6       ? 1
+                       : cutNode && moveCount <= 7 ? 1
                        :                             0;
 
           Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
