c7c74c467504fa5cdd75a09e11b33c529a0e75fc
Michael Chaly <Vizvezdenec@gmail.com>
2021-12-20 20:17:55 +0300

    bench 4493235

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -174,7 +174,8 @@ top:
 
   case GOOD_CAPTURE:
       if (select<Best>([&](){
-                       return pos.see_ge(*cur, Value(-69 * cur->value / 1024)) ?
+                       int history = (*captureHistory)[pos.moved_piece(cur->move)][to_sq(cur->move)][type_of(pos.piece_on(to_sq(cur->move)))];
+                       return pos.see_ge(*cur, Value(-(69 + history / 2048) * cur->value / 1024)) ?
                               // Move losing capture to endBadCaptures to be tried later
                               true : (*endBadCaptures++ = *cur, false); }))
           return *(cur - 1);
