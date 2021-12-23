cd81fd1fa108c0d96c39e6d7346173c053e8eceb
Michael Chaly <Vizvezdenec@gmail.com>
2021-12-23 03:39:49 +0300

    bench 4488731

--- src/search.cpp
+++ src/search.cpp
@@ -1190,7 +1190,7 @@ moves_loop: // When in check, search starts here
               r++;
 
           if (!ss->inCheck && thisThread->rootValue != VALUE_NONE)
-              r += abs(ss->staticEval * (2 * (us == WHITE) - 1) - thisThread->rootValue) / 512;
+              r += abs(ss->staticEval * (2 * (us == WHITE) - 1) - thisThread->rootValue) / (256 * (ss->ply * ss->ply + 1));
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
