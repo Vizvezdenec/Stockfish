8f5aa3923d8dc9636b840e7c93cb697e4b2f0536
Michael Chaly <Vizvezdenec@gmail.com>
2021-12-25 00:44:18 +0300

    bench 4737356

--- src/search.cpp
+++ src/search.cpp
@@ -1114,7 +1114,7 @@ moves_loop: // When in check, search starts here
 
           // If the eval of ttMove is greater than beta, we reduce it (negative extension)
           else if (ttValue >= beta)
-              extension = PvNode ? -2 : cutNode ? -1 : 0;
+              extension = PvNode || cutNode ? -2 : 0;
       }
 
       // Check extensions (~1 Elo)
