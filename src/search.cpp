b332171f50624b7a4baa4c9c354fa6619867125c
Michael Chaly <Vizvezdenec@gmail.com>
2021-12-25 17:35:31 +0300

    bench 4534828

--- src/search.cpp
+++ src/search.cpp
@@ -1221,14 +1221,6 @@ moves_loop: // When in check, search starts here
           didLMR = false;
       }
 
-      if (PvNode && captureOrPromotion && doFullDepthSearch && depth >= 3)
-      {
-          value = -qsearch<NonPV>(pos, ss+1, -(alpha+1), -alpha);
-          if (value > alpha)
-              doDeeperSearch = true;
-      }
-          
-
       // Step 17. Full depth search when LMR is skipped or fails high
       if (doFullDepthSearch)
       {
