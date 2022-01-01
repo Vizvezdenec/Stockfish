791bb66f0d86681b911d7c5f8d99eb40e9d44f96
Michael Chaly <Vizvezdenec@gmail.com>
2022-01-01 14:01:58 +0300

    bench 4185090

--- src/search.cpp
+++ src/search.cpp
@@ -1551,7 +1551,7 @@ moves_loop: // When in check, search starts here
 
       // Do not search moves with negative SEE values (~5 Elo)
       if (    bestValue > VALUE_TB_LOSS_IN_MAX_PLY
-          && !pos.see_ge(move, KnightValueMg - BishopValueMg))
+          && !pos.see_ge(move, captureOrPromotion ? KnightValueMg - BishopValueMg : Value(0)))
           continue;
 
       // Speculative prefetch as early as possible
