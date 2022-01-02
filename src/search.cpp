a98e3e11e243cd8ca8c5dd6a9a3c21c5bdd8d6a1
Michael Chaly <Vizvezdenec@gmail.com>
2022-01-02 10:14:01 +0300

    bench 3927439

--- src/search.cpp
+++ src/search.cpp
@@ -1550,7 +1550,7 @@ moves_loop: // When in check, search starts here
       }
 
       if (   captureOrPromotion && !givesCheck && !PvNode && bestValue > VALUE_TB_LOSS_IN_MAX_PLY
-          && thisThread->captureHistory[pos.moved_piece(move)][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
+          && thisThread->captureHistory[pos.moved_piece(move)][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < -5000)
           continue;
 
       // Do not search moves with negative SEE values (~5 Elo)
