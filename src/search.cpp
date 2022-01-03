e5217e886e59c5c8f62a428302f8569d6f6e53f5
Michael Chaly <Vizvezdenec@gmail.com>
2022-01-03 13:29:12 +0300

    bench 4397363

--- src/search.cpp
+++ src/search.cpp
@@ -78,7 +78,7 @@ namespace {
     return (3 + depth * depth) / (2 - improving);
   }
 
-  constexpr int maxSb = 2200;
+  constexpr int maxSb = 2049;
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
