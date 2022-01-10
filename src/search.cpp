e05d059e37e2abb81d9fe1bc1eb699c4fa7cebc2
Michael Chaly <Vizvezdenec@gmail.com>
2022-01-10 20:54:39 +0300

    bench 4675184

--- src/search.cpp
+++ src/search.cpp
@@ -1716,7 +1716,8 @@ moves_loop: // When in check, search starts here
     {
         // Increase stats for the best move in case it was a capture move
         captureHistory[moved_piece][to_sq(bestMove)][captured] << bonus1;
-        (*(ss-1)->continuationHistory1)[moved_piece][to_sq(bestMove)] << bonus1;
+        if (is_ok((ss-1)->currentMove))
+            (*(ss-1)->continuationHistory1)[moved_piece][to_sq(bestMove)] << bonus1;
     }
 
     // Extra penalty for a quiet early move that was not a TT move or
@@ -1731,7 +1732,8 @@ moves_loop: // When in check, search starts here
         moved_piece = pos.moved_piece(capturesSearched[i]);
         captured = type_of(pos.piece_on(to_sq(capturesSearched[i])));
         captureHistory[moved_piece][to_sq(capturesSearched[i])][captured] << -bonus1;
-        (*(ss-1)->continuationHistory1)[moved_piece][to_sq(capturesSearched[i])] << -bonus1;
+        if (is_ok((ss-1)->currentMove))
+            (*(ss-1)->continuationHistory1)[moved_piece][to_sq(capturesSearched[i])] << -bonus1;
     }
   }
 
