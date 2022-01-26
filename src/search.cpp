4f67f2a9aaab167eee7b13c1b09766e26de68cfe
Michael Chaly <Vizvezdenec@gmail.com>
2022-01-26 22:32:12 +0300

    bench 4759272

--- src/search.cpp
+++ src/search.cpp
@@ -1164,6 +1164,9 @@ moves_loop: // When in check, search starts here
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 14721;
 
+          if (move == ss->killers[0])
+              r = std::min(r, 0);
+
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
