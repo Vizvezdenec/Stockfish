02d566ac6f28597e9c60171f52b2577256aa3955
Michael Chaly <Vizvezdenec@gmail.com>
2022-01-27 03:03:49 +0300

    bench 4475744

--- src/search.cpp
+++ src/search.cpp
@@ -839,7 +839,7 @@ namespace {
     // If we have a good enough capture and a reduced search returns a value
     // much above beta, we can (almost) safely prune the previous move.
     if (   !PvNode
-        &&  depth > 1
+        &&  depth > 2
         &&  abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         // if value from transposition table is lower than probCutBeta, don't attempt probCut
         // there and in further interactions with transposition table cutoff depth is set to depth - 3
