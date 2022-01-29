8df31073739aedd403cced6c76aae972d5593084
Michael Chaly <Vizvezdenec@gmail.com>
2022-01-29 23:37:48 +0300

    bench 4706467

--- src/search.cpp
+++ src/search.cpp
@@ -1155,6 +1155,9 @@ moves_loop: // When in check, search starts here
           if (ttCapture)
               r++;
 
+          if (!ss->inCheck && !PvNode && complexity < 50 && std::abs(ss->staticEval - alpha) > 500)
+              r++;
+
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
@@ -1162,7 +1165,7 @@ moves_loop: // When in check, search starts here
                          - 4923;
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-          r -= ss->statScore / 14721 / (1 + 3 * captureOrPromotion);
+          r -= ss->statScore / 14721;
 
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
