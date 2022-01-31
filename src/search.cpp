b42ebade3b6c1bdf83aabe35d5a537312abf4a1f
Michael Chaly <Vizvezdenec@gmail.com>
2022-01-31 03:13:47 +0300

    bench 4819425

--- src/search.cpp
+++ src/search.cpp
@@ -1670,8 +1670,16 @@ moves_loop: // When in check, search starts here
         }
     }
     else
+    {
         // Increase stats for the best move in case it was a capture move
         captureHistory[moved_piece][to_sq(bestMove)][captured] << bonus1;
+        update_continuation_histories(ss, moved_piece, to_sq(bestMove), bonus1 / 4);
+        for (int i = 0; i < captureCount; ++i)
+        {
+            moved_piece = pos.moved_piece(capturesSearched[i]);
+            update_continuation_histories(ss, moved_piece, to_sq(capturesSearched[i]), - bonus1 / 4);
+        }
+    }
 
     // Extra penalty for a quiet early move that was not a TT move or
     // main killer move in previous ply when it gets refuted.
