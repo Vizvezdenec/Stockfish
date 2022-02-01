110d7d56f8065e1a3ae2669e97c073a94dfbe4aa
Michael Chaly <Vizvezdenec@gmail.com>
2022-02-01 14:29:41 +0300

    bench 4785656

--- src/search.cpp
+++ src/search.cpp
@@ -1332,7 +1332,7 @@ moves_loop: // When in check, search starts here
         bool extraBonus =    PvNode
                           || cutNode
                           || bestValue < alpha - 94 * depth
-                          || quietCount + captureCount > 32;
+                          || quietCount + captureCount > 20;
 
         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth) * (1 + extraBonus));
     }
