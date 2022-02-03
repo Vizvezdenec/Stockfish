69bb65a99a83b2e1799fe0b4eac014f9ea3af740
Michael Chaly <Vizvezdenec@gmail.com>
2022-02-03 09:02:42 +0300

    bench 4637392

--- src/search.cpp
+++ src/search.cpp
@@ -157,7 +157,7 @@ namespace {
 void Search::init() {
 
   for (int i = 1; i < MAX_MOVES; ++i)
-      Reductions[i] = int((21.9 + std::log(Threads.size()) / 2) * (std::log(i) + std::log(Threads.size()) / 16));
+      Reductions[i] = int((21.9 + std::log(Threads.size()) / 2) * (std::log(i) + std::log(Threads.size()) / 32));
 }
 
 
