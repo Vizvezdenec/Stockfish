464477640efd540504058410bb9b6d2c5b9368f4
Michael Chaly <Vizvezdenec@gmail.com>
2022-02-05 11:44:10 +0300

    bench blabla

--- src/search.cpp
+++ src/search.cpp
@@ -354,7 +354,7 @@ void Thread::search() {
               beta  = std::min(prev + delta, VALUE_INFINITE);
 
               // Adjust trend and optimism based on root move's previousScore
-              int tr = sigmoid(prev, 0, 0, 147, 113, 1);
+              int tr = sigmoid(prev, 0, 3, 137, 111, 0);
               trend = (us == WHITE ?  make_score(tr, tr / 2)
                                    : -make_score(tr, tr / 2));
 
