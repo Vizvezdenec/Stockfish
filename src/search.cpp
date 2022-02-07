f34475aab453dc91566d4cc6b9fa7dc824b3c188
Michael Chaly <Vizvezdenec@gmail.com>
2022-02-07 19:29:03 +0300

    bench 4761513

--- src/search.cpp
+++ src/search.cpp
@@ -1224,7 +1224,7 @@ moves_loop: // When in check, search starts here
           (ss+1)->pv = pv;
           (ss+1)->pv[0] = MOVE_NONE;
 
-          value = -search<PV>(pos, ss+1, -std::max(beta,value), -alpha,
+          value = -search<PV>(pos, ss+1, -std::max(beta,(value + 3 * beta) / 4), -alpha,
                               std::min(maxNextDepth, newDepth), false);
       }
 
