0390b1c5b17b164d0c99ccc99d36f3a386fbf545
Michael Chaly <Vizvezdenec@gmail.com>
2022-02-09 08:20:04 +0300

    bench 4955550

--- src/search.cpp
+++ src/search.cpp
@@ -1156,7 +1156,7 @@ moves_loop: // When in check, search starts here
               && !likelyFailLow)
               r -= 2;
 
-          if (ss->searchCmplx > 500)
+          if (ss->searchCmplx > 700)
               r--;
 
           // Decrease reduction if opponent's move count is high (~1 Elo)
