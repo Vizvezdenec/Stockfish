1cd13ffa3be440165f3ef223487055153b9a06b9
Michael Chaly <Vizvezdenec@gmail.com>
2022-02-11 03:30:50 +0300

    bench 5064286

--- src/search.cpp
+++ src/search.cpp
@@ -920,7 +920,7 @@ namespace {
     if (     !PvNode
           && !cutNode
           && !ttMove
-          && depth >= 7
+          && depth >= 11
           && ss->ttHit
           && (tte->bound() & BOUND_UPPER)
           && ttValue < alpha - 90 * depth)
