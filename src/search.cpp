1d3076c21cf596533cc6e52e596bdfd773cc7d8e
Michael Chaly <Vizvezdenec@gmail.com>
2022-02-14 00:22:53 +0300

    bench 4377728

--- src/search.cpp
+++ src/search.cpp
@@ -1108,8 +1108,7 @@ moves_loop: // When in check, search starts here
           else if (   PvNode
                    && move == ttMove
                    && move == ss->killers[0]
-                   && (*contHist[0])[movedPiece][to_sq(move)] >= 7546
-                   && !likelyFailLow)
+                   && (*contHist[0])[movedPiece][to_sq(move)] >= 7546)
               extension = 1;
       }
 
