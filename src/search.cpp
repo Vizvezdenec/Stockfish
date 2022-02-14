8af8735a5648aef53c06dd19a2c1a4b286acb12b
Michael Chaly <Vizvezdenec@gmail.com>
2022-02-15 01:39:31 +0300

    bench 4351767

--- src/search.cpp
+++ src/search.cpp
@@ -1059,7 +1059,7 @@ moves_loop: // When in check, search starts here
           // a reduced search on all the other moves but the ttMove and if the
           // result is lower than ttValue minus a margin, then we will extend the ttMove.
           if (   !rootNode
-              &&  depth >= 6 + 3 * (PvNode && tte->is_pv())
+              &&  depth >= 6 + 2 * (PvNode && tte->is_pv())
               &&  move == ttMove
               && !excludedMove // Avoid recursive singular search
            /* &&  ttValue != VALUE_NONE Already implicit in the next condition */
@@ -1353,10 +1353,10 @@ moves_loop: // When in check, search starts here
     // If no good move is found and the previous position was ttPv, then the previous
     // opponent move is probably good and the new position is added to the search tree.
     if (bestValue <= alpha)
-        ss->ttPv = ss->ttPv || ((ss-1)->ttPv && depth > 2);
+        ss->ttPv = ss->ttPv || ((ss-1)->ttPv && depth > 3);
     // Otherwise, a counter move has been found and if the position is the last leaf
     // in the search tree, remove the position from the search tree.
-    else if (depth > 2)
+    else if (depth > 3)
         ss->ttPv = ss->ttPv && (ss+1)->ttPv;
 
     // Write gathered information in transposition table
