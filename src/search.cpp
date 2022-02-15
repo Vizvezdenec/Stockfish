4f439e31ff57af5a04468277a4fa8f557b4ea278
Michael Chaly <Vizvezdenec@gmail.com>
2022-02-15 12:51:11 +0300

    bench 4510013

--- src/search.cpp
+++ src/search.cpp
@@ -778,7 +778,7 @@ namespace {
     // return a fail low.
     if (   !PvNode
         && depth <= 6
-        && eval < alpha - 486 - 314 * depth * depth + (ss-1)->statScore / 256)
+        && eval < alpha - 486 - 314 * depth * depth + (ss-1)->statScore / 128)
     {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
         if (value < alpha)
