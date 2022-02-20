35986686bc2839326d50ba6a3d23b93ed2147bdb
Michael Chaly <Vizvezdenec@gmail.com>
2022-02-21 01:37:35 +0300

    bench 6810304

--- src/search.cpp
+++ src/search.cpp
@@ -1182,7 +1182,7 @@ moves_loop: // When in check, search starts here
                        : moveCount <= 4            ? 2
                        : PvNode && depth > 4       ? 1
                        : cutNode && moveCount <= 8 ? 1
-                       : ss->ply < 4               ? 1
+                       : ss->ply < 5               ? 1
                        :                             0;
 
           Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
