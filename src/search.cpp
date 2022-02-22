5ee97ea09887950e163aa276b97e6240eb3092f1
Michael Chaly <Vizvezdenec@gmail.com>
2022-02-22 07:07:02 +0300

    bench 6988750

--- src/search.cpp
+++ src/search.cpp
@@ -1136,7 +1136,7 @@ moves_loop: // When in check, search starts here
       // been searched. In general we would like to reduce them, but there are many
       // cases where we extend a son if it has good chances to be "interesting".
       if (    depth >= 2
-          &&  moveCount > 1 + (PvNode && ss->ply <= 1)
+          &&  moveCount > 1
           && (   !ss->ttPv
               || !captureOrPromotion
               || (cutNode && (ss-1)->moveCount > 1)))
@@ -1182,8 +1182,10 @@ moves_loop: // When in check, search starts here
                        : moveCount <= 4            ? 2
                        : PvNode && depth > 4       ? 1
                        : cutNode && moveCount <= 8 ? 1
-                       : (ss->ply < 3 && PvNode)   ? 1
                        :                             0;
+
+          if (PvNode && ss->ply <= 1 && moveCount == 2)
+              r = std::min(r, 0);
 
           Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
 
