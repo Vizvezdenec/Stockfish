2fa7147219e8130fdc12a0cc16fdece02b8f6f69
Michael Chaly <Vizvezdenec@gmail.com>
2022-02-23 05:38:56 +0300

    bench 6359862

--- src/search.cpp
+++ src/search.cpp
@@ -1022,6 +1022,7 @@ moves_loop: // When in check, search starts here
 
               if (   lmrDepth < 1
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0
+                  && !PvNode
                   && !pos.see_ge(move))
                   continue;
 
