c5755f25c5430e904aa95a198bab15833ac58918
Michael Chaly <Vizvezdenec@gmail.com>
2022-03-02 02:34:46 +0300

    bench 6202920

--- src/search.cpp
+++ src/search.cpp
@@ -886,7 +886,7 @@ namespace {
                 value = -qsearch<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1);
 
                 // If the qsearch held, perform the regular search
-                if (value < probCutBeta + 150 * (depth - 4) * (depth - 4) && value >= probCutBeta)
+                if (value < probCutBeta + 450 * (depth - 4) * (depth - 4) && value >= probCutBeta)
                     value = -search<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1, depth - 4, !cutNode);
 
                 pos.undo_move(move);
