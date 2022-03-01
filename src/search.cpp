7ff447464f47c89c589656018e7cf040d74ccf2d
Michael Chaly <Vizvezdenec@gmail.com>
2022-03-01 14:58:04 +0300

    bench 7127800

--- src/search.cpp
+++ src/search.cpp
@@ -1190,7 +1190,7 @@ moves_loop: // When in check, search starts here
 
           // If the son is reduced and fails high it will be re-searched at full depth
           doFullDepthSearch = value > alpha && d < newDepth;
-          doDeeperSearch = value > (alpha + 78 + 11 * (newDepth - d) - 28 * (move == ss->killers[0]));
+          doDeeperSearch = value > (alpha + 78 + 11 * (newDepth - d) - 14 * (move == ss->killers[0]));
           didLMR = true;
       }
       else
