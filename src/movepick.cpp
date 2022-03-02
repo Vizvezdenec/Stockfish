756adf2401a52147a0c4aa0ec6d7c915da2c7bf5
Michael Chaly <Vizvezdenec@gmail.com>
2022-03-02 17:51:31 +0300

    bench 6865880

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -169,7 +169,7 @@ top:
       endMoves = generate<CAPTURES>(pos, cur);
 
       score<CAPTURES>();
-      partial_insertion_sort(cur, endMoves, -3000 * depth);
+      partial_insertion_sort(cur, endMoves, -3094 * depth);
       ++stage;
       goto top;
 
@@ -207,7 +207,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -3000 * depth);
+          partial_insertion_sort(cur, endMoves, -2758 * depth);
       }
 
       ++stage;
--- src/search.cpp
+++ src/search.cpp
@@ -886,7 +886,7 @@ namespace {
                 value = -qsearch<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1);
 
                 // If the qsearch held, perform the regular search
-                if ((depth > 6 || value < probCutBeta + 150 * (depth - 4) * (depth - 4)) && value >= probCutBeta)
+                if (value >= probCutBeta)
                     value = -search<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1, depth - 4, !cutNode);
 
                 pos.undo_move(move);
