835a6e093af7a25acf6daa0b1378ea0c937059b0
Michael Chaly <Vizvezdenec@gmail.com>
2022-03-04 21:07:02 +0300

    bench 6181026

--- src/search.cpp
+++ src/search.cpp
@@ -1196,6 +1196,7 @@ moves_loop: // When in check, search starts here
       else
       {
           doFullDepthSearch = !PvNode || moveCount > 1;
+          doDeeperSearch = PvNode && captureOrPromotion && moveCount <= 15;
           didLMR = false;
       }
 
