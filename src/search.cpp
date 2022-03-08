edde3427df934406adce5a8bcdc427cdcc944660
Michael Chaly <Vizvezdenec@gmail.com>
2022-03-08 06:32:08 +0300

    bench 6897911

--- src/search.cpp
+++ src/search.cpp
@@ -71,7 +71,7 @@ namespace {
 
   Depth reduction(bool i, Depth d, int mn, Value delta, Value rootDelta) {
     int r = Reductions[d] * Reductions[mn];
-    return (r + 1463 - int(delta) * 1024 / int(rootDelta) + 512 * !i) / 1024;
+    return (r + 1463 - int(delta) * 1024 / int(rootDelta) + 622 * !i) / 1024;
   }
 
   constexpr int futility_move_count(bool improving, Depth depth) {
