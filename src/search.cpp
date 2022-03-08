6a1a1555e097645fc14b440039abb80ddda716ac
Michael Chaly <Vizvezdenec@gmail.com>
2022-03-08 04:26:22 +0300

    bench 6777168

--- src/search.cpp
+++ src/search.cpp
@@ -1149,7 +1149,7 @@ moves_loop: // When in check, search starts here
               r--;
 
           if (bestValue > VALUE_KNOWN_WIN)
-              r++;
+              r += 2;
 
           // Decrease reduction if position is or has been on the PV
           // and node is not likely to fail low. (~3 Elo)
