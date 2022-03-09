81e4301f9dedd84c6ef80c79cfa378336c7c299c
Michael Chaly <Vizvezdenec@gmail.com>
2022-03-10 01:11:56 +0300

    bench 6604811

--- src/search.cpp
+++ src/search.cpp
@@ -1095,7 +1095,7 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
               else if (ttValue >= beta)
-                  extension = -2 - 2 * (ttValue >= beta + 2 * depth);
+                  extension = std::max(-4, -(newDepth - 1));
           }
 
           // Check extensions (~1 Elo)
