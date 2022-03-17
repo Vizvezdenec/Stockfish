cfba94efa627899aef1e85f2f9b6c05062d234f1
Michael Chaly <Vizvezdenec@gmail.com>
2022-03-18 02:38:18 +0300

    bench 7113002

--- src/search.cpp
+++ src/search.cpp
@@ -1155,8 +1155,8 @@ moves_loop: // When in check, search starts here
               r -= 2;
 
           // Decrease reduction if opponent's move count is high (~1 Elo)
-          if ((ss-1)->moveCount > 7 && (ss-1)->currentMove != (ss-1)->killers[0])
-              r--;
+          if ((ss-1)->moveCount > 7)
+              r -= 1 + (move == countermove);
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode && move != ss->killers[0])
