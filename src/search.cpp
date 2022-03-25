69413d87941f301e705a70f60d4bf7d5a9dfbd62
Michael Chaly <Vizvezdenec@gmail.com>
2022-03-25 22:30:57 +0300

    bench 7429831

--- src/search.cpp
+++ src/search.cpp
@@ -1145,6 +1145,7 @@ moves_loop: // When in check, search starts here
 
           // Decrease reduction at some PvNodes (~2 Elo)
           if (   PvNode
+              && !likelyFailLow
               && bestMoveCount <= 3)
               r--;
 
