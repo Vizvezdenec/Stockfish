8d858baa6021d484d84d5ffc6fda04e6d7b5ff81
Michael Chaly <Vizvezdenec@gmail.com>
2022-04-04 15:50:29 +0300

    bench 7086494

--- src/search.cpp
+++ src/search.cpp
@@ -781,7 +781,7 @@ namespace {
         && eval < alpha - 348 - 258 * depth * depth)
     {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
-        if (value < alpha || value > beta + 101 * (depth - 1))
+        if (value < alpha || value > beta + 101 * (depth - improving))
             return value;
     }
 
