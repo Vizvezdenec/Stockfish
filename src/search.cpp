ed7364315520aa2cb1f6414513435f6ae190762c
Michael Chaly <Vizvezdenec@gmail.com>
2022-04-04 15:48:57 +0300

    bench 7479263

--- src/search.cpp
+++ src/search.cpp
@@ -781,7 +781,7 @@ namespace {
         && eval < alpha - 348 - 258 * depth * depth)
     {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
-        if (value < alpha)
+        if (value < alpha || value > beta + 101 * (depth - 1))
             return value;
     }
 
