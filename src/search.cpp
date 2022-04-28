dc873492526f04d9eabd9e69d8d2f08fa7b4a478
Michael Chaly <Vizvezdenec@gmail.com>
2022-04-28 16:48:17 +0300

    bench 7735398

--- src/search.cpp
+++ src/search.cpp
@@ -1170,7 +1170,7 @@ moves_loop: // When in check, search starts here
           // Decrease reduction at PvNodes if bestvalue
           // is vastly different from static evaluation
           if (PvNode && !ss->inCheck && abs(ss->staticEval - bestValue) > 250)
-              r -= 1 + (abs(ss->staticEval - bestValue) > 2000);
+              r--;
 
           // Decrease reduction for PvNodes based on depth
           if (PvNode)
@@ -1184,6 +1184,9 @@ moves_loop: // When in check, search starts here
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 15914;
+
+          if (PvNode)
+              r = std::min(r, 3);
 
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
