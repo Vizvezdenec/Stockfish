9fb319157fda9c71ed92a5e5583a611ee4de1194
Michael Chaly <Vizvezdenec@gmail.com>
2022-04-29 11:45:15 +0300

    bench 8153966

--- src/search.cpp
+++ src/search.cpp
@@ -742,7 +742,7 @@ namespace {
 
         // ttValue can be used as a better position evaluation (~4 Elo)
         if (    ttValue != VALUE_NONE
-            && tte->depth() > 0
+            && tte->depth() >= 0
             && (tte->bound() & (ttValue > eval ? BOUND_LOWER : BOUND_UPPER)))
             eval = ttValue;
     }
