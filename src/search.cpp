f0683c20ad902286b95e6d2ac8bc9471713dc9b7
Michael Chaly <Vizvezdenec@gmail.com>
2022-05-03 03:04:43 +0300

    bench 7811594

--- src/search.cpp
+++ src/search.cpp
@@ -1116,8 +1116,6 @@ moves_loop: // When in check, search starts here
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5491)
               extension = 1;
-          else if (move == ss->captureReply)
-              extension = 1;
       }
 
       // Add extension to new depth
@@ -1145,6 +1143,7 @@ moves_loop: // When in check, search starts here
       // cases where we extend a son if it has good chances to be "interesting".
       if (    depth >= 2
           &&  moveCount > 1 + (PvNode && ss->ply <= 1)
+          &&  move != ss->captureReply
           && (   !ss->ttPv
               || !capture
               || (cutNode && (ss-1)->moveCount > 1)))
