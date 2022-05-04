53cd9413c0c6991e97de111711982da13dcd26b8
Michael Chaly <Vizvezdenec@gmail.com>
2022-05-04 14:03:30 +0300

    bench 6574118

--- src/search.cpp
+++ src/search.cpp
@@ -874,8 +874,6 @@ namespace {
             {
                 assert(pos.capture(move) || promotion_type(move) == QUEEN);
 
-                if (move == ttMove && eval < ss->staticEval && eval < probCutBeta - 10 * depth)
-                    continue;
                 captureOrPromotion = true;
 
                 ss->currentMove = move;
@@ -1176,7 +1174,7 @@ moves_loop: // When in check, search starts here
 
           // Decrease reduction for PvNodes based on depth
           if (PvNode)
-              r -= 1 + 15 / ( 3 + depth );
+              r -= 1 + 15 / ( 3 + depth ) + ((ss+1)->cutoffCnt == 0);
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
