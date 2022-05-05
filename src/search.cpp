be2226a9fa71f7d60bcdc07c996cd9d08bb0f8b8
Michael Chaly <Vizvezdenec@gmail.com>
2022-05-05 05:19:42 +0300

    bench 6088128

--- src/search.cpp
+++ src/search.cpp
@@ -1145,7 +1145,6 @@ moves_loop: // When in check, search starts here
           &&  moveCount > 1 + (PvNode && ss->ply <= 1)
           && (   !ss->ttPv
               || !capture
-              || ((ss+1)->cutoffCnt > 4 && !PvNode)
               || (cutNode && (ss-1)->moveCount > 1)))
       {
           Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
@@ -1178,8 +1177,8 @@ moves_loop: // When in check, search starts here
               r -= 1 + 15 / ( 3 + depth );
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
-          if ((ss+1)->cutoffCnt > 3 && !PvNode)
-              r++;
+          if (!PvNode)
+              r += (ss+1)->cutoffCnt / 4;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
