56a513f83e17b9760fc32a4f33501fbce129462a
Michael Chaly <Vizvezdenec@gmail.com>
2022-05-05 08:06:27 +0300

    bench 5993430

--- src/search.cpp
+++ src/search.cpp
@@ -1174,10 +1174,10 @@ moves_loop: // When in check, search starts here
 
           // Decrease reduction for PvNodes based on depth
           if (PvNode)
-              r -= 1 + 15 / ( 3 + depth );
+             r -= 1 + 15 / ( 3 + depth ) + ((ss+1)->cutoffCnt == 0 && depth < 9);
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
-          if (((ss+1)->cutoffCnt > 3 - cutNode) && !PvNode)
+          if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
