5f88fbd98500c50de174e6d399a371ffd15c91ed
Michael Chaly <Vizvezdenec@gmail.com>
2022-05-07 23:42:23 +0300

    bench 6983745

--- src/search.cpp
+++ src/search.cpp
@@ -1764,11 +1764,10 @@ moves_loop: // When in check, search starts here
     Thread* thisThread = pos.this_thread();
     thisThread->mainHistory[us][from_to(move)] << bonus;
     update_continuation_histories(ss, pos.moved_piece(move), to_sq(move), bonus);
-
     if (type_of(pos.moved_piece(move)) != PAWN && !ss->inCheck)
     {
         Move rm = make_move(to_sq(move), from_sq(move));
-        thisThread->mainHistory[us][from_to(rm)] << -bonus / 8;
+        thisThread->mainHistory[us][from_to(rm)] << -bonus / 16;
     }
 
     // Update countermove history
