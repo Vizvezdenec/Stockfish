d8d61d23f1c7cd1ef944420b7930300ac42b6c0e
Michael Chaly <Vizvezdenec@gmail.com>
2022-05-11 16:34:20 +0300

    bench 6665740

--- src/search.cpp
+++ src/search.cpp
@@ -801,7 +801,7 @@ namespace {
         && (ss-1)->statScore < 14695
         &&  eval >= beta
         &&  eval >= ss->staticEval
-        &&  ss->staticEval >= beta - 15 * depth - improvement / 15 + 198 + complexity / 28 + ss->nmpFailCnt * ss->nmpFailCnt * 5
+        &&  ss->staticEval >= beta - 15 * depth - improvement / 15 + 198 + complexity / 28 + 50 * std::max(ss->nmpFailCnt - 3, 0)
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))
