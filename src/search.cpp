1e6fb45cfd49246b1b1f156a18f31221ccaea738
Michael Chaly <Vizvezdenec@gmail.com>
2022-05-16 06:12:29 +0300

    bench 6704651

--- src/search.cpp
+++ src/search.cpp
@@ -1178,7 +1178,7 @@ moves_loop: // When in check, search starts here
               r--;
 
           // Decrease reduction for PvNodes based on depth
-          if (PvNode && bestMoveCount < 3)
+          if (PvNode && bestMoveCount < 4)
               r -= 1 + 15 / ( 3 + depth );
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
