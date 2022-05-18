35274261a52dff319a7c45001c4c6b1aba20b406
Michael Chaly <Vizvezdenec@gmail.com>
2022-05-18 20:28:35 +0300

    bench 6244569

--- src/search.cpp
+++ src/search.cpp
@@ -787,8 +787,7 @@ namespace {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
         if (value < alpha)
         {
-            if (value <= alpha - 193)
-                ss->cutoffCnt = 0;
+            ss->cutoffCnt--;
             return value;
         }
     }
