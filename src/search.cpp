1356e192da0249734e25e440934c9c496dcf20ac
Michael Chaly <Vizvezdenec@gmail.com>
2022-05-19 09:51:42 +0300

    bench 6699684

--- src/search.cpp
+++ src/search.cpp
@@ -1183,7 +1183,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (move == ss->killers[0] && ss->killerCnt > 6)
+          if (move == ss->killers[0] && ss->killerCnt > 7)
               r--;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
