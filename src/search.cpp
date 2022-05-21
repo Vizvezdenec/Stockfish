3c0b005d82ce6a3cf174a0555f5cd33bd10e8ab0
Michael Chaly <Vizvezdenec@gmail.com>
2022-05-21 21:21:11 +0300

    bench 7364993

--- src/search.cpp
+++ src/search.cpp
@@ -62,8 +62,8 @@ namespace {
   enum NodeType { NonPV, PV, Root };
 
   // Futility margin
-  Value futility_margin(Depth d, bool improving) {
-    return Value(168 * (d - improving));
+  Value futility_margin(Depth d, bool improving, bool ge) {
+    return Value((168 - 10 * ge) * (d - improving));
   }
 
   // Reductions lookup table, initialized at startup
@@ -793,7 +793,7 @@ namespace {
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
         &&  depth < 8
-        &&  eval - futility_margin(depth, improving) - (ss-1)->statScore / 256 >= beta
+        &&  eval - futility_margin(depth, improving, eval > ss->staticEval) - (ss-1)->statScore / 256 >= beta
         &&  eval >= beta
         &&  eval < 26305) // larger than VALUE_KNOWN_WIN, but smaller than TB wins.
         return eval;
@@ -1148,7 +1148,6 @@ moves_loop: // When in check, search starts here
           &&  moveCount > 1 + (PvNode && ss->ply <= 1)
           && (   !ss->ttPv
               || !capture
-              || (!PvNode && thisThread->id() % 8 == 7)
               || (cutNode && (ss-1)->moveCount > 1)))
       {
           Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
