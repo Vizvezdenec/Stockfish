3aa0319fe10ac2ff39a16be0e42c0abceccd9cd6
Michael Chaly <Vizvezdenec@gmail.com>
2022-05-25 13:59:29 +0300

    bench 6849979

--- src/search.cpp
+++ src/search.cpp
@@ -1145,7 +1145,7 @@ moves_loop: // When in check, search starts here
       // been searched. In general we would like to reduce them, but there are many
       // cases where we extend a son if it has good chances to be "interesting".
       if (    depth >= 2
-          &&  moveCount > 1 + (PvNode && ss->ply <= thisThread->previousDepth / 16)
+          &&  moveCount > 1 + (PvNode && ss->ply < thisThread->previousDepth / 8)
           && (   !ss->ttPv
               || !capture
               || (cutNode && (ss-1)->moveCount > 1)))
