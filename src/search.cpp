916c809f562893e4becaf59960f93520f21bb540
Michael Chaly <Vizvezdenec@gmail.com>
2022-05-26 20:59:34 +0300

    bench 6264360

--- src/search.cpp
+++ src/search.cpp
@@ -855,7 +855,7 @@ namespace {
 
     attackedCalc = true;
     attacked = pos.attacks_by<PAWN>(~us) | pos.attacks_by<KNIGHT>(~us) | pos.attacks_by<BISHOP>(~us) 
-             | pos.attacks_by<ROOK>(~us) | pos.attacks_by<KING>(~us) | pos.attacks_by<QUEEN>(~us);
+             | pos.attacks_by<ROOK>(~us);
 
     // Step 10. ProbCut (~4 Elo)
     // If we have a good enough capture and a reduced search returns a value
@@ -971,7 +971,7 @@ moves_loop: // When in check, search starts here
 
     if (!attackedCalc)
         attacked = pos.attacks_by<PAWN>(~us) | pos.attacks_by<KNIGHT>(~us) | pos.attacks_by<BISHOP>(~us) 
-                 | pos.attacks_by<ROOK>(~us) | pos.attacks_by<KING>(~us) | pos.attacks_by<QUEEN>(~us);
+                 | pos.attacks_by<ROOK>(~us);
 
     // Step 13. Loop through all pseudo-legal moves until no moves remain
     // or a beta cutoff occurs.
@@ -1520,7 +1520,7 @@ moves_loop: // When in check, search starts here
 
     Color us = pos.side_to_move();
     Bitboard attacked = pos.attacks_by<PAWN>(~us) | pos.attacks_by<KNIGHT>(~us) | pos.attacks_by<BISHOP>(~us) 
-                      | pos.attacks_by<ROOK>(~us) | pos.attacks_by<KING>(~us) | pos.attacks_by<QUEEN>(~us);
+                      | pos.attacks_by<ROOK>(~us);
 
     // Initialize a MovePicker object for the current position, and prepare
     // to search the moves. Because the depth is <= 0 here, only captures,
