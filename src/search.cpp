09e37b812bbf776703331dc33e89c13d15f3af8d
Michael Chaly <Vizvezdenec@gmail.com>
2022-05-29 22:42:51 +0300

    bench 6746613

--- src/search.cpp
+++ src/search.cpp
@@ -413,7 +413,7 @@ void Thread::search() {
               }
               else if (bestValue >= beta)
               {
-                  hardFH = bestValue >= beta + 200;
+                  hardFH = bestValue >= beta + 100;
                   beta = std::min(bestValue + delta, VALUE_INFINITE);
                   ++failedHighCnt;
               }
