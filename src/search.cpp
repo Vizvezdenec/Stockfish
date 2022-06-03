35aa58f8db7fbaf3d5c68cff71055141324b8953
Michael Chaly <Vizvezdenec@gmail.com>
2022-06-03 10:22:27 +0300

    bench 6430815

--- src/search.cpp
+++ src/search.cpp
@@ -1067,10 +1067,10 @@ moves_loop: // When in check, search starts here
            /* &&  ttValue != VALUE_NONE Already implicit in the next condition */
               &&  abs(ttValue) < VALUE_KNOWN_WIN
               && (tte->bound() & BOUND_LOWER)
-              &&  tte->depth() >= depth - 3)
+              &&  tte->depth() >= depth - 3 + tte->is_pv())
           {
               Value singularBeta = ttValue - 3 * depth;
-              Depth singularDepth = (depth - 1 + (!PvNode && tte->is_pv())) / 2;
+              Depth singularDepth = (depth - 1) / 2;
 
               ss->excludedMove = move;
               value = search<NonPV>(pos, ss, singularBeta - 1, singularBeta, singularDepth, cutNode);
