3cc31ee4c954cf64fa83ba6ddac77ee5873d0d7f
Michael Chaly <Vizvezdenec@gmail.com>
2022-06-04 11:00:44 +0300

    bench 6674565

--- src/search.cpp
+++ src/search.cpp
@@ -1015,7 +1015,7 @@ moves_loop: // When in check, search starts here
               // Futility pruning for captures (~0 Elo)
               if (   !pos.empty(to_sq(move))
                   && !givesCheck
-                  && !PvNode
+                  && (cutNode || !ss->ttPv)
                   && lmrDepth < 6
                   && !ss->inCheck
                   && ss->staticEval + 281 + 179 * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))]
