0f893d63d8e31c03bf9e99a020a94d4021ff92ca
Michael Chaly <Vizvezdenec@gmail.com>
2022-06-11 08:14:53 +0300

    bench 5585125

--- src/search.cpp
+++ src/search.cpp
@@ -1190,7 +1190,7 @@ moves_loop: // When in check, search starts here
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
-          int deeper =   r >= std::min(-1, depth - 4)                   ? 0
+          int deeper =   r >= std::min(-1, depth - 6)                   ? 0
                        : moveCount <= 4            ? 2
                        : PvNode || cutNode         ? 1
                        :                             0;
