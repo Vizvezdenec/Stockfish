f36174a476f4608bb5f9669e5768613baff23ca7
Michael Chaly <Vizvezdenec@gmail.com>
2022-06-11 06:51:10 +0300

    bench 5327169

--- src/search.cpp
+++ src/search.cpp
@@ -867,7 +867,7 @@ namespace {
     {
         assert(probCutBeta < VALUE_INFINITE);
 
-        if (ss->ttHit && ttCapture &&  tte->depth() >= depth - 2 && (tte->bound() & BOUND_LOWER) && ttValue != VALUE_NONE && ttValue >= probCutBeta)
+        if (ss->ttHit && ttCapture &&  tte->depth() >= depth - 2 && (tte->bound() == BOUND_LOWER) && ttValue != VALUE_NONE && ttValue >= probCutBeta)
             return ttValue;
 
         MovePicker mp(pos, ttMove, probCutBeta - ss->staticEval, depth - 3, &captureHistory);
