d31b551d40c2a0b7bc391f4f8a54459ee40b60cf
Michael Chaly <Vizvezdenec@gmail.com>
2022-06-13 01:09:40 +0300

    bench     Move killers[2] = {ss->killers[0], ss->killers[1]};

--- src/search.cpp
+++ src/search.cpp
@@ -948,7 +948,7 @@ moves_loop: // When in check, search starts here
     Move killers[2] = {ss->killers[0], ss->killers[1]};
     if (type_of(pos.piece_on(prevSq)) == PAWN)
     {
-        Bitboard pawnAttacks = pos.attacks_by<PAWN>(~us);
+        Bitboard pawnAttacks = pawn_attacks_bb(~us, prevSq);
         if (type_of(pos.moved_piece(ss->killers[0])) != PAWN && (pawnAttacks & to_sq(ss->killers[0])))
             killers[0] = MOVE_NONE;
         if (type_of(pos.moved_piece(ss->killers[1])) != PAWN && (pawnAttacks & to_sq(ss->killers[1])))
