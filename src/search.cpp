b90ca1e1185069285ce988000bab8f3827f8e531
Michael Chaly <Vizvezdenec@gmail.com>
2022-06-13 11:44:51 +0300

    bench 6166065

--- src/search.cpp
+++ src/search.cpp
@@ -1010,7 +1010,7 @@ moves_loop: // When in check, search starts here
           moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
-          int lmrDepth = std::max(newDepth + 2 * cutNode - 1 - reduction(improving, depth, moveCount, delta, thisThread->rootDelta), 0);
+          int lmrDepth = std::max(newDepth - 2 * cutNode + 1 - reduction(improving, depth, moveCount, delta, thisThread->rootDelta), 0);
 
           if (   capture
               || givesCheck)
