109e4d5cd7d17d534d05171dfa501991ead216f5
Michael Chaly <Vizvezdenec@gmail.com>
2022-06-14 23:41:13 +0300

    bench 5408227

--- src/search.cpp
+++ src/search.cpp
@@ -1148,7 +1148,7 @@ moves_loop: // When in check, search starts here
           &&  moveCount > 1 + (PvNode && ss->ply <= 1)
           && (   !ss->ttPv
               || !capture
-              || (!PvNode && depth < 5 && ss->staticEval + PieceValue[EG][type_of(pos.captured_piece())] + 555 <= alpha)
+              || (!PvNode && depth < 4 && ss->staticEval + PieceValue[EG][type_of(pos.captured_piece())] + 123 <= alpha)
               || (cutNode && (ss-1)->moveCount > 1)))
       {
           Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
