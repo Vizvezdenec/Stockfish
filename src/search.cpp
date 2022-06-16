1a6cd43d94d76f3518f25b922163f28e90817cfd
Michael Chaly <Vizvezdenec@gmail.com>
2022-06-16 10:13:30 +0300

    bench 5121907

--- src/search.cpp
+++ src/search.cpp
@@ -672,7 +672,7 @@ namespace {
         && ttCapture
         && tte->depth() >= depth - 2
         && (tte->bound() & BOUND_LOWER)
-        && ttValue >= beta + 400
+        && ttValue >= beta + 300
         && ttValue < VALUE_KNOWN_WIN)
         return ttValue;
 
