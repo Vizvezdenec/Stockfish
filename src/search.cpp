3c85c59aef1f3610de5f9115db491c0396c74bc7
Michael Chaly <Vizvezdenec@gmail.com>
2022-06-17 20:59:34 +0300

    bench 5608524

--- src/search.cpp
+++ src/search.cpp
@@ -1158,7 +1158,7 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode && move != ss->killers[0])
-              r += 4 - 12 / depth;
+              r += 4 - 6 / depth;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
