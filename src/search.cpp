a5d8e3020ffe7d7cabf8168b9525009a1a4a49eb
Michael Chaly <Vizvezdenec@gmail.com>
2022-06-19 21:47:40 +0300

    bench 5357745

--- src/search.cpp
+++ src/search.cpp
@@ -955,6 +955,8 @@ moves_loop: // When in check, search starts here
                          && (tte->bound() & BOUND_UPPER)
                          && tte->depth() >= depth;
 
+    bool singular = false;
+
     // Step 13. Loop through all pseudo-legal moves until no moves remain
     // or a beta cutoff occurs.
     while ((move = mp.next_move(moveCountPruning)) != MOVE_NONE)
@@ -1082,6 +1084,8 @@ moves_loop: // When in check, search starts here
                       && value < singularBeta - 26
                       && ss->doubleExtensions <= 8)
                       extension = 2;
+
+                  singular = true;
               }
 
               // Multi-cut pruning
@@ -1170,6 +1174,9 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
+              r++;
+
+          if (singular)
               r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
