099b7693a56a87fb7ea17dc3f558539c9b454ee7
Michael Chaly <Vizvezdenec@gmail.com>
2022-06-23 09:52:18 +0300

    bench 5669621

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -201,7 +201,7 @@ top:
       endMoves = generate<CAPTURES>(pos, cur);
 
       score<CAPTURES>();
-      partial_insertion_sort(cur, endMoves, depth >= 0 ? -3000 * depth : -2200 * depth);
+      partial_insertion_sort(cur, endMoves, -1000 * depth * abs(depth));
       ++stage;
       goto top;
 
