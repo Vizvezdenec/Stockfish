2ae9fde6a7a64e9be6100c92bcec29c5e612b033
Michael Chaly <Vizvezdenec@gmail.com>
2022-06-29 19:29:20 +0300

    bench 5700219

--- src/search.cpp
+++ src/search.cpp
@@ -933,7 +933,7 @@ moves_loop: // When in check, search starts here
         && !PvNode
         && ss->ttHit
         && ttMove
-        && depth <= 3
+        && depth <= 2
         && (tte->bound() & BOUND_UPPER)
         && tte->depth() >= depth - 3
         && ttValue <= alpha
