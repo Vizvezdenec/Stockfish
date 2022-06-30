a16964ab0a09706334fa39c7781499ee97784bbb
Michael Chaly <Vizvezdenec@gmail.com>
2022-06-30 09:26:35 +0300

    bench 5616078

--- src/search.cpp
+++ src/search.cpp
@@ -1178,10 +1178,12 @@ moves_loop: // When in check, search starts here
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 15914;
 
+          r += (ss-1)->statScore / 65536;
+
           // In general we want to cap the LMR depth search at newDepth, but when
           // reduction is negative, we allow this move a limited search extension
           // beyond the first move depth. This may lead to hidden double extensions.
-          Depth d = std::clamp(newDepth - r, 1, newDepth + 1 + 2 * capture);
+          Depth d = std::clamp(newDepth - r, 1, newDepth + 1);
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
