6fde39716c912c824f03b7acffda1108b19d2543
Michael Chaly <Vizvezdenec@gmail.com>
2022-07-01 22:15:50 +0300

    bench 5311641

--- src/search.cpp
+++ src/search.cpp
@@ -1096,7 +1096,7 @@ moves_loop: // When in check, search starts here
               // If the eval of ttMove is less than alpha and value, we reduce it (negative extension)
               else if (ttValue <= alpha && ttValue <= value)
               {
-                  if (ttValue < alpha - 200 && value < alpha - 100)
+                  if (value - ttValue < 50 && value < alpha - 100)
                       depth--;
                   else
                       extension = -1;
