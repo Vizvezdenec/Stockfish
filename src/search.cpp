21d5ee5259c2f40cba956b19638d7d075d27df43
Michael Chaly <Vizvezdenec@gmail.com>
2022-07-18 13:58:12 +0300

    bench 5905619

--- src/search.cpp
+++ src/search.cpp
@@ -1161,7 +1161,7 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
-              r++;
+              r += 1 + (!PvNode && (thisThread->id() % 2 == 1));
 
           // Decrease reduction for PvNodes based on depth
           if (PvNode)
