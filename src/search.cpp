af71acf6a2124e191e020950b23fa9609b66a3fc
Michael Chaly <Vizvezdenec@gmail.com>
2022-07-20 17:26:00 +0300

    bench 5191751

--- src/search.cpp
+++ src/search.cpp
@@ -1141,6 +1141,7 @@ moves_loop: // When in check, search starts here
           &&  moveCount > 1 + (PvNode && ss->ply <= 1)
           && (   !ss->ttPv
               || !capture
+              || (!ss->inCheck && complexity < 30)
               || (cutNode && (ss-1)->moveCount > 1)))
       {
           Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
