943fc17b294bc4091c4d3b671537fd413a74a25d
Michael Chaly <Vizvezdenec@gmail.com>
2022-07-21 15:47:44 +0300

    bench 6086986

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1086,7 +1086,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   // Damp down the evaluation linearly when shuffling
   v = v * (195 - pos.rule50_count()) / 211;
 
-  v += v > VALUE_ZERO ? -Value(20) : v < VALUE_ZERO ? Value(20) : VALUE_ZERO;
+  v += v > VALUE_ZERO ? -Value(30) : v < VALUE_ZERO ? Value(30) : VALUE_ZERO;
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
