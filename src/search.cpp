27f5e3d5f79207a95aad27583e431d1f6f746a60
Michael Chaly <Vizvezdenec@gmail.com>
2022-07-25 08:48:28 +0300

    bench 6027905

--- src/search.cpp
+++ src/search.cpp
@@ -954,6 +954,8 @@ moves_loop: // When in check, search starts here
                          && (tte->bound() & BOUND_UPPER)
                          && tte->depth() >= depth;
 
+    bool isGoodMove = false;
+
     // Step 13. Loop through all pseudo-legal moves until no moves remain
     // or a beta cutoff occurs.
     while ((move = mp.next_move(moveCountPruning)) != MOVE_NONE)
@@ -1195,6 +1197,7 @@ moves_loop: // When in check, search starts here
       else
       {
           doFullDepthSearch = !PvNode || moveCount > 1;
+          doDeeperSearch = PvNode && capture && !isGoodMove;
           didLMR = false;
       }
 
@@ -1279,6 +1282,8 @@ moves_loop: // When in check, search starts here
 
           if (value > alpha)
           {
+              isGoodMove = capture;
+
               bestMove = move;
 
               if (PvNode && !rootNode) // Update pv even in fail-high case
