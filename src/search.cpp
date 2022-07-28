b9b081206cbad5c8da0e31f083022c1212bcbd2b
Michael Chaly <Vizvezdenec@gmail.com>
2022-07-28 07:07:34 +0300

    bench 5353431

--- src/search.cpp
+++ src/search.cpp
@@ -1523,7 +1523,7 @@ moves_loop: // When in check, search starts here
       {
           mc++;
 
-          if (mc > 2)
+          if (mc > 1)
               continue;
 
           futilityValue = futilityBase + PieceValue[EG][pos.piece_on(to_sq(move))];
