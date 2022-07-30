99a27209a8580401d45e556c40a16072d9a15ca8
Michael Chaly <Vizvezdenec@gmail.com>
2022-07-30 05:47:29 +0300

    bench 5073560

--- src/search.cpp
+++ src/search.cpp
@@ -1286,7 +1286,7 @@ moves_loop: // When in check, search starts here
 
               if (PvNode && value < beta) // Update alpha! Always alpha < beta
               {
-                  if (!rootNode && value > (depth + 1) * delta / (depth + 2) + alpha)
+                  if (!rootNode && value > (depth * depth + 1) * delta / (depth * depth + 2) + alpha)
                       break;
                   alpha = value;
 
