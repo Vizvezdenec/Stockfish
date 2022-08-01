b6691464b5223e14869d9ae51e2e2289663b693f
Michael Chaly <Vizvezdenec@gmail.com>
2022-08-01 19:45:25 +0300

    bench 6396395

--- src/search.cpp
+++ src/search.cpp
@@ -1077,7 +1077,7 @@ moves_loop: // When in check, search starts here
                   extension = 1;
 
                   // Avoid search explosion by limiting the number of double extensions
-                  if (  !PvNode
+                  if (  (!PvNode || tte->bound() == BOUND_EXACT)
                       && value < singularBeta - 26
                       && ss->doubleExtensions <= 8)
                       extension = 2;
@@ -1165,7 +1165,7 @@ moves_loop: // When in check, search starts here
 
           // Decrease reduction for PvNodes based on depth
           if (PvNode)
-              r -= 1 + 15 / (3 + depth) - (ss->ttHit && ttMove && tte->depth() >= depth && (tte->bound() == BOUND_EXACT));
+              r -= 1 + 15 / (3 + depth);
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
