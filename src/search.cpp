ee6257f0a411801d210711e2346ac61b57d91ae8
Michael Chaly <Vizvezdenec@gmail.com>
2022-08-01 19:43:33 +0300

    bench 5531182

--- src/search.cpp
+++ src/search.cpp
@@ -1165,7 +1165,7 @@ moves_loop: // When in check, search starts here
 
           // Decrease reduction for PvNodes based on depth
           if (PvNode)
-              r -= 1 + 15 / (3 + depth) - (ss->ttHit && ttMove && (tte->bound() == BOUND_EXACT));
+              r -= 1 + 15 / (3 + depth) - (ss->ttHit && ttMove && tte->depth() >= depth && (tte->bound() == BOUND_EXACT));
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
