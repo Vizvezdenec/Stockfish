b12045a6dcb3415720982dcb9ce32b27d5b8e78e
Michael Chaly <Vizvezdenec@gmail.com>
2022-08-09 11:50:41 +0300

    bench 5242392

--- src/search.cpp
+++ src/search.cpp
@@ -1169,8 +1169,8 @@ moves_loop: // When in check, search starts here
               r -= 1 + 15 / (3 + depth);
 
           // Decrease reduction if ttMove has been singularly extended (~1 Elo)
-          if (singularQuietLMR)
-              r -= 1 + capture;
+          if (singularQuietLMR && capture)
+              r -= 2;
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
