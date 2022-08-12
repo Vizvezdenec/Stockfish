b066a1d217cb660b7b8edbe38523fc747f03b13a
Michael Chaly <Vizvezdenec@gmail.com>
2022-08-12 21:33:32 +0300

    bench 6107404

--- src/search.cpp
+++ src/search.cpp
@@ -754,7 +754,7 @@ namespace {
         if (!excludedMove)
             tte->save(posKey, VALUE_NONE, ss->ttPv, BOUND_NONE, DEPTH_NONE, MOVE_NONE, ss->staticEval);
 
-        if (!PvNode && ss->staticEval >= beta)
+        if (!PvNode && ss->staticEval <= alpha)
             eval = qsearch<NonPV>(pos, ss, alpha, beta);
         else eval = ss->staticEval;
     }
