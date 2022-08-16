beb55396e75ff90b8af59eb553644555e8bb3d8e
Michael Chaly <Vizvezdenec@gmail.com>
2022-08-16 10:44:12 +0300

    bench 5583181

--- src/search.cpp
+++ src/search.cpp
@@ -890,7 +890,7 @@ namespace {
                 if (value >= probCutBeta)
                 {
                     // Save ProbCut data into transposition table
-                    if (!ss->ttHit || tte->depth() <= depth - 2)
+                    if (!ss->ttHit || tte->depth() <= depth - 4)
                         tte->save(posKey, value_to_tt(value, ss->ply), ss->ttPv, BOUND_LOWER, depth - 3, move, ss->staticEval);
                     return value;
                 }
