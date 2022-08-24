b4c3c9c5b64f691cf86b1c7f9338f6cbb6e7965d
Michael Chaly <Vizvezdenec@gmail.com>
2022-08-25 02:34:26 +0300

    bench 5582504

--- src/search.cpp
+++ src/search.cpp
@@ -1028,7 +1028,7 @@ moves_loop: // When in check, search starts here
                   && history < -3875 * (depth - 1))
                   continue;
 
-              history += 2 * thisThread->mainHistory[us][from_to(move)];
+              history += 4 * thisThread->mainHistory[us][from_to(move)];
 
               // Futility pruning: parent node (~9 Elo)
               if (   !ss->inCheck
