9d319f40fdfb79b73ddecde199ace852c56cf675
Michael Chaly <Vizvezdenec@gmail.com>
2022-08-26 04:09:48 +0300

    bench 6046537

--- src/search.cpp
+++ src/search.cpp
@@ -798,7 +798,7 @@ namespace {
     if (   !PvNode
         && (ss-1)->currentMove != MOVE_NULL
         && (ss-1)->statScore < 14695
-        &&  eval >= beta + 3 * (eval - ss->staticEval) / 4
+        &&  eval >= beta + (eval - ss->staticEval) / 4
         &&  eval >= ss->staticEval
         &&  ss->staticEval >= beta - 15 * depth - improvement / 15 + 201 + complexity / 24
         && !excludedMove
