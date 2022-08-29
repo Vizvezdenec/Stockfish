5cd386791c9159806ea0f13960247f1ac87685dd
Michael Chaly <Vizvezdenec@gmail.com>
2022-08-29 08:40:39 +0300

    bench 6050484

--- src/search.cpp
+++ src/search.cpp
@@ -1170,7 +1170,7 @@ moves_loop: // When in check, search starts here
               r++;
 
           if (rootNode)
-              r += thisThread->failedHighCnt * moveCount / 64;
+              r += thisThread->failedHighCnt * moveCount / 128;
 
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
