e2241ba83e43bf1018f746011fd1ab716a3a3ac0
Michael Chaly <Vizvezdenec@gmail.com>
2022-09-04 01:52:21 +0300

    bench 5489973

--- src/search.cpp
+++ src/search.cpp
@@ -1149,15 +1149,15 @@ moves_loop: // When in check, search starts here
           if ((ss-1)->moveCount > 7)
               r--;
 
-          if (!PvNode && ss->ttHit && tte->bound() == BOUND_EXACT)
-              r++;
-
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
               r += 2;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
+              r++;
+
+          if (!PvNode && !cutNode && ss->ttHit && tte->bound() == BOUND_EXACT)
               r++;
 
           // Decrease reduction for PvNodes based on depth
