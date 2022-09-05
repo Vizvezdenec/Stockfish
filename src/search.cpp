7be4ea89ce06d18b6bc981accae7d1d6987ea591
Michael Chaly <Vizvezdenec@gmail.com>
2022-09-06 02:13:00 +0300

    bench 6041755

--- src/search.cpp
+++ src/search.cpp
@@ -1056,11 +1056,11 @@ moves_loop: // When in check, search starts here
               &&  move == ttMove
               && !excludedMove // Avoid recursive singular search
            /* &&  ttValue != VALUE_NONE Already implicit in the next condition */
-              &&  abs(ttValue) < VALUE_KNOWN_WIN
+              &&  abs(ttValue) < 9000
               && (tte->bound() & BOUND_LOWER)
               &&  tte->depth() >= depth - 3)
           {
-              Value singularBeta = ttValue - (3 - (ttCapture && !PvNode && tte->bound() == BOUND_EXACT)) * depth;
+              Value singularBeta = ttValue - 3 * depth;
               Depth singularDepth = (depth - 1) / 2;
 
               ss->excludedMove = move;
