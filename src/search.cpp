5e0c90f8af69a14e262340271ae6c65a7a2d8753
Michael Chaly <Vizvezdenec@gmail.com>
2022-09-06 08:53:20 +0300

    bench 5523719

--- src/search.cpp
+++ src/search.cpp
@@ -1738,7 +1738,7 @@ moves_loop: // When in check, search starts here
     Color us = pos.side_to_move();
     Thread* thisThread = pos.this_thread();
     thisThread->mainHistory[us][from_to(move)] << bonus;
-    if (type_of(move) == NORMAL)
+    if (type_of(move) == NORMAL && type_of(pos.moved_piece(move)) != PAWN)
         thisThread->mainHistory[us][from_to(make_move(to_sq(move), from_sq(move)))] << -bonus / 2;
     update_continuation_histories(ss, pos.moved_piece(move), to_sq(move), bonus);
 
