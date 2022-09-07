e1e47cb4d3cebb7e2c2ebed7a59183523dcefb7b
Michael Chaly <Vizvezdenec@gmail.com>
2022-09-07 04:30:32 +0300

    bench 5401910

--- src/search.cpp
+++ src/search.cpp
@@ -74,9 +74,9 @@ namespace {
     return (r + 1463 - int(delta) * 1024 / int(rootDelta)) / 1024 + (!i && r > 1010);
   }
 
-  constexpr int futility_move_count(bool improving, Depth depth) {
-    return improving ? (3 + depth * depth)
-                     : (3 + depth * depth) / 2;
+  constexpr int futility_move_count(bool improving, Depth depth, Value delta, Value rootDelta) {
+    return improving ? (3 + depth * depth + 2 * int(delta) / int(rootDelta))
+                     : (3 + depth * depth + 2 * int(delta) / int(rootDelta)) / 2;
   }
 
   // History and stats update bonus, based on depth
@@ -995,7 +995,7 @@ moves_loop: // When in check, search starts here
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~7 Elo)
-          moveCountPruning = moveCount >= futility_move_count(improving, depth);
+          moveCountPruning = moveCount >= futility_move_count(improving, depth, delta, thisThread->rootDelta);
 
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, delta, thisThread->rootDelta), 0);
@@ -1159,7 +1159,7 @@ moves_loop: // When in check, search starts here
 
           // Decrease reduction for PvNodes based on depth
           if (PvNode)
-              r -= 1 + 15 / (3 + depth / 2);
+              r -= 1 + 15 / (3 + depth);
 
           // Decrease reduction if ttMove has been singularly extended (~1 Elo)
           if (singularQuietLMR)
