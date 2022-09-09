7a8b52417b43e7982fb5727ec37315b54c83e662
Michael Chaly <Vizvezdenec@gmail.com>
2022-09-09 10:40:40 +0300

    bench 5320945

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -106,7 +106,7 @@ void MovePicker::score() {
 
   static_assert(Type == CAPTURES || Type == QUIETS || Type == EVASIONS, "Wrong type");
 
-  Bitboard threatened, threatenedByPawn, threatenedByMinor, threatenedByRook;
+  Bitboard threatened, threatenedByPawn, threatenedByMinor, threatenedByRook, checkByRook, checkByBishop, checkByKnight, checkByQueen;
   if constexpr (Type == QUIETS)
   {
       Color us = pos.side_to_move();
@@ -121,6 +121,12 @@ void MovePicker::score() {
       threatened =  (pos.pieces(us, QUEEN) & threatenedByRook)
                   | (pos.pieces(us, ROOK)  & threatenedByMinor)
                   | (pos.pieces(us, KNIGHT, BISHOP) & threatenedByPawn);
+
+
+      checkByRook = pos.check_squares(ROOK);
+      checkByBishop = pos.check_squares(BISHOP);
+      checkByKnight = pos.check_squares(KNIGHT);
+      checkByQueen = checkByRook | checkByBishop;
   }
   else
   {
@@ -129,6 +135,10 @@ void MovePicker::score() {
       (void) threatenedByPawn;
       (void) threatenedByMinor;
       (void) threatenedByRook;
+      (void) checkByRook;
+      (void) checkByBishop;
+      (void) checkByKnight;
+      (void) checkByQueen;
   }
 
   for (auto& m : *this)
@@ -147,7 +157,11 @@ void MovePicker::score() {
                           : type_of(pos.moved_piece(m)) == ROOK  && !(to_sq(m) & threatenedByMinor) ? 25000
                           :                                         !(to_sq(m) & threatenedByPawn)  ? 15000
                           :                                                                           0)
-                          :                                                                           0);
+                          :                                                                           0)
+                   + (  (type_of(pos.moved_piece(m)) == QUEEN && (to_sq(m) & checkByQueen))
+                     || (type_of(pos.moved_piece(m)) == ROOK && (to_sq(m) & checkByRook))
+                     || (type_of(pos.moved_piece(m)) == BISHOP && (to_sq(m) & checkByBishop))
+                     || (type_of(pos.moved_piece(m)) == KNIGHT && (to_sq(m) & checkByKnight)) ? pos.non_pawn_material() * int(pos.non_pawn_material() / 4096) : 0);
 
       else // Type == EVASIONS
       {
--- src/search.cpp
+++ src/search.cpp
@@ -1001,7 +1001,7 @@ moves_loop: // When in check, search starts here
           int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, delta, thisThread->rootDelta), 0);
 
           if (   capture
-              || (givesCheck && pos.non_pawn_material() > 4000))
+              || givesCheck)
           {
               // Futility pruning for captures (~0 Elo)
               if (   !pos.empty(to_sq(move))
