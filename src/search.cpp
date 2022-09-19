9439316269fcbfd66e3edbcefc183a290099c8c0
Michael Chaly <Vizvezdenec@gmail.com>
2022-09-19 21:05:56 +0300

    bench 4372187

--- src/search.cpp
+++ src/search.cpp
@@ -1168,7 +1168,7 @@ moves_loop: // When in check, search starts here
               r++;
 
           if (excludedMove && bestValue <= alpha - 25)
-              r--;
+              r++;
 
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
