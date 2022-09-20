a30d474732bd842dcc365c29b613af8837ccbe74
Michael Chaly <Vizvezdenec@gmail.com>
2022-09-20 16:54:39 +0300

    bench 4446586

--- src/search.cpp
+++ src/search.cpp
@@ -1105,7 +1105,7 @@ moves_loop: // When in check, search starts here
                    && move == ttMove
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5177)
-              extension = 1;
+              extension = 1, singularQuietLMR = true;
       }
 
       // Add extension to new depth
