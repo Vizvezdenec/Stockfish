5fcb8e4d9f7de0f7118e96ffaeb55af5b9793649
Michael Chaly <Vizvezdenec@gmail.com>
2022-09-21 01:23:45 +0300

    bench 4486408

--- src/search.cpp
+++ src/search.cpp
@@ -1087,7 +1087,7 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
               else if (ttValue >= beta)
-                  extension = -2;
+                  extension = -2, singularQuietLMR = true;
 
               // If the eval of ttMove is less than alpha and value, we reduce it (negative extension)
               else if (ttValue <= alpha && ttValue <= value)
