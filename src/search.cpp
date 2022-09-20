b0a35f5ec5e5a669bb9d6b60ce34c6dd68db1616
Michael Chaly <Vizvezdenec@gmail.com>
2022-09-20 11:10:14 +0300

    bench 4115702

--- src/search.cpp
+++ src/search.cpp
@@ -1023,7 +1023,7 @@ moves_loop: // When in check, search starts here
 
               // Continuation history based pruning (~2 Elo)
               if (   lmrDepth < 5
-                  && history < -(3875 - (ss-1)->statScore / 256) * (depth - 1))
+                  && history < -(3875 - (ss-1)->statScore / 64) * (depth - 1))
                   continue;
 
               history += 2 * thisThread->mainHistory[us][from_to(move)];
