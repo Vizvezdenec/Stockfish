8ae0a143f1aa1fc88f4b2785e83fc18704c8324a
Michael Chaly <Vizvezdenec@gmail.com>
2022-09-21 20:13:00 +0300

    bench 4202855

--- src/search.cpp
+++ src/search.cpp
@@ -1068,7 +1068,7 @@ moves_loop: // When in check, search starts here
               if (value < singularBeta)
               {
                   extension = 1;
-                  singularQuietLMR = !ttCapture && value >= singularBeta - 10 * singularDepth;
+                  singularQuietLMR = !ttCapture;
 
                   // Avoid search explosion by limiting the number of double extensions
                   if (  !PvNode
@@ -1160,7 +1160,7 @@ moves_loop: // When in check, search starts here
               r -= 1 + 11 / (3 + depth);
 
           // Decrease reduction if ttMove has been singularly extended (~1 Elo)
-          if (singularQuietLMR && (capture || moveCount < 20))
+          if (singularQuietLMR && (capture || moveCount < 6))
               r--;
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
