6f1021a75d4da38d7deaec986232ba6524e277e2
Michael Chaly <Vizvezdenec@gmail.com>
2022-09-21 16:01:45 +0300

    bench 4444532

--- src/search.cpp
+++ src/search.cpp
@@ -1068,7 +1068,7 @@ moves_loop: // When in check, search starts here
               if (value < singularBeta)
               {
                   extension = 1;
-                  singularQuietLMR = !ttCapture && value >= singularBeta - 200;
+                  singularQuietLMR = !ttCapture && value >= singularBeta - 100;
 
                   // Avoid search explosion by limiting the number of double extensions
                   if (  !PvNode
