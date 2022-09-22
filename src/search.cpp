423e993a627ce539b7b947f1dc6fdd79b76d9a91
Michael Chaly <Vizvezdenec@gmail.com>
2022-09-22 09:17:40 +0300

    bench 4632716

--- src/search.cpp
+++ src/search.cpp
@@ -1106,7 +1106,7 @@ moves_loop: // When in check, search starts here
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5177)
               extension = 1;
-          else if (PvNode && capture && depth <= 3 && moveCount > 1)
+          else if (PvNode && capture && depth <= 7 && moveCount > 1)
               extension = 1;
       }
 
