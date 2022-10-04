bc3fa0f7614028f0413e610bead9717f59a605e3
Michael Chaly <Vizvezdenec@gmail.com>
2022-10-04 06:56:05 +0300

    bench 4444088

--- src/search.cpp
+++ src/search.cpp
@@ -1101,7 +1101,7 @@ moves_loop: // When in check, search starts here
               extension = 1;
 
           // Quiet ttMove extensions (~0 Elo)
-          else if (  (PvNode || depth >= 7)
+          else if (  (PvNode || depth >= 9)
                    && move == ttMove
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5177)
