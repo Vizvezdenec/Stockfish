8fe50e99748e1fff74e6c736dfcb0fcbc7472b5a
Michael Chaly <Vizvezdenec@gmail.com>
2022-10-24 03:22:43 +0300

    bench 4112575

--- src/search.cpp
+++ src/search.cpp
@@ -1166,7 +1166,7 @@ moves_loop: // When in check, search starts here
               && (mp.threatenedPieces & from_sq(move)))
               r--;
 
-          if ((PvNode || cutNode) && !ttMove)
+          if ((PvNode || cutNode) && !ttMove && moveCount < 4)
               r--;
 
           // Increase reduction if next ply has a lot of fail high
