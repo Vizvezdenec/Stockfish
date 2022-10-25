4756bcba208c3cec6218fb507019785b861dcf13
Michael Chaly <Vizvezdenec@gmail.com>
2022-10-25 07:28:33 +0300

    bench 4325388

--- src/search.cpp
+++ src/search.cpp
@@ -1177,7 +1177,7 @@ moves_loop: // When in check, search starts here
                          - 4433;
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-          r -= ss->statScore / (16628 - 12000 / (depth + 2));
+          r -= ss->statScore / (16628 - 3000 / (depth - 1));
 
           // In general we want to cap the LMR depth search at newDepth, but when
           // reduction is negative, we allow this move a limited search extension
