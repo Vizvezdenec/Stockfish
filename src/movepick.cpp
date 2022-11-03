6c719453f8542dc8e098431ddff2748b82fc164a
Michael Chaly <Vizvezdenec@gmail.com>
2022-11-03 06:55:35 +0300

    bench 4806372

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -257,11 +257,13 @@ top:
       endMoves = generate<EVASIONS>(pos, cur);
 
       score<EVASIONS>();
+      partial_insertion_sort(cur, endMoves, -750 * depth);
+
       ++stage;
       [[fallthrough]];
 
   case EVASION:
-      return select<Best>([](){ return true; });
+      return select<Next>([&](){ return true; });
 
   case PROBCUT:
       return select<Next>([&](){ return pos.see_ge(*cur, threshold); });
