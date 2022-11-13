d23a1137bf5fd2d31407f6f7d8dff33207ce6f26
Michael Chaly <Vizvezdenec@gmail.com>
2022-11-13 16:52:02 +0300

    bench 4087363

--- src/search.cpp
+++ src/search.cpp
@@ -1185,7 +1185,8 @@ moves_loop: // When in check, search starts here
           if (value > alpha && d < newDepth)
           {
               const bool doDeeperSearch = value > (alpha + 64 + 11 * (newDepth - d));
-              value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth + doDeeperSearch, !cutNode);
+              const bool doShallowerSearch = value < bestValue + 16;
+              value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, std::max(d + 1, newDepth + doDeeperSearch - doShallowerSearch), !cutNode);
 
               int bonus = value > alpha ?  stat_bonus(newDepth)
                                         : -stat_bonus(newDepth);
