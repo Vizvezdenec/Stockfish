de7f551a07b5086228fecd55cfe7e1aef9a764d8
Michael Chaly <Vizvezdenec@gmail.com>
2022-11-20 17:13:45 +0300

    bench 4079015

--- src/search.cpp
+++ src/search.cpp
@@ -1124,7 +1124,6 @@ moves_loop: // When in check, search starts here
       // cases where we extend a son if it has good chances to be "interesting".
       if (    depth >= 2
           &&  moveCount > 1 + (PvNode && ss->ply <= 1)
-          && !(ttMove && !ttCapture && moveCount == 2)
           && (   !ss->ttPv
               || !capture
               || (cutNode && (ss-1)->moveCount > 1)))
@@ -1165,6 +1164,9 @@ moves_loop: // When in check, search starts here
           // Increase reduction if next ply has a lot of fail high
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
+
+          if (ttMove && !ttCapture && capture && moveCount == 2)
+              r--;
 
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
