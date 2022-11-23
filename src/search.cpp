abfd72c1d060ee50a8fe3d9053c73d8ae7a1acdc
Michael Chaly <Vizvezdenec@gmail.com>
2022-11-23 16:44:06 +0300

    bench 4625809

--- src/search.cpp
+++ src/search.cpp
@@ -74,9 +74,9 @@ namespace {
     return (r + 1642 - int(delta) * 1024 / int(rootDelta)) / 1024 + (!i && r > 916);
   }
 
-  constexpr int futility_move_count(bool improving, Depth depth) {
-    return improving ? (3 + depth * depth)
-                     : (3 + depth * depth) / 2;
+  constexpr int futility_move_count(bool improving, Depth depth, int value) {
+    return improving ? (3 + depth * depth + value / 256)
+                     : (3 + depth * depth + value / 256) / 2;
   }
 
   // History and stats update bonus, based on depth
@@ -987,7 +987,7 @@ moves_loop: // When in check, search starts here
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~7 Elo)
-          moveCountPruning = moveCount >= futility_move_count(improving, depth);
+          moveCountPruning = moveCount >= futility_move_count(improving, depth, ss->inCheck ? 0 : ss->staticEval - alpha);
 
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, delta, thisThread->rootDelta), 0);
