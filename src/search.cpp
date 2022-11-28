6fad20adef1edc3a95616118cb574bb3b8299d78
Michael Chaly <Vizvezdenec@gmail.com>
2022-11-28 04:34:35 +0300

    bench 3858201

--- src/search.cpp
+++ src/search.cpp
@@ -1159,7 +1159,7 @@ moves_loop: // When in check, search starts here
           // Dicrease reduction if we move a threatened piece (~1 Elo)
           if (   depth > 9
               && (mp.threatenedPieces & from_sq(move)))
-              r -= 1 + capture;
+              r--;
 
           // Increase reduction if next ply has a lot of fail high
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
@@ -1347,6 +1347,7 @@ moves_loop: // When in check, search starts here
         //or fail low was really bad
         bool extraBonus =    PvNode
                           || cutNode
+                          || (ss->ttHit && ttMove && tte->bound() == BOUND_LOWER)
                           || bestValue < alpha - 62 * depth;
 
         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth) * (1 + extraBonus));
