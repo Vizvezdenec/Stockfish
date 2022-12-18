1a98a5096f2b46ce41adf13c082570e04c53e9e9
Michael Chaly <Vizvezdenec@gmail.com>
2022-12-18 15:09:22 +0300

    bench 3573709

--- src/search.cpp
+++ src/search.cpp
@@ -979,7 +979,7 @@ moves_loop: // When in check, search starts here
       givesCheck = pos.gives_check(move);
 
       // Calculate new depth for this move
-      newDepth = depth - 1 + (extraDepth && moveCount < 6);
+      newDepth = depth - 1 + (extraDepth && moveCount < 12);
 
       Value delta = beta - alpha;
 
