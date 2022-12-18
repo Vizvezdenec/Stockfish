5d973a64106da43ad1852f4eba4c5f31748ec62f
Michael Chaly <Vizvezdenec@gmail.com>
2022-12-18 13:46:29 +0300

    bench 3645022

--- src/search.cpp
+++ src/search.cpp
@@ -1070,7 +1070,7 @@ moves_loop: // When in check, search starts here
                   {
                       extension = 2;
                       depth += depth < 12;
-                      if (value < singularBeta - 400 - 100 * depth)
+                      if (value < singularBeta - 800 - 30 * depth)
                           extension = 3;
                   }
               }
