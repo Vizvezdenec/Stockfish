52bfe5b8c63409dc4f4d762703bee65328c1a953
Michael Chaly <Vizvezdenec@gmail.com>
2022-12-19 19:44:51 +0300

    bench 4243762

--- src/search.cpp
+++ src/search.cpp
@@ -784,7 +784,16 @@ namespace {
         &&  eval - futility_margin(depth, improving) - (ss-1)->statScore / 303 >= beta
         &&  eval >= beta
         &&  eval < 28031) // larger than VALUE_KNOWN_WIN, but smaller than TB wins
-        return eval;
+    {
+        if (eval != ss->staticEval || depth < 4)
+            return eval;
+        else
+        {
+            value = qsearch<NonPV>(pos, ss, alpha, alpha + 1);
+            if (value >= beta)
+                return value;
+        }
+    }
 
     // Step 9. Null move search with verification search (~22 Elo)
     if (   !PvNode
