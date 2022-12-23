4e62bbc623915f287041e158b2ed96d52a65e6b2
Michael Chaly <Vizvezdenec@gmail.com>
2022-12-23 05:44:50 +0300

    bench 3580204

--- src/search.cpp
+++ src/search.cpp
@@ -1142,7 +1142,7 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction for PvNodes based on depth
       if (PvNode)
-          r -= 1 + 11 / (3 + depth) + capture;
+          r -= 1 + 11 / (3 + depth);
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
@@ -1212,7 +1212,7 @@ moves_loop: // When in check, search starts here
       // Step 18. Full depth search when LMR is skipped. If expected reduction is high, reduce its depth by 1.
       else if (!PvNode || moveCount > 1)
       {
-               value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 4), !cutNode);
+               value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 4 && (!PvNode || !capture)), !cutNode);
       }
 
       // For PV nodes only, do a full PV search on the first move or after a fail
