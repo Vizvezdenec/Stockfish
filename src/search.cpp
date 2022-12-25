5c0ac4b56750c9ca74c06bc1920bc1e513b9240b
Michael Chaly <Vizvezdenec@gmail.com>
2022-12-25 14:17:47 +0300

    bench 3696123

--- src/search.cpp
+++ src/search.cpp
@@ -840,7 +840,7 @@ namespace {
     // Step 10. ProbCut (~10 Elo)
     // If we have a good enough capture and a reduced search returns a value
     // much above beta, we can (almost) safely prune the previous move.
-    if (   (!PvNode || beta - alpha < 2 * ss->ply)
+    if (   (!PvNode || beta - alpha < 4 * ss->ply)
         &&  depth > 4
         &&  abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         // if value from transposition table is lower than probCutBeta, don't attempt probCut
