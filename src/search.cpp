44856b3a67a3040111759773748dc825571f545c
Michael Chaly <Vizvezdenec@gmail.com>
2022-12-29 14:00:42 +0300

    bench 3996004

--- src/search.cpp
+++ src/search.cpp
@@ -1184,8 +1184,7 @@ moves_loop: // When in check, search starts here
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
           // Do full depth search when reduced LMR search fails high
-          if (value > alpha && d < newDepth)
-          {
+
               // Adjust full depth search based on LMR results - if result
               // was good enough search deeper, if it was bad enough search shallower
               const bool doDeeperSearch = value > (alpha + 64 + 11 * (newDepth - d));
@@ -1195,6 +1194,9 @@ moves_loop: // When in check, search starts here
               ss->doubleExtensions = ss->doubleExtensions + doEvenDeeperSearch;
 
               newDepth += doDeeperSearch - doShallowerSearch + doEvenDeeperSearch;
+
+          if (value > alpha && d < newDepth)
+          {
 
               if (newDepth > d)
                   value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth, !cutNode);
