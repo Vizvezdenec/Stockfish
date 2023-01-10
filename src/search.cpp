6541d475a5ffb123eddf9489bffae358d02afc18
Michael Chaly <Vizvezdenec@gmail.com>
2023-01-11 01:28:56 +0300

    bench 3883614

--- src/search.cpp
+++ src/search.cpp
@@ -1217,7 +1217,10 @@ moves_loop: // When in check, search starts here
       {
                // Increase reduction for cut nodes and not ttMove (~1 Elo)
                if (!ttMove && cutNode)
-                         r += 2 + ((ss-1)->ttHit && (ss-1)->moveCount == 1);
+                         r += 2;
+
+               if (!ttMove && PvNode)
+                   r += 4;
 
                value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 4), !cutNode);
       }
