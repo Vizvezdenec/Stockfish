d179f848dca8868c02d07d79d0ad6930f4214e52
Michael Chaly <Vizvezdenec@gmail.com>
2023-01-17 02:08:05 +0300

    bench 3920365

--- src/search.cpp
+++ src/search.cpp
@@ -997,6 +997,7 @@ moves_loop: // When in check, search starts here
           if (   capture
               || givesCheck)
           {
+              lmrDepth = std::max(lmrDepth, 0);
               // Futility pruning for captures (~2 Elo)
               if (   !givesCheck
                   && !PvNode
@@ -1023,13 +1024,13 @@ moves_loop: // When in check, search starts here
 
               history += 2 * thisThread->mainHistory[us][from_to(move)];
 
+              lmrDepth = std::max(lmrDepth + history / 7208, 0);
+
               // Futility pruning: parent node (~13 Elo)
               if (   !ss->inCheck
                   && lmrDepth < 13
-                  && ss->staticEval + 103 + 136 * lmrDepth + history / 53 <= alpha)
+                  && ss->staticEval + 103 + 136 * lmrDepth <= alpha)
                   continue;
-
-              lmrDepth = std::max(lmrDepth, 0);
 
               // Prune moves with negative SEE (~4 Elo)
               if (!pos.see_ge(move, Value(-25 * lmrDepth * lmrDepth - 16 * lmrDepth)))
