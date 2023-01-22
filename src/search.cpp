aa970406fdaa3ea0d5d6dac5110e6d89f1793d27
Michael Chaly <Vizvezdenec@gmail.com>
2023-01-22 12:29:47 +0300

    bench 3775869

--- src/search.cpp
+++ src/search.cpp
@@ -991,8 +991,13 @@ moves_loop: // When in check, search starts here
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
           moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
+          int history =   (*contHist[0])[movedPiece][to_sq(move)]
+                            + (*contHist[1])[movedPiece][to_sq(move)]
+                            + (*contHist[3])[movedPiece][to_sq(move)] 
+                            + 2 * thisThread->mainHistory[us][from_to(move)];
+
           // Reduced depth of the next LMR search
-          int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, delta, thisThread->rootDelta), 0);
+          int lmrDepth = std::max(newDepth + history / 7208 - reduction(improving, depth, moveCount, delta, thisThread->rootDelta), 0);
 
           if (   capture
               || givesCheck)
@@ -1012,9 +1017,7 @@ moves_loop: // When in check, search starts here
           }
           else
           {
-              int history =   (*contHist[0])[movedPiece][to_sq(move)]
-                            + (*contHist[1])[movedPiece][to_sq(move)]
-                            + (*contHist[3])[movedPiece][to_sq(move)];
+              history -= 2 * thisThread->mainHistory[us][from_to(move)];
 
               // Continuation history based pruning (~2 Elo)
               if (   lmrDepth < 5
@@ -1022,8 +1025,6 @@ moves_loop: // When in check, search starts here
                   continue;
 
               history += 2 * thisThread->mainHistory[us][from_to(move)];
-
-              lmrDepth += history / 7208;
 
               // Futility pruning: parent node (~13 Elo)
               if (   !ss->inCheck
