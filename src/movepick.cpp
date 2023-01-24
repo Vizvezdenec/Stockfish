fe85e99d3dd8fbdb26f5e765a4d485067ed3e45a
Michael Chaly <Vizvezdenec@gmail.com>
2023-01-24 15:57:59 +0300

    bench 3860903

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -127,7 +127,7 @@ void MovePicker::score() {
                    +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))]) / 16
                    + (2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
-                   +     (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]) / 1024;
+                   +     (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]) / 4096;
 
       else if constexpr (Type == QUIETS)
           m.value =  2 * (*mainHistory)[pos.side_to_move()][from_to(m)]
