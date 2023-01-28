7ebcf2b6108e3004f55276aa352cc474af5eacd4
Michael Chaly <Vizvezdenec@gmail.com>
2023-01-28 06:51:22 +0300

    bench 3924484

--- src/search.cpp
+++ src/search.cpp
@@ -1238,6 +1238,9 @@ moves_loop: // When in check, search starts here
           (ss+1)->pv = pv;
           (ss+1)->pv[0] = MOVE_NONE;
 
+          if (moveCount == 1 && r > 1 && likelyFailLow)
+              newDepth--;
+
           value = -search<PV>(pos, ss+1, -beta, -alpha,
                               std::min(maxNextDepth, newDepth), false);
       }
