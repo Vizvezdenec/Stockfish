ecc49ec1c0a00769e06050757fa1fd689336c946
Michael Chaly <Vizvezdenec@gmail.com>
2023-02-06 02:30:55 +0300

    bench 3862613

--- src/search.cpp
+++ src/search.cpp
@@ -1583,6 +1583,12 @@ moves_loop: // When in check, search starts here
           && (*contHist[1])[pos.moved_piece(move)][to_sq(move)] < 0)
           continue;
 
+      if (   !capture
+          && !givesCheck
+          && bestValue > VALUE_TB_LOSS_IN_MAX_PLY
+          && ss->staticEval + 150 + 150 * depth <= alpha)
+          continue;
+
       // We prune after 2nd quiet check evasion where being 'in check' is implicitly checked through the counter
       // and being a 'quiet' apart from being a tt move is assumed after an increment because captures are pushed ahead.
       if (   bestValue > VALUE_TB_LOSS_IN_MAX_PLY
