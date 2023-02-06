9e26ee00f989ece7ead3586595067e384a1bc60d
Michael Chaly <Vizvezdenec@gmail.com>
2023-02-07 00:33:36 +0300

    bench 4053391

--- src/search.cpp
+++ src/search.cpp
@@ -1086,7 +1086,7 @@ moves_loop: // When in check, search starts here
                       depth += depth < 12;
                   }
 
-                  if (!ttCapture && value < singularBeta - 256)
+                  if (!ttCapture && value < singularBeta - 16 * depth)
                       update_continuation_histories(ss, movedPiece, to_sq(move), stat_bonus(singularDepth));
               }
 
