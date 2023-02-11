55a42638ed799a88143ba83a5f1bf5bae126e36d
Michael Chaly <Vizvezdenec@gmail.com>
2023-02-11 07:23:50 +0300

    bench 4366642

--- src/search.cpp
+++ src/search.cpp
@@ -1566,8 +1566,8 @@ moves_loop: // When in check, search starts here
 
       // We prune after 2nd quiet check evasion where being 'in check' is implicitly checked through the counter
       // and being a 'quiet' apart from being a tt move is assumed after an increment because captures are pushed ahead.
-      if (quietCheckEvasions > 1)
-          break;
+      if (quietCheckEvasions > 1 && !capture)
+          continue;
 
       // Continuation history based pruning (~3 Elo)
       if (   !capture
