12947566e53f36acea6ad1f7872191466ea869c0
Michael Chaly <Vizvezdenec@gmail.com>
2023-02-14 00:43:43 +0300

    bench 4269177

--- src/search.cpp
+++ src/search.cpp
@@ -1576,7 +1576,7 @@ moves_loop: // When in check, search starts here
           continue;
 
       // Do not search moves with bad enough SEE values (~5 Elo)
-      if (!pos.see_ge(move, Value(givesCheck && capture ? -300 : -108)))
+      if (!pos.see_ge(move, Value(givesCheck || capture ? -108 : 0)))
           continue;
 
     }
