0c682a1ec2fd9fb6d9801780ab40b32ad922a469
Michael Chaly <Vizvezdenec@gmail.com>
2023-02-16 02:43:08 +0300

    bench 3716599

--- src/search.cpp
+++ src/search.cpp
@@ -996,7 +996,7 @@ moves_loop: // When in check, search starts here
           moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
-          int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, delta, thisThread->rootDelta), 0);
+          int lmrDepth = newDepth - reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
 
           if (   capture
               || givesCheck)
@@ -1006,7 +1006,7 @@ moves_loop: // When in check, search starts here
                   && !PvNode
                   && lmrDepth < 7
                   && !ss->inCheck
-                  && ss->staticEval + 185 + 203 * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))]
+                  && ss->staticEval + 185 + 203 * std::max(lmrDepth, 0) + PieceValue[EG][pos.piece_on(to_sq(move))]
                    + captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] / 6 < alpha)
                   continue;
 
