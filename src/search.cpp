9c1b845960262ef47225bbec26962977116fcead
Michael Chaly <Vizvezdenec@gmail.com>
2023-02-18 10:50:07 +0300

    bench 4117621

--- src/search.cpp
+++ src/search.cpp
@@ -1028,7 +1028,7 @@ moves_loop: // When in check, search starts here
               history += 2 * thisThread->mainHistory[us][from_to(move)];
 
               lmrDepth += history / 7208;
-              lmrDepth -= (ttCapture && cutNode);
+              lmrDepth -= cutNode - PvNode;
               lmrDepth = std::max(lmrDepth, -2);
 
               // Futility pruning: parent node (~13 Elo)
