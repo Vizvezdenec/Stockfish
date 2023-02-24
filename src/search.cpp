999a8066a97a8e785c0b34eb2f538a8ba42935b0
Michael Chaly <Vizvezdenec@gmail.com>
2023-02-25 01:30:18 +0300

    bench 4843827

--- src/search.cpp
+++ src/search.cpp
@@ -946,7 +946,7 @@ moves_loop: // When in check, search starts here
                          && ttMove
                          && (tte->bound() & BOUND_UPPER)
                          && tte->depth() >= depth
-                         && (ttValue < beta || tte->bound() == BOUND_UPPER);
+                         && (ttValue <= alpha || tte->bound() == BOUND_UPPER);
 
     // Step 13. Loop through all pseudo-legal moves until no moves remain
     // or a beta cutoff occurs.
