f7b5d87802df67026246cf8a30f4cfc86feb3526
Michael Chaly <Vizvezdenec@gmail.com>
2023-02-25 14:30:06 +0300

    bench 4455094

--- src/search.cpp
+++ src/search.cpp
@@ -1175,8 +1175,7 @@ moves_loop: // When in check, search starts here
           && (*contHist[0])[movedPiece][to_sq(move)] >= 3722)
           r--;
 
-      if (ss->inCheck && move == countermove
-          && (*contHist[1])[movedPiece][to_sq(move)] >= 5000)
+      if (ss->inCheck && move == countermove)
           r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
