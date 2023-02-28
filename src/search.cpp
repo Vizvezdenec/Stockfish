fbaa107e37c7af23ffd8e2c7bda2e69730d580c0
Michael Chaly <Vizvezdenec@gmail.com>
2023-02-28 16:52:59 +0300

    bench 4739523

--- src/search.cpp
+++ src/search.cpp
@@ -785,7 +785,7 @@ namespace {
     // return a fail low.
     if (eval < alpha - 426 - 252 * depth * depth)
     {
-        int offset = 5;
+        int offset = 12;
         value = qsearch<NonPV>(pos, ss, alpha - offset, alpha - offset + 1);
         if (value <= alpha - offset)
             return value;
