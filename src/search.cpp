1ac75226850afadb582ae72281a086e6a10f9f9e
Michael Chaly <Vizvezdenec@gmail.com>
2023-03-05 19:48:57 +0300

    bench 4747020

--- src/search.cpp
+++ src/search.cpp
@@ -1109,6 +1109,8 @@ moves_loop: // When in check, search starts here
               // If the eval of ttMove is less than value, we reduce it (negative extension)
               else if (ttValue <= value)
                   extension = -1;
+              else if (ttValue <= alpha)
+                  extension = -1;
           }
 
           // Check extensions (~1 Elo)
