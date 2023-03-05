705dc4ceb9f4f6bce5520dedc49c935a7b79ca5a
Michael Chaly <Vizvezdenec@gmail.com>
2023-03-05 23:24:23 +0300

    bench 5313797

--- src/search.cpp
+++ src/search.cpp
@@ -1048,7 +1048,7 @@ moves_loop: // When in check, search starts here
               lmrDepth = std::max(lmrDepth, 0);
 
               // Prune moves with negative SEE (~4 Elo)
-              if (!pos.see_ge(move, Value(std::min(-54, -24 * lmrDepth * lmrDepth - 15 * lmrDepth))))
+              if (!pos.see_ge(move, Value(std::min(-162, -24 * lmrDepth * lmrDepth - 15 * lmrDepth))))
                   continue;
           }
       }
