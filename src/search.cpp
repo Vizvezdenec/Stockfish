3b27d68914ba3351733cd1d8c85ba5953253355c
Michael Chaly <Vizvezdenec@gmail.com>
2023-03-12 15:49:33 +0300

    bench 5135356

--- src/search.cpp
+++ src/search.cpp
@@ -906,9 +906,7 @@ namespace {
     // Step 11. If the position is not in TT, decrease depth by 3.
     // Use qsearch if depth is equal or below zero (~9 Elo)
     if (    PvNode
-        && !ttMove
-        && ss->ttHit
-        && tte->depth() >= depth - 3)
+        && !ttMove)
         depth -= 3;
 
     if (depth <= 0)
@@ -1073,7 +1071,7 @@ moves_loop: // When in check, search starts here
               && (tte->bound() & BOUND_LOWER)
               &&  tte->depth() >= depth - 3)
           {
-              Value singularBeta = ttValue - (2 + (ss->ttPv && !PvNode)) * depth;
+              Value singularBeta = ttValue - (3 + 2 * (ss->ttPv && !PvNode)) * depth / 2;
               Depth singularDepth = (depth - 1) / 2;
 
               ss->excludedMove = move;
