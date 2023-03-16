929d8aaa2fc0dcdddace92d585f34ebcabc2dde6
Michael Chaly <Vizvezdenec@gmail.com>
2023-03-16 13:36:10 +0300

    bench 4782621

--- src/search.cpp
+++ src/search.cpp
@@ -1002,7 +1002,7 @@ moves_loop: // When in check, search starts here
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
           moveCountPruning = moveCount >= futility_move_count(improving, depth);
-          moveCountCPruning = moveCount >= 5 + futility_move_count(improving, depth);
+          moveCountCPruning = moveCount >= futility_move_count(improving, depth + 2);
 
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - r, 0);
