9723eb41e157edd8117eaabdb40818a38b099a14
Michael Chaly <Vizvezdenec@gmail.com>
2023-03-16 05:39:35 +0300

    bench 4443911

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -273,7 +273,7 @@ top:
           return *(cur - 1);
 
       // If we did not find any move and we do not try checks, we have finished
-      if (depth < DEPTH_QS_CHECKS - 2 * pvnode)
+      if (depth != DEPTH_QS_CHECKS || !pvnode)
           return MOVE_NONE;
 
       ++stage;
