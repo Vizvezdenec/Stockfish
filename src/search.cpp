52d4d9d45b3400e54d3d905693798d649d40f567
Michael Chaly <Vizvezdenec@gmail.com>
2023-03-22 16:45:06 +0300

    bench 4345733

--- src/search.cpp
+++ src/search.cpp
@@ -1177,7 +1177,7 @@ moves_loop: // When in check, search starts here
           r--;
 
       // Increase reduction if next ply has a lot of fail high
-      if ((ss+1)->cutoffCnt > 2)
+      if ((ss+1)->cutoffCnt > 1)
           r++;
 
       // Decrease reduction if move is a killer and we have a good history
