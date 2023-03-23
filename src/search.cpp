3b14531150ab58cc1401e2e90367d4478ea22719
Michael Chaly <Vizvezdenec@gmail.com>
2023-03-23 20:04:59 +0300

    bench 5003968

--- src/search.cpp
+++ src/search.cpp
@@ -1074,7 +1074,7 @@ moves_loop: // When in check, search starts here
           }
           else
           {
-              if (moveCount >= futility_move_count(improving, std::max(depth - r / 2, 0)))
+              if (moveCount >= futility_move_count(improving, std::max(depth - (r > 4), 0)) + 1)
                   continue;
               int history =   (*contHist[0])[movedPiece][to_sq(move)]
                             + (*contHist[1])[movedPiece][to_sq(move)]
