4c24a1bcdb8dfcab65e53b8b4d347e9a77a5b320
Michael Chaly <Vizvezdenec@gmail.com>
2023-03-24 07:50:16 +0300

    bench 4782167

--- src/search.cpp
+++ src/search.cpp
@@ -607,7 +607,7 @@ namespace {
     (ss+2)->cutoffCnt    = 0;
     ss->doubleExtensions = (ss-1)->doubleExtensions;
     Square prevSq        = is_ok((ss-1)->currentMove) ? to_sq((ss-1)->currentMove) : SQ_NONE;
-    ss->statScore        = 0;
+    ss->statScore        = -5000;
 
     // Step 4. Transposition table lookup.
     excludedMove = ss->excludedMove;
