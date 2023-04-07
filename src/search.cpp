2374b5e34f38083aebe8a5102f637c865e4c5d1d
Michael Chaly <Vizvezdenec@gmail.com>
2023-04-07 06:41:06 +0300

    bench 4805050

--- src/search.cpp
+++ src/search.cpp
@@ -1192,7 +1192,7 @@ moves_loop: // When in check, search starts here
           r--;
 
       if (pepega && moveCount > 1)
-          r++;
+          r += 2;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
