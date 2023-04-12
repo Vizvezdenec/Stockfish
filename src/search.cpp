1b24d85176fe56fa5d539401c5738cc8b9b21e32
Michael Chaly <Vizvezdenec@gmail.com>
2023-04-12 09:12:33 +0300

    bench 3917493

--- src/search.cpp
+++ src/search.cpp
@@ -1116,11 +1116,11 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension) (~7 Elo)
               else if (ttValue >= beta)
-                  extension = -2 - !PvNode;
+                  extension = -2 - !PvNode - 2 * (cutNode && ss->ttPv);
 
               // If the eval of ttMove is less than value, we reduce it (negative extension) (~1 Elo)
               else if (ttValue <= alpha)
-                  extension = -1 - 3 * (cutNode && ss->ttPv);
+                  extension = -1 - (cutNode && ss->ttPv);
 
               // If the eval of ttMove is less than value, we reduce it (negative extension) (~1 Elo)
               else if (ttValue <= value)
