af5c1f15f164d9556b22202bb7ed7e5cabf96c31
Michael Chaly <Vizvezdenec@gmail.com>
2023-04-12 22:33:41 +0300

    bench 4142527

--- src/search.cpp
+++ src/search.cpp
@@ -1726,7 +1726,8 @@ moves_loop: // When in check, search starts here
     if (!pos.capture_stage(bestMove))
     {
         int bonus2 = bestValue > beta + 153 ? bonus1               // larger bonus
-                                            : stat_bonus(depth);   // smaller bonus
+                   : bestValue > beta       ? stat_bonus(depth)
+                                            : stat_bonus(depth) / 2;   // smaller bonus
 
         // Increase stats for the best move in case it was a quiet move
         update_quiet_stats(pos, ss, bestMove, bonus2);
