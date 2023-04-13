8abe76ee05e8822dbb9ea43edfce9e79a7e88dac
Michael Chaly <Vizvezdenec@gmail.com>
2023-04-13 15:22:02 +0300

    bench 3720823

--- src/search.cpp
+++ src/search.cpp
@@ -1245,8 +1245,8 @@ moves_loop: // When in check, search starts here
               r += 2;
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 4), !cutNode);
-          if (value < bestValue + newDepth / 2)
-              newDepth--;
+          if (value > alpha + 32)
+              newDepth++;
       }
 
       // For PV nodes only, do a full PV search on the first move or after a fail
