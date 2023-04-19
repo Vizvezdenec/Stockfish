8c73c31af8af11ea3a4cc046e1051261ac03d241
Michael Chaly <Vizvezdenec@gmail.com>
2023-04-19 10:25:56 +0300

    bench 3806989

--- src/search.cpp
+++ src/search.cpp
@@ -899,7 +899,7 @@ namespace {
     // Step 11. If the position is not in TT, decrease depth by 2 (or by 4 if the TT entry for the current position was hit and the stored depth is greater than or equal to the current depth).
     // Use qsearch if depth is equal or below zero (~9 Elo)
     if (    PvNode
-        && (ss->ttHit || depth <= 8)
+        && (ss->ttHit || depth <= 5)
         && !ttMove)
         depth -= 2 + 2 * (ss->ttHit &&  tte->depth() >= depth);
 
