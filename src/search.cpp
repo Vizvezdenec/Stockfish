70ce28a864a5a6a69bc54baa5c7bd1842ca64eb5
Michael Chaly <Vizvezdenec@gmail.com>
2023-04-23 18:12:21 +0300

    bench 3635809

--- src/search.cpp
+++ src/search.cpp
@@ -1372,7 +1372,8 @@ moves_loop: // When in check, search starts here
     // Bonus for prior countermove that caused the fail low
     else if (!priorCapture && prevSq != SQ_NONE)
     {
-        int bonus = (depth > 5) + (PvNode || cutNode) + (bestValue < alpha - 97 * depth) + ((ss-1)->moveCount > 10) + 2 * (PvNode && depth >= 5);
+        int bonus = (depth > 5) + (PvNode || cutNode) + (bestValue < alpha - 97 * depth) + ((ss-1)->moveCount > 10) + 2 * PvNode;
+        bonus = std::min(bonus, 4);
         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth) * bonus);
     }
 
