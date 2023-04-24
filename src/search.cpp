611a26f6974b414182f45b33323e903ed2d090e2
Michael Chaly <Vizvezdenec@gmail.com>
2023-04-24 20:58:54 +0300

    bench 3416571

--- src/search.cpp
+++ src/search.cpp
@@ -894,9 +894,6 @@ namespace {
         && !ttMove)
         depth -= 2;
 
-    if (ss->ply < 4 && !ttMove && depth > 1)
-        depth--;
-
 moves_loop: // When in check, search starts here
 
     // Step 12. A small Probcut idea, when we are in check (~4 Elo)
@@ -1522,6 +1519,7 @@ moves_loop: // When in check, search starts here
                                       prevSq);
 
     int quietCheckEvasions = 0;
+    bool ttRecapt = ttMove && to_sq(ttMove) == prevSq;
 
     // Step 5. Loop through all pseudo-legal moves until no moves remain
     // or a beta cutoff occurs.
@@ -1547,7 +1545,7 @@ moves_loop: // When in check, search starts here
           &&  futilityBase > -VALUE_KNOWN_WIN
           &&  type_of(move) != PROMOTION)
       {
-          if (moveCount > 2)
+          if (moveCount > 2 - ttRecapt)
               continue;
 
           futilityValue = futilityBase + PieceValue[EG][pos.piece_on(to_sq(move))];
