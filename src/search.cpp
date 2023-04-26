d7fae137f7cb33411b2100935507ac2b97388873
Michael Chaly <Vizvezdenec@gmail.com>
2023-04-26 11:45:48 +0300

    bench 3743388

--- src/search.cpp
+++ src/search.cpp
@@ -1174,7 +1174,7 @@ moves_loop: // When in check, search starts here
       else if (move == ttMove)
           r--;
 
-      r -= reducedD;
+      r += reducedD;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
