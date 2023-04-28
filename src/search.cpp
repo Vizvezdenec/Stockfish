14855533eb2b172d48b51edd78f438f5070e6248
Michael Chaly <Vizvezdenec@gmail.com>
2023-04-28 12:06:13 +0300

    bench 3607444

--- src/search.cpp
+++ src/search.cpp
@@ -1384,7 +1384,7 @@ moves_loop: // When in check, search starts here
     if (bestValue <= alpha)
         ss->ttPv = ss->ttPv || ((ss-1)->ttPv && depth > 3);
 
-    if (bestValue >= beta + 30 * depth && (ss-1)->ttPv && depth > 5)
+    if (bestValue >= beta + 40 * depth && (ss-1)->ttPv && depth > 8)
         ss->ttPv = true;
 
     // Write gathered information in transposition table
