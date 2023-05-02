2dabb35ca90c95e153634b2a52be9507427bd479
Michael Chaly <Vizvezdenec@gmail.com>
2023-05-03 00:39:42 +0300

    bench 3807261

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -123,7 +123,7 @@ void MovePicker::score() {
   for (auto& m : *this)
       if constexpr (Type == CAPTURES)
           m.value =  (7 * int(PieceValue[MG][pos.piece_on(to_sq(m))])
-                   + (type_of(m) == PROMOTION && promotion_type(m) == QUEEN) * 5 * QueenValueMg
+                   + (type_of(m) == PROMOTION && promotion_type(m) == QUEEN) * 3 * QueenValueMg
                    +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))]) / 16;
 
       else if constexpr (Type == QUIETS)
