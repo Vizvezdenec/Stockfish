c445e404d2debe05ad1ffa532e697929f1a2f7c3
Michael Chaly <Vizvezdenec@gmail.com>
2023-05-07 18:27:35 +0300

    bench 3838012

--- src/search.cpp
+++ src/search.cpp
@@ -1197,15 +1197,14 @@ moves_loop: // When in check, search starts here
           // beyond the first move depth. This may lead to hidden double extensions.
           Depth d = std::clamp(newDepth - r, 1, newDepth + 1);
 
-          Value pseudoAlpha = std::max(alpha - newDepth, bestValue);
-          value = -search<NonPV>(pos, ss+1, -(pseudoAlpha+1), -pseudoAlpha, d, true);
+          value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
           // Do full depth search when reduced LMR search fails high
-          if (value > pseudoAlpha && (d < newDepth || value <= alpha))
+          if (value > alpha && d < newDepth)
           {
               // Adjust full depth search based on LMR results - if result
               // was good enough search deeper, if it was bad enough search shallower
-              const bool doDeeperSearch = value > (alpha + 58 + 12 * (newDepth - d));
+              const bool doDeeperSearch = value > (bestValue + 88 + 12 * (newDepth - d));
               const bool doEvenDeeperSearch = value > alpha + 588 && ss->doubleExtensions <= 5;
               const bool doShallowerSearch = value < bestValue + newDepth;
 
