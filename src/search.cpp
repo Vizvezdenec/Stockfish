f3b66b0a11923a714b3c4914fbf37f46c8948be2
Michael Chaly <Vizvezdenec@gmail.com>
2023-05-10 17:46:01 +0300

    bench 3596528

--- src/search.cpp
+++ src/search.cpp
@@ -1165,7 +1165,7 @@ moves_loop: // When in check, search starts here
 
       // Increase reduction if next ply has a lot of fail high (~5 Elo)
       if ((ss+1)->cutoffCnt > 3)
-          r += 1 + (cutNode && (ss+1)->cutoffCnt > 5);
+          r += 1 + (cutNode && (ss+1)->cutoffCnt > 7);
 
       else if (move == ttMove)
           r--;
