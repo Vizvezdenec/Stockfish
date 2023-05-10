9758b8db08df3d0bed9cb013dc8b8e5d33df43e3
Michael Chaly <Vizvezdenec@gmail.com>
2023-05-10 10:42:11 +0300

    bench 3458623

--- src/search.cpp
+++ src/search.cpp
@@ -1186,7 +1186,7 @@ moves_loop: // When in check, search starts here
       if (    depth >= 2
           &&  moveCount > 1 + (PvNode && ss->ply <= 1)
           && (   !ss->ttPv
-              || (!ttMove && ss->ttHit && tte->depth() >= depth)
+              || (!ttMove && ss->ttHit && tte->depth() >= depth + 4)
               || !capture
               || (cutNode && (ss-1)->moveCount > 1)))
       {
