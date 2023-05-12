e06cf92a223f1616f73350880589df974b34cfc7
Michael Chaly <Vizvezdenec@gmail.com>
2023-05-13 02:40:51 +0300

    bench 3957334

--- src/search.cpp
+++ src/search.cpp
@@ -1149,7 +1149,7 @@ moves_loop: // When in check, search starts here
 
       // Increase reduction for cut nodes (~3 Elo)
       if (cutNode)
-          r += 2;
+          r += 2 - (move == ss->killers[0]);
 
       // Increase reduction if ttMove is a capture (~3 Elo)
       if (ttCapture)
@@ -1168,9 +1168,6 @@ moves_loop: // When in check, search starts here
           r++;
 
       else if (move == ttMove)
-          r--;
-
-      else if (!ttMove && move == ss->killers[0])
           r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
