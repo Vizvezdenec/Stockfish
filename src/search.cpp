0212de10f18463307d04b4c81732cb8fd493f00d
Michael Chaly <Vizvezdenec@gmail.com>
2023-05-17 10:38:11 +0300

    bench 3384381

--- src/search.cpp
+++ src/search.cpp
@@ -1179,8 +1179,8 @@ moves_loop: // When in check, search starts here
       // Decrease/increase reduction for moves with a good/bad history (~25 Elo)
       r -= ss->statScore / (11079 + 4626 * (depth > 6 && depth < 19));
 
-      if (depth <= 5 && (ss-1)->statScore > 50000 && ss->statScore < -50000)
-          r++;
+      if (depth <= 5 && (ss-1)->statScore < -20000 && ss->statScore > 20000)
+          r--;
 
       // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
       // We use various heuristics for the sons of a node after the first son has
