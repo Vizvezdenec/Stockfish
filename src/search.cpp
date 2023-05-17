b4237707804cdc3e6a8d0083b68b74b5cbde5e4b
Michael Chaly <Vizvezdenec@gmail.com>
2023-05-17 08:47:30 +0300

    bench 3745420

--- src/search.cpp
+++ src/search.cpp
@@ -1028,7 +1028,7 @@ moves_loop: // When in check, search starts here
               history += 2 * thisThread->mainHistory[us][from_to(move)];
 
               lmrDepth += history / 7278;
-              lmrDepth = std::max(lmrDepth, -2 - !PvNode);
+              lmrDepth = std::max(lmrDepth, -2);
 
               // Futility pruning: parent node (~13 Elo)
               if (   !ss->inCheck
@@ -1192,7 +1192,7 @@ moves_loop: // When in check, search starts here
           // In general we want to cap the LMR depth search at newDepth, but when
           // reduction is negative, we allow this move a limited search extension
           // beyond the first move depth. This may lead to hidden double extensions.
-          Depth d = std::clamp(newDepth - r, 1, newDepth + 1);
+          Depth d = std::clamp(newDepth - r, 1, newDepth + 1 + (PvNode && ss->ply <= 4));
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
