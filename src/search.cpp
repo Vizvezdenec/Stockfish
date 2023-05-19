08bbcb51c92598594fb5a2ed71204d35d96283f5
Michael Chaly <Vizvezdenec@gmail.com>
2023-05-19 20:39:22 +0300

    bench 3802504

--- src/search.cpp
+++ src/search.cpp
@@ -1569,7 +1569,7 @@ moves_loop: // When in check, search starts here
       if (   !capture
           && (*contHist[0])[pos.moved_piece(move)][to_sq(move)]
              + (*contHist[1])[pos.moved_piece(move)][to_sq(move)]
-             + (*contHist[3])[pos.moved_piece(move)][to_sq(move)] < 4000 * (1 - depth))
+             + (*contHist[3])[pos.moved_piece(move)][to_sq(move)] < 4000 * depth)
           continue;
 
       // Do not search moves with bad enough SEE values (~5 Elo)
