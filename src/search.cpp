fc08ae5c6a3ed23e36e7bb7c9b78571e855db53f
Michael Chaly <Vizvezdenec@gmail.com>
2023-05-20 12:09:16 +0300

    bench 3023537

--- src/search.cpp
+++ src/search.cpp
@@ -984,13 +984,13 @@ moves_loop: // When in check, search starts here
           if (   capture
               || givesCheck)
           {
-              lmrDepth += captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] / 1448;
-              lmrDepth = std::max(lmrDepth, -5);
+              lmrDepth += captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] / 1448 + PieceValue[EG][pos.piece_on(to_sq(move))] / 223;
+              lmrDepth = std::max(lmrDepth, -1);
               // Futility pruning for captures (~2 Elo)
               if (   !givesCheck
                   && lmrDepth < 7
                   && !ss->inCheck
-                  && ss->staticEval + 207 + 223 * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))] < alpha)
+                  && ss->staticEval + 207 + 223 * lmrDepth < alpha)
                   continue;
 
               Bitboard occupied;
