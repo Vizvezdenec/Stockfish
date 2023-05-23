42c95b0bd0f1ccd7af3561129dd4143e10f6e6bb
Michael Chaly <Vizvezdenec@gmail.com>
2023-05-23 21:46:35 +0300

    bench 3170804

--- src/search.cpp
+++ src/search.cpp
@@ -1310,7 +1310,7 @@ moves_loop: // When in check, search starts here
 
               if (value >= beta)
               {
-                  ss->cutoffCnt += 1 + !ttMove + (value >= beta + 222);
+                  ss->cutoffCnt += 1 + !ttMove + (!PvNode && !cutNode && value >= beta + 70 * depth);
                   assert(value >= beta); // Fail high
                   break;
               }
