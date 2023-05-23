eb33037b8775b576393be7acf7a320e1bb07fd9d
Michael Chaly <Vizvezdenec@gmail.com>
2023-05-23 09:03:44 +0300

    bench 3081337

--- src/search.cpp
+++ src/search.cpp
@@ -823,12 +823,12 @@ namespace {
         }
     }
 
-    probCutBeta = beta + 174 - 60 * improving;
+    probCutBeta = beta + 174 - 60 * improving + 50 * (ss->ttPv && eval != ss->staticEval);
 
     // Step 10. ProbCut (~10 Elo)
     // If we have a good enough capture (or queen promotion) and a reduced search returns a value
     // much above beta, we can (almost) safely prune the previous move.
-    if (   (!ss->ttPv || (!PvNode && eval == ss->staticEval))
+    if (   !PvNode
         &&  depth > 3
         &&  abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         // if value from transposition table is lower than probCutBeta, don't attempt probCut
