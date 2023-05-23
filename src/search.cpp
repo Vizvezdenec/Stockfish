5e9e8aa0f539d128e9f62658cc39683fd78eed19
Michael Chaly <Vizvezdenec@gmail.com>
2023-05-23 05:18:07 +0300

    bench 2988312

--- src/search.cpp
+++ src/search.cpp
@@ -894,7 +894,7 @@ namespace {
 moves_loop: // When in check, search starts here
 
     // Step 12. A small Probcut idea, when we are in check (~4 Elo)
-    probCutBeta = beta + 400 + 60 * (ss-1)->improving;
+    probCutBeta = beta + 380 + 100 * (ss-1)->improving;
     if (   ss->inCheck
         && !PvNode
         && depth >= 2
