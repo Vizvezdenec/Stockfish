fda283d316945a6130664846266d0601c0da39ca
Michael Chaly <Vizvezdenec@gmail.com>
2023-05-29 04:55:17 +0300

    bench 2240338

--- src/search.cpp
+++ src/search.cpp
@@ -1168,6 +1168,8 @@ moves_loop: // When in check, search starts here
 
       if (ss->staticEval <= beta && ss->staticEval >= alpha)
           r--;
+      else if (PvNode)
+          r++;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
