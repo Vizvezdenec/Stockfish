9157bcdffdeae0b95fa4fda20cacbc7417d42455
Michael Chaly <Vizvezdenec@gmail.com>
2023-05-30 10:08:20 +0300

    bench 2189502

--- src/search.cpp
+++ src/search.cpp
@@ -1028,7 +1028,7 @@ moves_loop: // When in check, search starts here
               // Futility pruning: parent node (~13 Elo)
               if (   !ss->inCheck
                   && lmrDepth < 12
-                  && ss->staticEval + std::max(-50, 111 + 136 * lmrDepth) <= alpha)
+                  && ss->staticEval + std::max(-100, 111 + 136 * lmrDepth) <= alpha)
                   continue;
 
               lmrDepth = std::max(lmrDepth, 0);
