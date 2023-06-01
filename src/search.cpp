df94190e0c187c38b67d31f1b0487d408a4f393b
Michael Chaly <Vizvezdenec@gmail.com>
2023-06-01 13:28:22 +0300

    bench 2331223

--- src/search.cpp
+++ src/search.cpp
@@ -1151,7 +1151,7 @@ moves_loop: // When in check, search starts here
           r++;
 
       // Decrease reduction for PvNodes based on depth (~2 Elo)
-      if (PvNode)
+      if (PvNode && !capture)
           r -= 1 + 11 / (3 + depth);
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
