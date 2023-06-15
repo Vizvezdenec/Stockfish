966faec0f59d8fddc1c28bf4386e21132c6eb0f2
Michael Chaly <Vizvezdenec@gmail.com>
2023-06-15 09:40:36 +0300

    bench 2706206

--- src/search.cpp
+++ src/search.cpp
@@ -1015,6 +1015,11 @@ moves_loop: // When in check, search starts here
           }
           else
           {
+              if (   move != ss->killers[0]
+                  && move != ss->killers[1]
+                  && (*contHist[0])[movedPiece][to_sq(move)] < - 4500 * depth)
+                  continue;
+
               int history =   (*contHist[0])[movedPiece][to_sq(move)]
                             + (*contHist[1])[movedPiece][to_sq(move)]
                             + (*contHist[3])[movedPiece][to_sq(move)];
