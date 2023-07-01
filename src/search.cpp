87429c230969d65bae5c5aa4b8085a43a4b9f268
Michael Chaly <Vizvezdenec@gmail.com>
2023-07-01 11:04:41 +0300

    bench 2906666

--- src/search.cpp
+++ src/search.cpp
@@ -1318,14 +1318,14 @@ moves_loop: // When in check, search starts here
               }
               else
               {
+                  ss->cutoffCnt += ss->ttHit && !ttMove && tte->depth() >= depth;
+
                   // Reduce other moves if we have found at least one score improvement (~1 Elo)
                   // Reduce more for depth > 3 and depth < 12 (~1 Elo)
                   if (   depth > 1
                       && beta  <  14362
                       && value > -12393)
                       depth -= depth > 3 && depth < 12 ? 2 : 1;
-
-                  ss->cutoffCnt += ss->ttHit && !ttMove && tte->depth() >= depth;
 
                   assert(depth > 0);
                   alpha = value; // Update alpha! Always alpha < beta
