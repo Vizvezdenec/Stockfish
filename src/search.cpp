c10d6a2b5ad3d8b9c702906616699a136509ce91
Michael Chaly <Vizvezdenec@gmail.com>
2023-07-15 10:05:11 +0300

    bench 1522004

--- src/search.cpp
+++ src/search.cpp
@@ -64,7 +64,7 @@ namespace {
 
   // Futility margin
   Value futility_margin(Depth d, bool stuff, bool improving) {
-    return Value((140 - 40 * stuff) * (d - improving));
+    return Value((140 - 20 * stuff) * (d - improving));
   }
 
   // Reductions lookup table initialized at startup
@@ -768,7 +768,7 @@ namespace {
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
         &&  depth < 9
-        &&  eval - futility_margin(depth, cutNode && !ss->ttHit, improving) - (ss-1)->statScore / 306 >= beta
+        &&  eval - futility_margin(depth, !ss->ttHit, improving) - (ss-1)->statScore / 306 >= beta
         &&  eval >= beta
         &&  eval < 24923) // larger than VALUE_KNOWN_WIN, but smaller than TB wins
         return eval;
