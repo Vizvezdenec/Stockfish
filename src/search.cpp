e0acc5af5bcc0ce09c0109c49d952e5aad045a26
Michael Chaly <Vizvezdenec@gmail.com>
2023-07-19 12:08:38 +0300

    bench 1662665

--- src/search.cpp
+++ src/search.cpp
@@ -64,7 +64,7 @@ namespace {
 
   // Futility margin
   Value futility_margin(Depth d, bool noTtCutNode, bool improving) {
-    return Value((140 - 10 * noTtCutNode) * (d - improving));
+    return Value((140 - 80 * noTtCutNode) * (d - improving));
   }
 
   // Reductions lookup table initialized at startup
