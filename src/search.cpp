f87dbe35c2bd5f379dc012b707dee58a4f63a954
Michael Chaly <Vizvezdenec@gmail.com>
2023-07-19 18:14:07 +0300

    bench 1677759

--- src/search.cpp
+++ src/search.cpp
@@ -778,7 +778,7 @@ namespace {
         && (ss-1)->statScore < 17329
         &&  eval >= beta
         &&  eval >= ss->staticEval
-        &&  ss->staticEval >= beta - 21 * depth + 258 - 20 * (cutNode && !ss->ttHit)
+        &&  ss->staticEval >= beta - (21 + 5 * (cutNode && !ss->ttHit)) * depth + 258
         && !excludedMove
         &&  pos.non_pawn_material(us)
         &&  ss->ply >= thisThread->nmpMinPly
