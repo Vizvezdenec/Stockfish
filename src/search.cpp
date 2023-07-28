51cd40105d09313583b9963e5134cf217b1887b2
Michael Chaly <Vizvezdenec@gmail.com>
2023-07-28 10:36:00 +0300

    bench 1597771

--- src/search.cpp
+++ src/search.cpp
@@ -1547,7 +1547,7 @@ moves_loop: // When in check, search starts here
                     futilityValue +=  (2 * thisThread->mainHistory[pos.side_to_move()][from_to(move)]
                                     + (*contHist[0])[pos.moved_piece(move)][to_sq(move)]
                                     + (*contHist[1])[pos.moved_piece(move)][to_sq(move)]
-                                    + (*contHist[3])[pos.moved_piece(move)][to_sq(move)]) / 4096;
+                                    + (*contHist[3])[pos.moved_piece(move)][to_sq(move)]) / 16384;
 
                 if (futilityValue <= alpha)
                 {
