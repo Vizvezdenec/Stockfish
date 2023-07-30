2a9c993476ed824361afe6a5ee069d5393c4a49d
Michael Chaly <Vizvezdenec@gmail.com>
2023-07-30 15:59:40 +0300

    bench 1604020

--- src/search.cpp
+++ src/search.cpp
@@ -1099,7 +1099,7 @@ moves_loop: // When in check, search starts here
 
               // If we are on a cutNode, reduce it based on depth (negative extension) (~1 Elo)
               else if (cutNode)
-                  extension = depth > 8 && depth < 17 ? -3 : ttValue <= alpha ? -3 : -1;
+                  extension = depth > 8 && depth < 17 ? -3 : -1;
 
               else if (ttValue <= alpha)
                   extension = -3;
