0d1e44d2505e2117b51c08a4540e430467c725a7
Michael Chaly <Vizvezdenec@gmail.com>
2023-08-21 16:25:59 +0300

    bench 1428499

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -161,7 +161,7 @@ void MovePicker::score() {
                        :                                                0 )
                        :                                                0 ;
 
-          m.value += type_of(pc) == KNIGHT && (attacks_bb<KNIGHT>(to) & pos.pieces(~pos.side_to_move(), QUEEN)) ? 5000 : 0;
+          m.value += type_of(pc) == KNIGHT && (attacks_bb<KNIGHT>(to) & pos.pieces(~pos.side_to_move(), QUEEN)) ? 35000 : 0;
       }
       
       else // Type == EVASIONS
