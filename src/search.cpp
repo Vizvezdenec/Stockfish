e86f96a9f5cdf7c3fb8b23b51a25016873883eef
Michael Chaly <Vizvezdenec@gmail.com>
2023-09-07 08:56:27 +0300

    bench 1704155

--- src/search.cpp
+++ src/search.cpp
@@ -72,8 +72,8 @@ namespace {
   enum NodeType { NonPV, PV, Root };
 
   // Futility margin
-  Value futility_margin(Depth d, bool noTtCutNode, bool improving) {
-    return Value((140 - 40 * noTtCutNode) * (d - improving));
+  Value futility_margin(Depth d, bool noTtCutNode, bool improving, bool excludedMove) {
+    return Value((140 - 40 * noTtCutNode + 10 * excludedMove) * (d - improving));
   }
 
   // Reductions lookup table initialized at startup
@@ -776,7 +776,7 @@ namespace {
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
         &&  depth < 9
-        &&  eval - futility_margin(depth, cutNode && !ss->ttHit, improving) - (ss-1)->statScore / 306 >= beta
+        &&  eval - futility_margin(depth, cutNode && !ss->ttHit, improving, excludedMove) - (ss-1)->statScore / 306 >= beta
         &&  eval >= beta
         &&  eval < 24923) // larger than VALUE_KNOWN_WIN, but smaller than TB wins
         return eval;
@@ -1575,7 +1575,7 @@ moves_loop: // When in check, search starts here
                 continue;
 
             // Do not search moves with bad enough SEE values (~5 Elo)
-            if (!pos.see_ge(move, Value(-125 - 10 * depth)))
+            if (!pos.see_ge(move, Value(-95)))
                 continue;
         }
 
