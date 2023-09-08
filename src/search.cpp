b792130be95618d68685bfedd0941d09490e68a9
Michael Chaly <Vizvezdenec@gmail.com>
2023-09-08 09:33:52 +0300

    bench 1611598

--- src/search.cpp
+++ src/search.cpp
@@ -1594,7 +1594,7 @@ moves_loop: // When in check, search starts here
 
         // Step 7. Make and search the move
         pos.do_move(move, st, givesCheck);
-        value = -qsearch<nodeType>(pos, ss+1, -beta, -alpha, depth - 1 + (priorCapture && ss->inCheck));
+        value = -qsearch<nodeType>(pos, ss+1, -beta, -alpha, depth - 1 + (priorCapture && ss->inCheck && capture));
         pos.undo_move(move);
 
         assert(value > -VALUE_INFINITE && value < VALUE_INFINITE);
