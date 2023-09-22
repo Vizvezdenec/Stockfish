66ec4ead395d0ae06a1f7fef6776029aa52eeafd
Michael Chaly <Vizvezdenec@gmail.com>
2023-09-22 05:02:43 +0300

    bench 1554890

--- src/search.cpp
+++ src/search.cpp
@@ -994,7 +994,7 @@ moves_loop: // When in check, search starts here
               if (   !givesCheck
                  &&  to_sq(move) != prevSq
                  &&  type_of(move) != PROMOTION
-                 &&  moveCount >= 5 + 2 * depth * depth)
+                 &&  moveCount >= (2 + depth * depth * (1 + improving)))
                  continue;
 
               // Futility pruning for captures (~2 Elo)
