7acb2fd761f45399d1d036e303ab0d35f0e18dfc
Michael Chaly <Vizvezdenec@gmail.com>
2023-09-27 10:39:24 +0300

    bench 1164710

--- src/search.cpp
+++ src/search.cpp
@@ -1026,7 +1026,7 @@ moves_loop: // When in check, search starts here
                   && lmrDepth < 12
                   && futilityValue + 138 * lmrDepth <= alpha)
                   {
-                      if (futilityValue <= alpha && depth > 4)
+                      if (futilityValue <= alpha && depth > 6)
                           bestValue = std::max(bestValue, Value(futilityValue));
                       continue;
                   }
