edbc7bc29342ddaae85a8e23e5f20d78963ff425
Michael Chaly <Vizvezdenec@gmail.com>
2023-10-02 03:07:36 +0300

    bench 1291481

--- src/search.cpp
+++ src/search.cpp
@@ -836,7 +836,7 @@ namespace {
     // Use qsearch if depth is equal or below zero (~9 Elo)
     if (    PvNode
         && !ttMove)
-        depth -= 2 + 2 * (ss->ttHit && tte->depth() >= depth) + ((ss-1)->statScore > 40000);
+        depth -= 2 + 2 * (ss->ttHit && tte->depth() >= depth) + ((ss-1)->statScore > 60000);
 
     if (depth <= 0)
         return qsearch<PV>(pos, ss, alpha, beta);
