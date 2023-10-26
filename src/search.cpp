cd2a21f2af0399ea7efaf1786e5157afb2264b98
Michael Chaly <Vizvezdenec@gmail.com>
2023-10-26 03:35:58 +0300

    bench 1382271

--- src/search.cpp
+++ src/search.cpp
@@ -746,7 +746,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
     {
         int bonus = std::clamp(-18 * int((ss - 1)->staticEval + ss->staticEval), -1812, 1812);
         thisThread->mainHistory[~us][from_to((ss - 1)->currentMove)] << bonus;
-        thisThread->mainHistory[~us][from_to(make_move(to_sq((ss - 1)->currentMove), from_sq((ss - 1)->currentMove)))] << -bonus;
+        thisThread->mainHistory[~us][from_to(make_move(to_sq((ss - 1)->currentMove), from_sq((ss - 1)->currentMove)))] << -bonus / 2;
     }
 
     // Set up the improving flag, which is true if current static evaluation is
