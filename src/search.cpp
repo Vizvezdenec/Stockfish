8ed0c033c789919f5eeabaa7d8208fd8198489da
Michael Chaly <Vizvezdenec@gmail.com>
2023-10-27 04:11:31 +0300

    bench 1281649

--- src/search.cpp
+++ src/search.cpp
@@ -1674,8 +1674,6 @@ void update_all_stats(const Position& pos,
         // Increase stats for the best move in case it was a quiet move
         update_quiet_stats(pos, ss, bestMove, bestMoveBonus);
 
-        thisThread->pawnHistory.get(pos, pos.pawn_key(), bestMove) << quietMoveBonus;
-
         // Decrease stats for all non-best quiet moves
         for (int i = 0; i < quietCount; ++i)
         {
@@ -1739,6 +1737,7 @@ void update_quiet_stats(const Position& pos, Stack* ss, Move move, int bonus) {
     Thread* thisThread = pos.this_thread();
     thisThread->mainHistory[us][from_to(move)] << bonus;
     update_continuation_histories(ss, pos.moved_piece(move), to_sq(move), bonus);
+    thisThread->pawnHistory.get(pos, pos.pawn_key(), move) << bonus;
 
     // Update countermove history
     if (is_ok((ss - 1)->currentMove))
