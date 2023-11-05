f3307552ce9a36520c749f94b469f9f191f677fd
Michael Chaly <Vizvezdenec@gmail.com>
2023-11-05 06:45:16 +0300

    bench 1305875

--- src/search.cpp
+++ src/search.cpp
@@ -842,7 +842,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
     // If we have a good enough capture (or queen promotion) and a reduced search returns a value
     // much above beta, we can (almost) safely prune the previous move.
     if (
-      !rootNode && depth > 3
+      !PvNode && depth > 3
       && abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
       // If value from transposition table is lower than probCutBeta, don't attempt probCut
       // there and in further interactions with transposition table cutoff depth is set to depth - 3
@@ -875,7 +875,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
 
                 // If the qsearch held, perform the regular search
                 if (value >= probCutBeta)
-                    value = -search<NonPV>(pos, ss + 1, -probCutBeta, -probCutBeta + 1, depth - 4 + 4 * PvNode,
+                    value = -search<NonPV>(pos, ss + 1, -probCutBeta, -probCutBeta + 1, depth - 4,
                                            !cutNode);
 
                 pos.undo_move(move);
@@ -1096,6 +1096,9 @@ moves_loop:  // When in check, search starts here
         // Add extension to new depth
         newDepth += extension;
         ss->doubleExtensions = (ss - 1)->doubleExtensions + (extension == 2);
+
+        if (thisThread->pawnHistory[pawn_structure(pos)][movedPiece][to_sq(move)] < -5000)
+            r++;
 
         // Speculative prefetch as early as possible
         prefetch(TT.first_entry(pos.key_after(move)));
