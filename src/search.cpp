5d3cf41b825bf09ded9ca15867b2b41e06bc5d64
Michael Chaly <Vizvezdenec@gmail.com>
2023-11-11 22:53:58 +0300

    bench 1065286

--- src/search.cpp
+++ src/search.cpp
@@ -1118,8 +1118,8 @@ moves_loop:  // When in check, search starts here
         pos.do_move(move, st, givesCheck);
 
         // Decrease reduction if position is or has been on the PV (~4 Elo)
-        if (ss->ttPv && !likelyFailLow)
-            r -= cutNode && tte->depth() >= depth ? 3 : !PvNode && !cutNode && !ttMove ? 1 : 2;
+        if (ss->ttPv && !likelyFailLow && (cutNode || PvNode || !ttMove))
+            r -= cutNode && tte->depth() >= depth ? 3 : 2;
 
         // Decrease reduction if opponent's move count is high (~1 Elo)
         if ((ss - 1)->moveCount > 7)
