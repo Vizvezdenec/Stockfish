e17a8be7ab6ab35ff36a267890eaf7b6587bfa17
Michael Chaly <Vizvezdenec@gmail.com>
2023-11-14 20:37:16 +0300

    bench 1186152

--- src/search.cpp
+++ src/search.cpp
@@ -1402,6 +1402,7 @@ Value qsearch(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth) {
     bool     pvHit, givesCheck, capture;
     int      moveCount;
     Color    us = pos.side_to_move();
+    bool priorCapture = pos.captured_piece();
 
     // Step 1. Initialize node
     if (PvNode)
@@ -1482,7 +1483,7 @@ Value qsearch(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth) {
     // queen promotions, and other checks (only if depth >= DEPTH_QS_CHECKS)
     // will be generated.
     Square     prevSq = is_ok((ss - 1)->currentMove) ? to_sq((ss - 1)->currentMove) : SQ_NONE;
-    MovePicker mp(pos, ttMove, depth, &thisThread->mainHistory, &thisThread->captureHistory,
+    MovePicker mp(pos, ttMove, !priorCapture ? 0 : depth, &thisThread->mainHistory, &thisThread->captureHistory,
                   contHist, &thisThread->pawnHistory, prevSq);
 
     int quietCheckEvasions = 0;
