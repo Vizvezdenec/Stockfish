2457b7b3a83b682ac77f5c5eabcc6e91369bf2b7
Michael Chaly <Vizvezdenec@gmail.com>
2023-11-20 18:31:13 +0300

    bench 1321624

--- src/search.cpp
+++ src/search.cpp
@@ -1300,7 +1300,7 @@ moves_loop:  // When in check, search starts here
 
                 if (value >= beta)
                 {
-                    ss->cutoffCnt += 1 + !ttMove * (1 + ss->ttHit);
+                    ss->cutoffCnt += 1 + !ttMove;
                     assert(value >= beta);  // Fail high
                     break;
                 }
@@ -1352,6 +1352,10 @@ moves_loop:  // When in check, search starts here
                                       stat_bonus(depth) * bonus);
         thisThread->mainHistory[~us][from_to((ss - 1)->currentMove)]
           << stat_bonus(depth) * bonus / 2;
+
+        if ((PvNode || cutNode) && ttMove && !pos.capture(ttMove) && (tte->bound() & BOUND_LOWER) && tte->depth() >= depth)
+            update_continuation_histories(ss, pos.moved_piece(ttMove), to_sq(ttMove),
+                                      -stat_bonus(depth));
     }
 
     if (PvNode)
