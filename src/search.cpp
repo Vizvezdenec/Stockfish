1139f5b7a8a845bd2d4bc6ceec0e72dffdf3b1d0
Michael Chaly <Vizvezdenec@gmail.com>
2023-11-28 09:30:05 +0300

    bench 1384377

--- src/search.cpp
+++ src/search.cpp
@@ -838,7 +838,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
     if (PvNode && !ttMove)
         depth -= 2 + 2 * (ss->ttHit && tte->depth() >= depth);
 
-    if (PvNode && ttMove && (tte->bound() & BOUND_UPPER) && ttValue <= alpha
+    if (PvNode && ttMove && (tte->bound() & BOUND_UPPER) && ttValue <= alpha && tte->is_pv()
         && !pos.capture(ttMove) && (*contHist[0])[pos.moved_piece(ttMove)][to_sq(ttMove)] < -10000)
         depth -= 2 + 2 * (tte->depth() >= depth);
 
