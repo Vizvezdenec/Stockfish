f2f3aea942ff1a45b80d4e34e34ccc62d825f5ad
Michael Chaly <Vizvezdenec@gmail.com>
2023-11-28 20:46:17 +0300

    bench 1492576

--- src/search.cpp
+++ src/search.cpp
@@ -836,7 +836,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
 
     // For cutNodes without a ttMove, we decrease depth by 2 if depth is high enough.
     if (cutNode && depth >= 8 && !ttMove)
-        depth -= 2 + (tte->is_pv() && tte->depth() >= depth - 3);
+        depth -= 2 + 2 * (tte->is_pv() && tte->depth() >= depth - 3);
 
     probCutBeta = beta + 168 - 70 * improving;
 
