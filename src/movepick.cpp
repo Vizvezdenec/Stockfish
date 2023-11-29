a65829dced416ea3feac706200243370129f591e
Michael Chaly <Vizvezdenec@gmail.com>
2023-11-29 12:22:05 +0300

    bench 2071573

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -268,7 +268,8 @@ top:
     case QSEARCH_COUNTERCAPTURE :
     case PROBCUT_COUNTERCAPTURE :
         ++stage;
-        if (counterCapture && pos.capture_stage(counterCapture) && pos.pseudo_legal(counterCapture))
+        if (counterCapture && pos.capture_stage(counterCapture) && pos.pseudo_legal(counterCapture)
+            && (stage != PROBCUT_COUNTERCAPTURE || pos.see_ge(counterCapture, threshold)))
             return counterCapture;
         [[fallthrough]];
 
