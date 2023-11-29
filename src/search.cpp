afbcf85774bf0c256f28a7347bfb74b61e128887
Michael Chaly <Vizvezdenec@gmail.com>
2023-11-29 17:29:51 +0300

    bench 1521553

--- src/search.cpp
+++ src/search.cpp
@@ -1218,7 +1218,7 @@ moves_loop:  // When in check, search starts here
 
         // For PV nodes only, do a full PV search on the first move or after a fail high,
         // otherwise let the parent node fail low with value <= alpha and try another move.
-        if (PvNode && (moveCount == 1 || (value > alpha && (rootNode || value < 2 * beta - alpha))) )
+        if (PvNode && (moveCount == 1 || (value > alpha && (rootNode || value < beta + 200))) )
         {
             (ss + 1)->pv    = pv;
             (ss + 1)->pv[0] = MOVE_NONE;
