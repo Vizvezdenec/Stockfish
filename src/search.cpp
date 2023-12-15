cccb27e2152b597de68fe93ebedc253755625afc
Michael Chaly <Vizvezdenec@gmail.com>
2023-12-15 16:27:04 +0300

    bench 931260

--- src/search.cpp
+++ src/search.cpp
@@ -1618,7 +1618,7 @@ Value qsearch(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth) {
     }
 
     if (abs(bestValue) < VALUE_TB_WIN_IN_MAX_PLY)
-        bestValue = bestValue >= beta ? (3 * bestValue + beta) / 4 : (3 * bestValue + alpha) / 4;
+        bestValue = bestValue >= beta ? (bestValue + beta) / 2 : (bestValue + alpha) / 2;
 
     // Save gathered info in transposition table
     tte->save(posKey, value_to_tt(bestValue, ss->ply), pvHit,
