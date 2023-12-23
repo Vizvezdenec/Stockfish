8b93c2062bb42fc512539c3ddd60cb2bc6ee4dc4
Michael Chaly <Vizvezdenec@gmail.com>
2023-12-23 08:21:30 +0300

    bench 1426586

--- src/search.cpp
+++ src/search.cpp
@@ -653,7 +653,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
         // Partial workaround for the graph history interaction problem
         // For high rule50 counts don't produce transposition table cutoffs.
         if (pos.rule50_count() < 90)
-            return ttValue >= beta && abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY && tte->depth() < depth + 4 ? (ttValue * 3 + beta) / 4 : ttValue;
+            return ttValue;
     }
 
     // Step 5. Tablebases probe
@@ -782,7 +782,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
              >= beta
         && eval >= beta && eval < 29008  // smaller than TB wins
         && (!ttMove || ttCapture))
-        return (eval + beta) / 2;
+        return cutNode && !ss->ttHit ? (eval + beta) / 2 : (3 * eval + 5 * beta) / 8;
 
     // Step 9. Null move search with verification search (~35 Elo)
     if (!PvNode && (ss - 1)->currentMove != MOVE_NULL && (ss - 1)->statScore < 17496 && eval >= beta
