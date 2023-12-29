f78839c613cf8041e7a49446dbcd4488d463a9ed
Michael Chaly <Vizvezdenec@gmail.com>
2023-12-29 07:11:05 +0300

    bench 1375547

--- src/search.cpp
+++ src/search.cpp
@@ -740,7 +740,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
     }
     else
     {
-        ss->staticEval = eval = evaluate(pos);
+        ss->staticEval = eval = evaluate(pos) + thisThread->corrHistory[us][corr_structure(pos)] / 8;
         // Save static evaluation into the transposition table
         tte->save(posKey, VALUE_NONE, ss->ttPv, BOUND_NONE, DEPTH_NONE, MOVE_NONE, eval);
     }
@@ -1367,6 +1367,11 @@ moves_loop:  // When in check, search starts here
                   : PvNode && bestMove ? BOUND_EXACT
                                        : BOUND_UPPER,
                   depth, bestMove, ss->staticEval);
+
+    if (!ss->inCheck 
+        && !(bestValue >= beta && bestValue <= ss->staticEval)
+        && !(bestValue <= alpha && bestValue >= ss->staticEval))
+        thisThread->corrHistory[us][corr_structure(pos)] << std::clamp(int(bestValue - ss->staticEval), -CORR_HISTORY_LIMIT, CORR_HISTORY_LIMIT);
 
     assert(bestValue > -VALUE_INFINITE && bestValue < VALUE_INFINITE);
 
