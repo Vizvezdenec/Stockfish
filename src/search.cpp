e08f53cb9e4f18bcca7e3bbc8088fc1717f22f2b
Michael Chaly <Vizvezdenec@gmail.com>
2024-01-06 07:59:14 +0300

    bench 1396897

--- src/search.cpp
+++ src/search.cpp
@@ -1612,7 +1612,7 @@ Value qsearch(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth) {
             if (quietCheckEvasions > 1)
                 break;
 
-            if (checkCount > 0 && !capture)
+            if (checkCount > 1)
                 break;
 
             // Continuation history based pruning (~3 Elo)
