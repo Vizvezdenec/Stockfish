ffa6ddb2fcf9049550822c6a10d77ac401b4f3fd
Michael Chaly <Vizvezdenec@gmail.com>
2024-01-12 20:18:44 +0300

    bench 1274135

--- src/search.cpp
+++ src/search.cpp
@@ -1027,9 +1027,6 @@ moves_loop:  // When in check, search starts here
             }
             else
             {
-                if (move == (ss - 4)->currentMove && pos.has_repeated())
-                    lmrDepth -= 2;
-
                 int history =
                   (*contHist[0])[movedPiece][move.to_sq()]
                   + (*contHist[1])[movedPiece][move.to_sq()]
@@ -1216,7 +1213,7 @@ moves_loop:  // When in check, search starts here
             // beyond the first move depth. This may lead to hidden double extensions.
             // To prevent problems when the max value is less than the min value,
             // std::clamp has been replaced by a more robust implementation.
-            Depth d = std::max(1, std::min(newDepth - r, newDepth + 1));
+            Depth d = std::max(int(ss->inCheck || ss->staticEval <= beta), std::min(newDepth - r, newDepth + 1));
 
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, d, true);
 
