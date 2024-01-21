62d9e7ac513ffbaf5bc5323ddd3cb7d11832be63
Michael Chaly <Vizvezdenec@gmail.com>
2024-01-21 18:40:53 +0300

    bench 1407154

--- src/search.cpp
+++ src/search.cpp
@@ -56,7 +56,7 @@ namespace {
 // Futility margin
 Value futility_margin(Depth d, bool noTtCutNode, bool improving) {
     Value futilityMult = 114 - 47 * noTtCutNode;
-    return (futilityMult * d - 3 * futilityMult / 2 * improving);
+    return improving ? futilityMult * d / 2 : futilityMult * d;
 }
 
 constexpr int futility_move_count(bool improving, Depth depth) {
