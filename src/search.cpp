5b8504d9431a449128259606f593c6dfce85b495
Michael Chaly <Vizvezdenec@gmail.com>
2024-02-21 23:06:52 +0300

    bench 1660275

--- src/search.cpp
+++ src/search.cpp
@@ -1043,7 +1043,7 @@ moves_loop:  // When in check, search starts here
                         extension = 2 + (value < singularBeta - 78 && !ttCapture);
                         depth += depth < 16;
                     }
-                    if (PvNode && !ttCapture && ss->multipleExtensions <= 5 && value < singularBeta - 50 && !tte->is_pv())
+                    if (PvNode && !ttCapture && ss->multipleExtensions <= 5 && value < singularBeta - 50)
                         extension = 2;
                 }
 
