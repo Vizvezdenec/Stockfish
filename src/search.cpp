ce09a9b01f953863ff2b967e9e2d86059e92d3c0
Michael Chaly <Vizvezdenec@gmail.com>
2024-02-27 07:26:45 +0300

    bench 1637669

--- src/search.cpp
+++ src/search.cpp
@@ -1427,7 +1427,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta,
     if (!PvNode && tte->depth() >= ttDepth
         && ttValue != VALUE_NONE  // Only in case of TT access race or if !ttHit
         && (tte->bound() & (ttValue >= beta ? BOUND_LOWER : BOUND_UPPER)))
-        return std::abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY && ttValue >= beta && tte->depth() <= 2 ? (3 * ttValue + beta) / 4 : ttValue;
+        return std::abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY && ttValue >= beta && tte->depth() <= 0 ? (3 * ttValue + beta) / 4 : ttValue;
 
     // Step 4. Static evaluation of the position
     Value unadjustedStaticEval = VALUE_NONE;
