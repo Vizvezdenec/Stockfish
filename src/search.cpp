26295825f8bf4b7f03001d201e32d7eae56156db
Michael Chaly <Vizvezdenec@gmail.com>
2024-02-27 07:28:50 +0300

    bench 1420526

--- src/search.cpp
+++ src/search.cpp
@@ -626,7 +626,7 @@ Value Search::Worker::search(
         // Partial workaround for the graph history interaction problem
         // For high rule50 counts don't produce transposition table cutoffs.
         if (pos.rule50_count() < 90)
-            return ttValue >= beta && std::abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY
+            return ttValue >= beta && std::abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY && tte->depth() - depth <= 1
                    ? (ttValue * 3 + beta) / 4
                    : ttValue;
     }
@@ -1427,7 +1427,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta,
     if (!PvNode && tte->depth() >= ttDepth
         && ttValue != VALUE_NONE  // Only in case of TT access race or if !ttHit
         && (tte->bound() & (ttValue >= beta ? BOUND_LOWER : BOUND_UPPER)))
-        return std::abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY && ttValue >= beta && tte->depth() <= 0 ? (3 * ttValue + beta) / 4 : ttValue;
+        return ttValue;
 
     // Step 4. Static evaluation of the position
     Value unadjustedStaticEval = VALUE_NONE;
