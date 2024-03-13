58807d8c94f4aeb0d79fc6f281e87046b29cfc46
Michael Chaly <Vizvezdenec@gmail.com>
2024-03-13 19:49:35 +0300

    bench 1849248

--- src/search.cpp
+++ src/search.cpp
@@ -1352,7 +1352,7 @@ moves_loop:  // When in check, search starts here
     {
         auto bonus = std::clamp(int(bestValue - ss->staticEval) * depth / 8,
                                 -CORRECTION_HISTORY_LIMIT / 4, CORRECTION_HISTORY_LIMIT / 4);
-        if (bonus > 0)
+        if (bonus < 0)
             bonus /= 2;
         thisThread->correctionHistory[us][pawn_structure_index<Correction>(pos)] << bonus;
     }
