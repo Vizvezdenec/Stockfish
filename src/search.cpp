b52c28f3504c4fc50a0ed83d91226b00022946da
Michael Chaly <Vizvezdenec@gmail.com>
2024-04-03 23:48:58 +0300

    bench 1939046

--- src/search.cpp
+++ src/search.cpp
@@ -1279,7 +1279,7 @@ moves_loop:  // When in check, search starts here
                 {
                     // Reduce other moves if we have found at least one score improvement (~2 Elo)
                     if (depth > 2 && depth < 12 && beta < 14206 && value > -12077)
-                        depth -= 2 + 2 * (move == ttMove);
+                        depth -= 2 + (move == ttMove && tte->depth() >= depth);
 
                     depth = std::max(depth, 0);
                     alpha = value;  // Update alpha! Always alpha < beta
