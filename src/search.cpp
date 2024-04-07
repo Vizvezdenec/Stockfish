1566b4f62c9ee6eb5ed8f3705b4281e399179f10
Michael Chaly <Vizvezdenec@gmail.com>
2024-04-07 22:03:09 +0300

    bench 2016953

--- src/search.cpp
+++ src/search.cpp
@@ -1049,7 +1049,7 @@ moves_loop:  // When in check, search starts here
                     // We make sure to limit the extensions in some way to avoid a search explosion
                     if (!PvNode && ss->multipleExtensions <= 16)
                     {
-                        extension = 2 + (value < singularBeta - 22 && !ttCapture) + (move == ss->killers[0]);
+                        extension = 2 + (value < singularBeta - 22 && !ttCapture) + (move == ss->killers[0] && (*contHist[0])[movedPiece][move.to_sq()] > 7777);
                         depth += depth < 14;
                     }
                     if (PvNode && !ttCapture && ss->multipleExtensions <= 5
