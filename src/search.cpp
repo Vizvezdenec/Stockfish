27c72cd45f4ab9548596c89e5e612e201f1f186c
Michael Chaly <Vizvezdenec@gmail.com>
2024-04-10 18:29:51 +0300

    bench 1707914

--- src/search.cpp
+++ src/search.cpp
@@ -796,7 +796,7 @@ Value Search::Worker::search(
         if (nullValue >= beta && nullValue < VALUE_TB_WIN_IN_MAX_PLY)
         {
             if (thisThread->nmpMinPly || depth < 16)
-                return std::min(nullValue + (nullValue - beta) / 8, VALUE_TB_WIN_IN_MAX_PLY - 1);
+                return std::min(nullValue + (nullValue - beta) / 16, VALUE_TB_WIN_IN_MAX_PLY - 1);
 
             assert(!thisThread->nmpMinPly);  // Recursive verification is not allowed
 
