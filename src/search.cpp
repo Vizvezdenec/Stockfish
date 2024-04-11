d658df14ed11a19b2565426bbd7ea0a42a4e0dc5
Michael Chaly <Vizvezdenec@gmail.com>
2024-04-11 10:07:40 +0300

    bench 1700852

--- src/search.cpp
+++ src/search.cpp
@@ -796,7 +796,7 @@ Value Search::Worker::search(
         if (nullValue >= beta && nullValue < VALUE_TB_WIN_IN_MAX_PLY)
         {
             if (thisThread->nmpMinPly || depth < 16)
-                return std::min(nullValue + (nullValue - beta) / 16, VALUE_TB_WIN_IN_MAX_PLY - 1);
+                return nullValue;
 
             assert(!thisThread->nmpMinPly);  // Recursive verification is not allowed
 
@@ -976,7 +976,7 @@ moves_loop:  // When in check, search starts here
                 }
 
                 // SEE based pruning for captures and checks (~11 Elo)
-                if (!pos.see_ge(move, -203 * depth))
+                if (!pos.see_ge(move, -203 * depth - thisThread->captureHistory[movedPiece][move.to_sq()][type_of(pos.piece_on(move.to_sq()))] / 128))
                     continue;
             }
             else
