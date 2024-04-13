08071d3f5786fe7ac331cd2e2d7e0ae9a85db2e0
Michael Chaly <Vizvezdenec@gmail.com>
2024-04-13 20:27:52 +0300

    bench 1670100

--- src/search.cpp
+++ src/search.cpp
@@ -975,7 +975,7 @@ moves_loop:  // When in check, search starts here
                 }
 
                 // SEE based pruning for captures and checks (~11 Elo)
-                if (!pos.see_ge(move, -199 * depth))
+                if (!pos.see_ge(move, -199 * depth - thisThread->captureHistory[movedPiece][move.to_sq()][type_of(pos.piece_on(move.to_sq()))] / 64))
                     continue;
             }
             else
