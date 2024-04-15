acc967fcdd3c15a492413eb05fb5f63dd750e895
Michael Chaly <Vizvezdenec@gmail.com>
2024-04-15 19:34:49 +0300

    bench 1454757

--- src/search.cpp
+++ src/search.cpp
@@ -1184,7 +1184,7 @@ moves_loop:  // When in check, search starts here
                 r += 2;
 
             if (extension)
-                r-= 2;
+                r-= 3;
 
             // Note that if expected reduction is high, we reduce search depth by 1 here (~9 Elo)
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth - (r > 3), !cutNode);
