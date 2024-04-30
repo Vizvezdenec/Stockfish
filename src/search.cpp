294a3cbcdae96e5be2d72781855e8abf12ce0476
Michael Chaly <Vizvezdenec@gmail.com>
2024-04-30 23:24:42 +0300

    bench 1638355

--- src/search.cpp
+++ src/search.cpp
@@ -833,7 +833,7 @@ Value Search::Worker::search(
     // Step 11. ProbCut (~10 Elo)
     // If we have a good enough capture (or queen promotion) and a reduced search returns a value
     // much above beta, we can (almost) safely prune the previous move.
-    probCutBeta = beta + 169 - 63 * improving - 5 * opponentWorsening;
+    probCutBeta = beta + 169 - 63 * improving - 15 * opponentWorsening;
     if (
       !PvNode && depth > 3
       && std::abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
