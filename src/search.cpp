4d4a95b25fff67562c081ab38239e48e9b9dd856
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-04 19:53:26 +0300

    bench 1603129

--- src/search.cpp
+++ src/search.cpp
@@ -1793,7 +1793,7 @@ void update_continuation_histories(Stack* ss, Piece pc, Square to, int bonus) {
         if (ss->inCheck && i > 2)
             break;
         if (((ss - i)->currentMove).is_ok())
-            (*(ss - i)->continuationHistory)[pc][to] << bonus / (1 + 3 * (i == 3) + 31 * (i == 5));
+            (*(ss - i)->continuationHistory)[pc][to] << bonus / (1 + 3 * (i == 3) + 3 * (i == 5));
     }
 }
 
