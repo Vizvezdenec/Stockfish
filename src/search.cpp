ec15c6f3a9a93efb19bdbaa8a96512dd1e4819af
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-04 17:05:20 +0300

    bench 1621483

--- src/search.cpp
+++ src/search.cpp
@@ -979,7 +979,7 @@ moves_loop:  // When in check, search starts here
                     {
                         if (bestValue <= futilityValue && std::abs(bestValue) < VALUE_TB_WIN_IN_MAX_PLY
                             && futilityValue < VALUE_TB_WIN_IN_MAX_PLY)
-                            bestValue = (7 * bestValue + futilityValue) / 8;
+                            bestValue = (bestValue + futilityValue) / 2;
                         continue;
                     }
                 }
