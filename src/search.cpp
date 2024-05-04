95f248ad1fa7328fd3cf5baa126cd74a4624d4ab
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-04 15:55:09 +0300

    bench 1558042

--- src/search.cpp
+++ src/search.cpp
@@ -834,7 +834,7 @@ Value Search::Worker::search(
     // much above beta, we can (almost) safely prune the previous move.
     probCutBeta = beta + 169 - 63 * improving;
     if (
-      !PvNode && depth > 4
+      !PvNode
       && std::abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
       // If value from transposition table is lower than probCutBeta, don't attempt probCut
       // there and in further interactions with transposition table cutoff depth is set to depth - 3
@@ -866,7 +866,7 @@ Value Search::Worker::search(
                 value = -qsearch<NonPV>(pos, ss + 1, -probCutBeta, -probCutBeta + 1);
 
                 // If the qsearch held, perform the regular search
-                if (value >= probCutBeta)
+                if (value >= probCutBeta && depth > 4)
                     value = -search<NonPV>(pos, ss + 1, -probCutBeta, -probCutBeta + 1, depth - 4,
                                            !cutNode);
 
@@ -875,7 +875,7 @@ Value Search::Worker::search(
                 if (value >= probCutBeta)
                 {
                     // Save ProbCut data into transposition table
-                    tte->save(posKey, value_to_tt(value, ss->ply), ss->ttPv, BOUND_LOWER, depth - 3,
+                    tte->save(posKey, value_to_tt(value, ss->ply), ss->ttPv, BOUND_LOWER, std::max(depth - 3, 0),
                               move, unadjustedStaticEval, tt.generation());
                     return std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY ? value - (probCutBeta - beta)
                                                                      : value;
