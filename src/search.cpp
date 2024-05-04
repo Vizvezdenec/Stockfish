587de3774151f9f24e763aadad50f4f904175e8e
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-05 01:25:15 +0300

    bench 1484584

--- src/search.cpp
+++ src/search.cpp
@@ -1533,7 +1533,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta,
             if (!givesCheck && move.to_sq() != prevSq && futilityBase > VALUE_TB_LOSS_IN_MAX_PLY
                 && move.type_of() != PROMOTION)
             {
-                if (moveCount > 2 - (depth < -5))
+                if (moveCount > 2 - (depth < -7))
                     continue;
 
                 Value futilityValue = futilityBase + PieceValue[pos.piece_on(move.to_sq())];
