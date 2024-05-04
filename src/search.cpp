9423ef3562f75a4b2835eb97f406dc5249ecc3b9
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-04 16:35:22 +0300

    bench 1704680

--- src/search.cpp
+++ src/search.cpp
@@ -888,7 +888,7 @@ Value Search::Worker::search(
 moves_loop:  // When in check, search starts here
 
     // Step 12. A small Probcut idea, when we are in check (~4 Elo)
-    probCutBeta = beta + 452 - 200 * (ttMove && type_of(pos.moved_piece(ttMove)) != KING);
+    probCutBeta = beta + 452 - 200 * (ttMove && type_of(pos.moved_piece(ttMove)) < type_of(pos.piece_on(ttMove.to_sq())));
     if (ss->inCheck && !PvNode && ttCapture && (tte->bound() & BOUND_LOWER)
         && tte->depth() >= depth - 4 && ttValue >= probCutBeta
         && std::abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY && std::abs(beta) < VALUE_TB_WIN_IN_MAX_PLY)
