c5ede7c58c631bd472f6f795596b77a1a9b0e70c
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-05 03:40:56 +0300

    bench 1446957

--- src/search.cpp
+++ src/search.cpp
@@ -990,7 +990,7 @@ moves_loop:  // When in check, search starts here
                   (*contHist[0])[movedPiece][move.to_sq()]
                   + (*contHist[1])[movedPiece][move.to_sq()]
                   + (*contHist[3])[movedPiece][move.to_sq()]
-                  + (*contHist[5])[movedPiece][move.to_sq()]
+                  + (*contHist[5])[movedPiece][move.to_sq()] / 2
                   + thisThread->pawnHistory[pawn_structure_index(pos)][movedPiece][move.to_sq()];
 
                 // Continuation history based pruning (~2 Elo)
