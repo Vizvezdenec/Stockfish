e71f7026132c7c4060c2229cc34c950b56037261
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-07 00:31:56 +0300

    bench 2298851

--- src/search.cpp
+++ src/search.cpp
@@ -1572,7 +1572,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta,
             // Continuation history based pruning (~3 Elo)
             if (!capture && (*contHist[0])[pos.moved_piece(move)][move.to_sq()]
                           + (*contHist[1])[pos.moved_piece(move)][move.to_sq()]
-                          + thisThread->pawnHistory[pawn_structure_index(pos)][pos.moved_piece(move)][move.to_sq()] <= 0)
+                          + thisThread->pawnHistory[pawn_structure_index(pos)][pos.moved_piece(move)][move.to_sq()] <= -1923)
                 continue;
 
             // Do not search moves with bad enough SEE values (~5 Elo)
