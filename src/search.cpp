f8b066a387d785c7dc31dd65cd7798a9e01cc2df
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-08 23:29:56 +0300

    bench 2610702

--- src/search.cpp
+++ src/search.cpp
@@ -1087,11 +1087,8 @@ moves_loop:  // When in check, search starts here
                     extension = -3;
 
                 // If we are on a cutNode but the ttMove is not assumed to fail high over current beta (~1 Elo)
-                else if (cutNode)
-                    extension = -2;
-
                 else if (ttValue <= alpha)
-                    extension = -1;
+                    extension = -2;
 
                 // If the ttMove is assumed to fail low over the value of the reduced search (~1 Elo)
                 else if (ttValue <= value)
