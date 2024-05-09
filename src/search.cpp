8560b8382ffb0074958d2ef4f9a14ace4ead7f9a
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-09 10:11:13 +0300

    bench 2035500

--- src/search.cpp
+++ src/search.cpp
@@ -1087,6 +1087,8 @@ moves_loop:  // When in check, search starts here
                 // If we are on a cutNode but the ttMove is not assumed to fail high over current beta (~1 Elo)
                 else if (cutNode)
                     extension = -2;
+                else if (ttValue <= alpha)
+                    extension = -1;
             }
 
             // Extension for capturing the previous moved piece (~0 Elo on STC, ~1 Elo on LTC)
