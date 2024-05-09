c63b8318c98adb0de54d9e4ac3f427fc3440bcd5
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-09 17:31:47 +0300

    bench 2084365

--- src/search.cpp
+++ src/search.cpp
@@ -1478,8 +1478,6 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta,
         // Stand pat. Return immediately if static value is at least beta
         if (bestValue >= beta)
         {
-            if (std::abs(bestValue) < VALUE_TB_WIN_IN_MAX_PLY && !PvNode)
-                bestValue = (5 * bestValue + beta) / 6;
             if (!ss->ttHit)
                 tte->save(posKey, value_to_tt(bestValue, ss->ply), false, BOUND_LOWER, DEPTH_NONE,
                           Move::none(), unadjustedStaticEval, tt.generation());
@@ -1626,7 +1624,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta,
         return mated_in(ss->ply);  // Plies to mate from the root
     }
 
-    if (std::abs(bestValue) < VALUE_TB_WIN_IN_MAX_PLY && bestValue >= beta)
+    if (std::abs(bestValue) < VALUE_TB_WIN_IN_MAX_PLY && bestValue >= beta && !PvNode)
         bestValue = (3 * bestValue + beta) / 4;
 
     // Save gathered info in transposition table
