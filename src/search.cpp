67269beabc0bde77b47cd08ff0de948adb73803f
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-14 23:20:41 +0300

    bench 2131813

--- src/search.cpp
+++ src/search.cpp
@@ -493,7 +493,7 @@ void Search::Worker::iterative_deepening() {
 
 void Search::Worker::clear() {
     counterMoves.fill(Move::none());
-    mainHistory.fill(-150);
+    mainHistory.fill(-50);
     captureHistory.fill(0);
     pawnHistory.fill(0);
     correctionHistory.fill(0);
