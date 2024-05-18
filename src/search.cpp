d39eb97cc89675d54ed9c2b276121c0487343e72
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-18 11:14:16 +0300

    bench 1196547

--- src/search.cpp
+++ src/search.cpp
@@ -893,7 +893,7 @@ moves_loop:  // When in check, search starts here
     // Step 12. A small Probcut idea, when we are in check (~4 Elo)
     probCutBeta = beta + 428;
     if (ss->inCheck && !PvNode && ttCapture && (tte->bound() & BOUND_LOWER)
-        && tte->depth() >= depth - 4 && ttValue >= probCutBeta
+        && tte->depth() >= depth - 6 && ttValue >= probCutBeta
         && std::abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY && std::abs(beta) < VALUE_TB_WIN_IN_MAX_PLY)
         return probCutBeta;
 
