3cac1171fb610f1806603c24164e6ac7a431a098
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-20 08:51:33 +0300

    bench 1169757

--- src/search.cpp
+++ src/search.cpp
@@ -1816,7 +1816,7 @@ void update_quiet_histories(
     update_continuation_histories(ss, pos.moved_piece(move), move.to_sq(), bonus);
 
     int pIndex = pawn_structure_index(pos);
-    workerThread.pawnHistory[pIndex][pos.moved_piece(move)][move.to_sq()] << bonus / 2;
+    workerThread.pawnHistory[pIndex][pos.moved_piece(move)][move.to_sq()] << bonus * 3 / 4;
 }
 
 // Updates move sorting heuristics
