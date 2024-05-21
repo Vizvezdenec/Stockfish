3231d9fbd2ef9f1164fa482ad7b742e4cf2062fc
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-22 02:38:12 +0300

    bench 1403300

--- src/search.cpp
+++ src/search.cpp
@@ -497,7 +497,7 @@ void Search::Worker::clear() {
     counterMoves.fill(Move::none());
     mainHistory.fill(0);
     captureHistory.fill(0);
-    pawnHistory.fill(-1400);
+    pawnHistory.fill(-1350);
     correctionHistory.fill(0);
 
     for (bool inCheck : {false, true})
