2a3c13826c055915c4925be28bb3c777b8a99c50
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-27 20:34:57 +0300

    bench 1659499

--- src/search.cpp
+++ src/search.cpp
@@ -1117,7 +1117,7 @@ moves_loop:  // When in check, search starts here
 
         uint64_t nodeCount = rootNode ? uint64_t(nodes) : 0;
 
-        if (thisThread->correctionHistory[us][pawn_structure_index<Correction>(pos)] > CORRECTION_HISTORY_LIMIT * 31 / 32)
+        if (thisThread->correctionHistory[us][pawn_structure_index<Correction>(pos)] == CORRECTION_HISTORY_LIMIT)
             r--;
 
         // Step 16. Make the move
