3115dedd0f1cc0fbda4454a8815b2fe58e8405b3
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-27 20:31:01 +0300

    bench 2131594

--- src/search.cpp
+++ src/search.cpp
@@ -1117,6 +1117,9 @@ moves_loop:  // When in check, search starts here
 
         uint64_t nodeCount = rootNode ? uint64_t(nodes) : 0;
 
+        if (thisThread->correctionHistory[us][pawn_structure_index<Correction>(pos)] > CORRECTION_HISTORY_LIMIT * 15 / 16)
+            r--;
+
         // Step 16. Make the move
         thisThread->nodes.fetch_add(1, std::memory_order_relaxed);
         pos.do_move(move, st, givesCheck);
