d9aed74d1fdf6a1902ecd1ae7c22305fb746ac52
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-29 02:49:55 +0300

    bench 1544901

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -187,7 +187,7 @@ void MovePicker::score() {
             m.value += (*continuationHistory[5])[pc][to];
 
             // bonus for checks
-            m.value += bool(pos.check_squares(pt) & to) * (pt == ROOK ? 25000 : pt == KNIGHT || pt == QUEEN ? 20000 : pt == BISHOP ? 17000 : 5000);
+            m.value += bool(pos.check_squares(pt) & to) * 16384;
 
             // bonus for escaping from capture
             m.value += threatenedPieces & from ? (pt == QUEEN && !(to & threatenedByRook)   ? 51700
--- src/search.cpp
+++ src/search.cpp
@@ -1591,7 +1591,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta,
                 continue;
 
             // Do not search moves with bad enough SEE values (~5 Elo)
-            if (!pos.see_ge(move, -67))
+            if (!pos.see_ge(move, (capture || givesCheck) ? -240 : 0))
                 continue;
         }
 
