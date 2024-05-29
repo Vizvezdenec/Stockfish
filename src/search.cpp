8f3648483f4c246c4885259315c3859eee3f2eb4
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-29 09:13:02 +0300

    bench 1516537

--- src/search.cpp
+++ src/search.cpp
@@ -788,8 +788,7 @@ Value Search::Worker::search(
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and eval
-        Depth R = std::min(int(eval - beta) / 177, 6) + depth / 3 + 5
-                        + (thisThread->correctionHistory[us][pawn_structure_index<Correction>(pos)] == CORRECTION_HISTORY_LIMIT);
+        Depth R = std::min(int(eval - beta) / 177, 6) + depth / 3 + 5;
 
         ss->currentMove         = Move::null();
         ss->continuationHistory = &thisThread->continuationHistory[0][0][NO_PIECE][0];
