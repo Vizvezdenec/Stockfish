a3838e2d6c71224aabc1127d43811d3b972eb98e
Michael Chaly <Vizvezdenec@gmail.com>
2024-05-31 19:32:52 +0300

    bench 1437164

--- src/search.cpp
+++ src/search.cpp
@@ -780,8 +780,8 @@ Value Search::Worker::search(
         return beta > VALUE_TB_LOSS_IN_MAX_PLY ? beta + (eval - beta) / 3 : eval;
 
     // Step 9. Null move search with verification search (~35 Elo)
-    if (!PvNode && (ss - 1)->currentMove != Move::null() && (ss - 1)->statScore < 13999
-        && eval >= beta && ss->staticEval >= beta - 21 * depth + 390 - 360 * ss->ttPv && !excludedMove
+    if (!PvNode && (ss - 1)->currentMove != Move::null() && ((ss - 1)->statScore < 13999 || priorCapture)
+        && eval >= beta && ss->staticEval >= beta - 21 * depth + 390 && !excludedMove
         && pos.non_pawn_material(us) && ss->ply >= thisThread->nmpMinPly
         && beta > VALUE_TB_LOSS_IN_MAX_PLY)
     {
