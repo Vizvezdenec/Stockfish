ef3f7fb07a2f67166a590591770a117c6cc5d7e2
Michael Chaly <Vizvezdenec@gmail.com>
2024-06-01 06:44:42 +0300

    bench 1293190

--- src/search.cpp
+++ src/search.cpp
@@ -1289,7 +1289,7 @@ moves_loop:  // When in check, search starts here
 
                 if (value >= beta)
                 {
-                    ss->cutoffCnt += 1 + !ttMove - (extension >= 2) - ((ss-1)->currentMove == Move::null());
+                    ss->cutoffCnt += 1 + !ttMove - (extension >= 2 || (ss-1)->currentMove == Move::null());
                     assert(value >= beta);  // Fail high
                     break;
                 }
