c21231fd3690c13ab8a61870979bd71e6de5037d
Michael Chaly <Vizvezdenec@gmail.com>
2024-06-02 00:13:30 +0300

    bench 1306540

--- src/search.cpp
+++ src/search.cpp
@@ -817,7 +817,7 @@ Value Search::Worker::search(
                     thisThread->correctionHistory[us][pawn_structure_index<Correction>(pos)]
                       << bonus;
                 }
-                ss->cutoffCnt += nullValue >= ss->staticEval + 200;
+                ss->cutoffCnt += nullValue >= ss->staticEval + 100;
                 return nullValue;
             }
 
