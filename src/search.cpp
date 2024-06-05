514bc79748ab0f06a612ff4188330cccc3d188bb
Michael Chaly <Vizvezdenec@gmail.com>
2024-06-05 20:35:16 +0300

    bench 1419701

--- src/search.cpp
+++ src/search.cpp
@@ -1498,9 +1498,9 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta,
         {
             // In case of null move search, use previous static eval with a different sign
             unadjustedStaticEval =
-              (ss - 1)->currentMove != Move::null() && (pos.captured_piece() || (ss-1)->inCheck)
+              (ss - 1)->currentMove != Move::null()
                 ? evaluate(networks[numaAccessToken], pos, refreshTable, thisThread->optimism[us])
-                : -(ss - 1)->staticEval;
+                : -(ss - 1)->staticEval + 220;
             ss->staticEval = bestValue =
               to_corrected_static_eval(unadjustedStaticEval, *thisThread, pos);
         }
