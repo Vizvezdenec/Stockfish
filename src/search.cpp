164313093622af5226a850e8eca9e15a1e14b9f2
Michael Chaly <Vizvezdenec@gmail.com>
2024-06-05 21:44:13 +0300

    bench 1238183

--- src/search.cpp
+++ src/search.cpp
@@ -1138,7 +1138,7 @@ moves_loop:  // When in check, search starts here
 
         // Decrease reduction for PvNodes (~0 Elo on STC, ~2 Elo on LTC)
         if (PvNode)
-            r -= 1 + ((ss-1)->moveCount == 1 && ((ss-1)->ttHit));
+            r -= 1 + 2 * ((ss-1)->moveCount == 1 && ((ss-1)->ttHit));
 
         // These reduction adjustments have no proven non-linear scaling.
 
