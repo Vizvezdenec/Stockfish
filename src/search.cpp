fd44951b77765c0b037077f046b4a352d0c078fd
Michael Chaly <Vizvezdenec@gmail.com>
2024-06-05 09:10:22 +0300

    bench 1226997

--- src/search.cpp
+++ src/search.cpp
@@ -778,7 +778,7 @@ Value Search::Worker::search(
                - (ss - 1)->statScore / 248
              >= beta
         && eval >= beta && eval < VALUE_TB_WIN_IN_MAX_PLY && (!ttMove || ttCapture))
-        return beta > VALUE_TB_LOSS_IN_MAX_PLY ? (ttCapture ? beta + (eval - beta) / 3 : beta + (eval - beta) / 6) : eval;
+        return beta > VALUE_TB_LOSS_IN_MAX_PLY ? beta + (eval - beta) / 3 : eval;
 
     // Step 9. Null move search with verification search (~35 Elo)
     if (!PvNode && (ss - 1)->currentMove != Move::null() && (ss - 1)->statScore < 13999
@@ -1015,7 +1015,7 @@ moves_loop:  // When in check, search starts here
 
                 history += 2 * thisThread->mainHistory[us][move.from_to()];
 
-                lmrDepth += history / 3670;
+                lmrDepth += history / 5500 + history / 11000 * std::abs(history) / 11000;
 
                 Value futilityValue =
                   ss->staticEval + (bestValue < ss->staticEval - 51 ? 149 : 55) + 141 * lmrDepth;
