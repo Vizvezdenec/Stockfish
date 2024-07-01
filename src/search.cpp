57621376a64b6b11999e378bab43acaad88079d9
Michael Chaly <Vizvezdenec@gmail.com>
2024-07-02 00:22:58 +0300

    bench 1154605

--- src/search.cpp
+++ src/search.cpp
@@ -1061,7 +1061,7 @@ moves_loop:  // When in check, search starts here
                 && std::abs(ttData.value) < VALUE_TB_WIN_IN_MAX_PLY && (ttData.bound & BOUND_LOWER)
                 && ttData.depth >= depth - 3)
             {
-                Value singularBeta  = ttData.value - (52 + 56 * (ss->ttPv && !PvNode)) * depth / 64;
+                Value singularBeta  = ttData.value - (52 + 69 * (ss->ttPv && !PvNode)) * depth / 64;
                 Depth singularDepth = newDepth / 2;
 
                 ss->excludedMove = move;
