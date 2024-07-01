601c5001442cdac28c426ce5652c86f094b05a2e
Michael Chaly <Vizvezdenec@gmail.com>
2024-07-01 20:54:46 +0300

    bench 2340706

--- src/search.cpp
+++ src/search.cpp
@@ -1111,7 +1111,7 @@ moves_loop:  // When in check, search starts here
                           > 3922)
                 extension = 1;
 
-            if (std::abs((std::abs(ss->staticEval) - std::abs((ss-1)->staticEval)) >= 500)
+            if (std::abs(ss->staticEval + (ss-1)->staticEval) >= 500
                 && ss->ply < 8
                 && !priorCapture
                 && !ss->inCheck && !(ss-1)->inCheck)
