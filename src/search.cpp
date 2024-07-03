8d0455e86f7e33ed65fd367a76917227883e0999
Michael Chaly <Vizvezdenec@gmail.com>
2024-07-03 19:09:54 +0300

    bench 1130174

--- src/search.cpp
+++ src/search.cpp
@@ -1513,7 +1513,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta,
     Value unadjustedStaticEval = VALUE_NONE;
     if (ss->inCheck)
     {
-        Value sss = beta + 222;
+        Value sss = beta + 433;
         if (ss->inCheck && (ttData.bound & BOUND_LOWER)
             && ttData.value >= sss && std::abs(ttData.value) < VALUE_TB_WIN_IN_MAX_PLY
             && std::abs(beta) < VALUE_TB_WIN_IN_MAX_PLY)
