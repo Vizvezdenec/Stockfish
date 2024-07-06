65733c99b8fa543ec4aef54788b635e608039933
Michael Chaly <Vizvezdenec@gmail.com>
2024-07-07 02:35:06 +0300

    bench 936521

--- src/search.cpp
+++ src/search.cpp
@@ -1218,7 +1218,7 @@ moves_loop:  // When in check, search starts here
 
                 // Post LMR continuation history updates (~1 Elo)
                 int bonus = value <= alpha ? -stat_malus(newDepth)
-                          : value >= beta  ? stat_bonus(newDepth)
+                          : value >= beta  ? stat_bonus(newDepth + 1)
                                            : 0;
 
                 update_continuation_histories(ss, movedPiece, move.to_sq(), bonus);
@@ -1620,7 +1620,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta,
             }
 
             // Continuation history based pruning (~3 Elo)
-            if (!capture && !ss->inCheck
+            if (!capture
                 && (*contHist[0])[pos.moved_piece(move)][move.to_sq()]
                        + (*contHist[1])[pos.moved_piece(move)][move.to_sq()]
                        + thisThread->pawnHistory[pawn_structure_index(pos)][pos.moved_piece(move)]
