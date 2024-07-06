40406427114eb288c626e239f40dc2da77a6a4b1
Michael Chaly <Vizvezdenec@gmail.com>
2024-07-06 14:45:50 +0300

    bench 1162663

--- src/search.cpp
+++ src/search.cpp
@@ -1391,7 +1391,7 @@ moves_loop:  // When in check, search starts here
                      + 147 * (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 75));
 
         // Proportional to "how much damage we have to undo"
-        bonus += std::clamp(-(ss - 1)->statScore / 100, 0, 224);
+        bonus += std::clamp(-(ss - 1)->statScore / 100, -50, 274);
 
         update_continuation_histories(ss - 1, pos.piece_on(prevSq), prevSq,
                                       stat_bonus(depth) * bonus / 100);
