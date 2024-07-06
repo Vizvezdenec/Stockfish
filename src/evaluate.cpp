748e4ed2212dcc98bd45ba38b096e241608550b9
Michael Chaly <Vizvezdenec@gmail.com>
2024-07-06 22:14:09 +0300

    bench 1242329

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -86,7 +86,7 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     v            = (nnue * (73921 + material) + optimism * (8112 + material)) / 73260;
 
     // Evaluation grain (to get more alpha-beta cuts) with randomization (for robustness)
-    v = (v / 16) * 16 - 2 + (pos.key() % 4);
+    v = (v / 16) * 16 - 4 + (pos.key() % 9);
 
     // Damp down the evaluation linearly when shuffling
     v -= v * pos.rule50_count() / 212;
