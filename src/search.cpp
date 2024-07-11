f06c647ff8266a8e9ea61ede6bbced4587bd3f74
Michael Chaly <Vizvezdenec@gmail.com>
2024-07-11 19:15:18 +0300

    bench 1570376

--- src/search.cpp
+++ src/search.cpp
@@ -1231,6 +1231,9 @@ moves_loop:  // When in check, search starts here
             (ss + 1)->pv    = pv;
             (ss + 1)->pv[0] = Move::none();
 
+            if (moveCount > 1 && value > bestValue + 120 + 12 * newDepth)
+                newDepth++;
+
             value = -search<PV>(pos, ss + 1, -beta, -alpha, newDepth, false);
         }
 
