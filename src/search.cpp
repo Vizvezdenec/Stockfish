3755d6a5eeffc8311734800a1ea9860aba107587
Michael Chaly <Vizvezdenec@gmail.com>
2024-07-12 23:13:14 +0300

    bench 1423571

--- src/search.cpp
+++ src/search.cpp
@@ -68,7 +68,7 @@ namespace {
 // Futility margin
 Value futility_margin(Depth d, bool noTtCutNode, bool improving, bool oppWorsening) {
     Value futilityMult       = 122 - 37 * noTtCutNode;
-    Value improvingDeduction = improving * futilityMult * 2;
+    Value improvingDeduction = improving * futilityMult;
     Value worseningDeduction = oppWorsening * futilityMult / 3;
 
     return futilityMult * d - improvingDeduction - worseningDeduction;
