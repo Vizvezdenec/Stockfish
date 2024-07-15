d60797d8c3a9e5602722f3e81775c65e8f1dc9c6
Michael Chaly <Vizvezdenec@gmail.com>
2024-07-15 20:37:42 +0300

    bench 1639629

--- src/search.cpp
+++ src/search.cpp
@@ -1371,7 +1371,7 @@ moves_loop:  // When in check, search starts here
                      + 32 * (!(ss - 1)->inCheck && bestValue > -(ss - 1)->staticEval + 76));
 
         // Proportional to "how much damage we have to undo"
-        bonus += std::clamp(-(ss - 1)->statScore / 100, -84, 300);
+        bonus += std::clamp(-(ss - 1)->statScore / 100, -94, 300);
 
         bonus = std::max(bonus, 0);
 
