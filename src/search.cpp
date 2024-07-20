fb699a05e599ab2538d85453b85f47e47f2430bf
Michael Chaly <Vizvezdenec@gmail.com>
2024-07-20 19:06:26 +0300

    bench 1561650

--- src/search.cpp
+++ src/search.cpp
@@ -1230,7 +1230,7 @@ moves_loop:  // When in check, search starts here
             (ss + 1)->pv    = pv;
             (ss + 1)->pv[0] = Move::none();
 
-            if (move == ttData.move && ss->ply <= thisThread->rootDepth * 2)
+            if (move == ttData.move && ss->ply <= thisThread->rootDepth / 2)
                 newDepth = std::max(newDepth, 1);
 
             value = -search<PV>(pos, ss + 1, -beta, -alpha, newDepth, false);
