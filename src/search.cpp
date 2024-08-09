64c62c65e978708d083db9c52deebe352bf9b0ab
Michael Chaly <Vizvezdenec@gmail.com>
2024-08-09 05:53:18 +0300

    bench 1292843

--- src/search.cpp
+++ src/search.cpp
@@ -894,7 +894,7 @@ Value Search::Worker::search(
 moves_loop:  // When in check, search starts here
 
     // Step 12. A small Probcut idea (~4 Elo)
-    probCutBeta = beta + 410 - 99 * (ttCapture && ss->inCheck);
+    probCutBeta = beta + 390;
     if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
         && std::abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         && std::abs(ttData.value) < VALUE_TB_WIN_IN_MAX_PLY)
@@ -1210,6 +1210,8 @@ moves_loop:  // When in check, search starts here
             // Extend move from transposition table if we are about to dive into qsearch.
             if (move == ttData.move && ss->ply <= thisThread->rootDepth * 2)
                 newDepth = std::max(newDepth, 1);
+
+            newDepth += !ttData.move && moveCount > 1;
 
             value = -search<PV>(pos, ss + 1, -beta, -alpha, newDepth, false);
         }
