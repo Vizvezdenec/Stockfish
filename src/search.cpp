3fd0613ca4befc5e2727d86720a0ffa425eead3a
Michael Chaly <Vizvezdenec@gmail.com>
2024-08-27 08:21:14 +0300

    bench 1129980

--- src/search.cpp
+++ src/search.cpp
@@ -1498,7 +1498,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
             unadjustedStaticEval =
               (ss - 1)->currentMove != Move::null()
                 ? evaluate(networks[numaAccessToken], pos, refreshTable, thisThread->optimism[us])
-                : -(ss - 1)->staticEval + 100;
+                : -(ss - 1)->staticEval + 80;
             ss->staticEval = bestValue =
               to_corrected_static_eval(unadjustedStaticEval, *thisThread, pos);
         }
