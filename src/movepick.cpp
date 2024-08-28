2c69512a6e5b4b345035132a8b0d46d3a67d891d
Michael Chaly <Vizvezdenec@gmail.com>
2024-08-28 07:09:12 +0300

    bench 1174380

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -175,7 +175,7 @@ void MovePicker::score() {
                         : pt == ROOK ? bool(to & threatenedByMinor) * 24335
                                      : bool(to & threatenedByPawn) * 14900);
             if ((pt == BISHOP || pt == KNIGHT) && (relative_square(pos.side_to_move(),to) == SQ_A1 || relative_square(pos.side_to_move(),to) == SQ_H1))
-                m.value -= 15000;
+                m.value -= 5000 + 8000 * (pt == KNIGHT);
         }
 
         else  // Type == EVASIONS
