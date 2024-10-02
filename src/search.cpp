278036dcd96d2d015fe387fb738c64e3d4f1a977
Michael Chaly <Vizvezdenec@gmail.com>
2024-10-02 11:02:58 +0300

    bench 1341658

--- src/search.cpp
+++ src/search.cpp
@@ -89,7 +89,7 @@ Value to_corrected_static_eval(Value v, const Worker& w, const Position& pos, St
     const auto  bnpcv = w.nonPawnCorrectionHistory[BLACK][us][non_pawn_index<BLACK>(pos)];
     const auto  ftcv  = w.fromToCorrectionHistory[~us][(ss-1)->currentMove.from_to()];
     const auto  cv =
-      (6245 * pcv + 3442 * mcv + 3471 * macv + 5958 * micv + 6566 * (wnpcv + bnpcv) + 5555 * ftcv) / 131072;
+      (6245 * pcv + 3442 * mcv + 3471 * macv + 5958 * micv + 6566 * (wnpcv + bnpcv) + 4444 * ftcv) / 131072;
     v += cv;
     return std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
 }
