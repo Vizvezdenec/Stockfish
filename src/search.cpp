d3456dd0f62bac27c878eaa38b11140672238749
Michael Chaly <Vizvezdenec@gmail.com>
2024-10-23 18:36:32 +0300

    bench 1204233

--- src/search.cpp
+++ src/search.cpp
@@ -1205,7 +1205,7 @@ moves_loop:  // When in check, search starts here
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
                 const bool doDeeperSearch    = value > (bestValue + 38 + 2 * newDepth);  // (~1 Elo)
-                const bool doShallowerSearch = value < bestValue + (12 + newDepth) / 2;                    // (~2 Elo)
+                const bool doShallowerSearch = value < bestValue + (10 + newDepth) / 2;                    // (~2 Elo)
 
                 newDepth += doDeeperSearch - doShallowerSearch;
 
