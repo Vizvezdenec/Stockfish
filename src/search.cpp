ac06c8786d5dc86f1e25316acc5c64b2f8178b4d
Michael Chaly <Vizvezdenec@gmail.com>
2024-11-01 09:44:13 +0300

    bench 1277549

--- src/search.cpp
+++ src/search.cpp
@@ -923,7 +923,7 @@ Value Search::Worker::search(
 moves_loop:  // When in check, search starts here
 
     // Step 12. A small Probcut idea (~4 Elo)
-    probCutBeta = beta + 379 + 20 - 60 * improving;
+    probCutBeta = beta + 379 + 27 - 90 * improving;
     if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
         && std::abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         && std::abs(ttData.value) < VALUE_TB_WIN_IN_MAX_PLY)
