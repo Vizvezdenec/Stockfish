e476b6c61d9fa2e5a37301a7ce801067768be79b
Michael Chaly <Vizvezdenec@gmail.com>
2024-11-26 09:31:32 +0300

    bench 928687

--- src/search.cpp
+++ src/search.cpp
@@ -1170,7 +1170,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction if ttMove is a capture but the current move is not a capture (~3 Elo)
         if (ttCapture && !capture)
-            r += 1043 + (depth < 8) * 999 - 555 * (ttData.depth == DEPTH_QS);
+            r += 1043 + (depth < 8) * 999 + (ttData.depth - depth) * 50;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
