565b33299bb0b4d0f09050161c81e4ebb7b07955
Michael Chaly <Vizvezdenec@gmail.com>
2024-12-05 16:26:28 +0300

    bench 995292

--- src/search.cpp
+++ src/search.cpp
@@ -1165,8 +1165,8 @@ moves_loop:  // When in check, search starts here
         // These reduction adjustments have no proven non-linear scaling
 
         // Increase reduction for cut nodes (~4 Elo)
-        if (!priorCapture && (ss-1)->moveCount > 25)
-            r--;
+        if (!priorCapture && (ss-1)->moveCount > 20)
+            r-= 682;
         if (cutNode)
             r += 2518 - (ttData.depth >= depth && ss->ttPv) * 991;
 
