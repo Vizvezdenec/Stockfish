becfbb8f781084a8a83255a1aa62a111bab345fa
Michael Chaly <Vizvezdenec@gmail.com>
2024-12-07 15:03:04 +0300

    bench 906560

--- src/search.cpp
+++ src/search.cpp
@@ -1174,7 +1174,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
-            r += 938 + allNode * 960 + 400 * cutNode;
+            r += 938 + allNode * 960 - 400 * cutNode;
 
         // For first picked move (ttMove) reduce reduction (~3 Elo)
         else if (move == ttData.move)
