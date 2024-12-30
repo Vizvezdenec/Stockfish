532e875ce109ff7c6ba842a0cdc5e236355d9227
Michael Chaly <Vizvezdenec@gmail.com>
2024-12-30 16:27:50 +0300

    bench 1128929

--- src/search.cpp
+++ src/search.cpp
@@ -1606,12 +1606,15 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
         // Step 6. Pruning
         if (!is_loss(bestValue) && pos.non_pawn_material(us))
         {
-            if (moveCount > 2)
+            if (moveCount > 6)
                 continue;
             // Futility pruning and moveCount pruning (~10 Elo)
             if (!givesCheck && move.to_sq() != prevSq && !is_loss(futilityBase)
                 && move.type_of() != PROMOTION)
             {
+                if (moveCount > 2)
+                    continue;
+
                 Value futilityValue = futilityBase + PieceValue[pos.piece_on(move.to_sq())];
 
                 // If static eval + value of piece we are going to capture is
