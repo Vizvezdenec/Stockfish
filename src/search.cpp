332610f35c388aff2e50f7c24ed1c2f4c71a848f
Michael Chaly <Vizvezdenec@gmail.com>
2025-01-12 02:45:20 +0300

    bench 1693125

--- src/search.cpp
+++ src/search.cpp
@@ -836,16 +836,18 @@ Value Search::Worker::search(
     }
 
     // Step 10. Internal iterative reductions (~9 Elo)
-    // For PV nodes without a ttMove as well as for deep enough cutNodes, we decrease depth.
-    // This heuristic is known to scale non-linearly, current version was tested at VVLTC.
-    // Further improvements need to be tested at similar time control if they make IIR
-    // more aggressive.
-    if ((PvNode || (cutNode && depth >= 7)) && !ttData.move)
-        depth -= 2;
+    // For PV nodes without a ttMove, we decrease depth.
+    if (PvNode && !ttData.move)
+        depth -= 3;
 
     // Use qsearch if depth <= 0
     if (depth <= 0)
         return qsearch<PV>(pos, ss, alpha, beta);
+
+    // For cutNodes, if depth is high enough, decrease depth by 2 if there is no ttMove,
+    // or by 1 if there is a ttMove with an upper bound.
+    if (cutNode && depth >= 7 && (!ttData.move || ttData.bound == BOUND_UPPER))
+        depth -= 1 + !ttData.move;
 
     // Step 11. ProbCut (~10 Elo)
     // If we have a good enough capture (or queen promotion) and a reduced search
@@ -1337,7 +1339,7 @@ moves_loop:  // When in check, search starts here
                 {
                     // Reduce other moves if we have found at least one score improvement (~2 Elo)
                     if (depth > 2 && depth < 14 && !is_decisive(value))
-                        depth -= 2;
+                        depth -= 1 + !ttData.move;
 
                     assert(depth > 0);
                     alpha = value;  // Update alpha! Always alpha < beta
