2ef9f03df9d52b87876c1a3ff68878f4fd81222b
Michael Chaly <Vizvezdenec@gmail.com>
2025-01-12 14:35:31 +0300

    bench 1087236

--- src/search.cpp
+++ src/search.cpp
@@ -792,7 +792,7 @@ Value Search::Worker::search(
         && eval >= beta && (!ttData.move || ttCapture) && !is_loss(beta) && !is_win(eval))
         return beta + (eval - beta) / 3;
 
-    improving |= ss->staticEval >= beta + 222;
+    improving |= ss->staticEval >= beta + 444;
 
     // Step 9. Null move search with verification search (~35 Elo)
     if (cutNode && (ss - 1)->currentMove != Move::null() && eval >= beta
