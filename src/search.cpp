9e66b77adcefab43a705a64b74c52adfe4641913
Michael Chaly <Vizvezdenec@gmail.com>
2025-01-18 19:05:32 +0300

    bench 1556997

--- src/search.cpp
+++ src/search.cpp
@@ -1178,7 +1178,7 @@ moves_loop:  // When in check, search starts here
               - 4666;
         else if (ss->inCheck)
             ss->statScore = thisThread->mainHistory[us][move.from_to()]
-                          + (*contHist[0])[movedPiece][move.to_sq()] - 2000;
+                          + (*contHist[0])[movedPiece][move.to_sq()] - 5000;
         else
             ss->statScore = 2 * thisThread->mainHistory[us][move.from_to()]
                           + (*contHist[0])[movedPiece][move.to_sq()]
