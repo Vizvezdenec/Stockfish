a482582f121aa30f5fe7011bd6f48cf4551de545
Michael Chaly <Vizvezdenec@gmail.com>
2025-01-19 18:36:12 +0300

    bench 1200434

--- src/search.cpp
+++ src/search.cpp
@@ -1199,7 +1199,7 @@ moves_loop:  // When in check, search starts here
 
 
             Depth d = std::max(
-              1, std::min(newDepth - r / 1024, newDepth + (!allNode || r < -4400) + (PvNode && !bestMove)));
+              1, std::min(newDepth - r / 1024, newDepth + 1 + (PvNode && !bestMove)));
 
             (ss + 1)->reduction = newDepth - d;
 
