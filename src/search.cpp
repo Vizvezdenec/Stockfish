2d67a60081fbadb442dfd1cfb9f544755d82ec44
Michael Chaly <Vizvezdenec@gmail.com>
2025-01-25 23:24:54 +0300

    bench 1649165

--- src/search.cpp
+++ src/search.cpp
@@ -914,7 +914,7 @@ Value Search::Worker::search(
                                probCutDepth + 1, move, unadjustedStaticEval, tt.generation());
 
                 if (!is_decisive(value))
-                    return value - (probCutBeta - beta);
+                    return (value * (probCutDepth + 0) + beta) / (probCutDepth + 1);
             }
         }
     }
