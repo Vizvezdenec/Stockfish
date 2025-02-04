29490e9fdfaaeb536c732c2d3ba4537c7e8762d7
Michael Chaly <Vizvezdenec@gmail.com>
2025-02-05 01:16:16 +0300

    bench 2600853

--- src/search.cpp
+++ src/search.cpp
@@ -999,8 +999,6 @@ moves_loop:  // When in check, search starts here
 
         Depth r = reduction(improving, depth, moveCount, delta);
 
-        r -= 32 * moveCount;
-
         // Increase reduction for ttPv nodes (*Scaler)
         // Smaller or even negative value is better for short time controls
         // Bigger value is better for long time controls
@@ -1165,7 +1163,7 @@ moves_loop:  // When in check, search starts here
 
         // These reduction adjustments have no proven non-linear scaling
 
-        r += 316 - moveCount * 32;
+        r += 316 - moveCount * 64;
 
         r -= std::abs(correctionValue) / 31568;
 
