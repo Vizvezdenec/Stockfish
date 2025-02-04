f14ce6570a86377f2f6f13c7de6d2f00608addad
Michael Chaly <Vizvezdenec@gmail.com>
2025-02-05 00:01:05 +0300

    bench 3284585

--- src/search.cpp
+++ src/search.cpp
@@ -1239,10 +1239,6 @@ moves_loop:  // When in check, search starts here
         // Step 18. Full-depth search when LMR is skipped
         else if (!PvNode || moveCount > 1)
         {
-            // Increase reduction if ttMove is not present
-            if (!ttData.move)
-                r += 1111;
-
             // Note that if expected reduction is high, we reduce search depth here
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha,
                                    newDepth - (r > 3554) - (r > 5373 && newDepth > 2), !cutNode);
