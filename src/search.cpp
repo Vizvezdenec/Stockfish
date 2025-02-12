e07123aeab78631245f32075f1502f11e8ea1921
Michael Chaly <Vizvezdenec@gmail.com>
2025-02-12 20:53:28 +0300

    bench 2699658

--- src/search.cpp
+++ src/search.cpp
@@ -877,7 +877,7 @@ Value Search::Worker::search(
     // If we have a good enough capture (or queen promotion) and a reduced search
     // returns a value much above beta, we can (almost) safely prune the previous move.
     probCutBeta = beta + 187 - 55 * improving;
-    if (depth >= 5 - 3 * cutNode
+    if (depth >= 3
         && !is_decisive(beta)
         // If value from transposition table is lower than probCutBeta, don't attempt
         // probCut there and in further interactions with transposition table cutoff
@@ -1417,7 +1417,7 @@ moves_loop:  // When in check, search starts here
               << scaledBonus * 1103 / 32768;
     }
 
-    else if (priorCapture && prevSq != SQ_NONE)
+    else if (priorCapture && !allNode && prevSq != SQ_NONE)
     {
         // bonus for prior countermoves that caused the fail low
         Piece capturedPiece = pos.captured_piece();
