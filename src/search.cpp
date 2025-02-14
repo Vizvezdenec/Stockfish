aa0cabf45e436f3e7a1a0dda28796e58a0489c1e
Michael Chaly <Vizvezdenec@gmail.com>
2025-02-14 10:39:46 +0300

    bench 2642342

--- src/search.cpp
+++ src/search.cpp
@@ -1426,7 +1426,7 @@ moves_loop:  // When in check, search starts here
         Piece capturedPiece = pos.captured_piece();
         assert(capturedPiece != NO_PIECE);
         thisThread->captureHistory[pos.piece_on(prevSq)][prevSq][type_of(capturedPiece)]
-          << stat_bonus(depth) * 2;
+        << stat_bonus(depth) * (2 - (ss->cutoffCnt > 2));
     }
 
     if (PvNode)
