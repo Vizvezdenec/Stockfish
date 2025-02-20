e823a474b0334d8a80dfc9e311799dc13d68af54
Michael Chaly <Vizvezdenec@gmail.com>
2025-02-20 11:09:18 +0300

    bench 2606745

--- src/search.cpp
+++ src/search.cpp
@@ -1174,7 +1174,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction for cut nodes
         if (cutNode)
-            r += 2608 + 1024 * (!ttData.move && depth >= 5);
+            r += 2608 + 1024 * !ttData.move;
 
         // Increase reduction if ttMove is a capture but the current move is not a capture
         if (ttCapture && !capture)
@@ -1226,7 +1226,7 @@ moves_loop:  // When in check, search starts here
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
                 const bool doDeeperSearch    = value > (bestValue + 41 + 2 * newDepth);
-                const bool doShallowerSearch = value < bestValue + 9;
+                const bool doShallowerSearch = !PvNode && value < bestValue + 9;
 
                 newDepth += doDeeperSearch - doShallowerSearch;
 
