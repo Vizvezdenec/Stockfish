71b9514555ef333dee31ba7a7befcf924b622106
Michael Chaly <Vizvezdenec@gmail.com>
2025-02-20 11:10:58 +0300

    bench 2377602

--- src/search.cpp
+++ src/search.cpp
@@ -1226,7 +1226,7 @@ moves_loop:  // When in check, search starts here
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
                 const bool doDeeperSearch    = value > (bestValue + 41 + 2 * newDepth);
-                const bool doShallowerSearch = !PvNode && value < bestValue + 9;
+                const bool doShallowerSearch = (!PvNode || bestMove) && value < bestValue + 9;
 
                 newDepth += doDeeperSearch - doShallowerSearch;
 
