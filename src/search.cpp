20434949a7f8afcc070d7a82ddccac0e252132b0
Michael Chaly <Vizvezdenec@gmail.com>
2025-03-01 10:55:45 +0300

    bench 2565094

--- src/search.cpp
+++ src/search.cpp
@@ -1271,7 +1271,7 @@ moves_loop:  // When in check, search starts here
             if (!ttData.move)
                 r += 1156;
 
-            if (cutNode && moveCount == 1 && r < -2500)
+            if (cutNode && moveCount == 1 && r < -3500)
                 newDepth++;
 
             // Note that if expected reduction is high, we reduce search depth here
