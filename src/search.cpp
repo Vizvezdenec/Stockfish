84884ba2786320e6305b918c125962335f6144d8
Michael Chaly <Vizvezdenec@gmail.com>
2025-03-05 11:19:50 +0300

    bench 1797112

--- src/search.cpp
+++ src/search.cpp
@@ -1057,7 +1057,8 @@ moves_loop:  // When in check, search starts here
                 int seeHist = capture ? captHist / 32 : 
                                        ((*contHist[0])[movedPiece][move.to_sq()]
                                       + (*contHist[1])[movedPiece][move.to_sq()]
-                                      + thisThread->pawnHistory[pawn_structure_index(pos)][movedPiece][move.to_sq()]) / 320;
+                                      + thisThread->pawnHistory[pawn_structure_index(pos)][movedPiece][move.to_sq()]
+                                      + 2 * thisThread->mainHistory[us][move.from_to()]) / 320;
                 seeHist = std::clamp(seeHist, -138 * depth, 135 * depth);
                 if (!pos.see_ge(move, -154 * depth - seeHist))
                     continue;
