59bd6ea77f78af0ea47da682a6d4c4775a540fab
Michael Chaly <Vizvezdenec@gmail.com>
2025-03-17 10:53:00 +0300

    bench 1866249

--- src/search.cpp
+++ src/search.cpp
@@ -1361,7 +1361,7 @@ moves_loop:  // When in check, search starts here
 
         if (value + inc > bestValue)
         {
-            bool dr = value > bestValue + 6;
+            bool dr = value > bestValue + 10;
             bestValue = value;
 
             if (value + inc > alpha)
