3f5156da12c57df2f87dd100742cefbaa9afaab1
Michael Chaly <Vizvezdenec@gmail.com>
2025-03-19 06:42:51 +0300

    bench 1865282

--- src/search.cpp
+++ src/search.cpp
@@ -1602,7 +1602,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
         if (bestValue > alpha)
             alpha = bestValue;
 
-        futilityBase = ss->staticEval + 359 + 20 * PvNode;
+        futilityBase = ss->staticEval + 359 + 120 * PvNode;
     }
 
     const PieceToHistory* contHist[] = {(ss - 1)->continuationHistory,
