ce6c0fce816054ad91ce5912e9c3c8e9d0e7e9fd
Michael Chaly <Vizvezdenec@gmail.com>
2025-04-07 06:53:07 +0300

    bench 1948031

--- src/search.cpp
+++ src/search.cpp
@@ -844,7 +844,7 @@ Value Search::Worker::search(
 
     if (priorReduction >= 3 && !opponentWorsening)
         depth++;
-    if (priorReduction >= 1 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 188)
+    if (priorReduction >= 1 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 188 - 138 * priorCapture)
         depth--;
 
     // Step 7. Razoring
