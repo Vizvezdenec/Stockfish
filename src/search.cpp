570a387cb5ec5a0ee1fa89409fc663bd70d73541
Michael Chaly <Vizvezdenec@gmail.com>
2025-04-10 09:31:09 +0300

    bench 1711403

--- src/search.cpp
+++ src/search.cpp
@@ -846,6 +846,8 @@ Value Search::Worker::search(
         depth++;
     if (priorReduction >= 1 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 188)
         depth--;
+    else if (priorReduction >= 1 && depth >= 2 && priorCapture && ss->staticEval + (ss - 1)->staticEval > -100)
+        depth--;
 
     // Step 7. Razoring
     // If eval is really low, skip search entirely and return the qsearch value.
