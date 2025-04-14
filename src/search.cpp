ba89807cef52b4d7f3d39d41f0a52bdafd57abcd
Michael Chaly <Vizvezdenec@gmail.com>
2025-04-14 08:05:01 +0300

    bench 1742898

--- src/search.cpp
+++ src/search.cpp
@@ -1142,10 +1142,10 @@ moves_loop:  // When in check, search starts here
                     int corrValAdj1  = std::abs(correctionValue) / 248873;
                     int corrValAdj2  = std::abs(correctionValue) / 255331;
                     int doubleMargin = 262 * PvNode - 188 * !ttCapture - corrValAdj1
-                                     - ttMoveHistory[pawn_structure_index(pos)][us] / 128;
+                                     - ttMoveHistory[pawn_structure_index(pos)][us] / 32;
                     int tripleMargin =
                       88 + 265 * PvNode - 256 * !ttCapture + 93 * ss->ttPv - corrValAdj2
-                                     - ttMoveHistory[pawn_structure_index(pos)][us] / 64;
+                                     - ttMoveHistory[pawn_structure_index(pos)][us] / 32;
 
                     extension = 1 + (value < singularBeta - doubleMargin)
                               + (value < singularBeta - tripleMargin);
