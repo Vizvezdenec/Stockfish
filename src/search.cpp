82b21b7a960ae87af88c44017fbd13e60c6c5acc
Michael Chaly <Vizvezdenec@gmail.com>
2025-04-18 20:37:19 +0300

    bench 1749437

--- src/search.cpp
+++ src/search.cpp
@@ -1285,7 +1285,10 @@ moves_loop:  // When in check, search starts here
                     value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
 
                 // Post LMR continuation history updates
-                update_continuation_histories(ss, movedPiece, move.to_sq(), 1600);
+                if (!capture)
+                    update_continuation_histories(ss, movedPiece, move.to_sq(), 1600);
+                else
+                    thisThread->captureHistory[movedPiece][move.to_sq()][type_of(pos.captured_piece())] << 3200;
             }
             else if (value > alpha && value < bestValue + 9)
                 newDepth--;
