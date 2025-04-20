602b2a7b90807219837a06565f2a658230e1c1e9
Michael Chaly <Vizvezdenec@gmail.com>
2025-04-21 01:26:55 +0300

    bench 2003407

--- src/search.cpp
+++ src/search.cpp
@@ -1183,11 +1183,6 @@ moves_loop:  // When in check, search starts here
                 // If the ttMove is assumed to fail high over current beta
                 else if (ttData.value >= beta)
                     extension = -3;
-
-                // If we are on a cutNode but the ttMove is not assumed to fail high
-                // over current beta
-                else if (cutNode)
-                    extension = -1;
             }
         }
 
