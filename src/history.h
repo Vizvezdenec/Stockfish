5a8a0424ea3289e64fa857db2083a429dd2041b1
Michael Chaly <Vizvezdenec@gmail.com>
2025-04-30 13:24:22 +0300

    bench

--- src/history.h
+++ src/history.h
@@ -126,7 +126,7 @@ using ContinuationHistory = MultiArray<PieceToHistory, PIECE_NB, SQUARE_NB>;
 // PawnHistory is addressed by the pawn structure and a move's [piece][to]
 using PawnHistory = Stats<std::int16_t, 8192, PAWN_HISTORY_SIZE, PIECE_NB, SQUARE_NB>;
 
-using PawnCaptureHistory = Stats<std::int16_t, 10692, PAWN_HISTORY_SIZE, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB>;
+using PawnCaptureHistory = Stats<std::int16_t, 8192, PAWN_HISTORY_SIZE, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB>;
 
 // Correction histories record differences between the static evaluation of
 // positions and their search score. It is used to improve the static evaluation
