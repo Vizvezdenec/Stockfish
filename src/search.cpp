9b4f7d5dd5d636e9f40ec4445be61a88fb726037
Michael Chaly <Vizvezdenec@gmail.com>
2025-05-17 22:22:41 +0300

    bench 2230728

--- src/search.cpp
+++ src/search.cpp
@@ -953,8 +953,7 @@ moves_loop:  // When in check, search starts here
 
     // Step 12. A small Probcut idea
     probCutBeta = beta + 400;
-    if ((ttData.bound & BOUND_LOWER) 
-        && ((ttData.depth >= depth - 4 && ttData.value >= probCutBeta) || (ttData.depth >= depth - 7 && ttData.value >= probCutBeta + 1600))
+    if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
         && !is_decisive(beta) && is_valid(ttData.value) && !is_decisive(ttData.value))
         return probCutBeta;
 
