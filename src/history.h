379f9e9e9f575cb9cecd2232f12abd3c0539a549
Michael Chaly <Vizvezdenec@gmail.com>
2025-05-23 10:40:26 +0300

    bench 2420436

--- src/history.h
+++ src/history.h
@@ -112,7 +112,7 @@ using ButterflyHistory = Stats<std::int16_t, 7183, COLOR_NB, int(SQUARE_NB) * in
 using LowPlyHistory =
   Stats<std::int16_t, 7183, LOW_PLY_HISTORY_SIZE, int(SQUARE_NB) * int(SQUARE_NB)>;
 
-using ContPlyHistory = Stats<std::int16_t, 7500, LOW_PLY_CONT_HISTORY_SIZE - 1, SQUARE_NB, PIECE_TYPE_NB, SQUARE_NB, PIECE_TYPE_NB>;
+using ContPlyHistory = Stats<std::int16_t, 30000, LOW_PLY_CONT_HISTORY_SIZE - 1, SQUARE_NB, PIECE_TYPE_NB, SQUARE_NB, PIECE_TYPE_NB>;
 
 // CapturePieceToHistory is addressed by a move's [piece][to][captured piece type]
 using CapturePieceToHistory = Stats<std::int16_t, 10692, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB>;
