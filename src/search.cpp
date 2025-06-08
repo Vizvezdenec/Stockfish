9887763c1ad33bbf3b9e2ef7b6c4273855511819
Michael Chaly <Vizvezdenec@gmail.com>
2025-06-08 11:15:16 +0300

    bench 2418426

--- src/search.cpp
+++ src/search.cpp
@@ -866,7 +866,7 @@ Value Search::Worker::search(
 
         do_null_move(pos, st);
 
-        Value nullValue = -search<NonPV>(pos, ss + 1, -beta, -beta + 1, depth - R, eval < ss->staticEval - 20);
+        Value nullValue = -search<NonPV>(pos, ss + 1, -beta, -beta + 1, depth - R, eval < ss->staticEval && eval < beta + 200);
 
         undo_null_move(pos);
 
