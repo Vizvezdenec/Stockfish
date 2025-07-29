121ecb5cff0d8b51d5ac01d70e939d44717756b3
Michael Chaly <Vizvezdenec@gmail.com>
2025-07-29 11:07:30 +0300

    bench 3201449

--- src/search.cpp
+++ src/search.cpp
@@ -1184,7 +1184,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction for cut nodes
         if (cutNode)
-            r += 3000;
+            r += 3000 + 256 * !ttData.move;
 
         // Increase reduction if ttMove is a capture
         if (ttCapture)
@@ -1646,7 +1646,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
                 continue;
 
             // Do not search moves with bad enough SEE values
-            if (!pos.see_ge(move, -74 - 200 * (move.to_sq() == prevSq)))
+            if (!pos.see_ge(move, -74))
                 continue;
         }
 
