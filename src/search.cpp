b149a6fd302b64140443f637a141966601c067df
Michael Chaly <Vizvezdenec@gmail.com>
2025-07-31 07:25:23 +0300

    bench 3140027

--- src/search.cpp
+++ src/search.cpp
@@ -1646,7 +1646,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
                 continue;
 
             // Do not search moves with bad enough SEE values
-            if (!pos.see_ge(move, -74 - 300 * (move.to_sq() == prevSq)))
+            if (move.to_sq() != prevSq && !pos.see_ge(move, -74))
                 continue;
         }
 
