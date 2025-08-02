67d1dde0b88a1df3bff25d1be959a3a16147de0c
Michael Chaly <Vizvezdenec@gmail.com>
2025-08-03 00:53:18 +0300

    bench 2963526

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -159,7 +159,7 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
             // histories
             m.value = 2 * (*mainHistory)[us][m.from_to()];
             m.value += 2 * (*pawnHistory)[pawn_structure_index(pos)][pc][to];
-            m.value += (*continuationHistory[0])[pc][to];
+            m.value += 2 * (*continuationHistory[0])[pc][to];
             m.value += (*continuationHistory[1])[pc][to];
             m.value += (*continuationHistory[2])[pc][to];
             m.value += (*continuationHistory[3])[pc][to];
