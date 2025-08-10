598ace1f6f62c05ce9d916a15137aecfa5606f68
Michael Chaly <Vizvezdenec@gmail.com>
2025-08-10 20:10:19 +0300

    bench 3270248

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -178,7 +178,7 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
             }
 
             if (ply < LOW_PLY_HISTORY_SIZE)
-                m.value += 12 * (*lowPlyHistory)[ply][m.from_to()] / (1 + 2 * ply);
+                m.value += 8 * (*lowPlyHistory)[ply][m.from_to()] / (1 + ply);
         }
 
         else  // Type == EVASIONS
@@ -189,7 +189,7 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
             {
                 m.value = (*mainHistory)[us][m.from_to()] + (*continuationHistory[0])[pc][to];
                 if (ply < LOW_PLY_HISTORY_SIZE)
-                    m.value += 3 * (*lowPlyHistory)[ply][m.from_to()] / (1 + 2 * ply);
+                    m.value += 2 * (*lowPlyHistory)[ply][m.from_to()] / (1 + ply);
             }
         }
     }
