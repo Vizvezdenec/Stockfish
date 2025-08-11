11acc93c621251bbb30f219c22db65ff9f0f628a
Michael Chaly <Vizvezdenec@gmail.com>
2025-08-11 08:38:50 +0300

    bench 3505252

--- src/search.cpp
+++ src/search.cpp
@@ -832,7 +832,7 @@ Value Search::Worker::search(
     // Step 7. Razoring
     // If eval is really low, skip search entirely and return the qsearch value.
     // For PvNodes, we must have a guard against mates being returned.
-    if (!PvNode && eval < alpha - 495 - 290 * depth * depth && priorReduction <= 3)
+    if (!PvNode && eval < alpha - 495 - 290 * depth * depth && priorReduction <= 0)
         return qsearch<NonPV>(pos, ss, alpha, beta);
 
     // Step 8. Futility pruning: child node
