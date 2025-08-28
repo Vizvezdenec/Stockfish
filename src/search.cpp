312a413f082facc56fd6a3d0684a21024483c171
Michael Chaly <Vizvezdenec@gmail.com>
2025-08-28 07:31:27 +0300

    bench 2789650

--- src/search.cpp
+++ src/search.cpp
@@ -849,7 +849,8 @@ Value Search::Worker::search(
         };
 
         if (!ss->ttPv && depth < 14 && eval - futility_margin(depth) >= beta && eval >= beta
-            && (!ttData.move || ttCapture) && !is_loss(beta) && !is_win(eval))
+            && (!ttData.move || ttCapture) && !is_loss(beta) && !is_win(eval)
+            && !(eval < ss->staticEval && eval - 2 * futility_margin(depth) < beta))
             return beta + (eval - beta) / 3;
     }
 
