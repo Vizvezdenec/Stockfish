95fcf6917ee52782a1843a08f70d33ebffcb4e3c
Michael Chaly <Vizvezdenec@gmail.com>
2025-09-03 10:26:35 +0300

    bench 2930403

--- src/search.cpp
+++ src/search.cpp
@@ -850,7 +850,7 @@ Value Search::Worker::search(
             return futilityMult * d                                //
                  - 2094 * improving * futilityMult / 1024          //
                  - 1324 * opponentWorsening * futilityMult / 4096  //
-                 - 666 * (cutNode && !ss->ttHit) * futilityMult / 1024
+                 - 333 * (cutNode && !ss->ttHit) * futilityMult / 1024
                  + (ss - 1)->statScore / 331                       //
                  + std::abs(correctionValue) / 158105;
         };
