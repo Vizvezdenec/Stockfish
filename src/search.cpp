2765735403a60a4b937cb5a69cdfcd653aea8ce6
Michael Chaly <Vizvezdenec@gmail.com>
2025-10-04 11:50:43 +0300

    bench 2328368

--- src/search.cpp
+++ src/search.cpp
@@ -860,7 +860,7 @@ Value Search::Worker::search(
     }
 
     // Step 9. Null move search with verification search
-    if (cutNode && ss->staticEval >= std::min(beta - 18 * depth + 390 , beta + 1) && !excludedMove
+    if (cutNode && ss->staticEval >= std::min(beta - 18 * depth + 390 , beta + 250) && !excludedMove
         && pos.non_pawn_material(us) && ss->ply >= nmpMinPly && !is_loss(beta))
     {
         assert((ss - 1)->currentMove != Move::null());
