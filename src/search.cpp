753338bea7e356dbe9d47ae48a0df4e87fd67df6
Michael Chaly <Vizvezdenec@gmail.com>
2025-11-18 16:39:26 +0300

    bench 2818189

--- src/search.cpp
+++ src/search.cpp
@@ -839,7 +839,7 @@ Value Search::Worker::search(
     // Similarly, opponentWorsening is true if our static evaluation is better
     // for us than at the last ply.
     improving         = ss->staticEval > (ss - 2)->staticEval;
-    opponentWorsening = !priorCapture ? ss->staticEval > -(ss - 1)->staticEval : ss->staticEval > -(ss - 1)->staticEval - PieceValue[pos.captured_piece()] / 2;
+    opponentWorsening = !priorCapture ? ss->staticEval > -(ss - 1)->staticEval : ss->staticEval > -(ss - 1)->staticEval - PieceValue[pos.captured_piece()];
 
     // Hindsight adjustment of reductions based on static evaluation difference.
     if (priorReduction >= 3 && !opponentWorsening)
