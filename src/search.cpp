08ebbcd5137e618433ed16145f5e51b0d5ec9403
Michael Chaly <Vizvezdenec@gmail.com>
2025-11-22 12:29:51 +0300

    bench 2591111

--- src/search.cpp
+++ src/search.cpp
@@ -1080,8 +1080,8 @@ moves_loop:  // When in check, search starts here
                 // (*Scaler): Generally, lower divisors scales well
                 lmrDepth += history / 3208;
 
-                Value futilityValue = ss->staticEval + 37 + 151 * !bestMove + 114 * lmrDepth
-                                    + 70 * (ss->staticEval > alpha);
+                Value futilityValue = ss->staticEval + 32 + 141 * !bestMove + 104 * lmrDepth
+                                    + 62 * (ss->staticEval > alpha);
 
                 // Futility pruning: parent node
                 // (*Scaler): Generally, more frequent futility pruning
