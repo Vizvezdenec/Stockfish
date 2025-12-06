e2460cbe772d70c3a30a3f832e60d08ebce7963d
Michael Chaly <Vizvezdenec@gmail.com>
2025-12-06 23:19:28 +0300

    bench 3055911

--- src/search.cpp
+++ src/search.cpp
@@ -873,7 +873,7 @@ Value Search::Worker::search(
     // The depth condition is important for mate finding.
     {
         auto futility_margin = [&](Depth d) {
-            Value futilityMult = 76 - 23 * !ss->ttHit;
+            Value futilityMult = 76 - 46 * !ss->ttHit;
 
             return futilityMult * d                               //
                  - 2474 * improving * futilityMult / 1024         //
@@ -1228,7 +1228,7 @@ moves_loop:  // When in check, search starts here
             // beyond the first move depth.
             // To prevent problems when the max value is less than the min value,
             // std::clamp has been replaced by a more robust implementation.
-            Depth d = std::max(1, std::min(newDepth - r / 1024, newDepth + 2 + PvNode)) + PvNode;
+            Depth d = std::max(1, std::min(newDepth - r / 1024, newDepth + 2)) + PvNode;
 
             ss->reduction = newDepth - d;
             value         = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, d, true);
