479d4767f520e91dbf4d2d221732ed75d4ceeda8
Michael Chaly <Vizvezdenec@gmail.com>
2026-02-02 04:03:47 +0300

    bench 2846882

--- src/search.cpp
+++ src/search.cpp
@@ -899,7 +899,7 @@ Value Search::Worker::search(
         assert((ss - 1)->currentMove != Move::null());
 
         // Null move dynamic reduction based on depth
-        Depth R = std::max(4, 4 + depth / 3 + (ss->staticEval - beta) / 128);
+        Depth R = 7 + depth / 3;
         do_null_move(pos, st, ss);
 
         Value nullValue = -search<NonPV>(pos, ss + 1, -beta, -beta + 1, depth - R, false);
