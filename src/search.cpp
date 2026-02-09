2765fe91b05b2ef7c4cd48815f99a82ab459ed40
Michael Chaly <Vizvezdenec@gmail.com>
2026-02-09 11:05:59 +0300

    bench 2332095

--- src/search.cpp
+++ src/search.cpp
@@ -982,7 +982,7 @@ Value Search::Worker::search(
 moves_loop:  // When in check, search starts here
 
     // Step 12. A small Probcut idea
-    probCutBeta = beta + 329;
+    probCutBeta = beta + 259;
     if ((ttData.bound & BOUND_LOWER) && ss->inCheck && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
         && !is_decisive(beta) && is_valid(ttData.value) && !is_decisive(ttData.value))
         return probCutBeta;
