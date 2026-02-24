00df13866e7bc00916d89e8b4554413590259650
Michael Chaly <Vizvezdenec@gmail.com>
2026-02-24 19:58:28 +0300

    bench 2673729

--- src/search.cpp
+++ src/search.cpp
@@ -753,7 +753,7 @@ Value Search::Worker::search(
         depth++;
     if (priorReduction >= 2 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 188)
         depth--;
-    if ((ss - 1)->currentMove == Move::null() && depth >= 2 && ss->staticEval <= alpha + 18)
+    if ((ss - 1)->currentMove == Move::null() && depth >= 2 && ss->staticEval <= alpha - 48)
         depth--;
 
     // At non-PV nodes we check for an early TT cutoff
