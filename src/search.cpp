caf0042dc0f1b28d61155c702f4642a44b165f91
Michael Chaly <Vizvezdenec@gmail.com>
2026-03-08 19:02:48 +0300

    bench 2723382

--- src/search.cpp
+++ src/search.cpp
@@ -1253,7 +1253,7 @@ moves_loop:  // When in check, search starts here
                 newDepth += doDeeperSearch - doShallowerSearch;
 
                 if (newDepth > d)
-                    value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
+                    value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, false);
 
                 // Post LMR continuation history updates
                 update_continuation_histories(ss, movedPiece, move.to_sq(), 1342);
