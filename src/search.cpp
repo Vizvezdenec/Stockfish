e0712cc574109137cb4795a2e57da0d7ed73af20
Michael Chaly <Vizvezdenec@gmail.com>
2026-03-21 20:13:30 +0300

    bench 3202206

--- src/search.cpp
+++ src/search.cpp
@@ -906,7 +906,7 @@ Value Search::Worker::search(
 
         if (!ss->ttPv && depth < 15 && eval - futility_margin(depth) >= beta && eval >= beta
             && (!ttData.move || ttCapture) && !is_loss(beta) && !is_win(eval))
-            return (3 * beta + eval) / 4;
+            return (beta + eval) / 2;
     }
 
     // Step 9. Null move search with verification search
