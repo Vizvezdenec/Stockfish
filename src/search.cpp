563121971ca358031eedd7afd4b7b585cca3afd1
Michael Chaly <Vizvezdenec@gmail.com>
2026-05-13 09:54:56 +0300

    bench 2784855

--- src/search.cpp
+++ src/search.cpp
@@ -1037,6 +1037,8 @@ moves_loop:  // When in check, search starts here
     value = bestValue;
 
     int moveCount = 0;
+    Move pseudoTtMove = move.none();
+    Value lmrAlpha = alpha;
 
     // Step 13. Loop through all pseudo-legal moves until no moves remain
     // or a beta cutoff occurs.
@@ -1297,6 +1299,12 @@ moves_loop:  // When in check, search starts here
 
                 // Post LMR continuation history updates
                 update_continuation_histories(ss, movedPiece, move.to_sq(), 1415);
+
+                if (value > lmrAlpha)
+                {
+                    pseudoTtMove = move;
+                    lmrAlpha = alpha;
+                }
             }
         }
 
@@ -1507,7 +1515,7 @@ moves_loop:  // When in check, search starts here
                        bestValue >= beta    ? BOUND_LOWER
                        : PvNode && bestMove ? BOUND_EXACT
                                             : BOUND_UPPER,
-                       moveCount != 0 ? depth : std::min(MAX_PLY - 1, depth + 6), bestMove,
+                       moveCount != 0 ? depth : std::min(MAX_PLY - 1, depth + 6), bestMove ? bestMove : ttData.move ? ttData.move : pseudoTtMove,
                        unadjustedStaticEval, tt.generation());
 
     // Adjust correction history if the best move is not a capture
