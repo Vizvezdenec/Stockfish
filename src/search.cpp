1f7bf364fcd176ffa41c218dc5ff9ab0338a13e0
Michael Chaly <Vizvezdenec@gmail.com>
2026-05-26 14:43:38 +0300

    bench 2538954

--- src/search.cpp
+++ src/search.cpp
@@ -1132,7 +1132,7 @@ moves_loop:  // When in check, search starts here
                     {
                         if (bestValue <= futilityValue && !is_decisive(bestValue)
                         && !is_win(futilityValue))
-                        bestValue = (futilityValue + 3 * bestValue) / 4;
+                            bestValue = (futilityValue + 7 * bestValue) / 8;
                         continue;
                     }
                 }
