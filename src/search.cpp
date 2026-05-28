960ad97560244a832ff851815fbfff1168938680
Michael Chaly <Vizvezdenec@gmail.com>
2026-05-28 20:57:59 +0300

    bench 2680276

--- src/search.cpp
+++ src/search.cpp
@@ -1210,7 +1210,7 @@ moves_loop:  // When in check, search starts here
                 extension =
                   1 + (value < singularBeta - doubleMargin) + (value < singularBeta - tripleMargin);
 
-                goodSing = ttData.value >= beta;
+                goodSing = ttData.value >= beta + 66;
 
                 depth++;
             }
