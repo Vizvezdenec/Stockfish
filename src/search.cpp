90b9e270fa0c8847d333c0a2c9699e777f981d67
Michael Chaly <Vizvezdenec@gmail.com>
2026-05-31 11:24:41 +0300

    bench 2947792

--- src/search.cpp
+++ src/search.cpp
@@ -1059,6 +1059,7 @@ moves_loop:  // When in check, search starts here
     value = bestValue;
 
     int moveCount = 0;
+    bool negext = false;
 
     // Step 13. Loop through all pseudo-legal moves until no moves remain
     // or a beta cutoff occurs.
@@ -1233,7 +1234,10 @@ moves_loop:  // When in check, search starts here
 
             // If the ttMove is assumed to fail high over current beta
             else if (ttData.value >= beta)
+            {
                 extension = -3;
+                negext = true;
+            }
 
             // If we are on a cutNode but the ttMove is not assumed to fail high
             // over current beta
@@ -1273,6 +1277,9 @@ moves_loop:  // When in check, search starts here
         // For first picked move (ttMove) reduce reduction
         else if (move == ttData.move)
             r = std::max(-10, r - 2016 + 150 * cutNode);
+
+        if (move != ttData.move && negext)
+            r -= 512;
 
         if (capture)
             ss->statScore = 809 * int(PieceValue[pos.captured_piece()]) / 128
