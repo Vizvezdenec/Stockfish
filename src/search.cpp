d5d762f1d45a9a72cc47cb8b717550cc8adfdb7f
Michael Chaly <Vizvezdenec@gmail.com>
2026-06-09 23:42:16 +0300

    bench 2590033

--- src/search.cpp
+++ src/search.cpp
@@ -1005,7 +1005,7 @@ Value Search::Worker::search(
         assert(probCutBeta < VALUE_INFINITE && probCutBeta > beta);
 
         MovePicker mp(pos, ttData.move, probCutBeta - ss->staticEval, &captureHistory);
-        Depth      probCutDepth = depth - 4 - (ss->staticEval - (ss - 2)->staticEval > 282);
+        Depth      probCutDepth = depth - 4 - std::clamp((ss->staticEval - (ss - 2)->staticEval) / 128, 0, 4);
 
         while ((move = mp.next_move()) != Move::none())
         {
