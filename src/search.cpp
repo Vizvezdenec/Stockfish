76304dbe904d63d12dec958eebdd1c34512927d6
Michael Chaly <Vizvezdenec@gmail.com>
2026-06-28 18:56:45 +0300

    bench 2336582

--- src/search.cpp
+++ src/search.cpp
@@ -1302,7 +1302,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction if ttMove is a capture
         if (ttCapture)
-            r += 1039;
+            r += 1039 + 512 * cutNode;
 
         // Increase reduction if next ply has a lot of fail high
         if ((ss + 1)->cutoffCnt > 1)
