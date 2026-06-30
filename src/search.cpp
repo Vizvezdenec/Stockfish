9de61d73d4acf662423b16dec178e8f933ec55e4
Michael Chaly <Vizvezdenec@gmail.com>
2026-06-30 18:57:44 +0300

    bench 2518242

--- src/search.cpp
+++ src/search.cpp
@@ -1365,7 +1365,7 @@ moves_loop:  // When in check, search starts here
         {
             // Increase reduction if ttMove is not present
             if (!ttData.move)
-                r += 1085 + 512 * cutNode;
+                r += 1085 + 768 * cutNode;
 
             // Note that if expected reduction is high, we reduce search depth here
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha,
