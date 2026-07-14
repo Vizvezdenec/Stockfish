4913faaf30a482b11a3da568fd67dfcd5d7430d5
Michael Chaly <Vizvezdenec@gmail.com>
2026-07-14 09:09:24 +0300

    bench 3023621

--- src/search.cpp
+++ src/search.cpp
@@ -1045,7 +1045,7 @@ Value Search::Worker::search(
         assert(probCutBeta < VALUE_INFINITE && probCutBeta > beta);
 
         MovePicker mp(pos, ttData.move, probCutBeta - ss->staticEval, &captureHistory);
-        Depth      probCutDepth = depth - 4 - improving;
+        Depth      probCutDepth = depth - 4 - improving - (ss->staticEval >= beta + 222);
 
         while ((move = mp.next_move()) != Move::none())
         {
