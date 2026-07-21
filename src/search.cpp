f9a95e6ec6ea8c2a54f3edab141dcc2b491e78a2
Michael Chaly <Vizvezdenec@gmail.com>
2026-07-21 21:43:29 +0300

    bench 3164697

--- src/search.cpp
+++ src/search.cpp
@@ -1026,6 +1026,8 @@ Value Search::Worker::search(
         }
     }
 
+    improving |= ss->staticEval >= beta;
+
     // Step 10. Internal iterative reductions
     // At sufficient depth, reduce depth for PV/Cut nodes without a TTMove.
     // (*Scaler) Making IIR more aggressive scales poorly.
@@ -1045,7 +1047,7 @@ Value Search::Worker::search(
         assert(probCutBeta < VALUE_INFINITE && probCutBeta > beta);
 
         MovePicker mp(pos, ttData.move, probCutBeta - ss->staticEval, &captureHistory);
-        Depth      probCutDepth = depth - (improving || ss->staticEval >= beta ? 5 : 3);
+        Depth      probCutDepth = depth - (improving ? 5 : 3);
 
         while ((move = mp.next_move()) != Move::none())
         {
@@ -1141,11 +1143,6 @@ moves_loop:  // When in check, search starts here
 
         int r = reduction(improving, depth, moveCount, delta);
 
-        // Increase reduction for ttPv nodes (*Scaler)
-        // Larger values scale well
-        if (ss->ttPv)
-            r += 929;
-
         // Step 14. Pruning at shallow depths.
         // Depth conditions are important for mate finding.
         if (!rootNode && pos.non_pawn_material(us) && !is_loss(bestValue))
@@ -1155,7 +1152,7 @@ moves_loop:  // When in check, search starts here
                 mp.skip_quiet_moves();
 
             // Reduced depth of the next LMR search
-            int lmrDepth = newDepth - r / 1024;
+            int lmrDepth = newDepth - r / 1024 - ss->ttPv;
 
             if (capture || givesCheck)
             {
@@ -1291,7 +1288,7 @@ moves_loop:  // When in check, search starts here
 
         // Decrease reduction for PvNodes (*Scaler)
         if (ss->ttPv)
-            r -= 3023 + PvNode * 1004 + (ttData.value > alpha) * 885
+            r -= 3023 - 929 + PvNode * 1004 + (ttData.value > alpha) * 885
                + (ttData.depth >= depth) * (816 + cutNode * 940);
 
         r += 697;  // Base reduction offset to compensate for other tweaks
