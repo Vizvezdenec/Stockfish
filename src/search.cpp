202788a57b50b20e6b3cd9313be4090a64191c85
Michael Chaly <Vizvezdenec@gmail.com>
2026-07-26 10:18:15 +0300

    bench 2068229

--- src/search.cpp
+++ src/search.cpp
@@ -1554,6 +1554,7 @@ moves_loop:  // When in check, search starts here
         bonusScale += 186 * ((ss - 1)->moveCount > 9);
         bonusScale += 142 * (!ss->inCheck && bestValue <= ss->staticEval - 106);
         bonusScale += 159 * (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 68);
+        bonusScale += 132 * (ss->cutoffCnt > 20);
 
         bonusScale = std::max(bonusScale, 0);
 
