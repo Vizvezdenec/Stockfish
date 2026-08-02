63a56b29ed50afd920f7dca3823b871fcfc0d21c
Michael Chaly <Vizvezdenec@gmail.com>
2026-08-02 23:51:29 +0300

    bench 2229987

--- src/search.cpp
+++ src/search.cpp
@@ -1323,8 +1323,8 @@ moves_loop:  // When in check, search starts here
             r -= 2179;
 
         if (capture)
-            ss->statScore = 873 * int(PieceValue[pos.captured_piece()]) / 128
-                          + captureHistory[movedPiece][move.to_sq()][type_of(pos.captured_piece())];
+            ss->statScore = (873 * int(PieceValue[pos.captured_piece()]) / 128
+                          + captureHistory[movedPiece][move.to_sq()][type_of(pos.captured_piece())]) * 135 / 128;
         else
             ss->statScore =
               (2252 * mainHistory[us][move.raw()] + 1126 * (*contHist[0])[movedPiece][move.to_sq()]
