e9ab6f2b4d0bae0892c20483dd1a162c4f7f92f7
Michael Chaly <Vizvezdenec@gmail.com>
2026-08-05 19:40:24 +0300

    bench 2240041

--- src/search.cpp
+++ src/search.cpp
@@ -1268,7 +1268,7 @@ moves_loop:  // When in check, search starts here
                 if (!ss->inCheck && value > ss->staticEval)
                 {
                     const int bonus =
-                      std::clamp(int(value - ss->staticEval) * depth * 177 / 1024,
+                      std::clamp(int(value - ss->staticEval) * depth * 157 / 1024,
                                  -CORRECTION_HISTORY_LIMIT / 4, CORRECTION_HISTORY_LIMIT / 4);
                     update_correction_history(pos, ss, *this, bonus);
                 }
