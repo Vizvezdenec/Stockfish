1ba88dee2daae5696806a3ab213b89131171986f
Michael Chaly <Vizvezdenec@gmail.com>
2026-09-07 08:58:24 +0300

    bench 2436184

--- src/search.cpp
+++ src/search.cpp
@@ -1326,7 +1326,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction for cut nodes
         if (cutNode)
-            r += 4026 + 933 * !ttData.move + 1024 * (ss->cnStreak > 3);
+            r += 4026 + 933 * !ttData.move + 728 * (ss->cnStreak > 5);
 
         // Increase reduction if ttMove is a capture
         if (ttCapture)
