7a8b41c721de19a0984dc8037cca3455ea5d2526
Michael Chaly <Vizvezdenec@gmail.com>
2026-09-09 21:54:34 +0300

    bench 2517786

--- src/search.cpp
+++ src/search.cpp
@@ -1062,7 +1062,7 @@ Value Search::Worker::search(
     // Step 12. ProbCut
     // If we have a good enough capture (or queen promotion) and a reduced search
     // returns a value much above beta, we can (almost) safely prune the previous move.
-    probCutBeta = beta + 241 - 64 * improving - std::min(ss->priorNMPFailHigh, 3) * 10;
+    probCutBeta = beta + 241 - 64 * improving - std::min(ss->priorNMPFailHigh, 6) * 5;
     if (depth >= 3 && !is_decisive(beta) && !(is_valid(ttData.value) && ttData.value < probCutBeta))
     {
         assert(probCutBeta < VALUE_INFINITE && probCutBeta > beta);
