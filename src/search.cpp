65c45ac17d0b80c8add6ea4e41e6960b991ef05f
Michael Chaly <Vizvezdenec@gmail.com>
2026-09-18 19:40:36 +0300

    bench 1756180

--- src/search.cpp
+++ src/search.cpp
@@ -1009,7 +1009,7 @@ Value Search::Worker::search(
 
         Value futilityMargin = (futilityMult * depth
                              - (2789 * improving + 335 * opponentWorsening) * futilityMult / 1024
-                             + std::abs(correctionValue) / 198435) * 125;
+                             + std::abs(correctionValue) / 198435) * 118;
         futilityMargin = futilityMargin / 128;
 
         if (eval - futilityMargin >= beta)
