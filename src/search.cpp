0d780112b91e85e9b4044d2a47d14766ffa498a0
Michael Chaly <Vizvezdenec@gmail.com>
2026-09-21 20:19:28 +0300

    bench 1541875

--- src/search.cpp
+++ src/search.cpp
@@ -1006,7 +1006,11 @@ Value Search::Worker::search(
     // Step 8. Razoring
     // If eval is really low, skip search entirely and return the qsearch value
     if (allNode && eval < alpha - 342 * depth && !seekMate)
-        return qsearch<NonPV>(pos, ss, alpha, beta);
+    {
+        Value aa = qsearch<NonPV>(pos, ss, alpha, beta);
+        if (aa <= alpha || depth > 3)
+            return aa;
+    }
 
     // Step 9. Futility pruning: child node
     // The depth condition is important for mate finding. It should NOT be tuned.
